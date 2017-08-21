0581888ab0301b16d300296b26ddf6f2f1f5a856
Ryan King <ryan@theryanking.com>
2017-08-21 15:50:01 -0700

    allo upload pipeline results. docs to follow

--- app/models/pipeline_output.rb
+++ app/models/pipeline_output.rb
@@ -1,4 +1,5 @@
 class PipelineOutput < ApplicationRecord
   belongs_to :sample
-  has_many :taxon_count, dependent: :destroy
+  has_many :taxon_counts, dependent: :destroy
+  accepts_nested_attributes_for :taxon_counts
 end
