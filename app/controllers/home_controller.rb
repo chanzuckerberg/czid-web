9c499ec6d2dd4995d44111137a5ace8c161c81f3
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2017-08-30 11:47:16 -0700

    add homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
-  def index
+ before_action :set_project, only: [:show, :edit, :update, :destroy]
+
+ def index
     @users = User.all
     @projects = Project.all
     @samples = Sample.all
