8235bc95c030c2fadbd96bac5be7cda3de014a22
yunfangjuan <yunfang@chanzuckerberg.com>
2017-09-01 11:03:26 -0700

    some small tweaks

--- config/initializers/airflow.rb
+++ config/initializers/airflow.rb
@@ -1,6 +1,6 @@
 # Module to connect to Airflow webserver
 module Airflow
-  CONFIG = YAML.load_file(Rails.root.join('config/airflow.yml'))[Rails.env]
+  CONFIG = YAML.load_file(Rails.root.join('config', 'airflow.yml'))[Rails.env]
   HOST = "#{CONFIG['protocol']}://#{CONFIG['host']}:#{CONFIG['port']}".freeze
   BASEURL = "#{HOST}#{CONFIG['uri']}".freeze
   def self.get_request_url(config_hash)
