f0fa8df6cd48312e9dbee67f126cf4867babea6b
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2017-09-05 15:09:58 -0700

    add z-score computation

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -25,10 +25,34 @@ class ReportsController < ApplicationController
   # POST /reports.json
   def create
     @report = Report.new(report_params)
+    @report.pipeline_output.taxon_counts.each do |taxon_count|
+      a = {}
+      a[:tax_id] = taxon_count.tax_id
+      a[:tax_level] = taxon_count.tax_level
+      a[:name] = taxon_count.name 
+      sum = taxon_count.count
+      sum_sq = taxon_count.count**2 
+      n = 1
+      @report.background.samples.each do |background_sample|
+        if background_sample.pipeline_outputs.first.taxon_counts.find_by(tax_id: taxon_count.tax_id)
+          bg_count = background_sample.pipeline_outputs.first.taxon_counts.find_by(tax_id: taxon_count.tax_id).count
+        else 
+          bg_count = 0
+        end
+        sum += bg_count
+        sum_sq += bg_count**2
+        n = n+1
+      end
+      mean = sum.to_f/n
+      stdev = (sum_sq.to_f - sum**2/n)/(n-1)
+      a[:nt_zscore] = (taxon_count.count-mean)/stdev
+      @report.taxon_zscores.new(a)
+    end
 
     respond_to do |format|
       if @report.save
-        format.html { redirect_to @report, notice: 'Report was successfully created.' }
+        format.html { redirect_to @report, 
+                      notice: 'Report was successfully created.' }
         format.json { render :show, status: :created, location: @report }
       else
         format.html { render :new }
