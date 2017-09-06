607c95bfda5d38927b8a98f36668f195d7e2f38c
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2017-09-05 17:41:18 -0700

    fix style

--- app/controllers/backgrounds_controller.rb
+++ app/controllers/backgrounds_controller.rb
@@ -61,13 +61,13 @@ class BackgroundsController < ApplicationController
 
   private
 
-    # Use callbacks to share common setup or constraints between actions.
-    def set_background
-      @background = Background.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_background
+    @background = Background.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def background_params
-      params.require(:background).permit(:name, sample_ids: [], reports: [])
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def background_params
+    params.require(:background).permit(:name, sample_ids: [], reports: [])
+  end
 end
