c84c381333be6236bb850d80c3c58c9ae964afe1
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2017-09-08 13:54:50 -0700

    allow posting taxonomy JSON via API

--- app/controllers/taxonomies_controller.rb
+++ app/controllers/taxonomies_controller.rb
@@ -1,5 +1,6 @@
 class TaxonomiesController < ApplicationController
   before_action :set_taxonomy, only: [:show, :edit, :update, :destroy]
+  protect_from_forgery unless: -> { request.format.json? }
 
   # GET /taxonomies
   # GET /taxonomies.json
