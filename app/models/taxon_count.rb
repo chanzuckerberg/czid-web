b03dfcaf2d5886d228e2be57fbe3613286360725
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2017-09-11 16:17:43 -0700

    fix style

--- app/models/taxon_count.rb
+++ app/models/taxon_count.rb
@@ -1,7 +1,7 @@
 class TaxonCount < ApplicationRecord
   belongs_to :pipeline_output
-  TAX_LEVEL_SPECIES = 1
-  TAX_LEVEL_GENUS = 2
-  COUNT_TYPE_NT = 'NT'
-  COUNT_TYPE_NR = 'NR'
+  TAX_LEVEL_SPECIES = 1.freeze
+  TAX_LEVEL_GENUS = 2.freeze
+  COUNT_TYPE_NT = 'NT'.freeze
+  COUNT_TYPE_NR = 'NR'.freeze
 end
