4717962c9986149c10b4b38a925c0db9e8160f4b
cdebourcy <cdebourcy@chanzuckerberg.com>
2017-09-11 16:33:03 -0700

    Add taxon count_type (and fix user signup) (#52)
    * remove name and projects from user signup
    
    * migration to add count_type
    
    * add count_type to view
    
    * fix style
    
    * define constants for tax_level / count_type
    
    * fix style
    
    * fix style

--- app/models/taxon_count.rb
+++ app/models/taxon_count.rb
@@ -1,3 +1,7 @@
 class TaxonCount < ApplicationRecord
   belongs_to :pipeline_output
+  TAX_LEVEL_SPECIES = 1
+  TAX_LEVEL_GENUS = 2
+  COUNT_TYPE_NT = 'NT'.freeze
+  COUNT_TYPE_NR = 'NR'.freeze
 end
