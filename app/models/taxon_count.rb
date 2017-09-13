f6641e18cce6fdeb05fabb52712c830b3e73ecdd
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2017-09-13 16:06:51 -0700

    fix errors

--- app/models/taxon_count.rb
+++ app/models/taxon_count.rb
@@ -4,7 +4,7 @@ class TaxonCount < ApplicationRecord
   TAX_LEVEL_GENUS = 2
   COUNT_TYPE_NT = 'NT'.freeze
   COUNT_TYPE_NR = 'NR'.freeze
-  scope :type, -> (count_type) { where(count_type: count_type) }
-  scope :level, -> (tax_level) { where(tax_level: tax_level) }
-  scope :tax_id, -> (tax_id) { where(tax_id: tax_id) }
+  scope :type, ->(count_type) { where(count_type: count_type) }
+  scope :level, ->(tax_level) { where(tax_level: tax_level) }
+  scope :tax_id, ->(tax_id) { where(tax_id: tax_id) }
 end
