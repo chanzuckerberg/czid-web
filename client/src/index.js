c174a47569ea19e1042ddf9974aeee4fd8c62159
sadelokiki <susan.adelokiki@andela.com>
2017-09-14 13:33:18 -0700

    [Susan] Implement redux on react environment

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom';
 import './assets/style.css';
+import store, { history } from './redux/store'
 import { BrowserRouter } from 'react-router-dom';
 import App from './pages/app';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-<App />, document.getElementById('root'));
+  <Provider store={store}>
+    <BrowserRouter history={history}>
+      <App />
+    </BrowserRouter>
+  </Provider>
+,document.getElementById('root'));
 registerServiceWorker();
+
+
