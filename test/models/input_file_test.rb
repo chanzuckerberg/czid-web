61035296aee37fce241495b7a38dfd2ad792cea9
Ryan King <ryan@theryanking.com>
2017-09-14 14:12:01 -0700

    use ids rather than names for s3 paths (#58)
    * use ids rather than names for organizing files in S3
    
    * add a test for InputFile#file_path
    
    * validate presenece and format for InputFile#name

--- test/models/input_file_test.rb
+++ test/models/input_file_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class InputFileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @input_file = input_files(:one)
+  end
+
+  test "file_path" do
+    expected = "samples/#{@input_file.sample.project.id}/#{@input_file.sample.id}/#{@input_file.name}"
+    assert_equal expected, @input_file.file_path
+  end
+
+  test "validate name presence" do
+    file = InputFile.new
+    assert_not file.valid?
+    assert_equal [:sample, :name], file.errors.keys
+  end
+
+  test "validate name format" do
+    invalid_names = ['.fastq', 'a .fastq.gz', 'a/b.fastq.gz']
+    invalid_names.each do |name|
+      file = InputFile.new
+      file.name = name
+      assert_not file.valid?
+      assert_equal [:sample, :name], file.errors.keys
+    end
+  end
 end
