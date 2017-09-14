27bb4ea802999bb695d201be36d1ba2c5a4b62b0
Ryan King <ryan@theryanking.com>
2017-09-14 13:40:57 -0700

    validate presenece and format for InputFile#name

--- app/models/input_file.rb
+++ app/models/input_file.rb
@@ -3,9 +3,14 @@ require 'aws-sdk-s3'
 class InputFile < ApplicationRecord
   belongs_to :sample
 
-  before_validation(on: :create) do
-    # TODO: investigate the content-md5 stuff https://github.com/aws/aws-sdk-js/issues/151 https://gist.github.com/algorist/385616
-    self.presigned_url = S3_PRESIGNER.presigned_url(:put_object, bucket: SAMPLES_BUCKET_NAME, key: file_path)
+  FILE_REGEX = %r{\A[^\s\/]+\.fastq.gz}
+  validates :name, presence: true, format: { with: FILE_REGEX, message: "file must match format '#{FILE_REGEX}'" }
+
+  after_validation(on: :create) do
+    if sample
+      # TODO: investigate the content-md5 stuff https://github.com/aws/aws-sdk-js/issues/151 https://gist.github.com/algorist/385616
+      self.presigned_url = S3_PRESIGNER.presigned_url(:put_object, bucket: SAMPLES_BUCKET_NAME, key: file_path)
+    end
   end
 
   def file_path
--- test/models/input_file_test.rb
+++ test/models/input_file_test.rb
@@ -9,4 +9,20 @@ class InputFileTest < ActiveSupport::TestCase
     expected = "samples/#{@input_file.sample.project.id}/#{@input_file.sample.id}/#{@input_file.name}"
     assert_equal expected, @input_file.file_path
   end
+
+  test "validate name presence" do
+    file = InputFile.new
+    assert_not file.valid?
+    assert_equal [:sample, :name], file.errors.keys
+  end
+
+  test "validate name format" do
+    invalid_names = ['.fastq', 'a .fastq.gz', 'a/b.fastq.gz']
+    invalid_names.each do |name|
+      file = InputFile.new
+      file.name = name
+      assert_not file.valid?
+      assert_equal [:sample, :name], file.errors.keys
+    end
+  end
 end
