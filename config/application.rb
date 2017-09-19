c5243f821035984ce18e7d132f328072aad0472d
sadelokiki <susan.adelokiki@andela.com>
2017-09-18 17:11:42 -0700

    Refactor config hash syntax

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Idseq
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
+        resource '*', headers: :any, methods: [:get, :post, :patch, :options]
       end
     end
 
