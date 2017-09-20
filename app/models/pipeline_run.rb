35841e0c0b74999ab61505c747a1ea76d2b6439f
yunfangjuan <yunfang@chanzuckerberg.com>
2017-09-19 17:02:57 -0700

    setup db load

--- app/models/pipeline_run.rb
+++ app/models/pipeline_run.rb
@@ -1,4 +1,10 @@
 class PipelineRun < ApplicationRecord
   belongs_to :sample
   has_one :pipeline_output
+  OUTPUT_JSON_NAME = 'idseq_web_sample.json'.freeze
+
+  def load_results_from_s3
+    output_json_path = "#{sample.sample_output_s3_path}/#{OUTPUT_JSON_NAME}"
+  end
+
 end
