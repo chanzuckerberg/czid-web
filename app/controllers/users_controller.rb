7e936c79da211d747883bc267ede1f23b022ffe2
Ryan King <ryan@theryanking.com>
2017-09-20 11:28:37 -0700

    cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,6 +71,6 @@ class UsersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name, project_ids:[])
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, project_ids: [])
   end
 end
