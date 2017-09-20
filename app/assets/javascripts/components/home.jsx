aaa0213ed73b28af5611e9b91f65ac613c109350
sadelokiki <susan.adelokiki@andela.com>
2017-09-20 14:40:35 -0700

    Replace mock data

--- app/assets/javascripts/components/home.jsx
+++ app/assets/javascripts/components/home.jsx
@@ -1,9 +1,19 @@
 class Home extends React.Component {
+
+  componentDidMount() {
+    console.log(this.props, this.props.pipelineOutputs, 'allofem');
+  }
+
   render() {
     return (
       <div>
-        <PipelineList />
+        <PipelineList {...this.props}/>
       </div>
     )
   }
 }
+
+
+Home.propTypes = {
+  pipelineOutputs: React.PropTypes.object,
+};
