b1eb9e83f2fed02a9969c92f0ffd18026221065c
Ologho Cyril <cyrielo911@GMAIL.COM>
2017-09-21 22:57:42 +0100

    Pipeline output design (#80)
    * Feature/react rails setup (#72)
    
    * New react-setup integraed with rails
    
    * Update view on HomePage
    
    * Replace mock data
    
    * Render Pipeline Outputsdata
    
    * add editor config for uniform text editor indentations
    
    * update .gitignore to ignore IDE generated files
    
    * add materialize and jquery setup
    
    * update application.css with correct setup
    
    * delete unwanted styles from scaffolds and add sample.scss
    
    * remove html block from sample reads
    
    * add header component
    
    * add PipelineSampleReads component
    
    * add subheader component
    
    * add the header partial design
    
    * initialize materialize plugins
    
    * remove no longer needed materialize_init file
    
    * initiliaze material plugin for Header and Pipeline components
    
    * refactor tab styles
    
    * change the tab indicator color

---
+++ app/assets/javascripts/components/SubHeader.jsx
@@ -0,0 +1,13 @@
+class SubHeader extends React.Component {
+	render() {
+		return (
+			<div className="sub-header-component">
+				<div className="container">
+					<div className="content">
+						{ this.props.children }
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
