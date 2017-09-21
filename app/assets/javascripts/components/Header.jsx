6851da3627e825433232648ef6fcc117d7399d3e
Cyril Ologho <cyril.paul@andela.com>
2017-09-21 11:45:54 -0700

    add header component

---
+++ app/assets/javascripts/components/Header.jsx
@@ -0,0 +1,48 @@
+class Header extends React.Component  {
+  render() {
+    return (
+      <div>
+        <div className="site-header">
+          {/* Dropdown menu */}
+          <ul id="dropdown1" className="dropdown-content">
+            <li><a href="#!">New Project</a></li>
+            <li><a href="#!">Account</a></li>
+            <li className="divider"></li>
+            <li><a href="#!">Sign out</a></li>
+          </ul>
+
+          <div>
+            <div className="">
+              <a href="#!" className="left brand-details">
+                <div className="brand-short">
+                    ID<i>seq</i>
+                </div>
+
+                <div className="brand-long">
+                  Infectious Disease Platform
+                </div>
+              </a>
+
+              <div className="right hide-on-med-and-down header-right-nav">
+                <div>
+                  <a className="dropdown-button profile-dropdown" href="#!" data-activates="dropdown1">
+                    Rebecca Egger <i className="fa fa-angle-down"> </i>
+                  </a>
+                </div>
+
+                <div className="settings-icon">
+                  <i className="fa fa-gear"> </i>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/*   */}
+        </div>
+        <SubHeader>
+          { this.props.children }
+        </SubHeader>
+      </div>
+
+    )
+  }
+}
