1f61e933acd2e3c51f93ac296f9fac761d9b1005
sadelokiki <susan.adelokiki@andela.com>
2017-09-21 21:23:41 -0700

    Include Header Component into Pipeline Hompage

--- app/assets/javascripts/components/pipelineList.jsx
+++ app/assets/javascripts/components/pipelineList.jsx
@@ -8,39 +8,62 @@ class PipelineList extends React.Component {
 
   render() {
     return (
-      <div className="content-wrapper">
-        <div className="search-wrapper container">
-          <i className="fa fa-search" aria-hidden="true"></i>
-          <span>PIPELINES OUTPUTS</span>
-        </div>
-        <div className="container sample-container">
-        {!this.props.pipelineOutputs ? <i className="fa fa-frown" aria-hidden="true">No outputs available</i> :
-          <table className="bordered highlight">
-          <thead>
-            <tr>
-                <th>Name</th>
-                <th>Date</th>
-                <th>Total Reads</th>
-                <th>Final Reads</th>
-            </tr>
-          </thead>
+      <div>
+        <Header />
+        <div className="sub-header-home">
+				  <div className="container">
+					  <div className="content">
+              <div className="title">
+                All Projects  >  Uganda Project
+              </div>
 
-           {this.props.pipelineOutputs.map((output, i) => {
-            return (
-              <tbody key={i}>
-                <tr>
-                <td ><i className="fa fa-flask" aria-hidden="true"></i>{output.id}</td>
-                  <td>{moment(output.created_at).format('MM-DD-YYYY')}</td>
-                  <td>{output.total_reads}</td>
-                  <td>{output.remaining_reads }</td>
-                </tr>
-              </tbody>
-            )
-          })}
-        </table>
-         }
+              <div className="sub-title">
+                Uganda Project
+              </div>
+
+              <div className="title-filter">
+                <i class="fa fa-question-circle" aria-hidden="true"></i>
+
+              </div>
+					  </div>
+				  </div>
+			</div>
+        <div className="content-wrapper">
+          <div className="search-wrapper container">
+            <i className="fa fa-search" aria-hidden="true"></i>
+            <span>PIPELINES OUTPUTS</span>
+          </div>
+          <div className="container sample-container">
+          {!this.props.pipelineOutputs ? 'Nothing to show' :
+            <table className="bordered highlight">
+            <thead>
+              <tr>
+                  <th>Name</th>
+                  <th>Date</th>
+                  <th>Created by</th>
+                  <th>Total Reads</th>
+                  <th>Final Reads</th>
+              </tr>
+            </thead>
+
+            {this.props.pipelineOutputs.map((output, i) => {
+              return (
+                <tbody key={i}>
+                  <tr>
+                  <td ><i className="fa fa-flask" aria-hidden="true"></i>{output.Name}</td>
+                    <td>{output.created_at}</td>
+                    <td>{output.created_by}</td>
+                    <td>{output.total_reads}</td>
+                    <td>{output.remaining_reads }</td>
+                  </tr>
+                </tbody>
+              )
+            })}
+          </table>
+          }
+          </div>
         </div>
-      </div>
+    </div>
     )
   }
 
