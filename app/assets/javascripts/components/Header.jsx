01f8d1bb92d3ea1aa51d6a92a50f0424e64894e0
Cyril Ologho <cyril.paul@andela.com>
2017-09-25 15:09:09 -0700

    remove thee gear icon from the header

--- app/assets/javascripts/components/Header.jsx
+++ app/assets/javascripts/components/Header.jsx
@@ -18,7 +18,7 @@ class Header extends React.Component  {
 
           <div>
             <div className="">
-              <a href="#!" className="left brand-details">
+              <a href="/" className="left brand-details">
                 <div className="brand-short">
                     ID<i>seq</i>
                 </div>
@@ -35,9 +35,6 @@ class Header extends React.Component  {
                   </a>
                 </div>
 
-                <div className="settings-icon">
-                  <i className="fa fa-gear"> </i>
-                </div>
               </div>
             </div>
           </div>
