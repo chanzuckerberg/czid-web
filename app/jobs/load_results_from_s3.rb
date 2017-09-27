36af13fe389cc7449b0c1e42c3337ed8a29e6d2b
yunfangjuan <yunfang@chanzuckerberg.com>
2017-09-27 16:00:11 -0700

    rubocop

--- app/jobs/load_results_from_s3.rb
+++ app/jobs/load_results_from_s3.rb
@@ -6,8 +6,9 @@ class LoadResultsFromS3
   def self.lock(pipeline_run_id)
     "LoadResultsFromS3-#{pipeline_run_id}"
   end
+
   def self.perform(pipeline_run_id)
-    pr =  PipelineRun.find(pipeline_run_id)
+    pr = PipelineRun.find(pipeline_run_id)
     pr.load_results_from_s3 unless pr.completed?
   end
 end
