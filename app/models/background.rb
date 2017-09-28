58778e87c9475c22501e31e08c15676b0932b536
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2017-09-28 09:50:49 -0700

    rubocop

--- app/models/background.rb
+++ app/models/background.rb
@@ -10,7 +10,7 @@ class Background < ApplicationRecord
     errors.add(:base, "Need to select at least 2 pipeline runs.") if pipeline_outputs.size < 2
   end
 
-  def summarize 
+  def summarize
     results = TaxonCount.joins(:pipeline_output).select("tax_id, count_type, tax_level, name, sum((1.0*1e6*count)/total_reads) as sum_rpm, sum((1.0*1e6*count*1e6*count)/(total_reads*total_reads)) as sum_rpm2").group("tax_id, count_type, tax_level, name").where("pipeline_output_id in (select pipeline_output_id from backgrounds_pipeline_outputs where background_id = #{id})")
     n = pipeline_outputs.count
     results.map { |h| h.attributes.merge(mean: h[:sum_rpm] / n.to_f, stdev: compute_stdev(h[:sum_rpm], h[:sum_rpm2], n)) }
