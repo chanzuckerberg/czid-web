cef666862c453bb390bcb0b097de7dace7ebbd72
yunfangjuan <yunfangjuan@gmail.com>
2017-10-03 15:11:19 -0700

    adding a bunch of debug info for resque (#125)

--- app/jobs/load_results_from_s3.rb
+++ app/jobs/load_results_from_s3.rb
@@ -1,13 +1,16 @@
 # Jos to initiate database download
+require 'logger'
 require 'resque/plugins/lock'
 class LoadResultsFromS3
   extend Resque::Plugins::Lock
   @queue = :q03_pipeline_run
+  @logger = Logger.new(STDOUT)
   def self.lock(pipeline_run_id)
     "LoadResultsFromS3-#{pipeline_run_id}"
   end
 
   def self.perform(pipeline_run_id)
+    @logger.info("load pipeline run #{pipeline_run_id} into database")
     pr = PipelineRun.find(pipeline_run_id)
     pr.load_results_from_s3 unless pr.completed?
   end
