1a97629639e33b1b9e1f8f16932bb1a21a85b68c
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2017-10-03 16:16:45 -0700

    add job stats model

--- app/models/pipeline_run.rb
+++ app/models/pipeline_run.rb
@@ -5,6 +5,7 @@ class PipelineRun < ApplicationRecord
   has_one :pipeline_output
 
   OUTPUT_JSON_NAME = 'idseq_web_sample.json'.freeze
+  STATS_JSON_NAME = 'stats.json'.freeze
   LOCAL_JSON_PATH = '/app/tmp/results_json'.freeze
   STATUS_CHECKED = 'CHECKED'.freeze
   STATUS_SUCCESS = 'SUCCEEDED'.freeze
@@ -66,13 +67,17 @@ class PipelineRun < ApplicationRecord
   def load_results_from_s3
     return if pipeline_output
     output_json_s3_path = "#{sample.sample_output_s3_path}/#{OUTPUT_JSON_NAME}"
+    stats_json_s3_path = "#{sample.sample_output_s3_path}/#{STATS_JSON_NAME}"
     # Get the file
     downloaded_json_path = download_file(output_json_s3_path)
-    return unless downloaded_json_path
+    downloaded_stats_path = download_file(stats_json_s3_path)
+    return unless downloaded_json_path && stats_json_s3_path
     json_dict = JSON.parse(File.read(downloaded_json_path))
+    stats_array = JSON.parse(File.read(stats_json_path))
     pipeline_output_dict = json_dict['pipeline_output']
     pipeline_output_dict.slice!('name', 'total_reads',
                                 'remaining_reads', 'taxon_counts_attributes')
+    pipeline_output_dict['job_stats_attributes'] = stats_array
     po = PipelineOutput.new(pipeline_output_dict)
     po.sample = sample
     po.pipeline_run = self
@@ -80,7 +85,7 @@ class PipelineRun < ApplicationRecord
     self.pipeline_output_id = po.id
     save
     # rm the json
-    _stdout, _stderr, _status = Open3.capture3("rm -f #{downloaded_json_path}")
+    _stdout, _stderr, _status = Open3.capture3("rm -f #{downloaded_json_path} #{downloaded_stats_path}")
   end
 
   def download_file(s3_path)
