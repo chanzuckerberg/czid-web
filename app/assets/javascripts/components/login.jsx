08ecf9d46cd5daebc31b8e1fcc74ca6d085a03f3
sadelokiki <susan.adelokiki@andela.com>
2017-10-08 21:56:38 -0700

    UI Update Login

--- app/assets/javascripts/components/login.jsx
+++ app/assets/javascripts/components/login.jsx
@@ -10,19 +10,27 @@ class Login extends React.Component {
         <div className="form-wrapper">
           <div className="row">
             <form>
+              <div className="row title">
+                <p className="col s6 verify">Already a user? Sign in</p>
+                <p className="col s6 signup">Login</p>
+              </div>
               <div className="row content-wrapper">
-                <div className="input-field col s12">
+                <div className="input-field">
                   <i className="fa fa-envelope" aria-hidden="true"></i>
                   <input type="email" className="validate" />
                   <label htmlFor="icon_prefix">Email</label>
                 </div>
-                <div className="input-field col s12">
+                <div className="input-field">
                   <i className="fa fa-key" aria-hidden="true"></i>
                   <input type="password" className="" />
                   <label htmlFor="icon_telephone">Password</label>
                 </div>
+                <div className="input-field">
+                  <input type="password" className="" />
+                  <label htmlFor="icon_telephone">Confirm Password</label>
+                </div>
               </div>
-              <div className="center-align login-wrapper">Login</div>
+              <div className="center-align login-wrapper">Submit</div>
             </form>
           </div>
         </div>
