94d2826caaf4084e6e4acca51097eae95a903edd
yunfangjuan <yunfang@chanzuckerberg.com>
2017-10-09 12:46:15 -0700

    put pipeline output back

--- app/controllers/pipeline_outputs_controller.rb
+++ app/controllers/pipeline_outputs_controller.rb
@@ -1,6 +1,7 @@
 class PipelineOutputsController < ApplicationController
   include ReportHelper
   before_action :set_pipeline_output, only: [:show, :edit, :update, :destroy]
+  before_action :typed_counts, only: [:show]
   protect_from_forgery unless: -> { request.format.json? }
 
   # GET /pipeline_outputs
@@ -89,6 +90,20 @@ class PipelineOutputsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_pipeline_output
     @pipeline_output = PipelineOutput.find(params[:id])
+  end
+
+  def typed_counts
+    counts = @pipeline_output.taxon_counts
+    @nt_species_counts = counts.type('NT').level(TaxonCount::TAX_LEVEL_SPECIES)
+    @nr_species_counts = counts.type('NR').level(TaxonCount::TAX_LEVEL_SPECIES)
+    @ordered_nt_species_tax_ids = @nt_species_counts.order(count: :desc).where.not("tax_id < 0").map(&:tax_id)
+    @ordered_nr_species_tax_ids = @nr_species_counts.order(count: :desc).where.not("tax_id < 0").map(&:tax_id)
+    @ordered_species_tax_ids = (@ordered_nt_species_tax_ids + @ordered_nr_species_tax_ids).uniq
+    @nt_genus_counts = counts.type('NT').level(TaxonCount::TAX_LEVEL_GENUS)
+    @nr_genus_counts = counts.type('NR').level(TaxonCount::TAX_LEVEL_GENUS)
+    @ordered_nt_genus_tax_ids = @nt_genus_counts.order(count: :desc).where.not("tax_id < 0").map(&:tax_id)
+    @ordered_nr_genus_tax_ids = @nr_genus_counts.order(count: :desc).where.not("tax_id < 0").map(&:tax_id)
+    @ordered_genus_tax_ids = (@ordered_nt_genus_tax_ids + @ordered_nr_genus_tax_ids).uniq
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
