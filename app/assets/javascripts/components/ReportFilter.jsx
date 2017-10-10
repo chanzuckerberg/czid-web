48b3e345a2044a48d5c86fb44a9775e0a6b77727
Cyril Ologho <cyril.paul@andela.com>
2017-10-10 14:42:11 -0700

    Fix Filter component to handle invalid range filter

--- app/assets/javascripts/components/ReportFilter.jsx
+++ app/assets/javascripts/components/ReportFilter.jsx
@@ -1,11 +1,24 @@
 class ReportFilter extends React.Component {
   constructor(props) {
     super(props);
-    const view_level = props.view_level || 'Genus';
     this.background_model = props.background_model || 'N/A';
     this.report_title = props.report_title || 'Not Set';
-    const default_nt_zscore_threshold = `0,${this.props.highest_tax_counts.highest_nt_zscore}`;
-    const default_nt_rpm_threshold = `0,${this.props.highest_tax_counts.highest_nt_rpm}`;
+
+    this.highest_nt_zscore = this.props.highest_tax_counts.highest_nt_zscore;
+    this.highest_nt_rpm = this.props.highest_tax_counts.highest_nt_rpm;
+
+    this.lowest_nt_zscore = this.props.highest_tax_counts.lowest_nt_zscore;
+    this.lowest_nt_rpm = this.props.highest_tax_counts.lowest_nt_rpm;
+
+    this.has_valid_nt_zscore_range =
+    (this.highest_nt_zscore && (this.lowest_nt_zscore !== this.highest_nt_zscore));
+
+    this.has_valid_nt_rpm_range =
+    (this.highest_nt_rpm && (this.lowest_nt_rpm !== this.highest_nt_rpm));
+
+
+    const default_nt_zscore_threshold = `${this.lowest_nt_zscore},${this.highest_nt_zscore}`;
+    const default_nt_rpm_threshold = `${this.lowest_nt_rpm},${this.highest_nt_rpm}`;
 
     const nt_zscore_threshold = ReportFilter.getFilter('nt_zscore_threshold')
       ? ReportFilter.getFilter('nt_zscore_threshold') : default_nt_zscore_threshold;
@@ -14,22 +27,23 @@ class ReportFilter extends React.Component {
       ? ReportFilter.getFilter('nt_rpm_threshold') : default_nt_rpm_threshold;
 
     const nt_zscore_start =
-      (nt_zscore_threshold.split(',').length > 0) ? parseInt(nt_zscore_threshold.split(',')[0], 10) : 0;
+      (nt_zscore_threshold.split(',').length > 0) ? parseInt(nt_zscore_threshold.split(',')[0], 10) :
+      this.lowest_nt_zscore;
     const nt_zscore_end =
       (nt_zscore_threshold.split(',').length > 1) ? parseInt(nt_zscore_threshold.split(',')[1], 10) :
-        this.props.highest_tax_counts.highest_nt_zscore;
+      this.highest_nt_zscore;
 
     const nt_rpm_start =
-      (nt_rpm_threshold.split(',').length > 0) ? parseInt(nt_rpm_threshold.split(',')[0], 10) : 0;
+      (nt_rpm_threshold.split(',').length > 0) ? parseInt(nt_rpm_threshold.split(',')[0], 10) :
+      this.lowest_nt_rpm;
     const nt_rpm_end =
       (nt_rpm_threshold.split(',').length > 1) ? parseInt(nt_rpm_threshold.split(',')[1], 10) :
-        this.props.highest_tax_counts.highest_nt_rpm;
+      this.highest_nt_rpm;
 
-
-    this.state = { nt_zscore_start, nt_zscore_end, nt_rpm_start, nt_rpm_end, view_level };
+    this.state = { nt_zscore_start, nt_zscore_end, nt_rpm_start, nt_rpm_end };
 
     this.applyFilter = this.applyFilter.bind(this);
-    this.selectViewLevel = this.selectViewLevel.bind(this);
+
   }
 
   applyFilter() {
@@ -38,7 +52,7 @@ class ReportFilter extends React.Component {
     const sort_by = currentSort.sort_query ? `&${currentSort.sort_query}` : '';
     window.location =
       `${current_url}?nt_zscore_threshold=${this.state.nt_zscore_start},${this.state.nt_zscore_end}&nt_rpm_threshold=${
-      this.state.nt_rpm_start},${this.state.nt_rpm_end}&view_level=${this.state.view_level}${sort_by}`;
+      this.state.nt_rpm_start},${this.state.nt_rpm_end}${sort_by}`;
   }
 
   static getFilter(name) {
@@ -55,65 +69,65 @@ class ReportFilter extends React.Component {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
   }
 
-  selectViewLevel(event) {
-    this.setState({
-      view_level: event.target.value
-    });
-  }
-
   componentDidMount() {
-    const nt_zscore = document.getElementById('nt_zscore');
-    const nt_rpm = document.getElementById('nt_rpm');
 
-    noUiSlider.create(nt_zscore, {
-      start: [this.state.nt_zscore_start, this.state.nt_zscore_end],
-      connect: true,
-      step: 1,
-      orientation: 'horizontal', // 'horizontal' or 'vertical',
-      behaviour: 'tap-drag',
-      range: {
-        'min': 0,
-        'max': this.props.highest_tax_counts.highest_nt_zscore
-      },
-      format: wNumb({
-        decimals: 0
-      })
-    });
-    noUiSlider.create(nt_rpm, {
-      start: [this.state.nt_rpm_start, this.state.nt_rpm_end],
-      connect: true,
-      step: 1,
-      orientation: 'horizontal', // 'horizontal' or 'vertical'
-      range: {
-        'min': 0,
-        'max': this.props.highest_tax_counts.highest_nt_rpm
-      },
-      format: wNumb({
-        decimals: 0
-      })
-    });
-    nt_zscore.noUiSlider.on('update', (values, handle) => {
-      if (handle === 0) {
-        this.setState({
-          nt_zscore_start: Math.round(values[handle])
-        });
-      } else {
-        this.setState({
-          nt_zscore_end: Math.round(values[handle])
-        });
-      }
-    });
-    nt_rpm.noUiSlider.on('update', (values, handle) => {
-      if (handle === 0) {
-        this.setState({
-          nt_rpm_start: Math.round(values[handle])
-        });
-      } else {
-        this.setState({
-          nt_rpm_end: Math.round(values[handle])
-        });
-      }
-    });
+    if (this.has_valid_nt_zscore_range) {
+      const nt_zscore = document.getElementById('nt_zscore');
+      noUiSlider.create(nt_zscore, {
+        start: [this.state.nt_zscore_start, this.state.nt_zscore_end],
+        connect: true,
+        step: 1,
+        orientation: 'horizontal', // 'horizontal' or 'vertical',
+        behaviour: 'tap-drag',
+        range: {
+          min: this.props.highest_tax_counts.lowest_nt_zscore,
+          max: this.props.highest_tax_counts.highest_nt_zscore
+        },
+        format: wNumb({
+          decimals: 0
+        })
+      });
+
+      nt_zscore.noUiSlider.on('update', (values, handle) => {
+        if (handle === 0) {
+          this.setState({
+            nt_zscore_start: Math.round(values[handle])
+          });
+        } else {
+          this.setState({
+            nt_zscore_end: Math.round(values[handle])
+          });
+        }
+      });
+    }
+
+    if (this.has_valid_nt_rpm_range) {
+      const nt_rpm = document.getElementById('nt_rpm');
+      noUiSlider.create(nt_rpm, {
+        start: [this.state.nt_rpm_start, this.state.nt_rpm_end],
+        connect: true,
+        step: 1,
+        orientation: 'horizontal', // 'horizontal' or 'vertical'
+        range: {
+          min: 0,
+          max: this.props.highest_tax_counts.highest_nt_rpm
+        },
+        format: wNumb({
+          decimals: 0
+        })
+      });
+      nt_rpm.noUiSlider.on('update', (values, handle) => {
+        if (handle === 0) {
+          this.setState({
+            nt_rpm_start: Math.round(values[handle])
+          });
+        } else {
+          this.setState({
+            nt_rpm_end: Math.round(values[handle])
+          });
+        }
+      });
+    }
 
   }
   render() {
@@ -174,54 +188,42 @@ class ReportFilter extends React.Component {
 
                 <div className="filter-controls">
                   <div className="filter-title">
-                    VIEW LEVEL
-                  </div>
-
-                  <div className="filter-values">
-                    <p className="">
-                      <input onChange={this.selectViewLevel} name="group1" value='Genus'
-                             checked={(this.state.view_level === 'Genus')} type="radio" id="genus-select" />
-                      <label htmlFor="genus-select">Genus</label>
-                    </p>
-                    <p className="">
-                      <input onChange={this.selectViewLevel} name="group1" value='Species'
-                             checked={(this.state.view_level === 'Species')} type="radio" id="specie-select" />
-                      <label htmlFor="specie-select">Species</label>
-                    </p>
-                  </div>
-                </div>
-
-                <div className="filter-controls">
-                  <div className="filter-title">
                     THRESHOLDS
                   </div>
 
                   <div className="filter-values">
-                    <div className="">
-                      <div className="slider-title">
-                        NT { this.state.view_level } Z Score
+                    { (this.highest_nt_zscore && (this.lowest_nt_zscore !== this.highest_nt_zscore)) ? (
+                      <div className="">
+                        <div className="slider-title">
+                          NT Species Z Score
+                        </div>
+                        <div className="slider-values">
+                          <div className="nt_zscore start">{ this.state.nt_zscore_start }</div>
+                          <div className="nt_zscore end">{ this.state.nt_zscore_end }</div>
+                        </div>
+                        <div id="nt_zscore"></div>
                       </div>
-                      <div className="slider-values">
-                        <div className="nt_zscore start">{ this.state.nt_zscore_start }</div>
-                        <div className="nt_zscore end">{ this.state.nt_zscore_end }</div>
-                      </div>
-                      <div id="nt_zscore"></div>
-                    </div>
+                    ) : ''}
 
-                    <div className="">
-                      <div className="slider-title">
-                        NT { this.state.view_level } RPM
+                     { (this.highest_nt_rpm && (this.lowest_nt_rpm !== this.highest_nt_rpm)) ? (
+                      <div className="">
+                        <div className="slider-title">
+                          NT Species RPM
+                        </div>
+                        <div className="slider-values">
+                          <div className="nt_rpm start">{ this.state.nt_rpm_start }</div>
+                          <div className="nt_rpm end">{ this.state.nt_rpm_end }</div>
+                        </div>
+                        <div id="nt_rpm"></div>
                       </div>
-                      <div className="slider-values">
-                        <div className="nt_rpm start">{ this.state.nt_rpm_start }</div>
-                        <div className="nt_rpm end">{ this.state.nt_rpm_end }</div>
-                      </div>
-                      <div id="nt_rpm"></div>
-                    </div>
+                    ) : ''}
+                     { (!this.has_valid_nt_rpm_range && !this.has_valid_nt_zscore_range) ?
+                    <div className="center"><small>Cannot set thresholds</small></div> : '' }
                   </div>
                 </div>
                 <div className="apply-filter-button center-align">
-                  <a onClick={this.applyFilter} className="btn btn-flat waves-effect grey text-grey text-lighten-5 waves-light apply-filter-button">
+                  <a onClick={this.applyFilter}
+                  className="btn btn-flat waves-effect grey text-grey text-lighten-5 waves-light apply-filter-button">
                     Apply filter
                   </a>
                 </div>
@@ -231,6 +233,6 @@ class ReportFilter extends React.Component {
 
         </div>
       </div>
-    )
+    );
   }
 }
