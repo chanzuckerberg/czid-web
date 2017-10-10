1d6b83df954e76338e35e622abce34f3d864fc71
yunfangjuan <yunfang@chanzuckerberg.com>
2017-10-10 14:23:05 -0700

    add more tests

--- test/controllers/samples_controller_test.rb
+++ test/controllers/samples_controller_test.rb
@@ -7,7 +7,7 @@ class SamplesControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
     @user.authentication_token = 'sdfsdfsdff'
     @user.save
-    post user_session_path, params: { 'user[email]' => @user.email, 'user[password]' => 'password' }
+    @user_params = { 'user[email]' => @user.email, 'user[password]' => 'password' }
   end
 
   test 'should get index' do
@@ -16,6 +16,7 @@ class SamplesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get new' do
+    post user_session_path, params: @user_params
     get new_sample_url
     assert_response :success
   end
@@ -36,23 +37,83 @@ class SamplesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to sample_url(Sample.last)
   end
 
+  test 'should redirect ' do
+    input_files = [{ source: "RR004_water_2_S23_R1_001.fastq.gz",
+                     name: "RR004_water_2_S23_R1_001.fastq.gz",
+                     source_type: "local" },
+                   { source: "RR004_water_2_S23_R2_001.fastq.gz",
+                     name: "RR004_water_2_S23_R2_001.fastq.gz",
+                     source_type: "local" }]
+    assert_difference('Sample.count', 0) do
+      post samples_url, params: { sample: { name: 'new sample', project_name: @project.name, input_files_attributes: input_files } }
+    end
+
+    assert_redirected_to new_user_session_url
+  end
+
+  test 'pipeline_runs' do
+    get pipeline_runs_sample_url(@sample)
+    assert :success
+  end
+
+  test 'reupload source' do
+    post user_session_path, params: @user_params
+    put reupload_source_sample_url(@sample)
+    assert :success
+  end
+
+  test 'reupload source 2' do
+    put reupload_source_sample_url(@sample)
+    assert_redirected_to new_user_session_url
+  end
+
+  test 'kick off pipeline' do
+    post user_session_path, params: @user_params
+    put kickoff_pipeline_sample_url(@sample)
+    assert :success
+  end
+
+  test 'kick off pipeline redirect' do
+    put kickoff_pipeline_sample_url(@sample)
+    assert_redirected_to new_user_session_url
+  end
+
+  test 'should create sample through web' do
+    post user_session_path, params: @user_params
+    input_files = [{ source: "RR004_water_2_S23_R1_001.fastq.gz",
+                     name: "RR004_water_2_S23_R1_001.fastq.gz",
+                     source_type: "local" },
+                   { source: "RR004_water_2_S23_R2_001.fastq.gz",
+                     name: "RR004_water_2_S23_R2_001.fastq.gz",
+                     source_type: "local" }]
+    assert_difference('Sample.count') do
+      post samples_url, params: { sample: { name: 'new sample', project_name: @project.name, input_files_attributes: input_files } }
+    end
+
+    assert_redirected_to sample_url(Sample.last)
+  end
+
   test 'should show sample' do
+    post user_session_path, params: @user_params
     get sample_url(@sample)
     assert_response :success
   end
 
   test 'should get edit' do
+    post user_session_path, params: @user_params
     get edit_sample_url(@sample)
     assert_response :success
   end
 
   test 'should update sample' do
+    post user_session_path, params: @user_params
     assert @sample.valid?
     patch sample_url(@sample), params: { sample: { name: @sample.name + ' asdf' } }
     assert_redirected_to sample_url(@sample)
   end
 
   test 'should destroy sample' do
+    post user_session_path, params: @user_params
     assert_difference('Sample.count', -1) do
       delete sample_url(@sample)
     end
