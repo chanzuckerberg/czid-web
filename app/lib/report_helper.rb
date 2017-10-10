0fc52599529bb9859e082aa6c8381e4a29f566e9
Boris Dimitrov <bdimitrov@chanzuckerberg.com>
2017-10-10 16:55:36 -0700

    fix filters -- zscore, species/genus, slider min/max (#159)
    because we are changing the URL min/max separator from - to ,
    filtering will appear broken until the matching UI change
    goes in;  this change is necessary because negative zscores
    need to be supported

--- app/lib/report_helper.rb
+++ app/lib/report_helper.rb
@@ -1,11 +1,11 @@
 # rubocop:disable ModuleLength
 module ReportHelper
   def external_report_info(report, view_level, params)
-    taxon_zscores = compute_taxon_zscores(report)
     data = {}
     data[:report_details] = report_details(report)
-    data[:taxonomy_details] = taxonomy_details(view_level, report, params, taxon_zscores)
-    data[:highest_tax_counts] = highest_tax_counts(view_level, report, taxon_zscores)
+    htc, td = taxonomy_details(view_level, report, params)
+    data[:highest_tax_counts] = htc
+    data[:taxonomy_details] = td
     data[:view_level] = view_level
     data
   end
@@ -33,53 +33,65 @@ module ReportHelper
     zscore_array
   end
 
-  def taxonomy_details(view_level, _report, params, taxon_zscores)
-    sz = select_zscore(view_level, taxon_zscores)
+  # rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/PerceivedComplexity
+  def taxonomy_details(view_level, report, params)
+    taxon_zscores = compute_taxon_zscores(report)
+    wanted_level = view_level == 'species' ? TaxonCount::TAX_LEVEL_SPECIES : TaxonCount::TAX_LEVEL_GENUS
+    data = taxon_zscores.group_by { |h| h[:tax_level] == wanted_level && h[:hit_type] }
+    nt = data['NT'] || []
+    nr = data['NR'] || []
+    htc = highest_tax_counts(nt, nr)
     # filter and sort the nt_scores
-    sort_report!(sz[:nt_zscores], params[:sort_by])
-    rp = resolve_params(params)
-    sz[:nt_zscores].keep_if do |h|
+    rp = resolve_params(params, htc)
+    nt.keep_if do |h|
       (h[:tax_id] >= 0 &&
         h[:zscore] >= rp[:nt_zscore_threshold][:start] &&
-        h[:zscore] < rp[:nt_zscore_threshold][:end] &&
+        h[:zscore] <= rp[:nt_zscore_threshold][:end] &&
         h[:rpm] >= rp[:nt_rpm_threshold][:start] &&
-        h[:rpm] < rp[:nt_rpm_threshold][:end]
+        h[:rpm] <= rp[:nt_rpm_threshold][:end]
       )
     end
-    # cross-references nr_zscores by taxon id
-    nr_zscore_by_taxon = {}
-    sz[:nr_zscores].each do |h|
-      nr_zscore_by_taxon[h[:tax_id]] = h
-    end
-    # pull nt and nr scores for each taxon in order sorted above
+    sort_report!(nt, params[:sort_by])
+    nr_zscore_by_taxon = nr.group_by { |h| h[:tax_id] }
     tax_details = []
-    sz[:nt_zscores].each do |h|
-      tax_details.push(nt_ele: h, nr_ele: nr_zscore_by_taxon[h[:tax_id]])
+    nt.each do |h|
+      x = nr_zscore_by_taxon[h[:tax_id]]
+      tax_details.push(nt_ele: h, nr_ele: x && x[0])
     end
-    tax_details
+    [htc, tax_details]
   end
 
-  def resolve_params(params)
+  def resolve_params(params, data_ranges)
     new_params = {}
     [:nt_zscore_threshold, :nt_rpm_threshold].each do |k|
-      s, e = params[k] ? params[k].split('-') : []
+      s, e = params[k] ? params[k].split(',') : []
+      ntnr, metric, _threshold = k.to_s.split "_"
+      dr_s = data_ranges[('lowest_' + ntnr + "_" + metric).to_sym]
+      dr_e = data_ranges[('highest_' + ntnr + "_" + metric).to_sym]
       new_params[k] = {
-        start: Float(s || -1_000_000_000),
-        end: Float(e || 1_000_000_000) + 1.0
+        start: Float(s || dr_s),
+        end: Float(e || dr_e)
       }
     end
     new_params
   end
 
-  def highest_tax_counts(view_level, _report, taxon_zscores)
-    sz = select_zscore(view_level, taxon_zscores)
-    nt = sz[:nt_zscores]
-    nr = sz[:nr_zscores]
+  def highest_tax_counts(nt, nr)
+    nt_z = nt.map { |h| h[:zscore] }.sort
+    nr_z = nr.map { |h| h[:zscore] }.sort
+    nt_rpm = nt.map { |h| h[:rpm] }.sort
+    nr_rpm = nr.map { |h| h[:rpm] }.sort
     {
-      highest_nt_zscore: nt.map { |h| h[:z_score] }.sort[-1] || 0,
-      highest_nr_zscore: nr.map { |h| h[:z_score] }.sort[-1] || 0,
-      highest_nt_rpm: nt.map { |h| h[:rpm] }.sort[-1] || 0,
-      highest_nr_rpm: nr.map { |h| h[:rpm] }.sort[-1] || 0
+      # if you change keys here, update data_ranges[...] in resolve_params(...)
+      lowest_nt_zscore: nt_z[0] || 0,
+      lowest_nr_zscore: nr_z[0] || 0,
+      lowest_nt_rpm: nt_rpm[0] || 0,
+      lowest_nr_rpm: nr_rpm[0] || 0,
+      highest_nt_zscore: nt_z[-1] || 0,
+      highest_nr_zscore: nr_z[-1] || 0,
+      highest_nt_rpm: nt_rpm[-1] || 0,
+      highest_nr_rpm: nr_rpm[-1] || 0
     }
   end
 
@@ -90,20 +102,6 @@ module ReportHelper
     nt_zscores.sort! { |hl, hr| hl[sort_field] <=> hr[sort_field] }
     nt_zscores.reverse! if sort_direction == 'highest'
     nt_zscores
-  end
-
-  def select_zscore(level, zscores)
-    wanted_level = level == :species ? TaxonCount::TAX_LEVEL_SPECIES : TaxonCount::TAX_LEVEL_GENUS
-    result = {
-      nr_zscores: [],
-      nt_zscores: []
-    }
-    zscores.each do |z|
-      next unless z[:tax_level] == wanted_level
-      t =  z[:hit_type] == :NR || z[:hit_type] == "NR" ? :nr_zscores : :nt_zscores
-      result[t].push(z)
-    end
-    result
   end
 
   def compute_rpm(count, total_reads)
@@ -118,7 +116,7 @@ module ReportHelper
     if rpm && stdev && stdev != 0
       (rpm - mean) / stdev
     elsif rpm
-      1e6
+      100
     else
       0
     end
