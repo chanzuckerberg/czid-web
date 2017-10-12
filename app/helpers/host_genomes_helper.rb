dc40b676ca7922992635227fb7622f623401a92e
yunfangjuan <yunfang@chanzuckerberg.com>
2017-10-12 15:53:53 -0700

    host genome controllers

---
+++ app/helpers/host_genomes_helper.rb
@@ -0,0 +1,2 @@
+module HostGenomesHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     get :pipeline_runs, on: :member
   end
   resources :projects
+  resources :host_genomes
   resources :users, only: [:create, :new, :edit, :update, :destroy, :index]
   mount Resque::Server.new, at: '/resque'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
