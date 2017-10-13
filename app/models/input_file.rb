7b167e9affea6df06d0eae6f8af72f1156c9daa1
yunfangjuan <yunfangjuan@gmail.com>
2017-10-13 15:49:16 -0700

    Make input less error prone (#184)
    * strip out white spaces
    
    * most robust
    
    * rubocop
    
    * fix tests

--- app/models/input_file.rb
+++ app/models/input_file.rb
@@ -11,6 +11,8 @@ class InputFile < ApplicationRecord
   validate :s3_source_check
 
   def s3_source_check
+    source ||= ''
+    source.strip!
     if source_type == SOURCE_TYPE_S3 && source[0..4] != 's3://'
       errors.add(:input_files, "file source doesn't start with s3:// for s3 input")
     end
