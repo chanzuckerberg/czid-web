2a69f7eb35f2e516e229d678e200dbea8ec1c275
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2017-10-17 11:18:42 -0700

    rubocop

--- app/helpers/samples_helper.rb
+++ app/helpers/samples_helper.rb
@@ -3,29 +3,29 @@ module SamplesHelper
     HostGenome.all.map { |h| [h.name, h.id] }
   end
 
-  def getSummaryStats(jobstats)
-    { remaining_reads: getRemainingReads(jobstats),
-      compression_ratio: computeCompressionRatio(jobstats),
-      qc_percent: computeQcValue(jobstats),
-      percent_remaining: computePercentageReads(jobstats) }
+  def get_summary_stats(jobstats)
+    { remaining_reads: get_remaining_reads(jobstats),
+      compression_ratio: compute_compression_ratio(jobstats),
+      qc_percent: compute_qc_value(jobstats),
+      percent_remaining: compute_percentage_reads(jobstats) }
   end
 
-  def getRemainingReads(jobstats)
+  def get_remaining_reads(jobstats)
     # reads remaining after host filtering
     jobstats.find_by(task: 'run_bowtie2').reads_after
   end
 
-  def computeCompressionRatio(jobstats)
+  def compute_compression_ratio(jobstats)
     cdhitdup_stats = jobstats.find_by(task: 'run_cdhitdup')
-    (1.0 * cdhitdup_stats.reads_before)/cdhitdup_stats.reads_after
-  end
-  
-  def computeQcValue(jobstats)
-    priceseqfilter_stats = jobstats.find_by(task: 'run_priceseqfilter')
-    (100.0 * priceseqfilter_stats.reads_after)/priceseqfilter_stats.reads_before
+    (1.0 * cdhitdup_stats.reads_before) / cdhitdup_stats.reads_after
   end
 
-  def computePercentageReads(jobstats)
-    (100.0 * jobstats.find_by(task: 'run_bowtie2').reads_after)/jobstats.find_by(task: 'run_star').reads_before
+  def compute_qc_value(jobstats)
+    priceseqfilter_stats = jobstats.find_by(task: 'run_priceseqfilter')
+    (100.0 * priceseqfilter_stats.reads_after) / priceseqfilter_stats.reads_before
+  end
+
+  def compute_percentage_reads(jobstats)
+    (100.0 * jobstats.find_by(task: 'run_bowtie2').reads_after) / jobstats.find_by(task: 'run_star').reads_before
   end
 end
