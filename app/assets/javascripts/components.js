19759d2c3dbec00a9b52face310831a99fd2b084
cdebourcy <cdebourcy@chanzuckerberg.com>
2017-10-17 19:51:49 -0700

    Revert "rebase (#202)" (#204)
    This reverts commit d6c6bac47b7fe47e4f6e2649208afbdc049971b1.

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,2 +1,1 @@
 //= require_tree ./components
-//= require_tree ./helpers
--- app/assets/javascripts/helpers/strings.jsx
+++
@@ -1,7 +0,0 @@
-export function numberWithCommas(x) {
-  x = x.toString();
-  var pattern = /(-?\d+)(\d{3})/;
-  while (pattern.test(x))
-      x = x.replace(pattern, "$1,$2");
-  return x;
-}
