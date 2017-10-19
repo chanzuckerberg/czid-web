702981d8a0ee9b82202fc3bdfa7c1faff1a6af08
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2017-10-19 14:36:09 -0700

    move transaction inside function

--- app/models/background.rb
+++ app/models/background.rb
@@ -27,21 +27,23 @@ class Background < ApplicationRecord
   end
 
   def store_summary
-    ActiveRecord::Base.connection.execute <<-SQL
-    DELETE FROM taxon_summaries WHERE background_id = #{id}
-    SQL
-    data = summarize.map { |h| h.slice('tax_id', 'count_type', 'tax_level', :background_id, :created_at, :updated_at, :mean, :stdev) }
-    data_chunks = data.in_groups_of(TAXON_SUMMARY_CHUNK_SIZE, false)
-    data_chunks.each do |chunk|
-      columns = chunk.first.keys
-      values_list = chunk.map do |hash|
-        hash.values.map do |value|
-          ActiveRecord::Base.connection.quote(value)
-        end
-      end
+    ActiveRecord::Base.transaction do
       ActiveRecord::Base.connection.execute <<-SQL
-      INSERT INTO taxon_summaries (#{columns.join(',')}) VALUES #{values_list.map { |values| "(#{values.join(',')})" }.join(', ')}
+      DELETE FROM taxon_summaries WHERE background_id = #{id}
       SQL
+      data = summarize.map { |h| h.slice('tax_id', 'count_type', 'tax_level', :background_id, :created_at, :updated_at, :mean, :stdev) }
+      data_chunks = data.in_groups_of(TAXON_SUMMARY_CHUNK_SIZE, false)
+      data_chunks.each do |chunk|
+        columns = chunk.first.keys
+        values_list = chunk.map do |hash|
+          hash.values.map do |value|
+            ActiveRecord::Base.connection.quote(value)
+          end
+         end
+        ActiveRecord::Base.connection.execute <<-SQL
+        INSERT INTO taxon_summaries (#{columns.join(',')}) VALUES #{values_list.map { |values| "(#{values.join(',')})" }.join(', ')}
+        SQL
+      end
     end
   end
 
