8f5ee0f42bdd86292b57a4bebe444e692e1fc97a
yunfangjuan <yunfang@chanzuckerberg.com>
2017-10-20 11:04:25 -0700

    try something else

--- app/assets/javascripts/server_rendering.js
+++ app/assets/javascripts/server_rendering.js
@@ -1,7 +1,6 @@
 //= require react-server
 //= require react_ujs
 //= require ./components
-//= require ./helpers
 //
 // By default, this file is loaded for server-side rendering.
 // It should require your components and any dependencies.
