9d62c16213a069029430f852b4886a78c60a5629
Boris Dimitrov <dimiroll@gmail.com>
2017-10-19 18:15:17 -0700

    further simply report gen

--- app/lib/report_helper.rb
+++ app/lib/report_helper.rb
@@ -3,6 +3,10 @@ module ReportHelper
   ZSCORE_MAX =  99
   ZSCORE_WHEN_ABSENT_FROM_SAMPLE = -100
   ZSCORE_WHEN_ABSENT_FROM_BACKGROUND = 100
+  # Only as a data cleanliness bug, there may be a taxon_count 'specoes' row
+  # without a corresponding 'genus' row;  then we create a fake singleton
+  # genus containing just that species;  the fake genus IDs start here:
+  FAKE_GENUS_BASE = -1900000000
 
   def external_report_info(report, view_level, params)
     data = {}
@@ -58,7 +62,7 @@ module ReportHelper
         taxon_lineages.genus_name
       )                                AS  name,
       taxon_lineages.superkingdom_name AS  category_name,
-      taxon_counts.count               AS  count,
+      taxon_counts.count               AS  r,
       (count / #{total_reads}.0
         * 1000000.0)                   AS  rpm,
       IF(
@@ -82,165 +86,170 @@ module ReportHelper
     ).to_a.map(&:attributes)
   end
 
-  def flip_type(t)
-    t == 'NR' ? 'NT' : 'NR'
+  def zero_metrics(count_type)
+    {
+      'count_type' => count_type,
+      'r' => 0,
+      'rpm' => 0,
+      'zscore' => ZSCORE_WHEN_ABSENT_FROM_SAMPLE
+    }
   end
 
-  def zero_twin(taxon)
-    result = taxon.clone
-    result['count'] = 0
-    result['rpm'] = 0
-    result['count_type'] = flip_type(result['count_type'])
-    result['zscore'] = ZSCORE_WHEN_ABSENT_FROM_SAMPLE
-    result
-  end
-
-  def sort_params(params_sort_by, view_level_str, details_key)
-    default_sort_by = 'highest_species_nt_zscore'
-    sort_by = params_sort_by || default_sort_by
+  def decode_sort_params(params_sort_by, default='highest_nt_zscore')
+    sort_by = params_sort_by || default
     parts = sort_by.split "_"
-    if parts.length == 2
-      # handle previous frontend version
-      parts = [parts[0], view_level_str, 'nt', parts[1]]
+    if parts.length != 3
+      parts = default.split "_"
     end
-    if parts.length != 4
-      # this is for general malformed parameter
-      parts = default_sort_by.split "_"
+    {
+      direction:    parts[0],
+      count_type:   parts[1].upcase,
+      metric:       parts[2]
+    }
+  end
+
+  METRICS = ['r', 'rpm', 'zscore']
+  COUNT_TYPES = ['NT', 'NR']
+  SORT_DIRECTIONS = ['lowest', 'highest']
+  PROPERTIES_OF_TAXID = ['tax_id', 'name', 'tax_level', 'genus_taxid', 'category_name']
+
+  def tax_info_base(taxon)
+    tax_info_base = {}
+    PROPERTIES_OF_TAXID.each do |prop|
+      tax_info_base[prop] = taxon[prop]
     end
-    sort_direction, sort_tax_level, sort_count_type, sort_field = parts.map(&:to_sym)
-    sort_details_key = details_key[[sort_tax_level, sort_count_type]]
-    [sort_field, sort_details_key, sort_direction]
+    COUNT_TYPES.each do |count_type|
+      tax_info_base[count_type] = zero_metrics(count_type)
+    end
+    tax_info_base
+  end
+
+  def metric_props(taxon)
+    metric_props = {
+      'count_type' => taxon['count_type']
+    }
+    METRICS.each do |metric|
+      metric_props[metric] = taxon[metric]
+    end
+    metric_props
+  end
+
+  def fake_genus!(tax_info)
+    # Create a singleton genus containing just this taxon
+    fake_genus_info = tax_info.clone
+    fake_genus_id = FAKE_GENUS_BASE - tax_info['tax_id']
+    fake_genus_info['tax_id'] = fake_genus_id
+    fake_genus_info['genus_taxid'] = fake_genus_id
+    fake_genus_info['tax_level'] = TaxonCount::TAX_LEVEL_GENUS
+    tax_info['genus_taxid'] = fake_genus_id
+    fake_genus_info
   end
 
   def get_taxon_counts_2d(report)
-    taxon_counts_from_sql = get_raw_taxon_counts(report)
+    # Return data structured as
+    #    tax_id => {
+    #       tax_id,
+    #       tax_level,
+    #       genus_taxid,
+    #       name,
+    #       category_name,
+    #       NR => {
+    #         count_type,
+    #         r,
+    #         rpm
+    #         zscore
+    #       }
+    #       NT => {
+    #         count_type,
+    #         r,
+    #         rpm
+    #         zscore
+    #       }
+    #    }
     taxon_counts_2d = {}
-    taxon_counts_from_sql.each do |taxon|
-      tax_id = taxon['tax_id']
-      count_type = taxon['count_type']
-      taxon_counts_2d[tax_id] ||= {}
-      taxon_counts_2d[tax_id][count_type] = taxon
+    taxon_counts_from_sql = get_raw_taxon_counts(report)
+    taxon_counts_from_sql.each do |t|
+      taxon_counts_2d[t['tax_id']] ||= tax_info_base(t)
+      taxon_counts_2d[t['tax_id']][t['count_type']] = metric_props(t)
     end
-    taxon_counts_2d.each do |_tax_id, tax_pair|
-      tax_pair['NT'] ||= zero_twin(tax_pair['NR'])
-      tax_pair['NR'] ||= zero_twin(tax_pair['NT'])
+    # We might rewrite the query to be super sure of this
+    taxon_counts_2d.each do |tax_id, tax_info|
+      if tax_info['tax_level'] == TaxonCount::TAX_LEVEL_GENUS
+        tax_info['genus_taxid'] = tax_info['tax_id']
+      end
+    end
+    # there should be a genus_pair for every species (even if it is the pseudo
+    # genus id -200);  anything else indicates a bug in data import;
+    # warn about that bug and ensure affected data is NOT hidden from view
+    fake_genuses = []
+    taxon_counts_2d.each do |tax_id, tax_info|
+      unless taxon_counts_2d[tax_info['genus_taxid']]
+        logger.warn "Missing taxon_counts for genus_taxid #{tax_info['genus_taxid']} referenced by taxid #{tax_info['tax_id']}. Will 'promote' the taxon itself into singleton genus.  This may indicate a bug in the DB uploader code.  Try rerunning the sample."
+        fake_genuses << fake_genus!(tax_info)
+      end
+    end
+    fake_genuses.each do |fake_genus_info|
+      taxon_counts_2d[fake_genus_info['genus_taxid']] = fake_genus_info
     end
     taxon_counts_2d
   end
 
-  def taxonomy_details(report, params, view_level)
-    tax2d = get_taxon_counts_2d(report)
-
-    view_level_int = view_level_name2int(view_level)
-    view_level_str = view_level.downcase
-    view_level_sym = view_level_str.to_sym
-
-    details_key = {
-      [:species, :nt] => :nt_ele,
-      [:species, :nr] => :nr_ele,
-      [:genus, :nt] => :genus_nt_ele,
-      [:genus, :nr] => :genus_nr_ele
-    }
-    sort_field, sort_details_key, sort_direction = sort_params(params[:sort_by], view_level_str, details_key)
-
-    htc = highest_tax_counts(tax2d, view_level_str)
-    rp = resolve_params(params, view_level_str, htc)
-
-    sortable = []
-    unsortable = []
-
-    tax2d.each do |tax_id, tax_pair|
-      next unless tax_id >= 0 && tax_pair['NT']['tax_level'] == view_level_int
-
-      if view_level_int == TaxonCount::TAX_LEVEL_SPECIES
-        genus_taxid = tax_pair['NT']['genus_taxid']
-        # the genus_taxid should always be present in tax2d
-        # but sometimes it isn't... possibly due to a bug
-        details = {
-          nt_ele: tax_pair['NT'],
-          nr_ele: tax_pair['NR'],
-          category: tax_pair['NT']['category_name'],
-          genus_nt_ele: tax2d.fetch(genus_taxid, {})['NT'],
-          genus_nr_ele: tax2d.fetch(genus_taxid, {})['NR']
-        }
-      else
-        details = {
-          nt_ele: nil,
-          nr_ele: nil,
-          category: tax_pair['NT']['category_name'],
-          genus_nt_ele: tax_pair['NT'],
-          genus_nr_ele: tax_pair['NR']
-        }
-      end
-
-      # For now we sort by species RPM or Z
-      # TODO: Allow sorting by genus RPM or Z
-      sort_details = details[sort_details_key]
-      sort_field_str = sort_field.to_s
-      if sort_details
-        sort_key = sort_details[sort_field_str]
-        if sort_key
-          sort_key = 0.0 - sort_key if sort_direction == :highest
-          details[:sort_key] = sort_key
-        end
-      end
-
-      out_of_bounds = false
-      [:nt, :nr].each do |type|
-        filter_details = details[details_key[[view_level_sym, type]]]
-        next unless filter_details
-        [:zscore, :rpm].each do |metric|
-          fm_val = filter_details[metric.to_s]
-          high = "highest_#{view_level_str}_#{type}_#{metric}".to_sym
-          if rp[high] < fm_val
-            out_of_bounds = true
-            break
-          end
-          low = "lowest_#{view_level_str}_#{type}_#{metric}".to_sym
-          if rp[low] > fm_val
-            out_of_bounds = true
-            break
-          end
-        end
-        break if out_of_bounds
-      end
-
-      unless out_of_bounds
-        if details[:sort_key]
-          sortable.push(details)
-        else
-          unsortable.push(details)
-        end
-      end
-    end
-
-    sortable.sort! { |dl, dr| dl[:sort_key] <=> dr[:sort_key] }
-    # HACK: -- the UI gets really slow if we return all results
-    # about 1000 results is what keeps it super snappy for now
-    rows = (sortable + unsortable)[0...1000]
-    [htc, rows]
+  def negative(vec_3d)
+    x, y, z = vec_3d
+    [-x, -y, -z]
   end
 
-  def resolve_params(params, view_level_str, data_ranges)
+  def sort_key(tax_2d, tax_info, sort_by)
+    # sort by (genus, species) in the chosen metric;  making sure that
+    # the genus comes before its species in either sort direction
+    genus_info = tax_2d[tax_info['genus_taxid']]
+    sort_key_genus = genus_info[sort_by[:count_type]][sort_by[:metric]]
+    if tax_info['tax_level'] == TaxonCount::TAX_LEVEL_SPECIES
+      sort_key_species = tax_info[sort_by[:count_type]][sort_by[:metric]]
+      sort_key_3d = [sort_key_genus, 1, sort_key_species]
+    else
+      sort_key_3d = [sort_key_genus, 0, 0]
+    end
+    sort_by[:direction] == 'lowest' ? sort_key_3d : negative(sort_key_3d)
+  end
+
+  def taxonomy_details(report, params, view_level)
+    tax_2d = get_taxon_counts_2d(report)
+    data_ranges = min_max(tax_2d)
+
+    view_level_int = view_level_name2int(view_level)
+    sort_by = decode_sort_params(params[:sort_by])
+    rp = decode_range_params(params, data_ranges)
+
+    rows = []
+    tax_2d.each do |tax_id, tax_info|
+      next unless tax_id >= 0 && tax_info['tax_level'] >= view_level_int
+      tax_info[:sort_key] = sort_key(tax_2d, tax_info, sort_by)
+      rows << tax_info
+    end
+
+    rows.sort! { |dl, dr| dl[:sort_key] <=> dr[:sort_key] }
+
+    rows.each do |tax_info|
+      tax_info.delete(:sort_key)
+    end
+
+    puts "BORIS: #{rows[0..10]}"
+
+    [data_ranges, rows[0...1000]]
+  end
+
+  def decode_range_params(params, data_ranges)
     new_params = data_ranges.clone
-    [:species_nt_zscore_threshold,
-     :species_nt_rpm_threshold,
-     :species_nr_zscore_threshold,
-     :species_nr_rpm_threshold,
-     :genus_nt_zscore_threshold,
-     :genus_nt_rpm_threshold,
-     :genus_nr_zscore_threshold,
-     :genus_nr_rpm_threshold,
-     :nt_zscore_threshold,
+    [:nt_zscore_threshold,
      :nt_rpm_threshold,
+     :nt_r_threshold,
      :nr_zscore_threshold,
-     :nr_rpm_threshold].each do |k|
-      level_type_metric = k.to_s.chomp('_threshold')
-      if level_type_metric.split("_").length < 3
-        level_type_metric = "#{view_level_str}_#{level_type_metric}".to_sym
-      end
-      high = "highest_#{level_type_metric}".to_sym
-      low = "lowest_#{level_type_metric}".to_sym
+     :nr_rpm_threshold,
+     :nr_r_threshold].each do |k|
+      type_metric = k.to_s.chomp('_threshold')
+      high = "highest_genus_#{type_metric}".to_sym
+      low = "lowest_genus__#{type_metric}".to_sym
       thresholds = params[k]
       next unless thresholds
       t_low, t_high = thresholds.split(',')
@@ -252,39 +261,30 @@ module ReportHelper
     new_params
   end
 
-  def highest_tax_counts(taxon_counts_2d, view_level_str)
-    # compute min/max extents for 8 columns
+  def min_max(taxon_counts_2d)
+    # compute min/max extents for all genus metrics
     bounds = {}
-    taxon_counts_2d.each do |_tax_id, tax_pair|
-      tax_pair.each do |_count_type, taxon|
-        level = taxon['tax_level'] == TaxonCount::TAX_LEVEL_SPECIES ? :species : :genus
-        type = taxon['count_type'] == 'NT' ? :nt : :nr
-        [:zscore, :rpm].each do |metric|
-          tm_val = taxon[metric.to_s]
-          high = [:highest, level, type, metric]
-          bounds[high] = tm_val unless bounds[high] && bounds[high] > tm_val
-          low = [:lowest, level, type, metric]
-          bounds[low] = tm_val unless bounds[low] && bounds[low] < tm_val
+    taxon_counts_2d.each do |_tax_id, tax_info|
+      next unless tax_info['tax_level'] == TaxonCount::TAX_LEVEL_GENUS
+      COUNT_TYPES.each do |count_type|
+        count_bucket = tax_info[count_type]
+        METRICS.each do |metric|
+          val = count_bucket[metric]
+          high = "highest_genus_#{count_type}_#{metric}".to_sym
+          bounds[high] = val unless bounds[high] && bounds[high] > val
+          low = "lowest_genus_#{count_type}_#{metric}".to_sym
+          bounds[low] = val unless bounds[low] && bounds[low] < val
         end
       end
     end
     # flatten & handle empty columns
     flat = {}
     default_value = 0
-    [:highest, :lowest].each do |extent|
-      [:species, :genus].each do |level|
-        [:nt, :nr].each do |type|
-          [:zscore, :rpm].each do |metric|
-            key_l = [extent, level, type, metric]
-            key_s = "#{extent}_#{level}_#{type}_#{metric}".to_sym
-            flat[key_s] = bounds.delete(key_l) || default_value
-            # For now, JS likes to get the extents of the chosen view_level
-            # without express view_level qualification
-            if level != view_level_str.to_sym
-              key_ui = "#{extent}_#{type}_#{metric}".to_sym
-              flat[key_ui] = flat[key_s]
-            end
-          end
+    SORT_DIRECTIONS.each do |extent|
+      COUNT_TYPES.each do |type|
+        METRICS.each do |metric|
+          key = "#{extent}_genus_#{type}_#{metric}".to_sym
+          flat[key] = bounds.delete(key) || default_value
         end
       end
     end
