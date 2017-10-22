935f8bd7554e0667833b6f7da1fb3b178d9a3191
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2017-10-22 13:02:36 -0700

    s3 upload

--- app/models/taxon_sequence_file.rb
+++ app/models/taxon_sequence_file.rb
@@ -10,15 +10,18 @@ class TaxonSequenceFile < ApplicationRecord
 
   def generate_fasta
     # currently only works with species-level taxids (ANNOTATED_FASTA is species-level)
-    input_fasta_s3_path = "#{pipeline_output.sample.sample_output_s3_path}/#{ANNOTATED_FASTA}"
+    s3_input_folder = "#{pipeline_output.sample.sample_output_s3_path}"
+    s3_input_fasta = "#{s3_input_folder}/#{ANNOTATED_FASTA}"
     local_fasta_path = "#{LOCAL_FASTA_PATH}"
     local_input = "#{local_fasta_path}/#{ANNOTATED_FASTA}"
     local_output = "#{local_fasta_path}/#{fasta_name}"
+    s3_output = "#{s3_input_folder}/#{fasta_name}"
     command = "mkdir -p #{local_fasta_path};"
-    command += "aws s3 cp #{input_fasta_s3_path} #{local_fasta_path}/;"
-    command += "grep -A 1 -E 'nr:#{taxid}:|nt:#{taxid}:' #{local_input} | sed '/^--$/d' > #{local_output}"
+    command += "aws s3 cp #{s3_input_fasta} #{local_fasta_path}/;"
+    command += "grep -A 1 -E 'nr:#{taxid}:|nt:#{taxid}:' #{local_input} | sed '/^--$/d' > #{local_output};"
+    command += "aws s3 cp #{local_output} #{s3_output};"
     _stdout, _stderr, status = Open3.capture3(command)
     return nil unless status.exitstatus.zero?
-    "#{local_output}"
+    "#{s3_output}"
   end
 end
