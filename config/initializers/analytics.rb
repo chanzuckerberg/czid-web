2b5a96b90f10d197457bb47d7f814e76b21cb632
yunfangjuan <yunfang@chanzuckerberg.com>
2017-10-25 13:27:42 -0700

    fix rubocop

--- config/initializers/analytics.rb
+++ config/initializers/analytics.rb
@@ -1,4 +1,4 @@
 module Analytics
-  CONFIG = YAML.load_file("#{Rails.root.to_s}/config/analytics.yml")[Rails.env]
+  CONFIG = YAML.load_file(Rails.root.join('config', 'analytics.yml'))[Rails.env]
   TRACKING_SOURCE = CONFIG['tracking_src']
 end
