385399833a45f492265c45cd17ef85a615ae3a54
yunfangjuan <yunfangjuan@gmail.com>
2017-10-25 12:54:39 -0700

    Genus search (#241)
    * genus search take 1
    
    * finish type ahead
    
    * compose query string
    
    * enable backend
    
    * rubocop
    
    * remove console.log
    
    * remove tabs

--- app/assets/javascripts/components/PipelineSampleReport.jsx
+++ app/assets/javascripts/components/PipelineSampleReport.jsx
@@ -6,6 +6,8 @@ class PipelineSampleReport extends React.Component {
     this.taxonomy_details = props.taxonomy_details;
     this.all_categories = props.all_categories || [];
     this.checked_categories = props.checked_categories || props.all_categories
+    this.genus_info = props.genus_info
+    this.sample_id = props.sample_id
 
     this.view_level = ReportFilter.getFilter('view_level') || 'species';
     this.highest_tax_counts = props.highest_tax_counts;
@@ -103,7 +105,9 @@ class PipelineSampleReport extends React.Component {
                   background_model = {this.report_details.background_model.name}
                   report_title = { this.report_details.report_info.name }
                   view_level={this.view_level}
-                  highest_tax_counts={this.highest_tax_counts}/>
+                  highest_tax_counts={this.highest_tax_counts}
+                  sample_id = {this.sample_id}
+                  genus_info = {this.genus_info} />
               </div>
               <div className="col s10 reports-main ">
                 <table id="report-table" className='bordered report-table'>
@@ -251,7 +255,7 @@ class PipelineSampleReport extends React.Component {
                         <i className='fa fa-caret-down'></i>
                       </div>
                     </th> : '' }
- 
+
                   </tr>
                   </thead>
                   <tbody>
@@ -330,7 +334,7 @@ class PipelineSampleReport extends React.Component {
                         <td>
                           { (this.view_level==='species' && (taxon.nr_ele && taxon.nr_ele.hasOwnProperty('count')) ) ? numberWithCommas(taxon.nr_ele.count) : '' }
                         </td>
- 
+
                       </tr>
                     )
                   })}
