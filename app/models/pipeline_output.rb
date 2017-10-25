b1a575912753db913cb4fc19ddd6b40e81f93b1e
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2017-10-24 19:30:11 -0700

    remove postprocess_run wiring

--- app/models/pipeline_output.rb
+++ app/models/pipeline_output.rb
@@ -4,18 +4,10 @@ class PipelineOutput < ApplicationRecord
   has_many :reports, dependent: :destroy
   has_many :job_stats, dependent: :destroy
   has_and_belongs_to_many :backgrounds
-  has_many :postprocess_runs, dependent: :destroy
   has_one :sequence_locator
   accepts_nested_attributes_for :taxon_counts
   accepts_nested_attributes_for :job_stats
   belongs_to :pipeline_run
-
-  STATUS_POSTPROCESS = 'need_postprocess'.freeze
-  STATUS_CHECKED_POSTPROCESS = 'checked_postprocess'.freeze
-  DEFAULT_POSTPROCESS_MEMORY = 8_000
-  DEFAULT_POSTPROCESS_QUEUE = 'aegea_batch_ondemand'.freeze
-
-  before_save :check_postprocess_status
 
   def name
     ['ID#', id, ' (', sample.name, ')'].join('')
@@ -52,50 +44,5 @@ class PipelineOutput < ApplicationRecord
              taxon_counts.tax_level = #{TaxonCount::TAX_LEVEL_SPECIES}
       GROUP BY 1,2,3,4,5"
     )
-  end
-
-  def check_postprocess_status
-    return unless status == STATUS_POSTPROCESS
-    self.status = STATUS_CHECKED_POSTPROCESS
-    kickoff_postprocess_pipeline(false)
-  end
-
-  def postprocess_command
-    script_name = File.basename(IdSeqPostprocess::S3_SCRIPT_LOC)
-    batch_command_env_variables = "INPUT_BUCKET=#{sample.sample_output_s3_path} " \
-      "OUTPUT_BUCKET=#{sample.sample_postprocess_s3_path} "
-    batch_command = "aws s3 cp #{IdSeqPostprocese::S3_SCRIPT_LOC} .; chmod 755 #{script_name}; " +
-                    batch_command_env_variables + "./#{script_name}"
-    command = "aegea batch submit --command=\"#{batch_command}\" "
-    memory = DEFAULT_POSTPROCESS_MEMORY
-    queue = DEFAULT_POSTPROCESS_QUEUE
-    command += " --storage /mnt=1500 --ecr-image idseq --memory #{memory} --queue #{queue} --vcpus 16"
-    command
-  end
-
-  def kickoff_postprocess_pipeline(dry_run = true)
-    # only kickoff pipeline when no active postprocess_run running
-    return unless postprocess_runs.in_progress.empty?
-
-    command = postprocess_command
-    if dry_run
-      Rails.logger.debug(command)
-      return command
-    end
-
-    stdout, stderr, status = Open3.capture3(command)
-    pr = PostprocessRun.new
-    pr.sample = self
-    pr.command = command
-    pr.command_stdout = stdout
-    pr.command_error = stderr
-    pr.command_status = status.to_s
-    if status.exitstatus.zero?
-      output = JSON.parse(pr.command_stdout)
-      pr.job_id = output['jobId']
-    else
-      pr.job_status = PostprocessRun::STATUS_FAILED
-    end
-    pr.save
   end
 end
