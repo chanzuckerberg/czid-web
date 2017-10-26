ea8311f82585a68a1de5f05876c0247a2e1a74c6
cdebourcy <cdebourcy@chanzuckerberg.com>
2017-10-25 17:42:35 -0700

    Update PipelineSampleReport.jsx

--- app/assets/javascripts/components/PipelineSampleReport.jsx
+++ app/assets/javascripts/components/PipelineSampleReport.jsx
@@ -344,7 +344,7 @@ class PipelineSampleReport extends React.Component {
                         </td>
 
                         <td>
-                          ## TO DO: Add info button explaining what aggregate score is
+                          # TO DO: Add info button explaining what aggregate score is
                           { (this.view_level==='species' && taxon.aggregate_score) ? numberWithCommas(Number(taxon.aggregate_score).toFixed(3)) : '' }
                         </td>
 
