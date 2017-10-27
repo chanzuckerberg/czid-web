4e56b9d5b8d3d6b5ac5904014d1f1d6be552c907
Boris Dimitrov <bdimitrov@chanzuckerberg.com>
2017-10-27 13:21:55 -0700

    fix uncategorized genus aggregation (#258)
    without this fix, each of the few dozen uncategorizable tax_ids will be
    given its own singleton pseudo genus in the report_helper.rb,
    which is not ideal for many reasons

--- app/models/pipeline_output.rb
+++ app/models/pipeline_output.rb
@@ -35,19 +35,46 @@ class PipelineOutput < ApplicationRecord
   def generate_aggregate_counts(tax_level_name)
     current_date = Time.zone.now.strftime("%Y-%m-%d")
     tax_level_id = TaxonCount::NAME_2_LEVEL[tax_level_name]
-    # TODO(yf): take into account the case when tax_id doesn't appear in the taxon_lineages table
+    missing_lineage_id_for_level = {
+      species: -100,
+      genus: -200,
+      family: -300,
+      order: -400,
+      class: -500,
+      phyllum: -600,
+      superkingdom: -700
+    }
+    # The unctagorizable_name chosen here is not important. The report page
+    # endpoint makes its own choice about what to display in this case.  It
+    # has general logic to handle this and other undefined cases uniformly.
+    # What is crucially important is the uncategorizable_id.  Must match
+    # the constant at the top of report_helper.rb.
+    uncategorizable_id = missing_lineage_id_for_level.fetch(tax_level_name.to_sym, -9999)
+    uncategorizable_name = "Uncategorizable as a #{tax_level_name}"
     TaxonCount.connection.execute(
       "INSERT INTO taxon_counts(pipeline_output_id, tax_id, name,
                                 tax_level, count_type, count,
                                 percent_identity, alignment_length, e_value,
                                 created_at, updated_at)
-       SELECT #{id}, taxon_lineages.#{tax_level_name}_taxid, taxon_lineages.#{tax_level_name}_name,
-              #{tax_level_id}, taxon_counts.count_type,
+       SELECT #{id},
+              IF(
+                taxon_lineages.#{tax_level_name}_taxid IS NOT NULL,
+                taxon_lineages.#{tax_level_name}_taxid,
+                #{uncategorizable_id}
+              ),
+              IF(
+                taxon_lineages.#{tax_level_name}_taxid IS NOT NULL,
+                taxon_lineages.#{tax_level_name}_name,
+                '#{uncategorizable_name}'
+              ),
+              #{tax_level_id},
+              taxon_counts.count_type,
               sum(taxon_counts.count),
               sum(taxon_counts.percent_identity * taxon_counts.count) / sum(taxon_counts.count),
               sum(taxon_counts.alignment_length * taxon_counts.count) / sum(taxon_counts.count),
               sum(taxon_counts.e_value * taxon_counts.count) / sum(taxon_counts.count),
-              '#{current_date}', '#{current_date}'
+              '#{current_date}',
+              '#{current_date}'
        FROM  taxon_lineages, taxon_counts
        WHERE taxon_lineages.taxid = taxon_counts.tax_id AND
              taxon_counts.pipeline_output_id = #{id} AND
