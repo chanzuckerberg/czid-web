ab9ff19d0d8253a15e938c87c85056c48d58fe9c
sadelokiki <susan.adelokiki@andela.com>
2017-10-27 12:11:58 -0700

    Refactor api to receive server error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,7 +53,7 @@ class UsersController < ApplicationController
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: @user.errors.full_messages, status: :unprocessable_entity }
       end
     end
   end
