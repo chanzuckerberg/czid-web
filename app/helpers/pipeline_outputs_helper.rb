2a2da2e8ef7075be9c8d3015285b7bc2bb54f3cf
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2017-10-27 13:04:05 -0700

    address comments

--- app/helpers/pipeline_outputs_helper.rb
+++ app/helpers/pipeline_outputs_helper.rb
@@ -3,6 +3,7 @@ module PipelineOutputsHelper
 
   def get_taxid_fasta(pipeline_output, taxid, tax_level, hit_type)
     uri = pipeline_output.sample.s3_paths_for_taxon_byteranges[tax_level][hit_type]
+    # e.g. "s3://czbiohub-idseq-samples-development/samples/8/74/postprocess/taxid_annot_sorted_genus_nt.fasta"
     uri_parts = uri.split("/", 4)
     bucket = uri_parts[2]
     key = uri_parts[3]
