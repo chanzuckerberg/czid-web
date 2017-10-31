61cb413fb432fc96c31a20cc71c3f3d0637a4fb7
sadelokiki <susan.adelokiki@andela.com>
2017-10-31 00:16:07 -0700

    Refactor pagination

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -27,7 +27,7 @@ class SamplesController < ApplicationController
   # GET /samples/search.json
   def search
     search_query = params[:search]
-    @samples = Sample.includes(:pipeline_runs, :pipeline_outputs).search(search_query).paginate(page: params[:page], per_page: 10)
+    @samples = Sample.includes(:pipeline_runs, :pipeline_outputs).search(search_query).paginate(page: params[:page])
     if @samples.length
       respond_to do |format|
         format.json { render json: @samples, message: 'Search results found' }
