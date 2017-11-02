d7ec3dd86421f2d56344a63daa24efe50449ee26
yunfangjuan <yunfang@chanzuckerberg.com>
2017-11-02 11:16:26 -0700

    blacklist

--- app/models/taxon_lineage.rb
+++ app/models/taxon_lineage.rb
@@ -1,5 +1,6 @@
 # The TaxonLineage model gives the taxids forming the taxonomic lineage of any given species-level taxid.
 class TaxonLineage < ApplicationRecord
+  BLACKLIST_GENUS_ID = -201
   def self.get_genus_info(genus_tax_id)
     r = find_by(genus_taxid: genus_tax_id)
     return { query: r.genus_name, tax_id: genus_tax_id } if r
