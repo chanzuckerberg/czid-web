92aee33cc4b5b7ffbe26499426741e4823fd7d8b
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2017-11-06 14:17:53 -0800

    realtime

--- aws_batch/pipeline.py
+++ aws_batch/pipeline.py
@@ -469,6 +469,14 @@ def return_merged_dict(dict1, dict2):
 def execute_command(command):
     print command
     sys.stdout.flush()
+    output = subprocess.check_output(command, shell=True)
+    print output
+    sys.stdout.flush()
+    return output
+
+def execute_command_realtime_stdout(command):
+    print command
+    sys.stdout.flush()
     process = subprocess.Popen(command, shell=True, stdout=PIPE)
     while True:
         line = process.stdout.readline().rstrip()
@@ -819,7 +827,7 @@ def run_star(sample_name, fastq_file_1, fastq_file_2, star_genome_s3_path,
                            '--runThreadN', str(multiprocessing.cpu_count()),
                            '--genomeDir', REF_DIR + '/STAR_genome',
                            '--readFilesIn', fastq_file_1, fastq_file_2]
-    execute_command(" ".join(star_command_params))
+    execute_command_realtime_stdout(" ".join(star_command_params))
     logging.getLogger().info("finished job")
     # extract out unmapped files
     execute_command("cp %s/%s %s/%s;" % (scratch_dir, 'Unmapped.out.mate1', result_dir, STAR_OUT1))
@@ -847,7 +855,7 @@ def run_priceseqfilter(sample_name, input_fq_1, input_fq_2,
                        '-rqf','85','0.98',
                        '-rnf','90',
                        '-log','c']
-    execute_command(" ".join(priceseq_params))
+    execute_command_realtime_stdout(" ".join(priceseq_params))
     logging.getLogger().info("finished job")
     # copy back to aws
     execute_command("aws s3 cp %s/%s %s/;" % (result_dir, PRICESEQFILTER_OUT1, sample_s3_output_path))
@@ -882,7 +890,7 @@ def run_cdhitdup(sample_name, input_fa_1, input_fa_2,
                        '-o',  result_dir + '/' + CDHITDUP_OUT1,
                        '-o2', result_dir + '/' + CDHITDUP_OUT2,
                        '-e',  '0.05', '-u', '70']
-    execute_command(" ".join(cdhitdup_params))
+    execute_command_realtime_stdout(" ".join(cdhitdup_params))
     logging.getLogger().info("finished job")
     # copy back to aws
     execute_command("aws s3 cp %s/%s %s/;" % (result_dir, CDHITDUP_OUT1, sample_s3_output_path))
@@ -931,7 +939,7 @@ def run_bowtie2(sample_name, input_fa_1, input_fa_2, bowtie_genome_s3_path,
                      '--very-sensitive-local',
                      '-f', '-1', input_fa_1, '-2', input_fa_2,
                      '-S', result_dir + '/' + BOWTIE2_OUT]
-    execute_command(" ".join(bowtie2_params))
+    execute_command_realtime_stdout(" ".join(bowtie2_params))
     logging.getLogger().info("finished alignment")
     # extract out unmapped files from sam
     output_prefix = result_dir + '/' + EXTRACT_UNMAPPED_FROM_SAM_OUT1[:-8]
