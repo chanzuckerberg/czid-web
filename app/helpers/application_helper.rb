a4394fd9a0ef45411e692c8da35896042ea6d5dc
cdebourcy <cdebourcy@chanzuckerberg.com>
2017-11-13 16:00:03 -0800

    separate postprocess loading task (#317)
    * separate postprocess loading task
    
    * fix bug
    
    * rubocop
    
    * fix bug
    
    * check flag
    
    * change schedule
    
    * rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
+require 'csv'
+require 'json'
+
 module ApplicationHelper
+  def rds_host
+    Rails.env == 'development' ? 'db' : '$RDS_ADDRESS'
+  end
+
+  def hash_array_json2csv(input_file, output_file, keys)
+    CSV.open(output_file, "w") do |csv|
+      JSON.parse(File.open(input_file).read).each do |hash|
+        csv << hash.values_at(*keys)
+      end
+    end
+  end
 end
