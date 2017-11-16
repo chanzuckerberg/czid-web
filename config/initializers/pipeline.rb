1181bf02b72f9610dab0c1afd19f8c4daa12fad7
yunfangjuan <yunfang@chanzuckerberg.com>
2017-11-15 16:44:38 -0800

    adding all the relevant pipeline command

--- config/initializers/pipeline.rb
+++ config/initializers/pipeline.rb
@@ -1,4 +1,6 @@
 module IdSeqPipeline
-  S3_SCRIPT_LOC = ENV['IDSEQ_S3_SCRIPT_LOC'] || "s3://czbiohub-infectious-disease/bin/#{Rails.env}/pipeline.py"
+  S3_SCRIPT_LOC = ENV['IDSEQ_S3_SCRIPT_LOC'] || "s3://czbiohub-infectious-disease/bin/#{Rails.env}/pipeline.py" # TO BE DEPRECATED
   S3_POSTPROCESS_SCRIPT_LOC = ENV['IDSEQ_POSTPROCESS_S3_SCRIPT_LOC'] || "s3://czbiohub-infectious-disease/bin/#{Rails.env}/postprocess.py"
+  S3_HOST_FILTER_SCRIPT_LOC = ENV['IDSEQ_POSTPROCESS_S3_SCRIPT_LOC'] || "s3://czbiohub-infectious-disease/bin/#{Rails.env}/host_filtering.py"
+  S3_ALIGNMENT_SCRIPT_LOC = ENV['IDSEQ_POSTPROCESS_S3_SCRIPT_LOC'] || "s3://czbiohub-infectious-disease/bin/#{Rails.env}/non_host_alignment.py"
 end
