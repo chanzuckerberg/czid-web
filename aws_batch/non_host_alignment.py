0ab40e7e442ab902340fc04b16d041c4b455c28d
cdebourcy <cdebourcy@chanzuckerberg.com>
2017-11-16 17:01:59 -0800

    Update non_host_alignment.py

--- aws_batch/non_host_alignment.py
+++ aws_batch/non_host_alignment.py
@@ -639,9 +639,10 @@ def run_stage2(lazy_run = True):
     input3_present = check_s3_file_presence(input3_s3_path)
     if input1_present and input2_present and input3_present:
         # output of previous stage
-        execute_command("aws s3 cp %s %s/" % (input1_s3_path, SAMPLE_S3_OUTPUT_PATH))
-        execute_command("aws s3 cp %s %s/" % (input2_s3_path, SAMPLE_S3_OUTPUT_PATH))
-        execute_command("aws s3 cp %s %s/" % (input3_s3_path, SAMPLE_S3_OUTPUT_PATH))
+        if SAMPLE_S3_INPUT_PATH != SAMPLE_S3_OUTPUT_PATH:
+            execute_command("aws s3 cp %s %s/" % (input1_s3_path, SAMPLE_S3_OUTPUT_PATH))
+            execute_command("aws s3 cp %s %s/" % (input2_s3_path, SAMPLE_S3_OUTPUT_PATH))
+            execute_command("aws s3 cp %s %s/" % (input3_s3_path, SAMPLE_S3_OUTPUT_PATH))
         gsnapl_input_files = [EXTRACT_UNMAPPED_FROM_SAM_OUT1, EXTRACT_UNMAPPED_FROM_SAM_OUT2]
         before_file_name_for_log = os.path.join(RESULT_DIR, EXTRACT_UNMAPPED_FROM_SAM_OUT1)
         before_file_type_for_log = "fasta_paired"
