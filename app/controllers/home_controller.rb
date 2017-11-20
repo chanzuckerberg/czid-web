cda991b7497f4f5dcc30ebe7898fb88287b08743
sadelokiki <susan.adelokiki@andela.com>
2017-11-20 01:44:59 -0800

    Rubocop fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -26,18 +26,16 @@ class HomeController < ApplicationController
         samples = sort_by(Sample.includes(:pipeline_runs, :pipeline_outputs).where(project_id: project_id), sort)
       end
       @project_info = Project.find(project_id)
+    elsif filter_query.present? && search_query.present?
+      search_results = sort_by(Sample.includes(:pipeline_runs, :pipeline_outputs).search(search_query), sort)
+      samples = filter_samples(search_results, filter_query)
+    elsif search_query.present?
+      samples = sort_by(Sample.includes(:pipeline_runs, :pipeline_outputs).search(search_query), sort)
+    elsif filter_query.present?
+      unfiltered_samples = sort_by(Sample.includes(:pipeline_runs, :pipeline_outputs), sort)
+      samples = filter_samples(unfiltered_samples, filter_query)
     else
-      elsif filter_query.present? && search_query.present?
-        search_results = sort_by(Sample.includes(:pipeline_runs, :pipeline_outputs).search(search_query), sort)
-        samples = filter_samples(search_results, filter_query)
-      elsif search_query.present?
-        samples = sort_by(Sample.includes(:pipeline_runs, :pipeline_outputs).search(search_query), sort)
-      elsif filter_query.present?
-        unfiltered_samples = sort_by(Sample.includes(:pipeline_runs, :pipeline_outputs), sort)
-        samples = filter_samples(unfiltered_samples, filter_query)
-      else
-        samples = sort_by(Sample.includes(:pipeline_runs, :pipeline_outputs), sort)
-      end
+      samples = sort_by(Sample.includes(:pipeline_runs, :pipeline_outputs), sort)
     end
     @samples = samples.paginate(page: params[:page], per_page: 10)
     @samples_count = samples.size
