d23a1af87329ffb85fd4d8898834d6f6cc87501b
sadelokiki <susan.adelokiki@andela.com>
2017-11-20 16:48:05 -0800

    Infinite scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,16 +8,4 @@
 //= require materialize
 //= require moment
 //= require materialize/extras/nouislider
-
-$(document).ready(function() {
-  if ($('.pagination').length) {
-    $(window).scroll(function() {
-      var url = $('.pagination .next_page').attr('href');
-      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-        $('.pagination').text("Please Wait...");
-        return $.getScript(url);
-      }
-    });
-    return $(window).scroll();
-  }
-});
+//= require jquery.infinite-pages
