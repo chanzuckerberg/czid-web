dd4983ae78baa47882be70551f813ad7eadeae6f
sadelokiki <susan.adelokiki@andela.com>
2017-11-21 10:03:15 -0800

    Rubocop fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,30 +11,25 @@ class HomeController < ApplicationController
     sort = params[:sort_by]
     @project_info = nil
     results = sort_by(Sample.includes(:pipeline_runs, :pipeline_outputs), sort)
-    
-    if params[:ids].present?
-      results = results.where("id in (#{params[:ids]})")
-    end
+
+    results = results.where("id in (#{params[:ids]})") if params[:ids].present?
 
     if project_id.present?
       @project_info = Project.find(project_id)
       results = results.where(project_id: project_id)
     end
 
-    results = results.search(name_search_query) if name_search_query.present? 
+    results = results.search(name_search_query) if name_search_query.present?
     results = filter_samples(results, filter_query) if filter_query.present?
 
     @samples = results.paginate(page: params[:page], per_page: 10)
     @samples_count = results.size
     @all_samples = format_samples(@samples)
-  end
-
-
-
+  end  
+  
   def sort_by(samples, dir = nil)
     default_dir = 'newest'
     dir ||= default_dir
     dir == 'newest' ? samples.order(created_at: :desc) : samples.order(created_at: :asc)
   end
 end
-
