784057494fb994ebaa9bccf656bf5e8892a3c0be
Cyril Ologho <cyril.paul@andela.com>
2017-11-28 10:25:27 -0800

    disable heatmap button and restyle sample upload button

--- app/assets/javascripts/components/samples.jsx
+++ app/assets/javascripts/components/samples.jsx
@@ -224,24 +224,11 @@ class Samples extends React.Component {
         <div className="sub-header-home">
           <div className="sub-header-items">
             <div className="content">
-              <div className="sub-header-buttons right">
-                <ul>
-                  <li>
-                    <a href='/samples/new'>
-                      <i className="fa fa-flask" aria-hidden="true"></i>
-                      <span>Upload Sample</span> 
-                    </a>
-                  </li>
-                  {(this.project) ?
-                  <li>
-                    <a href={`/projects/${this.project.id}/visuals`}>
-                      <i className="fa fa-bar-chart" aria-hidden="true"></i>
-                      <span>Project Heatmap</span>
-                    </a>
-                  </li>
-                  : undefined
-                }
-                </ul>
+              <div  className="upload">
+                <a href='/samples/new'>
+                  <i className="fa fa-flask" aria-hidden="true"/>
+                  <span>Upload Sample</span>
+                </a>
               </div>
 
               <div className="sub-title">
