deddf59850a6e475fdb71d01efd131e2c2aca99a
sadelokiki <susan.adelokiki@andela.com>
2017-11-28 00:01:38 -0800

    Rubocop fixes

--- app/helpers/samples_helper.rb
+++ app/helpers/samples_helper.rb
@@ -33,7 +33,7 @@ module SamplesHelper
 
   def sample_status_display(sample)
     if sample.status == Sample::STATUS_CREATED
-      return 'uploading'
+      'uploading'
     elsif sample.status == Sample::STATUS_CHECKED
       pipeline_run = sample.pipeline_runs.first
       return '' unless pipeline_run
@@ -119,21 +119,21 @@ module SamplesHelper
           host_filtering = run_stages ? run_stages.first.job_status : nil
           gsnap_alignment = run_stages ? run_stages.second.job_status : nil
           post_processing = run_stages ? run_stages.third.job_status : nil
-          pipeline_run_entry[:post_processing] = post_processing 
-          pipeline_run_entry[:gsnap_alignment] = gsnap_alignment 
+          pipeline_run_entry[:post_processing] = post_processing
+          pipeline_run_entry[:gsnap_alignment] = gsnap_alignment
           pipeline_run_entry[:host_filtering] = host_filtering
         else
           pipeline_run_status = recent_pipeline_run.job_status
-          pipeline_run_entry[:job_status_description] = 
-          if %w[CHECKED SUCCEEDED].include?(pipeline_run_status)
-            'COMPLETE'
-          elsif %w[FAILED ERROR].include?(pipeline_run_status)
-            'FAILED'
-          elsif %w[RUNNING LOADED].include?(pipeline_run_status)
-            'IN PROGRESS'
-          elsif pipeline_run_status == 'RUNNABLE'
-            'INITIALIZING'
-          end
+          pipeline_run_entry[:job_status_description] =
+            if %w[CHECKED SUCCEEDED].include?(pipeline_run_status)
+              'COMPLETE'
+            elsif %w[FAILED ERROR].include?(pipeline_run_status)
+              'FAILED'
+            elsif %w[RUNNING LOADED].include?(pipeline_run_status)
+              'IN PROGRESS'
+            elsif pipeline_run_status == 'RUNNABLE'
+              'INITIALIZING'
+            end
         end
       pipeline_run_entry[:finalized] = output.pipeline_runs.first ? output.pipeline_runs.first.finalized : 0
       else
@@ -143,7 +143,6 @@ module SamplesHelper
     end
     pipeline_run_info
   end
-
 
   def format_samples(samples)
     formatted_samples = []
