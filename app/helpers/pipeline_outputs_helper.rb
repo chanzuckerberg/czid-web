414e72a789db39a4c409c6076a4f19183b38acad
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2017-11-29 13:37:02 -0800

    fix bug

--- app/helpers/pipeline_outputs_helper.rb
+++ app/helpers/pipeline_outputs_helper.rb
@@ -55,16 +55,16 @@ module PipelineOutputsHelper
     sequence_length = `
       QUERY=#{accession_id}
       BASE=https://eutils.ncbi.nlm.nih.gov/entrez/eutils
-      SEARCH_URL=${BASE}/esearch.fcgi?db=nuccore\&term=${QUERY}\&usehistory=y
+      SEARCH_URL=${BASE}/esearch.fcgi?db=nuccore\\&term=${QUERY}\\&usehistory=y
       OUTPUT=$(curl $SEARCH_URL)
-      WEB=$(echo $OUTPUT | sed -e 's/.*<WebEnv>\(.*\)<\/WebEnv>.*/\1/')
-      KEY=$(echo $OUTPUT | sed -e 's/.*<QueryKey>\(.*\)<\/QueryKey>.*/\1/')
-      FETCH_URL=${BASE}/efetch.fcgi?db=nuccore\&query_key=${KEY}\&WebEnv=${WEB}\&rettype=fasta\&retmode=xml
+      WEB=$(echo $OUTPUT | sed -e 's/.*<WebEnv>\\(.*\\)<\\/WebEnv>.*/\\1/')
+      KEY=$(echo $OUTPUT | sed -e 's/.*<QueryKey>\\(.*\\)<\\/QueryKey>.*/\\1/')
+      FETCH_URL=${BASE}/efetch.fcgi?db=nuccore\\&query_key=${KEY}\\&WebEnv=${WEB}\\&rettype=fasta\\&retmode=xml
       RESULT=$(curl $FETCH_URL)
-      SEQUENCE_LENGTH=$(echo $RESULT | sed -e 's/.*<TSeq_length>\(.*\)<\/TSeq_length>.*/\1/')
+      SEQUENCE_LENGTH=$(echo $RESULT | sed -e 's/.*<TSeq_length>\\(.*\\)<\\/TSeq_length>.*/\\1/')
       echo $SEQUENCE_LENGTH
     `
     raise "sequence length lookup failed" unless $CHILD_STATUS.success?
-    sequence_length
+    sequence_length.strip
   end
 end
