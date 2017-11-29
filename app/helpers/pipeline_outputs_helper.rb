67aed9f90a3311742dc32f00691eb894e28f8df2
cdebourcy <cdebourcy@chanzuckerberg.com>
2017-11-28 17:24:02 -0800

    Update pipeline_outputs_helper.rb

--- app/helpers/pipeline_outputs_helper.rb
+++ app/helpers/pipeline_outputs_helper.rb
@@ -24,7 +24,31 @@ module PipelineOutputsHelper
       return 'Coming soon' # Temporary fix
     end
   end
-  
+
+  def parse_alignment_from_taxid_fasta(taxid_fasta, hit_type)
+    alignment_info = {}
+    taxid_fasta.each_line do |line|
+      next unless line.start_with? '>'
+      # example line: 
+      #  >family_nr:481:family_nt:-300:genus_nr:482:genus_nt:-200:species_nr:-100:species_nt:-100:NR::NT:WP_039856594.1: \
+      #  NT-pident:91.1:NT-length:135:NT-mismatch:12:NT-gapopen:0:NT-qstart:1:NT-qend:135:NT-sstart:4070:NT-send:3936:NT-evalue:1.4e-64:NT-bitscore:256.3 \
+      #  M05295:49:000000000-G17RL:1:1103:18620:22956/2
+      read_id = line.strip.split(":read_id:")[1]
+      accession_id = line.split(":" + hit_type + ":")[1].split(":")[0]
+      alignment_start = line.split(":" + hit_type + "-sstart")[1].split(":")[0]
+      alignment_end = line.split(":" + hit_type + "-send")[1].split(":")[0]
+      read_info = { :read_id => read_id,
+                    :alignment_start => alignment_start,
+                    :alignment_end => alignment_end }
+      if alignment_info.key?(accession_id)
+        alignment_info[accession_id][:aligned_reads].push(read_info)
+      else
+        alignment_info[accession_id] = { :aligned_reads => [ read_info ] }
+      end
+    end
+    alignment_info
+  end
+
   def get_sequence_length_from_accession(accession_id)
     sequence_length = `
       QUERY=#{accession_id}
