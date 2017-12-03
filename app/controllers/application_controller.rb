f237d97190661ab9ec7853395a1f7feafd6ff917
sadelokiki <susan.adelokiki@andela.com>
2017-12-03 12:57:05 -0800

    Update Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  protect_from_forgery with: :null_session
 
   def after_sign_out_path_for(_resource_or_scope)
     root_path
