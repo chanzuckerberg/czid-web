8d97dd82766ef52676cae1e98f53d8ef60ae1197
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2017-12-04 16:21:40 -0800

    fix bug

--- aws_batch/non_host_alignment.py
+++ aws_batch/non_host_alignment.py
@@ -212,12 +212,12 @@ def check_pair_concordance(read_to_taxid):
             continue
         species_taxid_1, genus_taxid_1, family_taxid_1 = read_to_taxid[read_id]
         species_taxid_2, genus_taxid_2, family_taxid_2 = read_to_taxid[read_2_id]
-        if species_taxid_1 == species_taxid_2:
-            species_taxid_concordance_map[species_taxid_1] += 2 # add both reads to the concordance count
+        if species_taxid_1 == species_taxid_2: # add both reads to the concordance count
+            species_taxid_concordance_map[species_taxid_1] = species_taxid_concordance_map.get(species_taxid_1, 0) + 2
         if genus_taxid_1 == genus_taxid_2:
-            genus_taxid_concordance_map[genus_taxid_1] += 2
+            genus_taxid_concordance_map[genus_taxid_1] = genus_taxid_concordance_map.get(genus_taxid_1, 0) + 2
         if family_taxid_1 == family_taxid_2:
-            family_taxid_concordance_map[family_taxid_1] += 2
+            family_taxid_concordance_map[family_taxid_1] = family_taxid_concordance_map.get(family_taxid_1, 0) + 2
     return species_taxid_concordance_map, genus_taxid_concordance_map, family_taxid_concordance_map
 
 def generate_rpm_from_taxid_counts(taxidCountsInputPath, taxid2infoPath, speciesOutputPath, genusOutputPath):
