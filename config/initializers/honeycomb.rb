226a99475dc28458b3072768d54604bd1a1b63be
Ryan King <rking@chanzuckerberg.com>
2017-12-21 13:51:12 -0800

    remove puts

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -1,6 +1,5 @@
 
 if ENV["IDSEQ_HONEYCOMB_WRITE_KEY"] && ENV["IDSEQ_HONEYCOMB_DATA_SET"] && ENV["IDSEQ_HONEYCOMB_DB_DATA_SET"]
-  puts "setting up honeycomb"
   HoneycombRails.configure do |conf|
     conf.writekey = ENV["IDSEQ_HONEYCOMB_WRITE_KEY"]
     conf.dataset = ENV["IDSEQ_HONEYCOMB_DATA_SET"]
