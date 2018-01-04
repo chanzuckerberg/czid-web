a8151d29ab6ee1417b0ca6d36cf4088c6dcf53c6
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-04 12:12:35 -0800

    refaqctor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   clear_respond_to
   respond_to :json
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-  acts_as_token_authentication_handler_for User, only: :create, fallback: :devise
+  before_action :set_user, only: [:show, :edit, :destroy]
+  acts_as_token_authentication_handler_for User, only: [:create, :update], fallback: :devise
 
   # GET /users
   # GET /users.json
@@ -19,21 +19,15 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     Rails.logger.debug(user_params.inspect)
-
-    # Only allow request to set user role if authenticated as admin user
-    if params[:role] && @user && @user.role == User::ROLE_ADMIN
-      user_params[:role] = params[:role]
-    end
-
     new_user(user_params)
 
     respond_to do |format|
-      if @user.save
-        format.html { redirect_to edit_user_path(@user), notice: 'User was successfully created.' }
+      if @new_user.save
+        format.html { redirect_to edit_user_path(@new_user), notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: root_path }
       else
         format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: @new_user.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -45,6 +39,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+    @user_to_update ||= User.find(params[:id])
     respond_to do |format|
       input_params = user_params
       if input_params[:password] && input_params[:password] == ''
@@ -53,12 +48,12 @@ class UsersController < ApplicationController
       if input_params[:password_confirmation] && input_params[:password_confirmation] == ''
         input_params.delete(:password_confirmation)
       end
-      if @user.update(input_params)
-        format.html { redirect_to edit_user_path(@user), notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+      if @user_to_update.update(input_params)
+        format.html { redirect_to edit_user_path(@user_to_update), notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user_to_update }
       else
         format.html { render :edit }
-        format.json { render json: @user.errors.full_messages, status: :unprocessable_entity }
+        format.json { render json: @user_to_update.errors.full_messages, status: :unprocessable_entity }
       end
     end
   end
@@ -81,11 +76,18 @@ class UsersController < ApplicationController
   end
 
   def new_user(attrs = {})
-    @user ||= User.new(attrs)
+    @new_user ||= User.new(attrs)
+  end
+
+  def valid_role_assignment(params)
+    # Only allow request to set user role if authenticated as admin user
+    params[:role] && @user && @user.role == User::ROLE_ADMIN
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:email, :authentication_token, :password, :password_confirmation, :name, project_ids: [])
+    return_params = params.require(:user).permit(:email, :authentication_token, :password, :password_confirmation, :name, project_ids: [])
+    return_params[:role] = params[:role] if valid_role_assignment(params)
+    return_params
   end
 end
