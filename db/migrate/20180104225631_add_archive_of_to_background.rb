bf5fa2dfaa3d8d372bc181ce25aadbb212ca9567
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-04 15:31:24 -0800

    rollback

--- db/migrate/20180104225631_add_archive_of_to_background.rb
+++
@@ -1,5 +0,0 @@
-class AddArchiveOfToBackground < ActiveRecord::Migration[5.1]
-  def change
-    add_column :backgrounds, :archive_of, :bigint
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180104225631) do
+ActiveRecord::Schema.define(version: 20180102204635) do
 
   create_table "backgrounds", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "archive_of"
     t.index ["name"], name: "index_backgrounds_on_name", unique: true
   end
 
