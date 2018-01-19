0741322e3e94e2a805c981b17799f3d81885c319
yunfangjuan <yunfang@chanzuckerberg.com>
2018-01-19 12:55:14 -0800

    admin access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(_resource_or_scope)
     root_path
   end
+
+  def admin_required
+    redirect_to root_path unless current_user && current_user.admin?
+  end
 end
