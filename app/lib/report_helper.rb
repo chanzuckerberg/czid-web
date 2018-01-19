8926ce391b7698f8db30208b2389289785f5b897
cdebourcy <cdebourcy@chanzuckerberg.com>
2018-01-19 15:48:34 -0800

    Update report_helper.rb

--- app/lib/report_helper.rb
+++ app/lib/report_helper.rb
@@ -653,14 +653,14 @@ module ReportHelper
     sample_names_used = []
     project.samples.each do |sample|
       csv_data = report_csv_from_params(sample, params)
-      clean_sample_name = sample.name.downcase.gsub(/\W/, "-").gsub(/[^0-9A-Za-z]/, '-')
+      clean_sample_name = sample.name.downcase.gsub(/\W/, "-")
       used_before = sample_names_used.include? clean_sample_name
       sample_names_used << clean_sample_name
       clean_sample_name += "_#{sample.id}" if used_before
       filename = "#{csv_dir}/#{clean_sample_name}.csv"
       File.write(filename, csv_data)
     end
-    tar_filename = "#{project.name}_reports.tar.gz"
+    tar_filename = "#{project.name.gsub(/\W/, "-")}_reports.tar.gz"
     `cd #{csv_dir}; tar cvzf #{tar_filename} .`
     output_file = "#{csv_dir}/#{tar_filename}"
     `rm #{csv_dir}/*.csv`
