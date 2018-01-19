f68ddea11d8a89f53d04d55b7914cae26698cb3a
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-19 14:55:50 -0800

    fix bug

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -210,7 +210,7 @@ class Sample < ApplicationRecord
 
     pr = PipelineRun.new
     pr.sample = self
-    pr.subsample = (subsample.nil? || subsample.zero? ? nil : PipelineRun::DEFAULT_SUBSAMPLING)
+    pr.subsample = PipelineRun::DEFAULT_SUBSAMPLING if !(subsample.nil? || subsample.zero?)
     # The subsample field of "sample" is currently used as a simple flag (UI checkbox),
     # but was made an integer type in case we want to allow users to enter the desired number
     # of reads to susbample to in the future
