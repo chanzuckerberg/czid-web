b7bbee2cb507ddc743b206f545576f36537ba8b1
yunfangjuan <yunfangjuan@gmail.com>
2018-01-23 17:58:06 -0800

    disbale asset compile alpha (#507)
    * disbale asset compile alpha
    
    * rubocop
    
    * fix several bugs

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'coffee-rails', '~> 4.2'
 gem 'devise', '4.3.0'
 gem "health_check"
-gem 'honeycomb-rails'
+gem 'honeycomb-rails', '0.4.1'
 gem 'mailgun_rails'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
--- config/environments/alpha.rb
+++ config/environments/alpha.rb
@@ -27,8 +27,12 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.assets.debug = true
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
