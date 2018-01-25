959721df2582772fba6b4e9c29d4a7c174d84cb3
yunfangjuan <yunfang@chanzuckerberg.com>
2018-01-25 14:00:04 -0800

    power class

--- app/models/power.rb
+++ app/models/power.rb
@@ -6,7 +6,31 @@ class Power
   end
 
   power :users do
-    User if @user.admin
+    User if @user.admin?
+  end
+
+  power :viewable_projects do
+    Project.viewable(@user)
+  end
+
+  power :editable_projects do
+    Project.editable(@user)
+  end
+
+  power :viewable_samples do |project|
+    if project
+      project.samples.viewable(@user)
+    else
+      Sample.viewable(@user)
+    end
+  end
+
+  power :editable_samples do |project|
+    if project
+      project.samples.editable(@user)
+    else
+      Sample.editable(@user)
+    end
   end
 
 end
