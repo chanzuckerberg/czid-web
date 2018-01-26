dc12813f5dc62a609bbafb5b141e7a088bd081eb
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-26 15:27:45 -0800

    fix bug

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -109,6 +109,7 @@ class ProjectsController < ApplicationController
   # POST /projects.json
   def create
     @project = Project.new(project_params)
+    @project.users << current_user
 
     respond_to do |format|
       if @project.save
