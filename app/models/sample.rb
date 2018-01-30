2e59e40a29322c4dac053920ebaa3cdaa4889fd0
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-29 18:49:59 -0800

    first pass complete

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -241,6 +241,7 @@ class Sample < ApplicationRecord
     # The subsample field of "sample" is currently used as a simple flag (UI checkbox),
     # but was made an integer type in case we want to allow users to enter the desired number
     # of reads to susbample to in the future
+    pr.pipeline_branch = pipeline_branch.blank? ? "master" : pipeline_branch
     pr.save
 
     archive_old_pipeline_runs
