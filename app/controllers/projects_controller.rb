58a91c4ef2b0313a1f4adc5d4e0e9662992303d7
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-30 15:13:40 -0800

    fix bug

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
   include ReportHelper
   before_action :login_required
 
-  before_action :set_project, only: [:show, :edit, :update, :destroy, :add_favorite, :remove_favorite, :make_project_reports_csv, :project_reports_csv_status, :send_project_reports_csv, :add_user_to_project, :all_emails]
+  before_action :set_project, only: [:show, :edit, :update, :destroy, :add_favorite, :remove_favorite, :make_project_reports_csv, :project_reports_csv_status, :send_project_reports_csv, :add_user, :all_emails]
   clear_respond_to
   respond_to :json
   # GET /projects
@@ -189,9 +189,9 @@ class ProjectsController < ApplicationController
   end
 
   def add_user
+    Rails.logger.debug(params.inspect)
     @user = User.find_by(email: params[:user_email_to_add])
     create_new_user_random_password(params[:user_email_to_add]) unless @user
-    @project = Project.find(params[:project_id])
     UserMailer.added_to_projects_email(@user, current_user, [@project]).deliver_now unless @project.user_ids.include? @user.id
     @project.user_ids |= [@user.id]
   end
