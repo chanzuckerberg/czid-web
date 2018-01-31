8830f2100ba94e66323b7a7f729eb450ccdab8e5
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-30 17:15:39 -0800

    wip

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -1,12 +1,12 @@
-class MyMailer < Devise::Mailer   
+class DeviseMailer < Devise::Mailer   
   helper :application
   include Devise::Controllers::UrlHelpers
   default template_path: 'devise/mailer'
 
-  def new_user_new_project_email(new_user, sharing_user, project)
+  def new_user_new_project_email(new_user, sharing_user, projects)
     @new_user = new_user
     @sharing_user = sharing_user
-    @project = project
+    @projects = projects
     mail(to: @new_user.email, from: @sharing_user.email, subject: "New project on IDseq")
   end
 end
