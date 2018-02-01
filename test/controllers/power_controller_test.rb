ae044327f7600d08898110299876b20edc923739
yunfangjuan <yunfang@chanzuckerberg.com>
2018-01-31 18:13:54 -0800

    unit tests with some bug fixes

--- test/controllers/power_controller_test.rb
+++ test/controllers/power_controller_test.rb
@@ -7,29 +7,85 @@ class PowerControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'joe can create project' do
+    post "#{projects_url}.json", params: { project: { name: "2nd Joe Project" } }
+    assert_response :success
   end
 
   test 'joe can add users to joe_project ' do
+    @joe_project = projects(:joe_project)
+    put add_user_project_url(@joe_project), params: { user_email_to_add: "abc@xyz.com"  }
+    assert_response :success
+  end
+
+  test 'joe can change project visibility to joe_project ' do
+    @joe_project = projects(:joe_project)
+    put update_project_visibility_project_url(@joe_project), params: { public_access: 0  }
+    assert_response :success
   end
 
   test 'joe can create sample to joe_project' do
+    @joe_project = projects(:joe_project)
+    input_files = [{ source: "RR004_water_2_S23_R1_001.fastq.gz",
+                     name: "RR004_water_2_S23_R1_001.fastq.gz",
+                     source_type: "local" },
+                   { source: "RR004_water_2_S23_R2_001.fastq.gz",
+                     name: "RR004_water_2_S23_R2_001.fastq.gz",
+                     source_type: "local" }]
+    assert_difference('Sample.count') do
+      post "#{samples_url}.json", params: { sample: { name: 'joe new sample', project_name: @joe_project.name, input_files_attributes: input_files } }
+    end
+    assert_response :success
   end
 
   test 'joe can update sample to joe_project' do
+    @joe_sample = samples(:joe_sample)
+    post "#{save_metadata_sample_url(@joe_sample)}.json", params: {field: 'sample_tissue', value: 'bone'}
+    assert_response :success
+    @joe_sample.reload
+    assert @joe_sample.sample_tissue == 'bone'
   end
 
   test 'joe can see samples  in joe_project' do
+    @joe_project = projects(:joe_project)
+    get "/samples.json?project_id=#{@joe_project.id}"
+    assert_response :success
+    assert JSON::parse(@response.body)["total_count"] == 1
   end
 
   test 'joe can see joe_sample' do
+    @joe_sample = samples(:joe_sample)
+    get sample_url(@joe_sample)
+    assert_response :success
   end
 
   # public projects
 
   test 'joe cannot add users to public_project ' do
+    @public_project = projects(:public_project)
+    assert_raises(ActiveRecord::RecordNotFound) do
+      put add_user_project_url(@public_project), params: { user_email_to_add: "abc@xyz.com"  }
+    end
+  end
+
+  test 'joe cannot change project visibility to public_project ' do
+    @public_project = projects(:public_project)
+    assert_raises(ActiveRecord::RecordNotFound) do
+      put update_project_visibility_project_url(@public_project), params: { public_access: 0 }
+    end
   end
 
   test 'joe cannot create sample to public_project' do
+    @public_project = projects(:public_project)
+    input_files = [{ source: "RR004_water_2_S23_R1_001.fastq.gz",
+                     name: "RR004_water_2_S23_R1_001.fastq.gz",
+                     source_type: "local" },
+                   { source: "RR004_water_2_S23_R2_001.fastq.gz",
+                     name: "RR004_water_2_S23_R2_001.fastq.gz",
+                     source_type: "local" }]
+    assert_no_difference('Sample.count') do
+      post "#{samples_url}.json", params: { sample: { name: 'joe new sample', project_name: @public_project.name, input_files_attributes: input_files } }
+    end
+    assert_response 422
   end
 
   test 'joe cannot update sample to public_project' do
@@ -44,6 +100,11 @@ class PowerControllerTest < ActionDispatch::IntegrationTest
   # private project
 
   test 'joe cannot add users to project one ' do
+  end
+
+  test 'joe cannot change project visibility to project one ' do
+    post "#{projects_url}.json", params: { project: { name: "2nd Joe Project" } }
+    assert_response :success
   end
 
   test 'joe cannot create sample to project one' do
