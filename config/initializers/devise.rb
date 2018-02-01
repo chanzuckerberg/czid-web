d7008358425431f27ea0182e6c9e13802c659123
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-31 16:48:24 -0800

    use new mailer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   config.mailer_sender = 'regger@chanzuckerberg.com'
 
   # Configure the class responsible to send e-mails.
-  config.mailer = 'DeviseMailer'
+  config.mailer = 'CustomDeviseMailer'
 
   # Configure the parent class responsible to send e-mails.
   config.parent_mailer = 'ActionMailer::Base'
