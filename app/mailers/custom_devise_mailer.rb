827f92691dc765311005e7fa361928198d1bd222
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-31 17:59:49 -0800

    add more detail to email

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -1,19 +1,31 @@
-class CustomDeviseMailer < Devise::Mailer   
+class CustomDeviseMailer < Devise::Mailer
   helper :application
   include Devise::Controllers::UrlHelpers
   default template_path: 'devise/mailer'
 
-  def reset_password_instructions(record, token, opts={})
-    set_email_template record
+  def reset_password_instructions(record, token, opts = {})
+    assign_email_template record
     opts[:template_name] = @template if @template
     super
   end
 
   private
 
-  def set_email_template(user)
-    @sharing_user_email = User.find(user.sharing_user_id).email
-    @shared_project_name = Project.find(user.shared_project_id).name
-    @template = user.email_template rescue nil
+  def assign_email_template(user)
+    @sharing_user_email = begin
+                            User.find(user.sharing_user_id).email
+                          rescue
+                            nil
+                          end
+    @shared_project_name = begin
+                             Project.find(user.shared_project_id).name
+                           rescue
+                             nil
+                           end
+    @template = begin
+                  user.email_template
+                rescue
+                  nil
+                end
   end
 end
