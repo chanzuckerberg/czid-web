d3650fa11d9a5f5e420d2fd6661e733d60ecf755
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-02-01 14:57:12 -0800

    refactor wip

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,20 +1,20 @@
 class UserMailer < ApplicationMailer
-  def added_to_projects_email(new_user_id, sharing_user_id, shared_project_id)
+  def added_to_projects_email(new_user_id, email_arguments)
     @new_user_email = begin
-                        User.find(new_user_id).email
+                        User.find(email_arguments[:new_user_id]).email
                       rescue
                         nil
                       end
     @sharing_user_email = begin
-                            User.find(sharing_user_id).email
+                            User.find(email_arguments[:sharing_user_id]).email
                           rescue
                             nil
                           end
     @shared_project_name = begin
-                             Proejct.find(shared_project_id).name
+                             Project.find(email_arguments[:shared_project_id]).name
                            rescue
                              nil
                            end
-    mail(to: @new_user_email, from: @sharing_user_email, subject: "New project on IDseq")
+    mail(to: @new_user_email, from: @sharing_user_email, subject: email_arguments[:email_subject])
   end
 end
