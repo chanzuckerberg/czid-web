97ede8c51a6614a21ff2c19689a007de44448081
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-31 18:22:58 -0800

    rubocop

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,20 @@
 class UserMailer < ApplicationMailer
   def added_to_projects_email(new_user_id, sharing_user_id, shared_project_id)
-    @new_user_email = User.find(new_user_id).email rescue nil
-    @sharing_user_email = User.find(sharing_user_id).email rescue nil
-    @shared_project_name = Proejct.find(shared_project_id).name rescue nil
+    @new_user_email = begin
+                        User.find(new_user_id).email
+                      rescue
+                        nil
+                      end
+    @sharing_user_email = begin
+                            User.find(sharing_user_id).email
+                          rescue
+                            nil
+                          end
+    @shared_project_name = begin
+                             Proejct.find(shared_project_id).name
+                           rescue
+                             nil
+                           end
     mail(to: @new_user_email, from: @sharing_user_email, subject: "New project on IDseq")
   end
 end
