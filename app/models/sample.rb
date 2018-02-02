13ed4a58169e3548319f074dae4b38a557376189
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-02-02 12:16:21 -0800

    rubocop

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -132,7 +132,7 @@ class Sample < ApplicationRecord
     stderr_array = []
     input_files.each do |input_file|
       fastq = input_file.source
-      _stdout, stderr, status = Open3.capture3("aws", "s3", "cp", fastq.to_s, "#{input_file.file_path}")
+      _stdout, stderr, status = Open3.capture3("aws", "s3", "cp", fastq.to_s, input_file.file_path.to_s)
       stderr_array << stderr unless status.exitstatus.zero?
     end
     if s3_preload_result_path.present? && s3_preload_result_path[0..4] == 's3://'
