527628944a42037a6d6c60a55dace06ff1321a8e
cdebourcy <cdebourcy@chanzuckerberg.com>
2018-02-02 15:04:02 -0800

    Charles/user email (#551)
    * wip
    
    * wip
    
    * unrelated cosmetic change
    
    * wip
    
    * wip
    
    * wip
    
    * override reset_password_instructions
    
    * add transient field
    
    * use new mailer
    
    * works
    
    * add html email
    
    * add html email
    
    * add more detail to email
    
    * add more detail to email
    
    * edit subject
    
    * refactor
    
    * rubocop
    
    * refactor wip
    
    * refactor wip
    
    * refactor wip
    
    * refactor
    
    * fix bug
    
    * fix bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,20 @@
 class UserMailer < ApplicationMailer
-  def added_to_projects_email(new_user, sharing_user, projects)
-    @new_user = new_user
-    @sharing_user = sharing_user
-    @projects = projects
-    mail(to: @new_user.email, from: @sharing_user.email, subject: "New project on IDseq")
+  def added_to_projects_email(new_user_id, email_arguments)
+    @new_user_email = begin
+                        User.find(new_user_id).email
+                      rescue
+                        nil
+                      end
+    @sharing_user_email = begin
+                            User.find(email_arguments[:sharing_user_id]).email
+                          rescue
+                            nil
+                          end
+    @shared_project_name = begin
+                             Project.find(email_arguments[:shared_project_id]).name
+                           rescue
+                             nil
+                           end
+    mail(to: @new_user_email, subject: email_arguments[:email_subject])
   end
 end
