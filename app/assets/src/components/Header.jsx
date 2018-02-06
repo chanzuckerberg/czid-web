decd1437e163bc8bcf24f8007f9bbf8afdfd56d4
Cyril Ologho <cyril.paul@andela.com>
2018-02-06 20:47:27 +0100

    make the modal fit the content so the commandline code stays inline

--- app/assets/src/components/Header.jsx
+++ app/assets/src/components/Header.jsx
@@ -65,7 +65,9 @@ class Header extends React.Component  {
             <p> -u https://idseq.net -e <span className="code-personal">{this.userDetails.email}</span> -t <span className="code-personal">{this.user_auth_token}</span> \</p>
             <p> --r1 <span className="code-to-edit">your_sample_R1</span>.fastq.gz --r2 <span className="code-to-edit">your_sample_R2</span>.fastq.gz</p>
           </div>
-          <p>The project you specify must already exist on IDseq: you can create it using the <span className="code">+ New project</span> button on the sample upload page.</p>
+          <p>The project you specify must already exist on IDseq: you can create it using the
+            <br/>
+            <span className="code">+ New project</span> button on the sample upload page.</p>
           <p className='upload-question'>For more information on the IDseq CLI, have a look at its <a href='https://github.com/chanzuckerberg/idseq-web/blob/master/README.md' target='_blank'>GitHub repository</a>.</p>
           <button className='modal-close'>Done</button>
         </div>
