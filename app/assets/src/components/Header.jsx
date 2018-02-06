1e1a5957eda721a495598e114e0fdf426e07b76f
Cyril Ologho <cyrielo911@GMAIL.COM>
2018-02-06 22:58:27 +0100

    make the modal fit the content so the commandline code stays inline (#606)
    * make the modal fit the content so the commandline code stays inline
    
    * make code scroll-able on small screen

--- app/assets/src/components/Header.jsx
+++ app/assets/src/components/Header.jsx
@@ -60,12 +60,14 @@ class Header extends React.Component  {
           <p>2. Install the IDseq CLI:</p>
           <p><span className="code">pip install git+https://github.com/chanzuckerberg/idseq-cli.git</span></p>
           <p>3. Upload a sample using a command of the form:</p>
-          <div className="code">
+          <div className="code center-code">
             <p>idseq -p '<span className="code-to-edit">Your Project Name</span>' -s '<span className="code-to-edit">Your Sample Name</span>' \</p>
             <p> -u https://idseq.net -e <span className="code-personal">{this.userDetails.email}</span> -t <span className="code-personal">{this.user_auth_token}</span> \</p>
             <p> --r1 <span className="code-to-edit">your_sample_R1</span>.fastq.gz --r2 <span className="code-to-edit">your_sample_R2</span>.fastq.gz</p>
           </div>
-          <p>The project you specify must already exist on IDseq: you can create it using the <span className="code">+ New project</span> button on the sample upload page.</p>
+          <p>The project you specify must already exist on IDseq: you can create it using the
+            <br/>
+            <span className="code">+ New project</span> button on the sample upload page.</p>
           <p className='upload-question'>For more information on the IDseq CLI, have a look at its <a href='https://github.com/chanzuckerberg/idseq-web/blob/master/README.md' target='_blank'>GitHub repository</a>.</p>
           <button className='modal-close'>Done</button>
         </div>
