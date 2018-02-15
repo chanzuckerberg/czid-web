8759290f32948b07babe6777228ee8622964f975
andela-sadelokiki <susan.adelokiki@andela.com>
2018-02-15 14:42:24 +0100

    Create filters for metadata on the samples page

--- app/assets/src/components/Samples.jsx
+++ app/assets/src/components/Samples.jsx
@@ -729,6 +729,16 @@ class Samples extends React.Component {
     })
   }
 
+  displayMetaDataFilter() {
+      $('.metadata-dropdown').dropdown({
+      constrainWidth: false, // Does not change width of dropdown to that of the activator
+      gutter: 0, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: false // Stops event propagation
+    });
+  }
+
 
 
   addFavIconClass(project) {
