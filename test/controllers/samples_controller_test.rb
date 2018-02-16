6532e898aa3234a7db8e6662a6eb529ae1b79a14
cdebourcy <cdebourcy@chanzuckerberg.com>
2018-02-16 11:12:12 -0800

    Update samples_controller_test.rb

--- test/controllers/samples_controller_test.rb
+++ test/controllers/samples_controller_test.rb
@@ -99,7 +99,7 @@ class SamplesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get correct report' do
-    get "/samples/#{@sample.id}/report_info&background_id=#{@background.id}"
+    get "/samples/#{@sample.id}/report_info?background_id=#{@background.id}"
     json_response = JSON.parse(response.body)
     assert_equal json_response["taxonomy_details"][2][0]["NT"]["r"], 209.0
     assert_equal json_response["taxonomy_details"][2][0]["NT"]["rpm"], "186274.5"
