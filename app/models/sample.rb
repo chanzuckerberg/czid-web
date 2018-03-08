6b19e54d6c0898e9083c27429e5b753f56e15b18
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-03-08 10:48:26 -0800

    fix bug

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -274,7 +274,7 @@ class Sample < ApplicationRecord
   end
 
   def concatenate_input_parts
-    return unless [STATUS_CREATED, STATUS_UPLOADED].include?(status)
+    return unless status == STATUS_UPLOADED
     input_files.each do |f|
       next unless f.source_type == 'local'
       parts = f.parts.split(", ")
