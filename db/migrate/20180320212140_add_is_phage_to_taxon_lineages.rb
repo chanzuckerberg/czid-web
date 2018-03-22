b9b7a52d70881dd9d9d6f0e5cf08b113af690ea0
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-03-22 15:47:16 -0700

    add column

--- db/migrate/20180320212140_add_is_phage_to_taxon_lineages.rb
+++ db/migrate/20180320212140_add_is_phage_to_taxon_lineages.rb
@@ -1,5 +1,6 @@
 class AddIsPhageToTaxonLineages < ActiveRecord::Migration[5.1]
   def change
-    add_column :taxon_counts, :is_phage, :tinyint, null:false, default: 0
+    add_column :taxon_counts, :family_taxid, :integer, null: false, default: 0
+    add_column :taxon_counts, :is_phage, :tinyint, null: false, default: 0
   end
 end
