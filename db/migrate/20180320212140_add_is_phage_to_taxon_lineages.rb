051fafd6a62894ba93dc7f66044b7645a52ca3d9
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-03-23 14:12:42 -0700

    fix migrations

--- db/migrate/20180320212140_add_is_phage_to_taxon_lineages.rb
+++ db/migrate/20180320212140_add_is_phage_to_taxon_lineages.rb
@@ -1,7 +1,6 @@
 class AddIsPhageToTaxonLineages < ActiveRecord::Migration[5.1]
   def change
     add_column :taxon_counts, :family_taxid, :integer, null: false, default: TaxonLineage::MISSING_FAMILY_ID
-    add_column :taxon_counts, :phylum_taxid, :integer, null: false, default: TaxonLineage::MISSING_PHYLUM_ID
     add_column :taxon_counts, :is_phage, :tinyint, null: false, default: 0
   end
 end
