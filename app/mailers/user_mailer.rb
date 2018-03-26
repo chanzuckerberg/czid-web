435d509248e5fca93add0c768d5f2d15acfb384c
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-03-26 11:21:53 -0700

    add link to project in email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,20 +1,11 @@
 class UserMailer < ApplicationMailer
   def added_to_projects_email(new_user_id, email_arguments)
-    @new_user_email = begin
-                        User.find(new_user_id).email
-                      rescue
-                        nil
-                      end
-    @sharing_user_email = begin
-                            User.find(email_arguments[:sharing_user_id]).email
-                          rescue
-                            nil
-                          end
-    @shared_project_name = begin
-                             Project.find(email_arguments[:shared_project_id]).name
-                           rescue
-                             nil
-                           end
+    @new_user_email = User.find(new_user_id).email
+    @sharing_user_email = User.find(email_arguments[:sharing_user_id]).email
+    @shared_project_name = Project.find(email_arguments[:shared_project_id]).name
+    @shared_project_id = email_arguments[:shared_project_id]
     mail(to: @new_user_email, subject: email_arguments[:email_subject])
+  rescue
+    Airbrake.notify("added_to_projects_email(#{email_arguments}) failed")
   end
 end
