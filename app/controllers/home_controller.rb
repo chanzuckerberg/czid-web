61cb09e1149af422d611a67fb41034ac79401fff
Trevor Berg <trevor@chanzuckerberg.com>
2018-03-26 23:10:27 +0000

    Add simple feedback route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,4 +17,10 @@ class HomeController < ApplicationController
     dir ||= default_dir
     dir == 'newest' ? samples.order(id: :desc) : samples.order(id: :asc)
   end
+
+  def feedback()
+    render json: {
+      status: 'ok',
+    }
+  end
 end
