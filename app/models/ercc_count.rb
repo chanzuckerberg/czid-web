acacb337dc34ac48f629eee7dcacbec78936a703
Trevor Berg <trevor@chanzuckerberg.com>
2018-04-02 20:17:24 +0000

    fix lint

--- app/models/ercc_count.rb
+++ app/models/ercc_count.rb
@@ -2,5 +2,5 @@ require 'csv'
 
 class ErccCount < ApplicationRecord
   belongs_to :pipeline_run
-  BASELINE = CSV.table('app/lib/ercc_data.csv').map { |row| row.to_hash }
+  BASELINE = CSV.table('app/lib/ercc_data.csv').map(&:to_hash)
 end
