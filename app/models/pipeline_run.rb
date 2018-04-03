bed41c332284596e7aae503e276ea00f860e9f35
yunfangjuan <yunfang@chanzuckerberg.com>
2018-04-02 17:31:12 -0700

    rubocop

--- app/models/pipeline_run.rb
+++ app/models/pipeline_run.rb
@@ -61,7 +61,6 @@ class PipelineRun < ApplicationRecord
   def self.top_completed_runs
     where("id in (select max(id) from pipeline_runs where job_status = 'CHECKED' and
                   sample_id in (select id from samples) group by sample_id)")
-
   end
 
   def finalized?
