b676b9963e0f1627c195c31d82f28276413b43d7
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-04-03 13:49:49 -0700

    fix migration

--- db/migrate/20180402214120_add_kingdom_to_taxon_lineages.rb
+++ db/migrate/20180402214120_add_kingdom_to_taxon_lineages.rb
@@ -12,6 +12,14 @@ class AddKingdomToTaxonLineages < ActiveRecord::Migration[5.1]
     change_column :taxon_lineages, :phylum_taxid, :integer, default: TaxonLineage::MISSING_PHYLUM_ID, null: false
     change_column :taxon_lineages, :superkingdom_taxid, :integer, default: TaxonLineage::MISSING_SUPERKINGDOM_ID, null: false
 
+    TaxonLineage.where(species_name: nil).update_all(species_name: '')
+    TaxonLineage.where(genus_name: nil).update_all(genus_name: '')
+    TaxonLineage.where(family_name: nil).update_all(family_name: '')
+    TaxonLineage.where(order_name: nil).update_all(order_name: '')
+    TaxonLineage.where(class_name: nil).update_all(class_name: '')
+    TaxonLineage.where(phylum_name: nil).update_all(phylum_name: '')
+    TaxonLineage.where(superkingdom_name: nil).update_all(superkingdom_name: '')
+
     change_column :taxon_lineages, :species_name, :string, default: '', null: false
     change_column :taxon_lineages, :genus_name, :string, default: '', null: false
     change_column :taxon_lineages, :family_name, :string, default: '', null: false
@@ -19,6 +27,14 @@ class AddKingdomToTaxonLineages < ActiveRecord::Migration[5.1]
     change_column :taxon_lineages, :class_name, :string, default: '', null: false
     change_column :taxon_lineages, :phylum_name, :string, default: '', null: false
     change_column :taxon_lineages, :superkingdom_name, :string, default: '', null: false
+
+    TaxonLineage.where(species_common_name: nil).update_all(species_common_name: '')
+    TaxonLineage.where(genus_common_name: nil).update_all(genus_common_name: '')
+    TaxonLineage.where(family_common_name: nil).update_all(family_common_name: '')
+    TaxonLineage.where(order_common_name: nil).update_all(order_common_name: '')
+    TaxonLineage.where(class_common_name: nil).update_all(class_common_name: '')
+    TaxonLineage.where(phylum_common_name: nil).update_all(phylum_common_name: '')
+    TaxonLineage.where(superkingdom_common_name: nil).update_all(superkingdom_common_name: '')
 
     change_column :taxon_lineages, :species_common_name, :string, default: '', null: false
     change_column :taxon_lineages, :genus_common_name, :string, default: '', null: false
