2fcbf4b0459e186ff52a792ba0e2b965c6d4235b
yunfangjuan <yunfang@chanzuckerberg.com>
2018-04-11 18:15:18 -0700

    component

--- app/assets/src/components/AlignmentViz.jsx
+++ app/assets/src/components/AlignmentViz.jsx
@@ -1,15 +1,15 @@
+import axios from "axios";
 import React from "react";
 
 class AccessionViz extends React.Component {
   constructor(props) {
     super(props);
-
   }
 
   render() {
     return (
       <div>
-        <Samples {...this.props} />
+        {this.props.accession} : {this.props.reads_count}
       </div>
     );
   }
@@ -21,16 +21,31 @@ class AlignmentViz extends React.Component {
     this.sampleId = props.sampleId
     this.alignmentQuery = props.alignmentQuery
     this.alignmentData = []
+
+    this.fetchAlignmentData = this.fetchAlignmentData.bind(this)
   }
 
   componentWillMount() {
     this.fetchAlignmentData();
   }
 
+  this.fetchAlignmentData() {
+    axios
+      .get(`/samples/${this.sampleId}/alignment/${this.alignmentQuery}.json`)
+      .then(res => {
+        this.alignmentData = res.data;
+      })
+      .catch(() => {});
+  }
+
   render() {
     return (
       <div>
-        <Samples {...this.props} />
+      {
+        this.alignmentData.map(function(item, i) {
+          <AccessionViz  {key=`accession_${i}` ,...item } />
+        })
+      }
       </div>
     );
   }
