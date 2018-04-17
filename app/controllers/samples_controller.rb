248bc81bab9d1f2d4c28a5fb804b65e145da44eb
Trevor Berg <trevor@chanzuckerberg.com>
2018-04-17 11:50:32 -0700

    Fix performance regression by inluding dependencies after filter (#1186)

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -49,7 +49,7 @@ class SamplesController < ApplicationController
     sort = params[:sort_by]
     samples_query = JSON.parse(params[:ids]) if params[:ids].present?
 
-    results = current_power.samples.includes([:user, :input_files], pipeline_runs: [:job_stats, :pipeline_run_stages])
+    results = current_power.samples
 
     results = results.where(id: samples_query) if samples_query.present?
 
@@ -68,7 +68,7 @@ class SamplesController < ApplicationController
     results = filter_by_tissue_type(results, tissue_type_query) if tissue_type_query.present?
     results = filter_by_host(results, host_query) if host_query.present?
 
-    @samples = sort_by(results, sort).paginate(page: params[:page], per_page: params[:per_page] || PAGE_SIZE)
+    @samples = sort_by(results, sort).paginate(page: params[:page], per_page: params[:per_page] || PAGE_SIZE).includes([:user, :input_files], pipeline_runs: [:job_stats, :pipeline_run_stages])
     @samples_count = results.size
     @all_samples = format_samples(@samples)
 
