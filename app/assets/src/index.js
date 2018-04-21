54a4896a4afb885084cb0b57f0ea7809f2318f62
Jonathan Sheu <jshoe@users.noreply.github.com>
2018-04-20 17:12:14 -0700

    Add a polyfill for URLSearchParams (#1207)

--- app/assets/src/index.js
+++ app/assets/src/index.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'materialize-css';
-import 'materialize-css/dist/css/materialize.css';
-import 'nouislider/distribute/nouislider.css';
-import 'react-tipsy/dist/react-tipsy.css';
-import 'font-awesome/scss/font-awesome.scss';
-import 'semantic-ui-css/semantic.min.css';
-import './loader.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import "url-search-params-polyfill";
+import "materialize-css";
+import "materialize-css/dist/css/materialize.css";
+import "nouislider/distribute/nouislider.css";
+import "react-tipsy/dist/react-tipsy.css";
+import "font-awesome/scss/font-awesome.scss";
+import "semantic-ui-css/semantic.min.css";
+import "./loader.scss";
 
-const context = require.context('./components', true, /\.(js|jsx)$/);
+const context = require.context("./components", true, /\.(js|jsx)$/);
 const foundComponents = {};
 const contextKeys = context.keys();
-contextKeys.forEach((key) => {
+contextKeys.forEach(key => {
   let a = null;
-  if (typeof context(key) === 'function') {
+  if (typeof context(key) === "function") {
     a = context(key);
-  } else if (typeof context(key) === 'object') {
+  } else if (typeof context(key) === "object") {
     a = context(key).default;
   }
   if (a && a.name) {
     foundComponents[a.name] = a;
   }
-});
-/* eslint camelcase: 0 */ // Turn off camelcase rule
-const react_component = (componentName, props, target) => {
+}); // Turn off camelcase rule
+/* eslint camelcase: 0 */ const react_component = (
+  componentName,
+  props,
+  target
+) => {
   const matchedComponent = foundComponents[componentName];
   if (matchedComponent) {
     ReactDOM.render(
