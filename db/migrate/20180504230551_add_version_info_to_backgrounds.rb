fbd7a08dc90f50c750ef090ea21a3c2ca2917efc
yunfangjuan <yunfang@chanzuckerberg.com>
2018-05-04 16:27:05 -0700

    version background

---
+++ db/migrate/20180504230551_add_version_info_to_backgrounds.rb
@@ -0,0 +1,8 @@
+class AddVersionInfoToBackgrounds < ActiveRecord::Migration[5.1]
+  def change
+    add_column :backgrounds, :pipeline_version, :string
+    add_column :backgrounds, :time_version, :bigint
+    remove_index :backgrounds, name: "index_backgrounds_on_name"
+    add_index :backgrounds, ["name", "pipeline_version", "time_version"], unique: true
+  end
+end
