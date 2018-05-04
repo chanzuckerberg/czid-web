b9cd8a2f7b15ba3ff87130857523e33062808a52
Trevor Berg <trevor@chanzuckerberg.com>
2018-05-04 14:49:05 -0700

    more optimal source maps (#1259)

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,66 +1,68 @@
 const path = require("path");
-const extractTextPlugin = require('extract-text-webpack-plugin');
+const extractTextPlugin = require("extract-text-webpack-plugin");
 
 const extractPlugin = new extractTextPlugin({
-  filename: 'dist/bundle.min.css'
+  filename: "dist/bundle.min.css"
 });
 
 const config = {
   entry: `${path.resolve(__dirname, "app/assets/src/")}/index.js`,
-  output:  {
+  output: {
     path: path.resolve(__dirname, "app/assets/"),
     filename: "dist/bundle.min.js"
   },
-  devtool: 'source-map',
-  target: 'web',
+  devtool: "cheap-module-eval-source-map",
+  target: "web",
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"]
   },
   module: {
-    rules: [{
-      test: /.js$/,
-      exclude: path.resolve(__dirname, "node_modules/"),
-      loader: "babel-loader"
-    },{
-      test: /.jsx$/,
-      exclude: path.resolve(__dirname, "node_modules/"),
-      loader: "babel-loader"
-    },
-    { // sass / scss loader for webpack
-      test: /\.(sass|css|scss)$/,
-      loader: extractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: 'css-loader?{"minimize":true}!sass-loader'
-      })
-    },
-    {
-      test: /\.(png|eot|ttf|svg)$/,
-      loader: 'url-loader',
-      options: {
-        limit: 10000,
-        name: 'fonts/[name].[ext]',
-        mimetype: 'application/font-woff',
-        publicPath: (url) => {
-          return `/assets/${url.replace(/fonts/, '')}`
+    rules: [
+      {
+        test: /.js$/,
+        exclude: path.resolve(__dirname, "node_modules/"),
+        loader: "babel-loader"
+      },
+      {
+        test: /.jsx$/,
+        exclude: path.resolve(__dirname, "node_modules/"),
+        loader: "babel-loader"
+      },
+      {
+        // sass / scss loader for webpack
+        test: /\.(sass|css|scss)$/,
+        loader: extractTextPlugin.extract({
+          fallback: "style-loader",
+          use: 'css-loader?{"minimize":true}!sass-loader'
+        })
+      },
+      {
+        test: /\.(png|eot|ttf|svg)$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "fonts/[name].[ext]",
+          mimetype: "application/font-woff",
+          publicPath: url => {
+            return `/assets/${url.replace(/fonts/, "")}`;
+          }
+        }
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: "fonts/[name].[ext]",
+          mimetype: "application/font-woff",
+          publicPath: url => {
+            return `/assets/${url.replace(/fonts/, "")}`;
+          }
         }
       }
-    },
-    {
-      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: 'url-loader',
-      options: {
-        limit: 10000,
-        name: 'fonts/[name].[ext]',
-        mimetype: 'application/font-woff',
-        publicPath: (url) => {
-          return `/assets/${url.replace(/fonts/, '')}`
-        }
-      }
-    }]
+    ]
   },
-  plugins: [
-    extractPlugin,
-  ]
-}
+  plugins: [extractPlugin]
+};
 
 module.exports = config;
