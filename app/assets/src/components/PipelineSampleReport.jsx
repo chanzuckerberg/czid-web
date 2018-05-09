10e8d0b596f9cc5f2526171b98e9fe1d029f2794
cdebourcy <cdebourcy@chanzuckerberg.com>
2018-05-09 16:48:54 -0700

    add truncation stats (#1266)
    * add truncation stats
    
    * fix bug
    
    * give number
    
    * Update pipeline_run.rb

--- app/assets/src/components/PipelineSampleReport.jsx
+++ app/assets/src/components/PipelineSampleReport.jsx
@@ -1193,6 +1193,12 @@ class PipelineSampleReport extends React.Component {
     const filter_stats = `${
       this.state.rows_passing_filters
     } rows passing filters, out of ${this.state.rows_total} total rows.`;
+    let truncation_stats =
+      this.report_details && this.report_details.pipeline_info.truncated
+        ? "Overly large input was truncated to " +
+          this.report_details.pipeline_info.truncated +
+          " reads."
+        : "";
     let subsampled_reads = this.report_details
       ? this.report_details.subsampled_reads
       : null;
@@ -1213,7 +1219,7 @@ class PipelineSampleReport extends React.Component {
     const filter_row_stats = this.state.loading ? null : (
       <div id="filter-message" className="filter-message">
         <span className="count">
-          {filter_stats} {subsampling_stats} {disable_filter}
+          {filter_stats} {truncation_stats} {subsampling_stats} {disable_filter}
         </span>
       </div>
     );
