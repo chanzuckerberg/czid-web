e160f54f41dce14e90c074fcdce5ee97ae2e3a97
yunfangjuan <yunfang@chanzuckerberg.com>
2018-05-09 18:05:31 -0700

    update background

--- app/controllers/backgrounds_controller.rb
+++ app/controllers/backgrounds_controller.rb
@@ -42,20 +42,14 @@ class BackgroundsController < ApplicationController
   # PATCH/PUT /backgrounds/1
   # PATCH/PUT /backgrounds/1.json
   def update
-    current_data = @background.to_json(include: [{ pipeline_runs: { only: [:id, :sample_id] } }])
-    current_data_full_string = @background.to_json(include: [{ pipeline_runs: { only: [:id, :sample_id] } },
-                                                             :taxon_summaries])
-    background_changed = assign_attributes_and_has_changed?(background_params)
-    if background_changed
-      s3_archive_successful, s3_backup_path = archive_background_to_s3(current_data_full_string)
-      db_archive_successful = archive_background_to_db(current_data, s3_backup_path)
-      update_successful = db_archive_successful && s3_archive_successful ? @background.save : false # this triggers recomputation of @background's taxon_summaries if archiving was successful
+    if background_params[:pipeline_run_ids].map { |p| p.to_i }.sort == @background.pipeline_runs.pluck(:id).sort
+      @background.name = background_params[:name]
     else
-      update_successful = true
-      @background.save # recompute
+      @background = Background.new(background_params)
     end
+
     respond_to do |format|
-      if update_successful
+      if @background.save
         format.html { redirect_to @background, notice: 'Background was successfully updated.' }
         format.json { render :show, status: :ok, location: @background }
       else
