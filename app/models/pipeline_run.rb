a99a96d10f3a144f99ba11800c28154d3d278d66
cdebourcy <cdebourcy@chanzuckerberg.com>
2018-05-15 13:06:53 -0700

    change pipeline version threshold for assembly (#1293)
    * change pipeline version threshold for assembly
    
    * Update pipeline_run.rb
    
    * Update pipeline_run.rb
    
    * Update pipeline_run.rb

--- app/models/pipeline_run.rb
+++ app/models/pipeline_run.rb
@@ -402,7 +402,9 @@ class PipelineRun < ApplicationRecord
   end
 
   def assembly?
-    after(pipeline_version || fetch_pipeline_version, "1.11")
+    after(pipeline_version || fetch_pipeline_version, "1000.1000")
+    # Very big version number so we don't accidentally start going into assembly mode.
+    # Once we decide to deploy the assembly pipeline, change "1000.1000" to the relevant version number of idseq-pipeline.
   end
 
   def alignment_output_s3_path
