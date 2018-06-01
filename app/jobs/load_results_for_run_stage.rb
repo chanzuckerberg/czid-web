e4caffe598352153546c5de54113fc3bc0c7256e
jameshowardwang <jwang@chanzuckerberg.com>
2018-06-01 14:03:31 -0700

    Standardize on using Rails.logger and added HONEYCOMB environments to resque containers as well (#1332)

--- app/jobs/load_results_for_run_stage.rb
+++ app/jobs/load_results_for_run_stage.rb
@@ -1,11 +1,9 @@
 # Jos to initiate database download
-require 'logger'
 class LoadResultForRunStage
   @queue = :q03_pipeline_run
-  @logger = Logger.new(STDOUT)
 
   def self.perform(pipeline_run_stage_id)
-    @logger.info("load pipeline run stage #{pipeline_run_stage_id} into database")
+    Rails.logger.info("load pipeline run stage #{pipeline_run_stage_id} into database")
     prs = PipelineRunStage.find(pipeline_run_stage_id)
     prs.run_load_db unless prs.completed?
   end
