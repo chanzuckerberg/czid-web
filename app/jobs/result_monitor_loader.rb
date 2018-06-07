4b4e001985570c8570811fa6aa4f17282472b81e
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-06-06 19:10:00 -0700

    fix bug

--- app/jobs/result_monitor_loader.rb
+++ app/jobs/result_monitor_loader.rb
@@ -5,7 +5,7 @@ class ResultMonitorLoader
   def self.perform(pipeline_run_id, output)
     Rails.logger.info("Loading #{output} for pipeline run #{pipeline_run_id}")
     pr = PipelineRun.find(pipeline_run_id)
-    output_state = pr.output_states.find_by(name: output)
+    output_state = pr.output_states.find_by(output: output)
     begin
       output_state.update(state: PipelineRun::STATUS_LOADING)
       pr.send(PipelineRun::LOADERS_BY_OUTPUT[output])
