1bb10e2bb32140e171baf1838e1d70220a1da8ff
yunfangjuan <yunfang@chanzuckerberg.com>
2018-07-17 16:33:45 -0700

    some changes

--- db/migrate/20180716215830_create_alignment_configs.rb
+++ db/migrate/20180716215830_create_alignment_configs.rb
@@ -1,5 +1,5 @@
 class CreateAlignmentConfigs < ActiveRecord::Migration[5.1]
-  def change
+  def up
     create_table :alignment_configs do |t|
       t.string :name, unique: true
       t.string :index_dir_suffix
@@ -19,7 +19,7 @@ class CreateAlignmentConfigs < ActiveRecord::Migration[5.1]
       s3_nt_loc_db_path: "s3://idseq-database/20170824/blast_db/nt_loc.db",
       s3_nr_db_path: "s3://idseq-database/20170824/blast_db/nr",
       s3_nr_loc_db_path: "s3://idseq-database/20170824/blast_db/nr_loc.db",
-      s3_lineage_path: "s3://idseq-database/taxonomy/2018-02-15-utc-1518652800-unixtime__2018-  02-15-utc-1518652800-unixtime/taxid-lineages.db",
+      s3_lineage_path: "s3://idseq-database/taxonomy/2018-02-15-utc-1518652800-unixtime__2018-02-15-utc-1518652800-unixtime/taxid-lineages.db",
       s3_accession2taxid_path: "s3://idseq-database/alignment_data/2018-02-15-utc-1518652800-unixtime__2018-02-15-utc-1518652800-unixtime/accession2taxid.db",
       s3_deuterostome_db_path: "s3://idseq-database/taxonomy/2018-02-15-utc-1518652800-unixtime__2018-02-15-utc-1518652800-unixtime/deuterostome_taxids.txt"
     )
@@ -32,9 +32,9 @@ class CreateAlignmentConfigs < ActiveRecord::Migration[5.1]
       s3_nt_loc_db_path: "s3://idseq-database/alignment_data/2018-04-01-utc-1522569777-unixtime__2018-04-04-utc-1522862260-unixtime/nt_loc.db",
       s3_nr_db_path: "s3://idseq-database/alignment_data/2018-04-01-utc-1522569777-unixtime__2018-04-04-utc-1522862260-unixtime/nr",
       s3_nr_loc_db_path: "s3://idseq-database/alignment_data/2018-04-01-utc-1522569777-unixtime__2018-04-04-utc-1522862260-unixtime/nr_loc.db",
-      s3_lineage_path: "s3://idseq-database/taxonomy/2018-02-15-utc-1518652800-unixtime__2018-  02-15-utc-1518652800-unixtime/taxid-lineages.db",
+      s3_lineage_path: "s3://idseq-database/taxonomy/2018-04-01-utc-1522569777-unixtime__2018-04-04-utc-1522862260-unixtime/taxid-lineages.db",
       s3_accession2taxid_path: "s3://idseq-database/alignment_data/2018-04-01-utc-1522569777-unixtime__2018-04-04-utc-1522862260-unixtime/accession2taxid.db",
-      s3_deuterostome_db_path: "s3://idseq-database/taxonomy/2018-02-15-utc-1518652800-unixtime__2018-02-15-utc-1518652800-unixtime/deuterostome_taxids.txt"
+      s3_deuterostome_db_path: "s3://idseq-database/taxonomy/2018-04-01-utc-1522569777-unixtime__2018-04-04-utc-1522862260-unixtime/deuterostome_taxids.txt"
     )
     ac2.save!
 
@@ -42,5 +42,10 @@ class CreateAlignmentConfigs < ActiveRecord::Migration[5.1]
     # set the alignment_config_id to the one we are using prior to alignment config
     ActiveRecord::Base.connection.execute("UPDATE pipeline_runs
                                            SET alignment_config_id = #{ac1.id}")
+  end
+
+  def down
+    drop_table :alignment_configs
+    remove_column :pipeline_runs, :alignment_config_id
   end
 end
