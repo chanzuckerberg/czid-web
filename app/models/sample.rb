bc79e864e7e628319225fc470cda1b5d44157e50
jshoe <github@jsheu.com>
2018-07-18 17:26:07 -0700

    Rename fq to fastq

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -215,10 +215,14 @@ class Sample < ApplicationRecord
     input_files.each do |input_file|
       fastq = input_file.source
       total_reads_json_path = File.join(File.dirname(fastq.to_s), TOTAL_READS_JSON)
+
+      name = input_file.name
+      name = name.sub(".fq", ".fastq")
+
       command = if fastq =~ /\.gz/
-                  "aws s3 cp #{fastq} - |gzip -dc |head -#{max_lines} | gzip -c | aws s3 cp - #{sample_input_s3_path}/#{input_file.name}"
+                  "aws s3 cp #{fastq} - |gzip -dc |head -#{max_lines} | gzip -c | aws s3 cp - #{sample_input_s3_path}/#{name}"
                 else
-                  "aws s3 cp #{fastq} #{sample_input_s3_path}/#{input_file.name}"
+                  "aws s3 cp #{fastq} #{sample_input_s3_path}/#{name}"
                 end
       _stdout, stderr, status = Open3.capture3(command)
       stderr_array << stderr unless status.exitstatus.zero?
