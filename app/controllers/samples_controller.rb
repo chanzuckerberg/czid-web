a2b2fd81a5be2696683b6980481c3a5184235a2f
Meera-Srinivasan <msrinivasan@chanzuckerberg.com>
2018-07-23 15:47:57 -0700

    Added tabke visualization, works

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -13,8 +13,8 @@ class SamplesController < ApplicationController
   #
   ##########################################
   skip_before_action :verify_authenticity_token, only: [:create, :update]
-
-  READ_ACTIONS = [:show, :report_info, :search_list, :report_csv, :assembly, :show_taxid_fasta, :nonhost_fasta, :unidentified_fasta, :results_folder, :fastqs_folder, :show_taxid_alignment, :show_taxid_alignment_viz].freeze
+  # ADDED HERE
+  READ_ACTIONS = [:show, :report_info, :search_list, :report_csv, :assembly, :show_taxid_fasta, :nonhost_fasta, :unidentified_fasta, :results_folder, :fastqs_folder, :show_taxid_alignment, :show_taxid_alignment_viz, :show_amr_counts].freeze
   EDIT_ACTIONS = [:edit, :add_taxon_confirmation, :remove_taxon_confirmation, :update, :destroy, :reupload_source, :kickoff_pipeline, :retry_pipeline, :pipeline_runs, :save_metadata].freeze
 
   OTHER_ACTIONS = [:create, :bulk_new, :bulk_upload, :bulk_import, :new, :index, :all, :samples_taxons, :top_taxons, :heatmap, :download_heatmap].freeze
@@ -186,6 +186,11 @@ class SamplesController < ApplicationController
         @pipeline_run_retriable = true
       end
     end
+  end
+
+  def show_amr_counts
+    @sample = samples_scope.find(params[:id])
+    @amr_counts = @sample.pipeline_runs.first.amr_counts
   end
 
   def top_taxons
