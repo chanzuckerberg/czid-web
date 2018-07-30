54a4ae175e15c8b3837bf67fe052c2dd7daf1ebf
yunfangjuan <yunfang@chanzuckerberg.com>
2018-07-30 12:49:22 -0700

    switch to new index

--- app/models/alignment_config.rb
+++ app/models/alignment_config.rb
@@ -1,5 +1,5 @@
 class AlignmentConfig < ApplicationRecord
   # configuration for alignment database for pipelines
-  DEFAULT_NAME = '2018-02-15'.freeze
+  DEFAULT_NAME = '2018-04-01'.freeze
   has_many :pipeline_runs
 end
