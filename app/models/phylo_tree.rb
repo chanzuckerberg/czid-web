ca5ce93fc3c0fe006009ef58ea8a477fd45c44b0
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-08-10 09:43:40 -0700

    validate name

--- app/models/phylo_tree.rb
+++ app/models/phylo_tree.rb
@@ -3,6 +3,7 @@ class PhyloTree < ApplicationRecord
   has_and_belongs_to_many :pipeline_runs
   belongs_to :user
   belongs_to :project
+  validates :name, presence: true, uniqueness: true
 
   STATUS_INITIALIZED = 0
   STATUS_READY = 1
