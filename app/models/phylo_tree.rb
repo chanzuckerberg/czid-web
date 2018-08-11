2654fdf4f2b07fdcc9a269e97daffc9be95ff040
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-08-10 17:16:44 -0700

    move byterange download to idseq-dag

--- app/models/phylo_tree.rb
+++ app/models/phylo_tree.rb
@@ -77,23 +77,42 @@ class PhyloTree < ApplicationRecord
     taxon_fasta_files
   end
 
-  def job_command(taxon_fasta_files)
-    alignment_config = pipeline_runs.last.alignment_config # TODO: revisit case where pipeline_runs have different alignment configs
-    align_viz_files = []
-    PipelineRun.where(id: pipeline_run_ids).find_each do |pr|
-      align_viz_files << { name: "align_viz_json_pipeline_run_#{pr.id}",
-                           s3_file: pr.alignment_viz_json_s3("nt.species.#{taxid}") } # TODO: also support genus level (based on tax_level)
+  def job_command
+    # Get byte ranges for each pipeline run's taxon fasta.
+    # We construct taxon_byteranges_hash to have the following format for integration with idseq-dag:
+    # { pipeline_run_id_1: [first_byte, last_byte, source_s3_file, destination_name],
+    #   pipeline_run_id_2: ... }
+    taxon_byteranges = TaxonByterange.where(pipeline_run_id: pipeline_runs.pluck(:id)).where(taxid: taxid).where(hit_type: 'NT')
+    taxon_byteranges_hash = {}
+    taxon_byteranges.each do |tbr|
+      taxon_byteranges_hash[tbr.pipeline_run_id] = [tbr.first_byte, tbr.last_byte]
     end
+    # Get sample names to construct a display name for the taxon fasta
+    samples = Sample.where(id: pipeline_runs.pluck(:sample_id))
+    taxon_fasta_name_by_sample_id = {}
+    samples.each do |s|
+      taxon_fasta_name_by_sample_id[s.id] = "#{s.name}__#{tax_name}.fasta"
+    end
+    # Get locations of align_viz files (needed for identifying NCBI accessions matched by the reads in the taxon fasta)
+    align_viz_files = {}
+    pipeline_runs.each do |pr|
+      align_viz_files[pr.id] = pr.alignment_viz_json_s3("nt.species.#{taxid}") # TODO: also support genus level (based on tax_level)
+      taxon_byteranges_hash[pr.id] += [pr.s3_paths_for_taxon_byteranges[tax_level][hit_type],
+                                       taxon_fasta_name_by_sample_id[pr.sample_id]]
+    end
+    # Get the alignment config specifying the location of the NCBI reference used in the pipeline run
+    alignment_config = pipeline_runs.last.alignment_config # TODO: revisit case where pipeline_runs have different alignment configs
+    # Generate DAG
     attribute_dict = {
       phylo_tree_output_s3_path: phylo_tree_output_s3_path,
-      taxon_fasta_files: taxon_fasta_files,
       taxid: taxid,
+      taxon_byteranges: taxon_byteranges_hash,
       align_viz_files: align_viz_files,
       nt_db: alignment_config.s3_nt_db_path,
       nt_loc_db: alignment_config.s3_nt_loc_db_path
     }
     dag_commands = prepare_dag("phylo_tree", attribute_dict)
-
+    # Dispatch command
     base_command = [PipelineRunStage.install_pipeline("charles/trees"),
                     PipelineRunStage.upload_version(dag_version_file),
                     dag_commands].join("; ")
@@ -117,8 +136,7 @@ class PhyloTree < ApplicationRecord
 
   def kickoff
     return unless [STATUS_INITIALIZED, STATUS_FAILED].include?(status)
-    taxon_fasta_files = upload_taxon_fasta_inputs_and_return_names
-    self.command_stdout, self.command_stderr, status = Open3.capture3(job_command(taxon_fasta_files))
+    self.command_stdout, self.command_stderr, status = Open3.capture3(job_command)
     if status.exitstatus.zero?
       output = JSON.parse(command_stdout)
       self.job_id = output['jobId']
