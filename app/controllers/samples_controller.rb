5501b25f1912bea98f4a3f420eada995caa98160
jshoe <github@jsheu.com>
2018-08-17 15:53:29 -0700

    Tweak

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -174,7 +174,10 @@ class SamplesController < ApplicationController
     background_id = check_background_id(@sample)
     @report_page_params = { pipeline_version: @pipeline_version, background_id: background_id } if background_id
     @report_page_params[:scoring_model] = params[:scoring_model] if params[:scoring_model]
-    if @pipeline_run && (((@pipeline_run.adjusted_remaining_reads.to_i > 0 || @pipeline_run.results_finalized?) && !@pipeline_run.failed?) || @pipeline_run.report_ready?)
+
+    # Report is ready if the pipeline run has results finalized and did not
+    # fail, or if all its output states are loaded.
+    if @pipeline_run && ((@pipeline_run.results_finalized? && !@pipeline_run.failed?) || @pipeline_run.report_ready?)
       if background_id
         @report_present = 1
         @report_ts = @pipeline_run.updated_at.to_i
