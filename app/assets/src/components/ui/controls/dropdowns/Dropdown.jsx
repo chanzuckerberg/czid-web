fff9a42d1951ba0d07166d985eb424ded84b3e91
Tiago Carvalho <tcarvalho@chanzuckerberg.com>
2018-08-16 19:38:44 -0700

    FIx: Update component when parent changes props.

--- app/assets/src/components/ui/controls/dropdowns/Dropdown.jsx
+++ app/assets/src/components/ui/controls/dropdowns/Dropdown.jsx
@@ -13,6 +13,13 @@ class Dropdown extends React.Component {
     }, {});
   }
 
+  componentWillReceiveProps(newProps) {
+    console.log("new props value", newProps.value);
+    if (newProps.value != this.props.value) {
+      this.setState({ value: newProps.value });
+    }
+  }
+
   handleOnChange(e, d) {
     this.setState({ value: d.value });
     this.onChange(e, d);
