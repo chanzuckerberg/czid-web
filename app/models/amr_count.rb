5182e58bbd2ea2b9fce07744b387b1d9f3165459
Meera-Srinivasan <msrinivasan@chanzuckerberg.com>
2018-08-20 03:28:54 -0700

    Admin permissions

--- app/models/amr_count.rb
+++ app/models/amr_count.rb
@@ -1,3 +1,14 @@
 class AmrCount < ApplicationRecord
   belongs_to :pipeline_run
+  def self.viewable(user)
+    if user.admin?
+      all
+    end
+  end
+
+  def self.editable(user)
+    if user.admin?
+      all
+    end
+  end
 end
