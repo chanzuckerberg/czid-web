aa6ab16097c98f29138b724b677046cd98e122ba
jshoe <github@jsheu.com>
2018-08-23 17:17:35 -0700

    Put postprocessing back in himem queue

--- app/models/pipeline_run_stage.rb
+++ app/models/pipeline_run_stage.rb
@@ -210,6 +210,8 @@ class PipelineRunStage < ApplicationRecord
     dag_commands = prepare_dag("postprocess", attribute_dict)
     batch_command = [install_pipeline(pipeline_run.pipeline_commit), dag_commands].join("; ")
     # Run it
-    aegea_batch_submit_command(batch_command, job_queue: pipeline_run.sample.job_queue)
+    memory = sample.sample_memory.present? ? sample.sample_memory : Sample::DEFAULT_MEMORY_IN_MB
+    job_queue = pipeline_run.sample.job_queue || Sample::DEFAULT_QUEUE_HIMEM
+    aegea_batch_submit_command(batch_command, memory, job_queue: job_queue)
   end
 end
