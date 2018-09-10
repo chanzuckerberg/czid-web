801edfc96caece2fffedbf5133e766cc02ea71ea
jshoe <github@jsheu.com>
2018-09-10 16:20:39 -0700

    Change CLI instructions to use host genome name

--- app/models/host_genome.rb
+++ app/models/host_genome.rb
@@ -4,4 +4,8 @@ class HostGenome < ApplicationRecord
   def default_background
     Background.find(default_background_id) if default_background_id
   end
+
+  def self.exposed_genomes
+    all.map(&:name).reject { |n| n.downcase.include?("__test__") || n.downcase.include?("no host subtraction") }
+  end
 end
