c625d1d1859f52ddf4e71175812e604aa7e446a8
Boris Dimitrov <bdimitrov@chanzuckerberg.com>
2018-09-12 13:32:54 -0700

    print backtrace on rescues that do not rerase
    a rescue should either reraise its exception or print a backtrace,
    to include sufficient debugging detail in the error logs

--- app/lib/log_util.rb
+++ app/lib/log_util.rb
@@ -3,4 +3,7 @@ class LogUtil
     Rails.logger.error(msg)
     Airbrake.notify(msg)
   end
+  def self.log_backtrace(exception)
+    Rails.logger.error("Backtrace:\n\t#{exception.backtrace.join("\n\t")}")
+  end
 end
