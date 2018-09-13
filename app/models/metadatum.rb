6a260d3b55219b9c936ff845fd5175c6bb678ab1
jshoe <github@jsheu.com>
2018-09-13 10:16:33 -0700

    Change to use validations

--- app/models/metadatum.rb
+++ app/models/metadatum.rb
@@ -1,9 +1,44 @@
 class Metadatum < ApplicationRecord
+  # ActiveRecord related
   belongs_to :sample
+  enum data_type: [:string, :number]
+  validates :text_validated_value, length: { maximum: 250 }
+  validates :number_raw_value, :number_validated_value, numericality: true, allow_nil: true
+  validate :set_validated_values
 
-  # Types: 0 for string, 1 for number, 2 for lists of values (as string)
-  KEY_TO_TYPE = { unique_id: 0, sample_type: 0, nucleotide_type: 0, collection_date: 0, collection_location: 0, collected_by: 0, age: 1, gender: 0, race: 0, primary_diagnosis: 0, antibiotic_administered: 0, admission_date: 0, admission_type: 0, discharge_date: 0, discharge_type: 0, immunocomp: 0, other_infections: 0, comorbitity: 0, known_organism: 0, infection_class: 0, detection_method: 0, library_prep: 0, sequencer: 0, rna_dna_input: 1, library_prep_batch: 0, extraction_batch: 0 }.freeze
+  STRING_TYPE = 0
+  NUMBER_TYPE = 1
+  # Key to the metadatum type. Supporting strings and numbers currently.
+  KEY_TO_TYPE = {
+    unique_id: STRING_TYPE,
+    sample_type: STRING_TYPE,
+    nucleotide_type: STRING_TYPE,
+    collection_date: STRING_TYPE,
+    collection_location: STRING_TYPE,
+    collected_by: STRING_TYPE,
+    age: NUMBER_TYPE,
+    gender: STRING_TYPE,
+    race: STRING_TYPE,
+    primary_diagnosis: STRING_TYPE,
+    antibiotic_administered: STRING_TYPE,
+    admission_date: STRING_TYPE,
+    admission_type: STRING_TYPE,
+    discharge_date: STRING_TYPE,
+    discharge_type: STRING_TYPE,
+    immunocomp: STRING_TYPE,
+    other_infections: STRING_TYPE,
+    comorbitity: STRING_TYPE,
+    known_organism: STRING_TYPE,
+    infection_class: STRING_TYPE,
+    detection_method: STRING_TYPE,
+    library_prep: STRING_TYPE,
+    sequencer: STRING_TYPE,
+    rna_dna_input: NUMBER_TYPE,
+    library_prep_batch: STRING_TYPE,
+    extraction_batch: STRING_TYPE
+  }.freeze
 
+  # Key to the valid string options.
   KEY_TO_STRING_OPTIONS = {
     nucleotide_type: %w[DNA RNA],
     gender: %w[Female Male],
@@ -15,84 +50,59 @@ class Metadatum < ApplicationRecord
     sequencer: %w[MiSeq NextSeq HiSeq NovaSeq Other]
   }.freeze
 
+  # Custom validator called on save or update. Writes to the *_validated_value column.
+  def set_validated_values
+    # Check if the key is valid
+    errors.add(:key, "not a supported metadatum") unless KEY_TO_TYPE.key?(key)
+
+    if data_type == NUMBER_TYPE
+      # Set number types. ActiveRecord validated.
+      self.number_validated_value = number_raw_value
+    elsif data_type == STRING_TYPE
+      check_and_set_string_type
+    end
+    save
+  end
+
+  # Called by set_validated_values custom validator
+  def check_and_set_string_type
+    if KEY_TO_STRING_OPTIONS.key?(key)
+      # If there are explicit string options, match the value to one of them.
+      matched = false
+      options = KEY_TO_STRING_OPTIONS[key]
+      options.each do |opt|
+        if str_to_basic_chars(text_raw_value) == str_to_basic_chars(opt)
+          # Ex: Match 'neb ultra-iifs dna' to 'NEB Ultra II FS DNA'
+          # Ex: Match '30-day mortality' to "30 Day Mortality"
+          self.text_validated_value = opt
+          matched = true
+          break
+        end
+      end
+      unless matched
+        errors.add(:text_raw_value, "did not match options #{options.join(', ')}")
+      end
+    else
+      self.text_validated_value = text_raw_value
+    end
+  end
+
   # Add a new Metadatum entry to the sample
   def self.add_to_sample(sample, key, raw_value)
     key = key.to_sym
-
-    # validate raises RuntimeError if invalid. Caller can handle and provide
-    # feedback.
-    validated_value = validate(key, raw_value)
 
     # Create the entry
     m = Metadatum.new
     m.key = key
     m.data_type = KEY_TO_TYPE[key]
-    if KEY_TO_TYPE[key].zero?
+    if KEY_TO_TYPE[key] == STRING_TYPE
       m.text_raw_value = raw_value
-      m.text_validated_value = validated_value
-    elsif KEY_TO_TYPE[key] == 1
+    elsif KEY_TO_TYPE[key] == NUMBER_TYPE
       m.number_raw_value = raw_value
-      m.number_validated_value = validated_value
     end
+    # *_validated_value field is set in the set_validated_values validator.
     m.sample = sample
-    m.save
-  end
-
-  def self.validate(key, raw_value)
-    # Check if the key is valid
-    unless KEY_TO_TYPE.key?(key)
-      msg = "Key #{key} is not a supported metadatum."
-      raise msg
-    end
-
-    # Validate the value for the key
-    validated_value = if KEY_TO_TYPE[key] == 1
-                        validate_number(key, raw_value)
-                      else
-                        validate_string(key, raw_value)
-                      end
-
-    validated_value
-  end
-
-  def self.validate_number(key, raw_value)
-    if raw_value.is_a?(Numeric)
-      validated_value = raw_value
-    else
-      begin
-        validated_value = Float(raw_value)
-      rescue
-        raise "#{raw_value} for #{key} is not a valid number."
-      end
-    end
-    validated_value
-  end
-
-  def self.validate_string(key, raw_value)
-    raw_value = raw_value.to_s
-    validated_value = nil
-
-    if KEY_TO_STRING_OPTIONS.key?(key)
-      # If there are explicit string options, match the value to one of them.
-      options = KEY_TO_STRING_OPTIONS[key]
-      options.each do |opt|
-        if str_to_basic_chars(raw_value) == str_to_basic_chars(opt)
-          # Ex: Match 'neb ultra-iifs dna' to 'NEB Ultra II FS DNA'
-          # Ex: Match '30-day mortality' to "30 Day Mortality"
-          validated_value = opt
-          break
-        end
-      end
-      unless validated_value
-        msg = "#{raw_value} for #{key} did not match options #{options.join(', ')}."
-        raise msg
-      end
-    else
-      # Otherwise any string without restricted options is OK.
-      validated_value = raw_value
-    end
-
-    validated_value
+    m.save!
   end
 
   def self.str_to_basic_chars(res)
