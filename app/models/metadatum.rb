1968e05e68815b50ce7d4ddafa13a15a43110380
Jonathan Sheu <jshoe@users.noreply.github.com>
2018-09-18 14:58:09 -0700

    Add bulk loader for Metadata (#1492)
    * WIP
    
    * Add update function
    
    * RuboCop again
    
    * Debug
    
    * Clean up
    
    * Clean up more
    
    * Finish bulk loading
    
    * Tweak
    
    * Add migration

--- app/models/metadatum.rb
+++ app/models/metadatum.rb
@@ -1,10 +1,16 @@
+require 'csv'
+
 class Metadatum < ApplicationRecord
+  Client = Aws::S3::Client.new
+
   # ActiveRecord related
   belongs_to :sample
   STRING_TYPE = 0
   NUMBER_TYPE = 1
   # When using an ActiveRecord enum, the type returned from reading records is String.
   enum data_type: { string: STRING_TYPE, number: NUMBER_TYPE }
+
+  # Validations
   validates :text_validated_value, length: { maximum: 250 }
   validates :number_raw_value, :number_validated_value, numericality: true, allow_nil: true
   validate :set_validated_values
@@ -28,7 +34,7 @@ class Metadatum < ApplicationRecord
     discharge_type: STRING_TYPE,
     immunocomp: STRING_TYPE,
     other_infections: STRING_TYPE,
-    comorbitity: STRING_TYPE,
+    comorbidity: STRING_TYPE,
     known_organism: STRING_TYPE,
     infection_class: STRING_TYPE,
     detection_method: STRING_TYPE,
@@ -49,6 +55,21 @@ class Metadatum < ApplicationRecord
     infection_class: ["Definite", "No Infection", "Suspected", "Unknown", "Water Control"],
     library_prep: ["NEB Ultra II FS DNA", "NEB RNA Ultra II", "NEB Ultra II Directional RNA", "NEB Utra II DNA", "Nextera DNA", "Other"],
     sequencer: %w[MiSeq NextSeq HiSeq NovaSeq Other]
+  }.freeze
+
+  # Mapping from alternative name to our name. Used at upload time.
+  KEY_ALT_NAMES = {
+    sample_unique_id: "unique_id",
+    sample_collection_date: "collection_date",
+    sample_collection_location: "collection_location",
+    :"rna/dna_input(ng)" => "rna_dna_input",
+    :"rna/dna_input (ng)" => "rna_dna_input",
+    :"rna/dna_input" => "rna_dna_input",
+    :"rna/dna input" => "rna_dna_input",
+    :"rna/dna input (ng)" => "rna_dna_input",
+    antibiotics_administered: "antibiotic_administered",
+    :"known_organism(s)" => "known_organism",
+    :known_organisms => "known_organism"
   }.freeze
 
   # Custom validator called on save or update. Writes to the *_validated_value column.
@@ -74,7 +95,7 @@ class Metadatum < ApplicationRecord
       matched = false
       options = KEY_TO_STRING_OPTIONS[key]
       options.each do |opt|
-        if str_to_basic_chars(text_raw_value) == str_to_basic_chars(opt)
+        if Metadatum.str_to_basic_chars(text_raw_value) == Metadatum.str_to_basic_chars(opt)
           # Ex: Match 'neb ultra-iifs dna' to 'NEB Ultra II FS DNA'
           # Ex: Match '30-day mortality' to "30 Day Mortality"
           self.text_validated_value = opt
@@ -90,25 +111,156 @@ class Metadatum < ApplicationRecord
     end
   end
 
-  # Add a new Metadatum entry to the sample
-  def self.add_to_sample(sample, key, raw_value)
-    key = key.to_sym
+  # Set value based on data type
+  def edit_value(val)
+    if data_type == "string"
+      self.text_raw_value = val
+    elsif data_type == "number"
+      self.number_raw_value = val
+    end
+    # Note: *_validated_value field is set in the set_validated_values
+    # validator.
+  end
 
-    # Create the entry
+  def self.str_to_basic_chars(res)
+    res.downcase.gsub(/[^0-9A-Za-z]/, '')
+  end
+
+  # Load bulk metadata from a CSV file from S3
+  def self.bulk_load_from_s3_csv(path)
+    csv_data = get_s3_csv(path)
+    to_create, errors = bulk_load_prepare(csv_data)
+    errors += bulk_load_import(to_create)
+    bulk_log_errors(errors)
+  end
+
+  # Construct objects to create without saving.
+  def self.bulk_load_prepare(csv_data)
+    to_create = []
+    errors = []
+    csv_data.each_with_index do |row, index|
+      begin
+        to_create += load_csv_single_sample_row(row, index)
+      rescue => err
+        # Catch ArgumentError for proj and sample, other errors
+        errors << err.message
+      end
+    end
+    [to_create, errors]
+  end
+
+  # Create the object instances with activerecord-import. Still uses the
+  # validations.
+  def self.bulk_load_import(to_create)
+    errors = []
+    begin
+      # The unique key is on sample and metadata.key, so the value fields will
+      # be updated if the key exists.
+      update_keys = [:text_raw_value, :text_validated_value, :number_raw_value, :number_validated_value]
+      results = Metadatum.import to_create, on_duplicate_key_update: update_keys
+      results.failed_instances.each do |model|
+        # Show the errors from ActiveRecord
+        msg = model.errors.full_messages[0]
+        errors << "#{model.key}: #{msg}"
+      end
+    rescue => err
+      # Record other errors
+      errors << err.message
+    end
+    errors
+  end
+
+  def self.bulk_log_errors(errors)
+    unless errors.empty?
+      msg = errors.join(".\n")
+      Rails.logger.error(msg)
+      errors
+    end
+  end
+
+  # Load CSV file from S3. Raise RuntimeError on download fail.
+  def self.get_s3_csv(path)
+    parts = path.split("/", 4)
+    bucket = parts[2]
+    key = parts[3]
+    begin
+      resp = Client.get_object(bucket: bucket, key: key)
+      csv_data = resp.body.read
+    rescue => err
+      raise "Error in loading S3 file. #{err.message}"
+    end
+
+    # Remove BOM if present (file likely comes from Excel)
+    csv_data = csv_data.delete("\uFEFF")
+    csv_data = CSV.parse(csv_data, headers: true)
+    csv_data
+  end
+
+  # Load metadata from a single CSV row corresponding to one sample.
+  # Return the Metadatum to create without saving.
+  def self.load_csv_single_sample_row(row, index)
+    # Setup
+    to_create = []
+    row = row.to_h
+    proj = load_csv_project(row, index)
+    sample = load_csv_sample(row, index, proj)
+
+    # Add or update Metadata items
+    done_keys = [:study_id, :project_name, :sample_name]
+    row.each do |key, value|
+      next unless key && value
+      # Strip whitespace and ensure symbol
+      key = key.to_s.strip.to_sym
+      next if done_keys.include?(key)
+      # Translate alternative names
+      if KEY_ALT_NAMES.include?(key)
+        key = KEY_ALT_NAMES[key]
+      end
+      to_create << new_without_save(sample, key, value)
+    end
+
+    to_create
+  end
+
+  # Get the project for the CSV row
+  def self.load_csv_project(row, index)
+    proj_name = row['study_id'] || row['project_name']
+    unless proj_name
+      raise ArgumentError, "No project name found in row #{index + 2}"
+    end
+    proj = Project.find_by(name: proj_name)
+    unless proj
+      raise ArgumentError, "No project found named #{proj_name}"
+    end
+    proj
+  end
+
+  # Get the sample for the CSV row
+  def self.load_csv_sample(row, index, proj)
+    sample_name = row['sample_name']
+    unless sample_name
+      raise ArgumentError, "No sample name found in row #{index + 2}"
+    end
+    sample = Sample.find_by(project: proj, name: sample_name)
+    unless sample
+      raise ArgumentError, "No sample found named #{sample_name} in #{proj.name}"
+    end
+    sample
+  end
+
+  # Make a new Metadatum instance without saving/creating.
+  def self.new_without_save(sample, key, value)
+    key = key.to_sym
     m = Metadatum.new
     m.key = key
     m.data_type = KEY_TO_TYPE[key]
     if KEY_TO_TYPE[key] == STRING_TYPE
-      m.text_raw_value = raw_value
+      m.text_raw_value = value
     elsif KEY_TO_TYPE[key] == NUMBER_TYPE
-      m.number_raw_value = raw_value
+      m.number_raw_value = value
     end
     # *_validated_value field is set in the set_validated_values validator.
     m.sample = sample
-    m.save!
-  end
-
-  def str_to_basic_chars(res)
-    res.downcase.gsub(/[^0-9A-Za-z]/, '')
+    m
   end
 end
