d390cc7fedc1fcecdf1f93fb4c018ef7100081f5
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-09-24 19:19:54 -0700

    fix insight icon size

--- app/assets/src/components/ui/icons/InsightIcon.jsx
+++ app/assets/src/components/ui/icons/InsightIcon.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const InsightIcon = () => {
   return (
-    <span class="fa-stack fa-lg">
-      <i class="fa fa-circle fa-stack-2x" />
-      <i class="fa fa-lightbulb-o fa-stack-1x fa-inverse" />
+    <span className="ui icon insight-icon">
+      <span className="fa-stack fa-lg">
+        <i className="fa fa-circle fa-stack-2x" />
+        <i className="fa fa-lightbulb-o fa-stack-1x fa-inverse" />
+      </span>
     </span>
   );
 };
