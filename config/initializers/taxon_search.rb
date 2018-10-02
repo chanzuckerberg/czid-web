388264296a3582a76eb702d44d5ff6a58717a182
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-10-02 10:42:46 -0700

    run initializer only of table exists

--- config/initializers/taxon_search.rb
+++ config/initializers/taxon_search.rb
@@ -1,3 +1,5 @@
 Rails.configuration.after_initialize do
-  TAXON_SEARCH_LIST = JSON.dump(TaxonLineage.taxon_search_list("genus"))
+  if ActiveRecord::Base.connection.table_exists?(:taxon_lineages)
+    TAXON_SEARCH_LIST = JSON.dump(TaxonLineage.taxon_search_list("genus"))
+  end
 end
