aa5dca70c8708054e559f8f16d16b976c38b7a19
Charles de Bourcy <cdebourcy@chanzuckerberg.com>
2018-10-02 13:35:35 -0700

    speed up taxon search (#1544)
    * speed up taxon search
    
    * remove swp file
    
    * refactor taxon list generation
    
    * run initializer only of table exists
    
    * run initializer only if table exists
    
    * if taxon data is not present, fetch it

--- app/controllers/phylo_trees_controller.rb
+++ app/controllers/phylo_trees_controller.rb
@@ -70,15 +70,12 @@ class PhyloTreesController < ApplicationController
   end
 
   def choose_taxon
-    max_ended_at = TaxonLineage.column_defaults["ended_at"] # infinitely far in the future
-    lineages = TaxonLineage.where("genus_taxid > 0").where.not(genus_name: "").where("ended_at = ?", max_ended_at)
-    lineages = lineages.select(:genus_taxid, :genus_name).distinct.order(:genus_name).index_by(&:genus_name) # index_by makes sure it's unique on genus_name alone
-    taxon_search = lineages.map do |genus_name, record|
-      { "title" => genus_name,
-        "description" => "Taxonomy ID: #{record.genus_taxid}",
-        "taxid" => record.genus_taxid }
-    end
-    render json: JSON.dump(taxon_search)
+    taxon_list = if defined?(TAXON_SEARCH_LIST) && !TAXON_SEARCH_LIST.empty?
+                   TAXON_SEARCH_LIST
+                 else
+                   TaxonLineage.taxon_search_list
+                 end
+    render json: taxon_list
   end
 
   def new
