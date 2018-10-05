6953febec020cb294501564fc89cf0c7ba2bccc1
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-10-05 10:57:28 -0700

    address Tiago's comment

--- app/assets/src/components/views/phylo_tree/PhyloTreeListView.jsx
+++ app/assets/src/components/views/phylo_tree/PhyloTreeListView.jsx
@@ -92,7 +92,7 @@ class PhyloTreeListView extends React.Component {
               onClick={() => {
                 location.href = `/phylo_trees/${currentTree.id}/download_snps`;
               }}
-              disabled={currentTree.snp_annotations == null}
+              disabled={!currentTree.snp_annotations}
             />
           </ViewHeader>
         </div>
