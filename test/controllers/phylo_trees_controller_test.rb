768a4bc826f981b5e37ac457773514c741c4c838
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-10-05 10:28:09 -0700

    fix tests

--- test/controllers/phylo_trees_controller_test.rb
+++ test/controllers/phylo_trees_controller_test.rb
@@ -12,6 +12,12 @@ class PhyloTreesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test 'should get new' do
+    taxid = 1
+    get "/phylo_trees/new.json?taxId=#{taxid}&projectId=#{@project.id}"
+    assert_response :success
+  end
+
   test 'should create phylo_tree' do
     entrypoint_taxon_count = taxon_counts(:three)
     assert_difference('PhyloTree.count') do
@@ -20,5 +26,12 @@ class PhyloTreesControllerTest < ActionDispatch::IntegrationTest
                                             taxName: entrypoint_taxon_count.name }
     end
     assert_equal "ok", JSON.parse(@response.body)['status']
+  end
+
+  test 'should show phylo_tree' do
+    pt = phylo_trees(:one)
+    get "/phylo_trees/index.json?taxId=#{pt.taxid}&projectId=#{pt.project_id}"
+    is_tree_in_response = JSON.parse(@response.body)['phyloTrees'].select { |tree| tree['id'] == pt.id }.count == 1
+    assert_equal is_tree_in_response, true
   end
 end
