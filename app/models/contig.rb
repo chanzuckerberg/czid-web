427dfcc65e867e962de5bb02f43d31d7944e70f2
Yun-Fang Juan <yunfang@chanzuckerberg.com>
2018-11-07 16:43:16 -0800

    documentation

--- app/models/contig.rb
+++ app/models/contig.rb
@@ -1,4 +1,5 @@
 class Contig < ApplicationRecord
+  # Contigs assembled from non_host reads
   belongs_to :pipeline_run
   def to_fa
     ">#{name}:#{read_count}\n#{sequence}"
--- app/models/contig_count.rb
+++ app/models/contig_count.rb
@@ -1,3 +1,4 @@
 class ContigCount < ApplicationRecord
+  # Contig read count associated with a particular taxid, '*' means unassembled reads
   belongs_to :pipeline_run
 end
