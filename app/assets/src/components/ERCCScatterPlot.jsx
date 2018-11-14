ffd73ff952c28182930c83a747d94d264aa253d2
jameshowardwang <jwang@chanzuckerberg.com>
2018-11-14 14:01:07 -0800

    fix typo (#1741)
    * fix typo
    
    * spike in --> spike-in

--- app/assets/src/components/ERCCScatterPlot.jsx
+++ app/assets/src/components/ERCCScatterPlot.jsx
@@ -28,7 +28,7 @@ class ERCCScatterPlot extends React.Component {
         yKey="actual"
         width={this.props.width}
         height={this.props.height}
-        xLabel="log10 spike in concetrations"
+        xLabel="log10 spike-in concentrations"
         yLabel="log10 read-pairs per gene"
       />
     );
