4b7b3ae0f7379679d1d822b745eda20117e27105
Charles de Bourcy <cdebourcy@chanzuckerberg.com>
2018-11-14 17:55:09 -0800

    WIP refactor to ASG class

--- app/jobs/autoscaling.py
+++ app/jobs/autoscaling.py
@@ -12,29 +12,8 @@ import dateutil.parser
 from operator import itemgetter
 from functools import wraps
 
-# As opposed to "development", where different containers belong to different developers.
-MULTICONTAINER_ENVIRONMENTS = ["staging", "prod"]
-
-
-# If you are running into problems with this autoscaler, just delete this tag from
-# any affected autoscaling group, and it will be left alone.  The value is a list
-# of environments permitted to trigger scaling, usually just "prod".
-SCALING_PERMISSION_TAG = "IDSeqEnvsThatCanScale"
-
-
-SCALING_METRIC_TAG_PREFIX = "IDSeqScalingData_"
-SCALING_METRIC_TAG_FORMAT = SCALING_METRIC_TAG_PREFIX + "{environment}"
-
-# A value recorded this many minutes earlier will be ignored
-EXPIRATION_PERIOD_MINUTES = 60
-
+SCALING_ENVIRONMENTS = ["staging", "prod"] # as opposed to 'development'
 DEBUG = False
-
-
-## constants specific to draining / safe scale-down:
-GRACE_PERIOD_SECONDS__JOB_DISPATCH_LAG = 300
-GRACE_PERIOD_SECONDS__JOB_TAG_KEEP_ALIVE = 300
-
 
 ## EXPLANATION OF STATE TRANSITIONS FOR GSNAP/RAPSEARCH MACHINES
 '''
@@ -108,86 +87,6 @@ def exactly(value):
     return lambda _n: value
 
 
-def get_tag(asg, tag_key):
-    for tag in asg['Tags']:
-        if tag['Key'] == tag_key:
-            return tag['Value']
-    return None
-
-
-def get_tags_by_prefix(asg, tag_key_prefix):
-    results = {}
-    for tag in asg['Tags']:
-        if tag['Key'].startswith(tag_key_prefix):
-            assert tag['Key'] not in results
-            results[tag['Key']] = tag['Value']
-    return results
-
-
-def set_tag(asg, key, value):
-    cmd = "aws autoscaling create-or-update-tags --tags ResourceId={asg_name},ResourceType=auto-scaling-group,Key={key},Value={value},PropagateAtLaunch=false"
-    cmd = cmd.format(asg_name=asg['AutoScalingGroupName'], key=key, value=value)
-    if DEBUG:
-        print cmd
-    aws_command(cmd)
-
-
-def set_metric_value(asg, value, my_environment):
-    tag_key = SCALING_METRIC_TAG_FORMAT.format(environment=my_environment)
-    # parsed in update_metric_values
-    tag_value = "{unixtime:d}::{value:d}".format(unixtime=unixtime_now(), value=int(value))
-    set_tag(asg, tag_key, tag_value)
-    return (tag_key, tag_value)
-
-
-def environment(tag):
-    return tag[len(SCALING_METRIC_TAG_PREFIX):]
-
-
-def delete_expired_metric_tags(asg, garbage_tag_keys):
-    # Gargbage collect expired development (non-cloud) environment tags so as to stay below the aws 50 tags limit.
-    garbage_tags = []
-    tag_pattern = "ResourceId={asg_name},ResourceType=auto-scaling-group,Key={key}"
-    for gtk in garbage_tag_keys:
-        if environment(gtk) not in MULTICONTAINER_ENVIRONMENTS:
-            garbage_tags.append(tag_pattern.format(asg_name=asg['AutoScalingGroupName'], key=gtk))
-    if garbage_tags:
-        cmd = "aws autoscaling delete-tags --tags " + " ".join(garbage_tags)
-        if DEBUG:
-            print cmd
-        aws_command(cmd)
-
-
-def permission_to_scale(asg, my_environment):
-    allowed_envs = get_tag(asg, SCALING_PERMISSION_TAG)
-    if allowed_envs == None:
-        # Let's hope this never overwrites anything important.
-        # set_tag(asg, SCALING_PERMISSION_TAG, "joe_test_123")
-        return False
-    return my_environment in set(s.strip() for s in allowed_envs.split(","))
-
-
-def update_metric_values(asg, value, my_environment):
-    mvals = get_tags_by_prefix(asg, SCALING_METRIC_TAG_PREFIX)
-    my_key, my_val = set_metric_value(asg, value, my_environment)
-    mvals[my_key] = my_val
-    if DEBUG:
-        print json.dumps(mvals, indent=2)
-    results = {}
-    expired_keys = []
-    for k, v in mvals.items():
-        parts = v.split("::")
-        unixtime = int(parts[0])
-        if unixtime_now() - unixtime < EXPIRATION_PERIOD_MINUTES * 60:
-            results[k] = int(parts[1])
-        else:
-            expired_keys.append(k)
-            if DEBUG:
-                print "Expired value {} for key {}".format(v, k)
-    delete_expired_metric_tags(asg, expired_keys)
-    return results
-
-
 def count_healthy_instances(asg, tag_list, draining_tag):
     draining_instance_ids = get_draining_servers(asg, tag_list, draining_tag).keys()
     num_draining = len(draining_instance_ids)
@@ -247,7 +146,7 @@ def iso2unixtime(iso_str):
     return int(dateutil.parser.parse(iso_str).strftime('%s'))
 
 
-def get_instance_tags():
+def get_tag_list():
     '''
     Return a list of tags, of the form:
     [
@@ -259,15 +158,6 @@ def get_instance_tags():
     cmd = "aws ec2 describe-tags --filters 'Name=resource-type,Values=instance' --no-paginate"
     tag_dict = json.loads(aws_command(cmd))
     return tag_dict["Tags"]
-
-
-def instances_in(asg):
-    return [item["InstanceId"] for item in asg["Instances"]]
-
-
-def find_tags_for_instances_in(asg, tag_list):
-    instance_ids = instances_in(asg)
-    return [tag for tag in tag_list if tag["ResourceId"] in instance_ids]
 
 
 def add_draining_tag(instance_ids, draining_tag):
@@ -326,7 +216,7 @@ def get_draining_servers(asg, tag_list, draining_tag):
 
 def count_running_alignment_jobs(asg, tag_list, job_tag_prefix, job_tag_expiration):
     ''' returns a map of instance IDs to number of jobs running on the instance '''
-    instance_ids = instances_in(asg)
+    instance_ids = self.instance_ids
     count_dict = { id: 0 for id in instance_ids }
     expired_jobs = []
     for item in tag_list:
@@ -375,112 +265,127 @@ def check_draining_servers(asg, tag_list, draining_tag, job_tag_prefix, job_tag_
         if can_scale:
             remove_termination_protection(instance_ids_to_terminate, asg)
 
-def count_running_batch_jobs():
-    queues_lock = threading.RLock()
-    queues = {"idseq_himem": [],
-              "idseq": []}
-    errors = []
-    def list_jobs(q):
-        try:
-            job_list = aws_command("aws batch list-jobs --job-queue {q} --job-status RUNNING".format(q=q))
-            with queues_lock:
-                queues[q] = json.loads(job_list)
-        except:
-            with queues_lock:
-                errors.append(q)
-    threads = [threading.Thread(target=list_jobs, args=[q]) for q in queues]
-    for t in threads:
-        t.start()
-    for t in threads:
-        t.join()
-    if errors:
-        return None
-    return sum(len(job_list["jobSummaryList"]) for job_list in queues.itervalues())
-
+def get_asg_list():
+    asg_json = aws_command("aws autoscaling describe-auto-scaling-groups")
+    asg_list = json.loads(asg_json).get('AutoScalingGroups', [])
+    return asg_list
 
 def autoscaling_update(my_num_jobs, my_environment="development",
                        max_job_dispatch_lag_seconds=900, job_tag_prefix="RunningIDseqBatchJob_",
                        job_tag_keep_alive_seconds=600, draining_tag="draining"):
-    min_draining_wait = max_job_dispatch_lag_seconds + GRACE_PERIOD_SECONDS__JOB_DISPATCH_LAG
-    job_tag_expiration = job_tag_keep_alive_seconds + GRACE_PERIOD_SECONDS__JOB_TAG_KEEP_ALIVE
-    if my_environment in MULTICONTAINER_ENVIRONMENTS:
-        asg_env = my_environment
-    else:
-        # Distinguish different developers' environments based on hostname
-        hostname = (
-            os.environ.get("HOSTNAME") or
-            os.environ.get("HOST") or
-            subprocess.check_output(["hostname"]).strip()
-        )
-        # sha hexdigest is used to sanitize the hostname so it's safe for tags
-        my_environment += "_" + hashlib.sha224(hostname).hexdigest()[:10]
-        asg_env = 'staging'
-    asg_json = aws_command("aws autoscaling describe-auto-scaling-groups")
-    asg_list = json.loads(asg_json).get('AutoScalingGroups', [])
-    gsnap_instance_name = "gsnapl-asg-" + asg_env
-    rapsearch_instance_name = "rapsearch2-asg-" + asg_env
-    gsnap_asg = find_asg(asg_list, gsnap_instance_name)
-    rapsearch2_asg = find_asg(asg_list, rapsearch_instance_name)
-    if DEBUG:
-        print json.dumps(gsnap_asg, indent=2)
-        print json.dumps(rapsearch2_asg, indent=2)
-    mvals = update_metric_values(gsnap_asg, my_num_jobs, my_environment)
-    if not DEBUG:
-        # when debugging is enabled this would be a duplicate print...
-        print json.dumps(mvals, indent=2)
-    num_development_jobs = sum(v for k, v in mvals.iteritems() if "development" in k)
-    num_real_jobs = sum(mvals.itervalues()) - num_development_jobs
-    print "ASG tags indicate {num_jobs} in-progress job(s) across {num_env} environments.".format(num_jobs=num_real_jobs + num_development_jobs, num_env=len(mvals))
-    print "From cloud environments: {num_real_jobs}".format(num_real_jobs=num_real_jobs)
-    print "From development environments: {num_development_jobs}".format(num_development_jobs=num_development_jobs)
-    can_scale = permission_to_scale(gsnap_asg, my_environment)
-    if not can_scale:
+    if my_environment not in SCALING_ENVIRONMENTS:
+        return
+    print "{my_num_jobs} jobs are in progress.".format(my_num_jobs=my_num_jobs)
+    asg_list = get_asg_list()
+    tag_list = get_tag_list()
+    gsnap_ASG = ASG(...)
+    if not gsnap_ASG.can_scale:
         print "Scaling by agents of {my_environment} is not permitted.".format(my_environment=my_environment)
         # when debugging is enabled and we can't scale we print the scaling commands without executing them
         # when debugging is disabled and we can't scale we exit early here
         if not DEBUG:
             return
-    tag_list = get_instance_tags()
-    gsnap_tags = find_tags_for_instances_in(gsnap_asg, tag_list)
-    rapsearch_tags = find_tags_for_instances_in(rapsearch2_asg, tag_list)
-    if num_real_jobs == 0 and num_development_jobs == 0:
-        set_desired_capacity(gsnap_asg, gsnap_instance_name, gsnap_tags, draining_tag, exactly(0), can_scale)
-        set_desired_capacity(rapsearch2_asg, rapsearch_instance_name, rapsearch_tags, draining_tag, exactly(0), can_scale)
-    elif num_real_jobs == 0 and num_development_jobs > 0:
-        count_running_servers = get_previous_desired(gsnap_asg) + get_previous_desired(rapsearch2_asg)
-        print "Only development environments are reporting in-progress jobs, and {crs} servers are running.".format(crs=count_running_servers)
-        if count_running_servers > 2 or random.random() < 0.2:
-            crbj = count_running_batch_jobs()
-            if crbj:
-                print "{crbj} jobs are running in aws batch queues".format(crbj=crbj)
-                # This is an unsafe scaling operation, but the only jobs that can get hurt
-                # are development jobs.  Just rerun those.
-                set_desired_capacity(gsnap_asg, gsnap_instance_name, gsnap_tags, draining_tag, exactly(1), can_scale)
-                set_desired_capacity(rapsearch2_asg, rapsearch_instance_name, rapsearch_tags, draining_tag, exactly(1), can_scale)
-            elif crbj == 0:
-                print "No jobs are running in batch."
-                # in this case num_development_jobs are either zombies or jobs caught in transition
-                # between pipeline stages;  the non-zombies won't be hurt, only delayed by this
-                set_desired_capacity(gsnap_asg, gsnap_instance_name, gsnap_tags, draining_tag, exactly(0), can_scale)
-                set_desired_capacity(rapsearch2_asg, rapsearch_instance_name, rapsearch_tags, draining_tag, exactly(0), can_scale)
-            else:
-                print "Failed to get information about running jobs in aws batch.  Deferring scaling decision."
-        else:
-            print "Deferring scaling decision to stay under the rate limit for 'aws batch list-jobs'."
-    elif num_real_jobs == 1:
-        # Often a test job or automatic benchmark job
-        set_desired_capacity(gsnap_asg, gsnap_instance_name, gsnap_tags, draining_tag, exactly(4), can_scale)
-        set_desired_capacity(rapsearch2_asg, rapsearch_instance_name, rapsearch_tags, draining_tag, exactly(4), can_scale)
-    elif 1 < num_real_jobs <= 6:
-        set_desired_capacity(gsnap_asg, gsnap_instance_name, gsnap_tags, draining_tag, exactly(8), can_scale)
-        set_desired_capacity(rapsearch2_asg, rapsearch_instance_name, rapsearch_tags, draining_tag, exactly(8), can_scale)
-    else:
-        set_desired_capacity(gsnap_asg, gsnap_instance_name, gsnap_tags, draining_tag, at_least(24), can_scale)
-        set_desired_capacity(rapsearch2_asg, rapsearch_instance_name, rapsearch_tags, draining_tag, at_least(24), can_scale)
+    print "Required capacity for GSNAP ASG: " + gsnap_ASG.determine_required_capacity()
+    gsnap_ASG.set_desired_capacity()
 
     check_draining_servers(gsnap_asg, gsnap_tags, draining_tag, job_tag_prefix, job_tag_expiration, min_draining_wait, can_scale)
     check_draining_servers(rapsearch2_asg, rapsearch_tags, draining_tag, job_tag_prefix, job_tag_expiration, min_draining_wait, can_scale)
 
+class ASG(object):
+    def __init__(self, service, environment, num_jobs, aws_describe_all_asgs, aws_describe_all_tags, draining_tag, job_tag_prefix, max_job_dispatch_lag_seconds, job_tag_keep_alive_seconds):
+        self.service = service
+        self.environment = environment
+        self.num_jobs = num_jobs
+        self.aws_describe_all_asgs = aws_describe_all_asgs
+        self.aws_describe_all_tags = aws_describe_all_tags
+        self.draining_tag = draining_tag
+        self.job_tag_prefix = job_tag_prefix
+        self.max_job_dispatch_lag_seconds = max_job_dispatch_lag_seconds
+        self.job_tag_keep_alive_seconds = job_tag_keep_alive_seconds
+
+        # If you are running into problems with this autoscaler, just delete this tag from
+        # any affected autoscaling group, and it will be left alone.  The value is a list
+        # of environments permitted to trigger scaling, usually just "prod".
+        self.scaling_permission_tag = 'IDSeqEnvsThatCanScale'
+
+        self.job_dispatch_lag_grace_period_seconds = 300
+        self.job_tag_keep_alive_grace_period_seconds = 300
+
+        self.min_draining_wait = self.max_job_dispatch_lag_seconds + self.job_dispatch_lag_grace_period_seconds
+        self.job_tag_expiration = self.job_tag_keep_alive_seconds + self.job_tag_keep_alive_grace_period_seconds
+        self.instance_name = self.service + "-asg-" + self.environment
+
+        self.can_scale = self.permission_to_scale()
+        self.asg = self.find_asg()
+        self.instance_ids = self.find_instance_ids()
+        self.tags = self.find_tags()
+
+        self.desired_capacity = 0
+
+     def permission_to_scale(self):
+         def get_tag(asg, tag_key):
+             for tag in asg['Tags']:
+                 if tag['Key'] == tag_key:
+                     return tag['Value']
+             return None
+         allowed_envs = get_tag(self.asg, self.scaling_permission_tag)
+         if allowed_envs == None:
+             return False
+         return self.environment in set(s.strip() for s in allowed_envs.split(","))
+
+     def find_asg(self):
+         matching = []
+         for asg in self.aws_describe_all_asgs:
+             if asg['AutoScalingGroupName'].startswith(self.instance_name):
+                 matching.append(asg)
+         assert len(matching) == 1
+         result = matching[0]
+         if DEBUG:
+             print json.dumps(result, indent=2)
+         return result
+
+    def find_instance_ids(self):
+        return [item["InstanceId"] for item in self.asg["Instances"]]
+
+    def find_tags(self):
+        return [tag for tag in self.tag_list if tag["ResourceId"] in self.instance_ids]
+
+    def determine_required_capacity(self):
+        if self.num_jobs == 0:
+            self.required_capacity = exactly(0)
+        elif self.num_jobs == 1:
+            # Often a test job or automatic benchmark job
+            self.required_capacity = exactly(4)
+        elif 1 < self.num_jobs <= 6:
+            self.required_capacity = exactly(8)
+        else:
+            self.required_capacity = at_least(24)
+        return self.required_capacity
+
+    def set_desired_capacity(self):
+        asg_name = self.asg['AutoScalingGroupName']
+        num_healthy, num_draining = self.count_healthy_instances()
+        previous_desired = self.get_previous_desired()
+        # Manually input MinCapacity will never be reduced so long as there are pending jobs.
+        num_desired = self.clamp_to_valid_range(self.required_capacity(previous_desired))
+        if num_desired == previous_desired:
+            action = "should remain"
+        else:
+            action = "should change to"
+        cmd = "aws autoscaling set-desired-capacity --auto-scaling-group-name {asg_name} --desired-capacity {num_desired}"
+        cmd = cmd.format(asg_name=asg_name, num_desired=num_desired)
+        msg = "Autoscaling group {asg_name} has {num_healthy} healthy and {num_draining} draining instance(s). Desired capacity {action} {num_desired}."
+        print msg.format(asg_name=asg_name, num_healthy=num_healthy, num_draining=num_draining, action=action, num_desired=num_desired)
+        if self.can_scale:
+            if DEBUG:
+                print cmd
+            if num_desired < num_healthy:
+                self.start_draining(num_healthy - num_desired)
+            elif num_desired > num_healthy:
+                self.stop_draining(num_desired - num_healthy)
+            aws_command(cmd)
+
+    def check_draining_servers():
 
 if __name__ == "__main__":
     assert len(sys.argv) > 2
