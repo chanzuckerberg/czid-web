be630303a359d1b69badb645a504befb8a22b261
jshoe <github@jsheu.com>
2018-11-15 16:08:44 -0800

    Migrate

--- db/migrate/20181115235300_add_taxon_lineage_columns.rb
+++ db/migrate/20181115235300_add_taxon_lineage_columns.rb
@@ -1,6 +1,7 @@
 class AddTaxonLineageColumns < ActiveRecord::Migration[5.1]
   def change
     add_column :taxon_lineages, :name, :string
+    add_index :taxon_lineages, :name
     # Limit = 1 byte for max 255
     add_column :taxon_lineages, :version_start, :integer, limit: 1
     add_column :taxon_lineages, :version_end, :integer, limit: 1
