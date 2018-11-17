58d201676a2b805216cd6e70ccf5cfdb7eb83aa8
Charles de Bourcy <cdebourcy@chanzuckerberg.com>
2018-11-17 13:08:06 -0800

    fix bugs

--- app/jobs/autoscaling.py
+++ app/jobs/autoscaling.py
@@ -1,45 +1,14 @@
 #!/usr/bin/env python
 import json
-import os
 import sys
 import subprocess
 import time
-import hashlib
 import random
-import threading
-import datetime
-import dateutil.parser
 from operator import itemgetter
 from functools import wraps
+import dateutil.parser
 
 DEBUG = False
-
-## EXPLANATION OF STATE TRANSITIONS FOR GSNAP/RAPSEARCH MACHINES
-'''
-{ service: gsnap-prod-initializing, ProtectedFromScaleIn: True }
-Initializing. Scale-in protection automatically granted at birth.
-    |
-    V
-{ service: gsnap-prod, ProtectedFromScaleIn: True }
-In service, accepting chunks. Scale-in protection present.
-    |
-    V
-{ service: gsnap-prod, ProtectedFromScaleIn: True,
-  RunningIDseqBatchJob_X1: Y1, RunningIDseqBatchJob_X2: Y2, ...
-}
-Processing chunks.
-    |
-    V
-{ service: gsnap-prod, draining: T0, ProtectedFromScaleIn: True }
-Draining. Scale-in protection still present.
-Can still accept chunks for up to min_draining_wait seconds after entering this state.
-Can still finish running chunks for up to (min_draining_wait + job_tag_expiration) seconds after entering this state.
-    |
-    V
-{ service: gsnap-prod, draining: T0, ProtectedFromScaleIn: False }
-Discarded. Scale-in protection withdrawn on transition to this state.
-'''
-
 
 def retry(operation, randgen=random.Random().random):
     # Note the use of a separate random generator for retries so transient
@@ -97,7 +66,7 @@ def delete_tags_from_instances(instance_ids, tag_keys):
     cmd = "aws ec2 delete-tags --resources {list_instances} --tags {list_tags}"
     cmd = cmd.format(list_instances=' '.join(instance_ids), list_tags=' '.join(['Key='+tag for tag in tag_keys]))
     aws_command(cmd)
-   
+
 def get_asg_list():
     asg_json = aws_command("aws autoscaling describe-auto-scaling-groups")
     asg_list = json.loads(asg_json).get('AutoScalingGroups', [])
@@ -105,24 +74,33 @@ def get_asg_list():
 
 def required_capacity(num_jobs):
     if num_jobs == 0:
-        required_capacity = exactly(0)
-    elif self.num_jobs == 1:
+        result = exactly(0)
+    elif num_jobs == 1:
         # Often a test job or automatic benchmark job
-        required_capacity = exactly(4)
-    elif 1 < self.num_jobs <= 6:
-        required_capacity = exactly(8)
+        result = exactly(4)
+    elif 1 < num_jobs <= 6:
+        result = exactly(8)
     else:
-        required_capacity = at_least(24)
-    return required_capacity
+        # This means, as long as we're in this regime, a DesiredCapacity > 24 set manually by the operator
+        # will be respected and not scaled down until we exit this regime.
+        result = at_least(24)
+    return result
 
 def autoscaling_update(my_num_jobs, my_environment="development",
                        max_job_dispatch_lag_seconds=900, job_tag_prefix="RunningIDseqBatchJob_",
                        job_tag_keep_alive_seconds=600, draining_tag="draining"):
+    '''
+    Plan and perform an update to the GSNAP/RAPSEARCH ASGs.
+    Note: Will not scale up for jobs originating in a development environment.
+          You can provision machines for development jobs by manually setting MinSize on the ASGs,
+          which is respected by this autoscaler.
+    '''
+
     asg_list = get_asg_list()
     tag_list = get_tag_list()
-    gsnap_ASG = ASG(...)
-    rapsearch_ASG = ASG(...)
-    if not (gsnap_ASG.can_scale && rapsearch_ASG.can_scale):
+    gsnap_ASG = ASG("gsnapl", my_environment, asg_list, tag_list, draining_tag, job_tag_prefix, max_job_dispatch_lag_seconds, job_tag_keep_alive_seconds)
+    rapsearch_ASG = ASG("rapsearch2", my_environment, asg_list, tag_list, draining_tag, job_tag_prefix, max_job_dispatch_lag_seconds, job_tag_keep_alive_seconds)
+    if not (gsnap_ASG.can_scale and rapsearch_ASG.can_scale):
         print "Scaling by agents of {my_environment} is not permitted.".format(my_environment=my_environment)
         return
 
@@ -144,33 +122,72 @@ def autoscaling_update(my_num_jobs, my_environment="development",
         print "MOVING FORWARD:"
         raw_new_num_desired = compute_num_instances(num_desired)
         print "In principle, the desired capacity needs to be {raw_new_num_desired}.".format(raw_new_num_desired=raw_new_num_desired)
-        new_num_desired = self.clamp_to_valid_range(raw_new_num_desired)
+        new_num_desired = service_ASG.clamp_to_valid_range(raw_new_num_desired)
         print "Enforcing the MinSize and MaxSize set by the operator, the desired capacity will be set to {new_num_desired}.".format(new_num_desired=new_num_desired)
         service_ASG.set_desired_capacity(new_num_desired)
 
         print "HEALTHY <--> DRAINING transitions:"
-        self.draining_instances = draining_instances
+        service_ASG.draining_instances = draining_instances
         if new_num_desired < num_healthy:
             num_to_drain = num_healthy - new_num_desired
             instances_to_drain = service_ASG.start_draining(num_to_drain)
             print "{num_to_drain} instances need to be drained: {instances_to_drain}.".format(num_to_drain=num_to_drain, instances_to_drain=instances_to_drain)
         elif new_num_desired > num_healthy:
             num_to_rescue = min(new_num_desired - num_healthy, num_draining)
-            instances_to_rescue = service_ASG.stop_draining(draining_instances, num_to_rescue)
+            instances_to_rescue = service_ASG.stop_draining(num_to_rescue)
             print "{num_to_rescue} instances need to stop draining: {instances_to_rescue}.".format(num_to_rescue=num_to_rescue, instances_to_rescue=instances_to_rescue)
         else:
-           "No instances need to make a HEALTHY <--> DRAINING transition."
+            print "No instances need to make a HEALTHY <--> DRAINING transition."
 
         print "DRAINING --> TERMINATING transitions:"
         instances_to_terminate = service_ASG.start_terminating_if_safe()
         num_to_terminate = len(instances_to_terminate)
         if num_to_terminate > 0:
-            "{num_to_terminate} instances have finished draining and can be terminated: {instances_to_terminate}.".format(num_to_terminate=num_to_terminate, instances_to_terminate=instances_to_terminate)
+            print "{num_to_terminate} instances have finished draining and can be terminated: {instances_to_terminate}.".format(num_to_terminate=num_to_terminate, instances_to_terminate=instances_to_terminate)
         else:
-            "There are no instances that need to move from draining to terminating state."
+            print "There are no instances that need to move from draining to terminating state."
 
 class ASG(object):
+    r'''
+    State machine of EC2 instances within the ASG as relevant to this autoscaler:
+
+                            _________________
+                           |                 |    Either initializing or ready to take jobs.
+                           |     HEALTHY     |    Scale-in protection automatically granted at birth.
+                           |_________________|
+
+
+        decrease DesiredCapacity  |  / \   increase DesiredCapacity
+              add draining_tag    |   |   remove draining_tag
+                                 \ /  |
+
+                            _________________     Draining.
+                           |                 |    Scale-in protection still present.
+                           |    DRAINING     |    May still accept jobs for up to min_draining_wait seconds after entering this state.
+                           |_________________|    Can still finish running jobs for up to (min_draining_wait + job_tag_expiration) seconds after entering this state.
+
+
+                                    |
+        remove scale-in protection  |
+                                   \ /
+
+                            _________________
+                           |                 |   Discarded.
+                           |   TERMINATING   |   Scale-in protection removed.
+                           |_________________|   Instance will terminate due to discrepancy between DesiredCapacity and actual number of instances.
+
+    '''
+
+    # If you are running into problems with this autoscaler, just delete this tag from
+    # any affected autoscaling group, and it will be left alone.  The value is a list
+    # of environments permitted to trigger scaling, usually just "prod".
+    scaling_permission_tag = 'IDSeqEnvsThatCanScale'
+
+    job_dispatch_lag_grace_period_seconds = 300
+    job_tag_keep_alive_grace_period_seconds = 300
+
     def __init__(self, service, environment, asg_list, tag_list, draining_tag, job_tag_prefix, max_job_dispatch_lag_seconds, job_tag_keep_alive_seconds):
+        self.draining_instances = []
         self.service = service
         self.environment = environment
         self.asg_list = asg_list
@@ -180,56 +197,49 @@ class ASG(object):
         self.max_job_dispatch_lag_seconds = max_job_dispatch_lag_seconds
         self.job_tag_keep_alive_seconds = job_tag_keep_alive_seconds
 
-        # If you are running into problems with this autoscaler, just delete this tag from
-        # any affected autoscaling group, and it will be left alone.  The value is a list
-        # of environments permitted to trigger scaling, usually just "prod".
-        self.scaling_permission_tag = 'IDSeqEnvsThatCanScale'
-
-        self.job_dispatch_lag_grace_period_seconds = 300
-        self.job_tag_keep_alive_grace_period_seconds = 300
-
         self.min_draining_wait = self.max_job_dispatch_lag_seconds + self.job_dispatch_lag_grace_period_seconds
         self.job_tag_expiration = self.job_tag_keep_alive_seconds + self.job_tag_keep_alive_grace_period_seconds
         self.instance_name = self.service + "-asg-" + self.environment
 
         self.can_scale = self.permission_to_scale()
-        self.asg = self.find_asg()
-        self.instance_ids = self.find_instance_ids()
-        self.tags = self.find_tags()
+        self.asg, self.instance_ids, self.tags = self.find_attributes()
 
-     def permission_to_scale(self):
-         def get_tag(asg, tag_key):
-             for tag in asg['Tags']:
-                 if tag['Key'] == tag_key:
-                     return tag['Value']
-             return None
-         allowed_envs = get_tag(self.asg, self.scaling_permission_tag)
-         if allowed_envs == None:
-             return False
-         return self.environment in set(s.strip() for s in allowed_envs.split(","))
+    def permission_to_scale(self):
+        def get_tag(asg, tag_key):
+            for tag in asg['Tags']:
+                if tag['Key'] == tag_key:
+                    return tag['Value']
+            return None
+        allowed_envs = get_tag(self.asg, self.scaling_permission_tag)
+        if allowed_envs == None:
+            return False
+        return self.environment in set(s.strip() for s in allowed_envs.split(","))
 
-     def find_asg(self):
-         matching = []
-         for asg in self.asg_list:
-             if asg['AutoScalingGroupName'].startswith(self.instance_name):
-                 matching.append(asg)
-         assert len(matching) == 1
-         result = matching[0]
-         if DEBUG:
-             print json.dumps(result, indent=2)
-         return result
-
-    def find_instance_ids(self):
-        return [item["InstanceId"] for item in self.asg["Instances"]]
-
-    def find_tags(self):
-        return [tag for tag in self.tag_list if tag["ResourceId"] in self.instance_ids]
+    def find_attributes(self):
+        def find_asg():
+            matching = []
+            for asg in self.asg_list:
+                if asg['AutoScalingGroupName'].startswith(self.instance_name):
+                    matching.append(asg)
+            assert len(matching) == 1
+            result = matching[0]
+            if DEBUG:
+                print json.dumps(result, indent=2)
+            return result
+        def find_instance_ids(asg):
+            return [item["InstanceId"] for item in asg["Instances"]]
+        def find_tags(instance_ids):
+            return [tag for tag in self.tag_list if tag["ResourceId"] in instance_ids]
+        asg = find_asg()
+        instance_ids = find_instance_ids(asg)
+        tags = find_tags(instance_ids)
+        return asg, instance_ids, tags
 
     def num_desired_instances(self):
         return int(self.asg.get("DesiredCapacity", self.asg.get("MinSize", 0)))
 
     def tags_by_instance_id(self):
-        return { item['ResourceId']: { item['Key']: item['Value'] } for item in self.tag_list }
+        return {item['ResourceId']: {item['Key']: item['Value']} for item in self.tag_list}
 
     def clamp_to_valid_range(self, desired_capacity):
         min_size = int(self.asg.get("MinSize", desired_capacity))
@@ -250,7 +260,7 @@ class ASG(object):
                 print cmd
             aws_command(cmd)
 
-    def classify_instances():
+    def classify_instances(self):
         healthy_instances = []
         draining_instances = []
         terminating_instances = []
@@ -274,7 +284,8 @@ class ASG(object):
             cmd = "aws ec2 describe-instances --filters 'Name=tag:Name,Values={instance_name}' --query 'Reservations[*].Instances[*].[InstanceId,LaunchTime,Tags]' --no-paginate"
             cmd = cmd.format(instance_name=self.instance_name)
             aws_response = json.loads(aws_command(cmd))
-            zipped_instance_ids_and_launch_times = [(instance[0], iso2unixtime(instance[1]))
+            zipped_instance_ids_and_launch_times = [
+                (instance[0], iso2unixtime(instance[1]))
                 for instance_list in aws_response
                 for instance in instance_list
                 if self.draining_tag not in [tag["Key"] for tag in instance[2]]
@@ -286,14 +297,14 @@ class ASG(object):
             cmd = cmd.format(list_instances=' '.join(instance_ids), draining_tag=self.draining_tag, timestamp=unixtime_now())
             aws_command(cmd)
         instance_ids = instances_to_drain()
-        if instance_ids && self.can_scale:
+        if instance_ids and self.can_scale:
             add_draining_tag(instance_ids)
         return instance_ids
 
     def get_drainage_times(self):
         ''' returns a map of draining instance IDs to the time they started draining '''
-        tag_dict = self.tags_by_instance_id
-        return { instance_id: int(tag_dict[instance_id][self.draining_tag]) for instance_id in self.draining_instances }
+        tag_dict = self.tags_by_instance_id()
+        return {instance_id: int(tag_dict[instance_id][self.draining_tag]) for instance_id in self.draining_instances}
 
     def stop_draining(self, num_to_rescue):
         def instances_to_rescue():
@@ -307,7 +318,7 @@ class ASG(object):
         def remove_draining_tag(instance_ids):
             delete_tags_from_instances(instance_ids, [self.draining_tag])
         instance_ids = instances_to_rescue()
-        if instance_ids && self.can_scale:
+        if instance_ids and self.can_scale:
             remove_draining_tag(instance_ids)
         return instance_ids
 
@@ -315,7 +326,7 @@ class ASG(object):
         def count_running_alignment_jobs():
             ''' returns a map of draining instance IDs to number of jobs still running on the instance '''
             instance_ids = self.draining_instances
-            count_dict = { id: 0 for id in instance_ids }
+            count_dict = {id: 0 for id in instance_ids}
             expired_jobs = []
             for item in self.tag_list:
                 if item['Key'].startswith(self.job_tag_prefix):
@@ -345,7 +356,7 @@ class ASG(object):
             cmd = cmd.format(list_instances=' '.join(instance_ids), asg_name=self.asg['AutoScalingGroupName'])
             aws_command(cmd)
         instance_ids = instances_to_terminate()
-        if instance_ids && self.can_scale:
+        if instance_ids and self.can_scale:
             remove_termination_protection(instance_ids)
         return instance_ids
 
