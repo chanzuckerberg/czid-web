a53b7a59c82ee0bceca0b834b8f3d5f3c853f92c
James Wang <jwang@chanzuckerberg.com>
2018-11-27 11:08:42 -0800

    Updates to deletion script  - removes race condition of sample being while s3 files are being scanned  - script is now robust to paths with greater than 1000 files

--- bin/remove_deleted_samples_from_s3.py
+++ bin/remove_deleted_samples_from_s3.py
@@ -40,24 +40,35 @@ def delete_unknown_samples(dryrun):
       print('{verb}: idseq-samples-{env}/samples/{subpath}'.format(
         verb = 'Found' if dryrun else 'Deleting', env=ENV, subpath=subpath))
       if not dryrun:
-        resp = s3.meta.client.list_object_versions(
-            Bucket=bucket,
-            Prefix='samples/{subpath}'.format(subpath=subpath))
-        objects_to_delete = resp.get('Versions', [])
-        if len(objects_to_delete) > 0:
+        while True:
+          resp = s3.meta.client.list_object_versions(
+              Bucket=bucket,
+              Prefix='samples/{subpath}'.format(subpath=subpath))
+          objects_to_delete = resp.get('Versions', [])
+          if len(objects_to_delete) == 0:
+            break
+
           delete_keys = {'Objects' : []}
+          count = 0
           for item in objects_to_delete:
             delete_keys['Objects'].append({'Key': item['Key'], 'VersionId': item['VersionId']})
+            count += 1
+            if count == 1000: #s3 API only returns and accepts up to 1000 objects at a time
+              break
+
+          print(' Deleting {count} objects'.format(count=count))
           s3.meta.client.delete_objects(Bucket=bucket, Delete=delete_keys)
-   
+          if count < 1000: #if we deleted fewer than 1000 objects then no need to check the path again
+            break
+
 if __name__ == "__main__":
   msg = 'This utility will delete all s3 sample data without db entries in the {env} environment. Type "y" or "yes" to continue.\n'.format(env=ENV)
   resp = input(msg)
   if resp.lower() not in ["y", "yes"]:
     print("Exiting...")
     quit()
-  fetch_sample_ids()
   fetch_s3_paths()
+  fetch_sample_ids()
   delete_unknown_samples(dryrun=True)
   input('Press enter to continue')
   delete_unknown_samples(dryrun=False)
