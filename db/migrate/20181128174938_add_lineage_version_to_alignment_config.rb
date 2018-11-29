cbef3e2e1b44ab23d47cc0db2fe7fdf10c0ede44
Jonathan Sheu <jshoe@users.noreply.github.com>
2018-11-29 11:15:25 -0800

    Add comments

--- db/migrate/20181128174938_add_lineage_version_to_alignment_config.rb
+++ db/migrate/20181128174938_add_lineage_version_to_alignment_config.rb
@@ -13,6 +13,10 @@ class AddLineageVersionToAlignmentConfig < ActiveRecord::Migration[5.1]
     # AlignmentConfig 2018-04-01 is our current one so it gets lineage_version = 2. AlignmentConfig 2018-02-15 gets lineage_version = 1.
     # Every pipeline run has one of these AlignmentConfigs assigned already (no nil ones).
     # Old records that have version_end == 0 will not be used / would have be succeeded by new records.
+    # 
+    # We use the lineage_version to figure out which lineage records to use in our pipeline run. Lineage records are highly duplicated 
+    # between versions, so instead of storing all of them, we store a [version_start, version_end] range with each lineage record.
+    # When the index is updated, if a lineage record is still valid, its version_end gets += 1. Otherwise it stays the same and has been replaced.
   end
 
   def down
