788bec2f9997e7d0e086e4358316e0b4f1b1f3a9
jshoe <github@jsheu.com>
2018-11-30 10:22:55 -0800

    Tweak

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-if test -z "$ENVIRONMENT"; then
-    # If ENVIRONMENT not set (e.g. local development), don't call chamber
-    exec bundle exec "$@"
-else
-    # Use Chamber to inject secrets via environment variables.
-    exec chamber exec idseq-$ENVIRONMENT-web -- bundle exec "$@"
-fi
+echo $(printenv)
+
+exec chamber exec idseq-prod-web -- bundle exec "$@"
+
+#if test -z "$ENVIRONMENT"; then
+#    # If ENVIRONMENT not set (e.g. local development), don't call chamber
+#    echo "NOT CALLING CHAMBER"
+#    exec bundle exec "$@"
+#else
+#    echo "CALLING WITH CHAMBER"
+#    # Use Chamber to inject secrets via environment variables.
+#    exec chamber exec idseq-$ENVIRONMENT-web -- bundle exec "$@"
+#fi
