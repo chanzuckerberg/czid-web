36c12ec23ef266ef8e9804302ec4409ff6ffc96a
jshoe <github@jsheu.com>
2018-11-30 11:23:44 -0800

    Update docker-compose

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-echo $(printenv)
-
-exec chamber exec idseq-prod-web -- bundle exec "$@"
-
-#if test -z "$ENVIRONMENT"; then
-#    # If ENVIRONMENT not set (e.g. local development), don't call chamber
-#    echo "NOT CALLING CHAMBER"
-#    exec bundle exec "$@"
-#else
-#    echo "CALLING WITH CHAMBER"
-#    # Use Chamber to inject secrets via environment variables.
-#    exec chamber exec idseq-$ENVIRONMENT-web -- bundle exec "$@"
-#fi
+if test -z "$ENVIRONMENT"; then
+    # If ENVIRONMENT not set (e.g. local development), don't call chamber
+    exec bundle exec "$@"
+else
+    # Use Chamber to inject secrets via environment variables.
+    exec chamber exec idseq-$ENVIRONMENT-web -- bundle exec "$@"
+fi
