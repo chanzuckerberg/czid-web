80fd31fbe4b12bfcb7621cc053e1109b13685cd9
Charles de Bourcy <cdebourcy@chanzuckerberg.com>
2018-12-05 15:15:11 -0800

    fix comment

--- app/helpers/dev_helper.rb
+++ app/helpers/dev_helper.rb
@@ -1,6 +1,6 @@
 module TestHelper
   def _alignment_cost_per_million_reads(month_starting, ec2_cost)
-    # Computes the average cost per read of gsnap/rapsearch alignment servers over the course of 1 month.
+    # Computes the average cost per million reads of gsnap/rapsearch alignment servers over the course of 1 month.
     # ec2_cost should be the cost (in $) of i3.16xlarge instances (=gsnap/rapsearch) for the month, as found in the AWS account.
     # Usage example:
     #   month_starting = "2018-08-01"
