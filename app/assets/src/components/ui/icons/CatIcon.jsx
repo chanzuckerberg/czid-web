f521c57af22397933204d031f90e5a8d14fbb942
jshoe <github@jsheu.com>
2018-12-06 15:55:56 -0800

    WIP

--- app/assets/src/components/ui/icons/CatIcon.jsx
+++ app/assets/src/components/ui/icons/CatIcon.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import cx from "classnames";
+import PropTypes from "prop-types";
 
-const CatIcon = () => {
+const CatIcon = ({ color }) => {
   return (
     <svg
-      className={cx(cs.removeIcon)}
       xmlns="http://www.w3.org/2000/svg"
       version="1.1"
       x="0px"
       y="0px"
       viewBox="0 0 100 125"
+      fill={color}
     >
       <g display="none">
         <g display="inline">
@@ -69,6 +69,10 @@ const CatIcon = () => {
       </g>
     </svg>
   );
+};
+
+CatIcon.propTypes = {
+  color: PropTypes.string
 };
 
 export default CatIcon;
