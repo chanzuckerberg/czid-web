41acad37f09f9181f9f24a7a684faf6201a72f18
Jonathan Sheu <jshoe@users.noreply.github.com>
2018-12-18 11:20:28 -0800

    Consolidate docker-compose and ALIGNMENT_CONFIG_DEFAULT_NAME (#1803)
    * Consolidate docker-compose and ALIGNMENT_CONFIG_DEFAULT_NAME
    
    * Tweak

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if test -z "$ENVIRONMENT"; then
-    # If ENVIRONMENT not set, use "dev" env
-    exec chamber exec idseq-dev-web -- bundle exec "$@"
+    # If ENVIRONMENT not set (e.g. local development), don't call chamber
+    exec bundle exec "$@"
 else
     # Use Chamber to inject secrets via environment variables.
     exec chamber exec idseq-$ENVIRONMENT-web -- bundle exec "$@"
