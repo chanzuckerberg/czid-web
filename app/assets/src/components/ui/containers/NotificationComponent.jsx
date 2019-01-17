01a51324a6dd01576b42654cf96a5bc2ba74cc60
Tiago Carvalho <tcarvalho@chanzuckerberg.com>
2019-01-17 10:55:52 -0800

    Install toaster and add generalized notification component

--- app/assets/src/components/ui/containers/NotificationComponent.jsx
+++ app/assets/src/components/ui/containers/NotificationComponent.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
+import AlertIcon from "~ui/icons/AlertIcon";
 import cs from "./notification_component.scss";
+import cx from "classnames";
 
-const NotificationComponent = ({ handleCloseToast }) => {
-  return (
-    <div className={cs.warningComponent}>
-      Warning Component
-      <div onClick={handleCloseToast}>Dismiss</div>
-    </div>
-  );
-};
+class NotificationComponent extends React.Component {
+  icons = {
+    error: <AlertIcon />,
+    warn: <AlertIcon />
+  };
+
+  render() {
+    return (
+      <div className={cx(cs.notification, cs[this.props.type])}>
+        <div className={cs.leftPane}>
+          {NotificationComponent.icons[this.props.type]}
+        </div>
+        <div className={cs.content}>This is a sample notification</div>
+        <div className={cs.rightPane} onClick={this.props.handleCloseToast}>
+          Dismiss
+        </div>
+      </div>
+    );
+  }
+}
 
 NotificationComponent.propTypes = {
-  handleCloseToast: PropTypes.func
+  handleCloseToast: PropTypes.func,
+  type: PropTypes.oneof(["success", "info", "warn", "error"])
 };
 
 export default NotificationComponent;
