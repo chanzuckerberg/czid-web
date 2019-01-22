dee7fa676abece8c72d6ef4f8ba2d5a664006dae
jshoe <github@jsheu.com>
2019-01-22 13:40:10 -0800

    Debug WIP

--- db/migrate/20190116005251_seed_metadata_fields.rb
+++ db/migrate/20190116005251_seed_metadata_fields.rb
@@ -135,138 +135,141 @@ class SeedMetadataFields < ActiveRecord::Migration[5.1]
     ##### HUMAN FIELDS #####
     ########################
 
-    to_create << MetadataField.new(
-      name: "participant_id",
-      display_name: "Participant ID",
-      description: "Unique identifier for the participant (e.g. for multiple samples from the same participant)",
-      base_type: Metadatum::STRING_TYPE,
-      is_core: 1,
-      is_default: 1,
-      group: "Host",
-      host_genomes: HostGenome.find_by(name: "Human")
-    )
+    human_genome = HostGenome.find_by(name: "Human")
+    if human_genome
+      to_create << MetadataField.new(
+        name: "participant_id",
+        display_name: "Participant ID",
+        description: "Unique identifier for the participant (e.g. for multiple samples from the same participant)",
+        base_type: Metadatum::STRING_TYPE,
+        is_core: 1,
+        is_default: 1,
+        group: "Host",
+        host_genomes: [human_genome]
+      )
 
-    to_create << MetadataField.new(
-      name: "age",
-      display_name: "Age",
-      description: "Age of the participant",
-      base_type: Metadatum::NUMBER_TYPE,
-      is_core: 1,
-      is_default: 1,
-      group: "Host",
-      host_genomes: HostGenome.find_by(name: "Human")
-    )
+      to_create << MetadataField.new(
+        name: "age",
+        display_name: "Age",
+        description: "Age of the participant",
+        base_type: Metadatum::NUMBER_TYPE,
+        is_core: 1,
+        is_default: 1,
+        group: "Host",
+        host_genomes: [human_genome]
+      )
 
-    to_create << MetadataField.new(
-      name: "race",
-      display_name: "Race/Ethnicity",
-      description: "Race/ethnicity of the participant",
-      base_type: Metadatum::STRING_TYPE,
-      options: %w[White Hispanic Black Asian Other],
-      is_core: 1,
-      is_default: 1,
-      group: "Host",
-      host_genomes: HostGenome.find_by(name: "Human")
-    )
+      to_create << MetadataField.new(
+        name: "race",
+        display_name: "Race/Ethnicity",
+        description: "Race/ethnicity of the participant",
+        base_type: Metadatum::STRING_TYPE,
+        options: %w[White Hispanic Black Asian Other],
+        is_core: 1,
+        is_default: 1,
+        group: "Host",
+        host_genomes: [human_genome]
+      )
 
-    to_create << MetadataField.new(
-      name: "primary_diagnosis",
-      display_name: "Primary Diagnosis",
-      description: "Diagnosed disease that resulted in hospital admission",
-      base_type: Metadatum::STRING_TYPE,
-      is_core: 1,
-      is_default: 1,
-      group: "Host",
-      host_genomes: HostGenome.find_by(name: "Human")
-    )
+      to_create << MetadataField.new(
+        name: "primary_diagnosis",
+        display_name: "Primary Diagnosis",
+        description: "Diagnosed disease that resulted in hospital admission",
+        base_type: Metadatum::STRING_TYPE,
+        is_core: 1,
+        is_default: 1,
+        group: "Host",
+        host_genomes: [human_genome]
+      )
 
-    to_create << MetadataField.new(
-      name: "admission_date",
-      display_name: "Admission Date",
-      description: "Date the patient was admitted to the facility",
-      base_type: Metadatum::DATE_TYPE,
-      is_core: 1,
-      group: "Host",
-      host_genomes: HostGenome.find_by(name: "Human")
-    )
+      to_create << MetadataField.new(
+        name: "admission_date",
+        display_name: "Admission Date",
+        description: "Date the patient was admitted to the facility",
+        base_type: Metadatum::DATE_TYPE,
+        is_core: 1,
+        group: "Host",
+        host_genomes: [human_genome]
+      )
 
-    to_create << MetadataField.new(
-      name: "admission_type",
-      display_name: "Admission Type",
-      description: "Type of admission to the facility (e.g. ICU)",
-      base_type: Metadatum::STRING_TYPE,
-      options: %w[ICU General],
-      is_core: 1,
-      group: "Host",
-      host_genomes: HostGenome.find_by(name: "Human")
-    )
+      to_create << MetadataField.new(
+        name: "admission_type",
+        display_name: "Admission Type",
+        description: "Type of admission to the facility (e.g. ICU)",
+        base_type: Metadatum::STRING_TYPE,
+        options: %w[ICU General],
+        is_core: 1,
+        group: "Host",
+        host_genomes: [human_genome]
+      )
 
-    to_create << MetadataField.new(
-      name: "discharge_date",
-      display_name: "Discharge Date",
-      description: "Date the patient was discharged or expired during the stay",
-      base_type: Metadatum::DATE_TYPE,
-      is_core: 1,
-      group: "Host",
-      host_genomes: HostGenome.find_by(name: "Human")
-    )
+      to_create << MetadataField.new(
+        name: "discharge_date",
+        display_name: "Discharge Date",
+        description: "Date the patient was discharged or expired during the stay",
+        base_type: Metadatum::DATE_TYPE,
+        is_core: 1,
+        group: "Host",
+        host_genomes: [human_genome]
+      )
 
-    to_create << MetadataField.new(
-      name: "discharge_type",
-      display_name: "Discharge Type",
-      description: "Type of discharge",
-      base_type: Metadatum::STRING_TYPE,
-      options: ["ICU", "Hospital", "30-Day Mortality", "Other"],
-      is_core: 1,
-      group: "Host",
-      host_genomes: HostGenome.find_by(name: "Human")
-    )
+      to_create << MetadataField.new(
+        name: "discharge_type",
+        display_name: "Discharge Type",
+        description: "Type of discharge",
+        base_type: Metadatum::STRING_TYPE,
+        options: ["ICU", "Hospital", "30-Day Mortality", "Other"],
+        is_core: 1,
+        group: "Host",
+        host_genomes: [human_genome]
+      )
 
-    to_create << MetadataField.new(
-      name: "antibiotic_administered",
-      display_name: "Antibiotic Administered",
-      description: "Information on antibiotics administered to the participant",
-      base_type: Metadatum::STRING_TYPE,
-      options: %w[Yes No],
-      is_core: 1,
-      is_default: 1,
-      group: "Host",
-      host_genomes: HostGenome.find_by(name: "Human")
-    )
+      to_create << MetadataField.new(
+        name: "antibiotic_administered",
+        display_name: "Antibiotic Administered",
+        description: "Information on antibiotics administered to the participant",
+        base_type: Metadatum::STRING_TYPE,
+        options: %w[Yes No],
+        is_core: 1,
+        is_default: 1,
+        group: "Host",
+        host_genomes: [human_genome]
+      )
 
-    to_create << MetadataField.new(
-      name: "immunocomp",
-      display_name: "Immunocompromised",
-      description: "Information on if the participant was immunocompromised",
-      base_type: Metadatum::STRING_TYPE,
-      options: %w[Yes No],
-      is_core: 1,
-      is_default: 1,
-      group: "Host",
-      host_genomes: HostGenome.find_by(name: "Human")
-    )
+      to_create << MetadataField.new(
+        name: "immunocomp",
+        display_name: "Immunocompromised",
+        description: "Information on if the participant was immunocompromised",
+        base_type: Metadatum::STRING_TYPE,
+        options: %w[Yes No],
+        is_core: 1,
+        is_default: 1,
+        group: "Host",
+        host_genomes: [human_genome]
+      )
 
-    to_create << MetadataField.new(
-      name: "comorbidity",
-      display_name: "Comorbidity",
-      description: "Information on other chronic diseases present (e.g. HIV, Diabetes, COPD, etc.)",
-      base_type: Metadatum::STRING_TYPE,
-      is_core: 1,
-      is_default: 1,
-      group: "Host",
-      host_genomes: HostGenome.find_by(name: "Human")
-    )
+      to_create << MetadataField.new(
+        name: "comorbidity",
+        display_name: "Comorbidity",
+        description: "Information on other chronic diseases present (e.g. HIV, Diabetes, COPD, etc.)",
+        base_type: Metadatum::STRING_TYPE,
+        is_core: 1,
+        is_default: 1,
+        group: "Host",
+        host_genomes: [human_genome]
+      )
 
-    to_create << MetadataField.new(
-      name: "detection_method",
-      display_name: "Detection Method",
-      description: "Detection method for the known organism",
-      base_type: Metadatum::STRING_TYPE,
-      is_core: 1,
-      is_default: 1,
-      group: "Host",
-      host_genomes: HostGenome.find_by(name: "Human")
-    )
+      to_create << MetadataField.new(
+        name: "detection_method",
+        display_name: "Detection Method",
+        description: "Detection method for the known organism",
+        base_type: Metadatum::STRING_TYPE,
+        is_core: 1,
+        is_default: 1,
+        group: "Host",
+        host_genomes: [human_genome]
+      )
+    end
 
     ######################
     ##### SEQUENCING #####
