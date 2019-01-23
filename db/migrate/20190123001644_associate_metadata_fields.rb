486bf506fa94fd93e717f04c57be733e1a004531
jshoe <github@jsheu.com>
2019-01-23 11:04:02 -0800

    Add more fields

--- db/migrate/20190123001644_associate_metadata_fields.rb
+++ db/migrate/20190123001644_associate_metadata_fields.rb
@@ -1,4 +1,130 @@
 class AssociateMetadataFields < ActiveRecord::Migration[5.1]
-  def change
+  def up
+    # Make custom metadata fields for Mosquito projects
+    to_create = []
+
+    to_create << MetadataField.new(
+      name: "other_infections",
+      display_name: "Other Infections",
+      description: "Information about infections at other sites",
+      base_type: Metadatum::STRING_TYPE,
+      group: "Infection",
+      host_genomes: HostGenome.all
+    )
+
+    mosquito_genome = HostGenome.find_by(name: "Mosquito")
+    if mosquito_genome
+      to_create << MetadataField.new(
+        name: "unique_id",
+        display_name: "Unique ID",
+        description: "ID for the specimen (in case of multiple samples from the same specimen)",
+        base_type: Metadatum::STRING_TYPE,
+        group: "Sample",
+        host_genomes: [mosquito_genome]
+      )
+
+      to_create << MetadataField.new(
+        name: "collection_lat",
+        display_name: "Collection Latitude",
+        description: "Latitude of the original collection location",
+        base_type: Metadatum::NUMBER_TYPE,
+        group: "Sample",
+        host_genomes: [mosquito_genome]
+      )
+
+      to_create << MetadataField.new(
+        name: "collection_long",
+        display_name: "Collection Longitude",
+        description: "Longitude of the original collection location",
+        base_type: Metadatum::NUMBER_TYPE,
+        group: "Sample",
+        host_genomes: [mosquito_genome]
+      )
+
+      to_create << MetadataField.new(
+        name: "comp_id_genus",
+        display_name: "Computed Genus",
+        description: "Computationally-determined genus of the specimen",
+        base_type: Metadatum::STRING_TYPE,
+        group: "Host",
+        host_genomes: [mosquito_genome]
+      )
+
+      to_create << MetadataField.new(
+        name: "comp_id_species",
+        display_name: "Computed Species",
+        description: "Computationally-determined species of the specimen",
+        base_type: Metadatum::STRING_TYPE,
+        group: "Host",
+        host_genomes: [mosquito_genome]
+      )
+
+      to_create << MetadataField.new(
+        name: "reported_id_genus",
+        display_name: "Reported Genus",
+        description: "Reported/expected/human-identified genus of the specimen",
+        base_type: Metadatum::STRING_TYPE,
+        group: "Host",
+        host_genomes: [mosquito_genome]
+      )
+
+      to_create << MetadataField.new(
+        name: "reported_id_species",
+        display_name: "Reported Species",
+        description: "Reported/expected/human-identified species of the specimen",
+        base_type: Metadatum::STRING_TYPE,
+        group: "Host",
+        host_genomes: [mosquito_genome]
+      )
+
+      to_create << MetadataField.new(
+        name: "reported_sex",
+        display_name: "Reported Sex",
+        description: "Reported/expected/human-identified species of the specimen",
+        base_type: Metadatum::STRING_TYPE,
+        group: "Host",
+        host_genomes: [mosquito_genome]
+      )
+
+      to_create << MetadataField.new(
+        name: "extraction_batch",
+        display_name: "Extraction Batch",
+        description: "Label for the extracted batch",
+        base_type: Metadatum::STRING_TYPE,
+        group: "Sequencing",
+        host_genomes: [mosquito_genome]
+      )
+
+      to_create << MetadataField.new(
+        name: "library_prep_batch",
+        display_name: "Library Prep Batch",
+        description: "Number given to library preparation from a group of samples",
+        base_type: Metadatum::STRING_TYPE,
+        group: "Sequencing",
+        host_genomes: [mosquito_genome]
+      )
+    end
+
+    # Create the fields unless they already exist
+    to_create.each do |m|
+      unless MetadataField.find_by(name: m.name)
+        m.save
+      end
+    end
+
+    # Fill in metadata_field for existing entries with a name match
+    MetadataField.all.each do |field|
+      entries = Metadatum.where(key: field.name)
+      entries.update(metadata_field_id: field.id)
+    end
+
+    # Change 'gender' field to 'sex'
+    Metadatum.where(key: "gender").update(key: "sex", metadata_field_id: MetadataField.find_by(name: "sex").id)
+  end
+
+  def down
+    MetadataField.where(name: %w[other_infections unique_id collection_lat collection_long comp_id_genus comp_id_species reported_id_genus reported_id_species reported_sex extraction_batch library_prep_batch]).delete_all
+
+    Metadatum.where(key: "sex").update(key: "gender", metadata_field_id: nil)
   end
 end
