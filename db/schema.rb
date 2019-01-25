173a71de97e3a168f576f2a75a12e72a7e0399ea
jshoe <github@jsheu.com>
2019-01-25 11:13:31 -0800

    Reset schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_124_230_435) do
+ActiveRecord::Schema.define(version: 20_190_116_005_251) do
   create_table "alignment_configs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.string "name"
     t.string "index_dir_suffix"
