4f58d00e069d5282991386a86cc2d37c0405025f
jshoe <github@jsheu.com>
2019-01-24 16:31:57 -0800

    Tweak

--- db/migrate/20190124230435_metadata_move_data_type.rb
+++ db/migrate/20190124230435_metadata_move_data_type.rb
@@ -1,5 +1,6 @@
 class MetadataMoveDataType < ActiveRecord::Migration[5.1]
   def up
+    # Make data_type optional but don't blow it all away yet
     change_column :metadata, :data_type, :integer, null: true
   end
 
