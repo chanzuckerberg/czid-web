655246f651de23aebe66458c2067e0c64496dc46
jshoe <github@jsheu.com>
2019-02-07 09:58:56 -0800

    Fix

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -180,9 +180,6 @@ class SamplesController < ApplicationController
         format.json { render json: { samples: @samples, errors: @errors }, status: :unprocessable_entity }
       end
     end
-
-    tags = %W[sample_id:#{@sample.id} user_id:#{current_user.id}]
-    MetricUtil.put_metric_now("samples.showed", 1, tags)
   end
 
   # GET /samples/1/report_csv
@@ -323,6 +320,9 @@ class SamplesController < ApplicationController
       @report_details = report_details(@pipeline_run, current_user.id)
       @ercc_comparison = @pipeline_run.compare_ercc_counts
     end
+
+    tags = %W[sample_id:#{@sample.id} user_id:#{current_user.id}]
+    MetricUtil.put_metric_now("samples.showed", 1, tags)
   end
 
   def heatmap
