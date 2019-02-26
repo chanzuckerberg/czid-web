5e8b63bd4635e4819c14ce08714235d28274dc8a
jshoe <github@jsheu.com>
2019-02-26 13:26:47 -0800

    Tweak

--- config/routes.rb
+++ config/routes.rb
@@ -117,7 +117,7 @@ Rails.application.routes.draw do
   get 'health_check' => "health_check/health_check#index"
 
   # Un-shorten URLs. This should go second-to-last.
-  # get '/:id' => "shortener/shortened_urls#show"
+  get '/:id' => "shortener/shortened_urls#show"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#landing'
