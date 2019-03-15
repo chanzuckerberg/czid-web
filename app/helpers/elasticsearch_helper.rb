34ea77f5ea915682546fa6f9d242d72caf3d6dd7
Greg Dingle <gdingle@chanzuckerberg.com>
2019-03-15 13:31:52 -0700

    wip

--- app/helpers/elasticsearch_helper.rb
+++ app/helpers/elasticsearch_helper.rb
@@ -19,9 +19,11 @@ module ElasticsearchHelper
     return {} if Rails.env == "test"
     prefix = sanitize(prefix)
     matching_taxa = {}
+
     tax_levels.each do |level|
       # TODO: (gdingle): pass in project filter here
       search_params = { query: { query_string: { query: "#{prefix}*", fields: ["#{level}_name"] } } }
+      # TODO: (gdingle): query taxon counts and see how expensive?
       TaxonLineage.__elasticsearch__.search(search_params).records.each do |record|
         name = record["#{level}_name"]
         taxid = record["#{level}_taxid"]
@@ -33,10 +35,23 @@ module ElasticsearchHelper
         }
       end
     end
-    matching_taxa.values
+    filter_by_project(matching_taxa.values)
   end
 
   private
+
+  def filter_by_project(matching_taxa, project_id)
+    # taxon_counts.pipeline_run_id -> pipeline_runs->sample_id -> samples.project_id
+    project_id = 70 # medical detectives
+    tax_ids = TaxonCount
+              .joins(:pipeline_run)
+              .joins(:sample)
+              .joins(:project)
+              .where(project: project_id)
+              .pluck(:taxid)
+    tax_ids = Set[tax_ids]
+    matching_taxa.filter { |_taxa| tax_ids.includes?(taxid) }
+  end
 
   def sanitize(prefix)
     # Add \\ to escape special characters. Four \ to escape the backslashes.
