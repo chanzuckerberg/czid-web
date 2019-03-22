e6f716fb5e59613892b72785345706a6c282c9ac
Tiago Carvalho <tcarvalho@chanzuckerberg.com>
2019-03-22 09:25:49 -0700

    removing redundant function.

--- app/assets/src/components/views/projects/ProjectsView.jsx
+++ app/assets/src/components/views/projects/ProjectsView.jsx
@@ -43,10 +43,7 @@ class ProjectsView extends React.Component {
       {
         dataKey: "number_of_samples",
         width: 140,
-        label: "No. Of Samples",
-        sortFunction: numSamples => {
-          return numSamples;
-        }
+        label: "No. Of Samples"
       }
     ];
   }
