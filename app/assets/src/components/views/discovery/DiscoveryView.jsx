236e5f629d82898e12b61867bc858e0c8ecb50f0
jshoe <github@jsheu.com>
2019-03-26 15:34:36 -0700

    WIP

--- app/assets/src/components/views/discovery/DiscoveryView.jsx
+++ app/assets/src/components/views/discovery/DiscoveryView.jsx
@@ -448,6 +448,7 @@ class DiscoveryView extends React.Component {
                   samples={samples}
                   projects={projects}
                   currentTab={currentTab}
+                  loading={loadingSamples || loadingProjects}
                 />
               )}
           </div>
