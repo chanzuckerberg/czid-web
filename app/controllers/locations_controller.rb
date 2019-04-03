a6dba3e9254e2eef8e5a3863321d8d34f19a662a
jshoe <github@jsheu.com>
2019-04-03 13:38:41 -0700

    Tidy up

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,25 +1,24 @@
 class LocationsController < ApplicationController
   def external_search
-    query = location_params[:query]
-    render(json: []) && return if query.blank?
-
     results = []
-    resp = Location.geosearch(query)
-
-    if resp.is_a?(Net::HTTPSuccess)
-      candidates = JSON.parse(resp.body)
-      candidates.each do |c|
-        name_parts = c["display_name"].partition(", ")
-        results << {
-          title: name_parts[0],
-          description: name_parts[-1],
-          country: c["address"]["country"] || "",
-          state: c["address"]["state"] || "",
-          county: c["address"]["county"] || "",
-          city: c["address"]["city"] || "",
-          lat: c["lat"] || "",
-          lon: c["lon"] || ""
-        }
+    query = location_params[:query]
+    if query.present?
+      resp = Location.geosearch(query)
+      if resp.is_a?(Net::HTTPSuccess)
+        candidates = JSON.parse(resp.body)
+        candidates.each do |c|
+          name_parts = c["display_name"].partition(", ")
+          results << {
+            title: name_parts[0],
+            description: name_parts[-1],
+            country: c["address"]["country"] || "",
+            state: c["address"]["state"] || "",
+            county: c["address"]["county"] || "",
+            city: c["address"]["city"] || "",
+            lat: c["lat"] || "",
+            lon: c["lon"] || ""
+          }
+        end
       end
     end
     render json: results
