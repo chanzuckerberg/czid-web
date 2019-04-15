bfae68b4d2c5d7a504383cc2dcaa280a57a9146e
Jonathan Sheu <jshoe@users.noreply.github.com>
2019-04-15 11:40:21 -0700

    [User Emails] Fix error catch line in reset_password_instructions (#2147)
    * Bug fix
    
    * Fill commit

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -5,22 +5,20 @@ class CustomDeviseMailer < Devise::Mailer
 
   def reset_password_instructions(record, token, opts = {})
     assign_email_arguments record
+    Rails.logger.info("Going to send password reset email to: #{record.email}")
+
+    # Block for adding extra arguments
     if @email_arguments
-      begin
-        Rails.logger.info("Going to send password reset email to: #{record.email}")
-        opts[:template_name] = @email_arguments[:email_template] if @email_arguments[:email_template]
-        opts[:subject] = @email_arguments[:email_subject] if @email_arguments[:email_subject]
-        @sharing_user = User.find(@email_arguments[:sharing_user_id])
-        @shared_project_name = Project.find(@email_arguments[:shared_project_id]).name
-        @shared_project_id = @email_arguments[:shared_project_id]
-      rescue => exception
-        LogUtil.log_err_and_airbrake("reset_password_instructions with #{@email_arguments} failed")
-        LogUtil.log_backtrace(exception)
-      end
-    else
-      LogUtil.log_err_and_airbrake("Couldn't send reset_password_instructions to #{record.email} failed due to missing arguments")
+      opts[:template_name] = @email_arguments[:email_template] if @email_arguments[:email_template]
+      opts[:subject] = @email_arguments[:email_subject] if @email_arguments[:email_subject]
+      @sharing_user = User.find(@email_arguments[:sharing_user_id])
+      @shared_project_name = Project.find(@email_arguments[:shared_project_id]).name
+      @shared_project_id = @email_arguments[:shared_project_id]
     end
-    super
+    super # Calls DeviseMailer
+  rescue => exception
+    LogUtil.log_err_and_airbrake("Error sending reset_password_instructions to #{record.email} #{@email_arguments || ''}")
+    LogUtil.log_backtrace(exception)
   end
 
   private
