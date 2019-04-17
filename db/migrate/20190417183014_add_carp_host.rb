57df420017e5e27840dbfb0a562fad81724dec0e
Charles de Bourcy <cdebourcy@chanzuckerberg.com>
2019-04-17 12:45:19 -0700

    fix migration

--- db/migrate/20190417183014_add_carp_host.rb
+++ db/migrate/20190417183014_add_carp_host.rb
@@ -6,7 +6,10 @@ class AddCarpHost < ActiveRecord::Migration[5.1]
     hg.s3_bowtie2_index_path = "s3://idseq-database/host_filter/carp/2019-04-17/bowtie2_genome.tar"
 
     human_host = HostGenome.find_by(name: "Human")
-    hg.default_background_id = human_host ? human_host.default_background_id : Background.first.id
+    # For lack of a better default, use Human background.
+    # In the future, consider asking carp users to make a background model
+    # from their uninfected samples that we can substitute as the default.
+    hg.default_background_id = human_host.default_background_id if human_host
     hg.save
   end
 
