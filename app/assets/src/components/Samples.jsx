21b06a58f23148a23d2d862ebc998584709bd011
Greg Dingle <gdingle@chanzuckerberg.com>
2019-04-23 12:00:49 -0700

    Fix typo in analytics event name

--- app/assets/src/components/Samples.jsx
+++ app/assets/src/components/Samples.jsx
@@ -1685,7 +1685,7 @@ class BackgroundModal extends React.Component {
           <Form
             onSubmit={withAnalytics(
               this.handleSubmit,
-              "Samples_collection-form-submitted",
+              "Samples_collection-form_submitted",
               {
                 new_background_name: this.state.new_background_name,
                 new_background_description: this.state
