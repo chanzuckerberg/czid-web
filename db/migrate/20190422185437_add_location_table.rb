f580631d9b22ad7dd3017d4e68d6e952cd87de94
jshoe <github@jsheu.com>
2019-04-24 12:41:43 -0700

    Add more fields

--- db/migrate/20190422185437_add_location_table.rb
+++ db/migrate/20190422185437_add_location_table.rb
@@ -2,15 +2,18 @@ class AddLocationTable < ActiveRecord::Migration[5.1]
   def up
     create_table :locations, force: true do |t|
       t.string :name, default: "", null: false, comment: "Full display name, such as a complete address"
-      t.string :level_name, default: "", null: false, limit: 20, comment: "Level of specificity (country, state, subdivision, or city)"
+      t.string :geo_level, default: "", null: false, limit: 20, comment: "Level of specificity (country, state, subdivision, or city)"
       t.string :country_name, default: "", null: false, limit: 100, comment: "Country (or equivalent) of this location if available"
+      t.string :country_code, default: "", null: false, limit: 5, comment: "ISO 3166 alpha-2 country codes. Can be used to resolve country_name if data sources ever change."
       t.string :state_name, default: "", null: false, limit: 100, comment: "State (or equivalent) of this location if available"
       t.string :subdivision_name, default: "", null: false, limit: 100, comment: "Second-level administrative division (e.g. county/district/division/province/etc.) of this location if available"
       t.string :city_name, default: "", null: false, limit: 100, comment: "City (or equivalent) of this location if available"
+      t.integer :osm_id, comment: "OpenStreetMap ID for traceability. May change at any time."
+      t.integer :locationiq_id, comment: "Data provider API ID for traceability."
 
       # Recommended settings for lat/lon
       t.decimal :lat, precision: 10, scale: 6, comment: "The latitude of this location if available"
-      t.decimal :lon, precision: 10, scale: 6, comment: "The longitude of this location if available"
+      t.decimal :lng, precision: 10, scale: 6, comment: "The longitude of this location if available"
 
       t.timestamps
     end
