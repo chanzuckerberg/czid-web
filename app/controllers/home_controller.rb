a189769063564b7f69f03e44f68374aa44810f76
Tiago Carvalho <tcarvalho@chanzuckerberg.com>
2019-04-26 11:36:06 -0700

    Add route for old home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -32,14 +32,22 @@ class HomeController < ApplicationController
     if current_user.allowed_feature_list.include?("data_discovery")
       render 'my_data'
     else
-      @favorite_projects = current_user.favorites
-      @projects = current_power.projects
-      @editable_project_ids = current_power.updatable_projects.pluck(:id)
-      @host_genomes = HostGenome.all.reject { |hg| hg.name.downcase.include?("__test__") }
-      @user_is_admin = current_user.role == 1 ? 1 : 0
-      @background_models = current_power.backgrounds
-      render 'home'
+      home
     end
+  end
+
+  def home
+    @favorite_projects = current_user.favorites
+    @projects = current_power.projects
+    @editable_project_ids = current_power.updatable_projects.pluck(:id)
+    @host_genomes = HostGenome.all.reject { |hg| hg.name.downcase.include?("__test__") }
+    @user_is_admin = current_user.role == 1 ? 1 : 0
+    @background_models = current_power.backgrounds
+  end
+
+  def home_old
+    home
+    render 'home'
   end
 
   def taxon_descriptions
