51fc876cc43d3250878b0545c84c9b4c4e01ac35
jshoe <github@jsheu.com>
2019-05-08 13:41:20 -0700

    Try out bounds

--- app/assets/src/components/views/discovery/mapping/BaseMap.jsx
+++ app/assets/src/components/views/discovery/mapping/BaseMap.jsx
@@ -24,7 +24,16 @@ class BaseMap extends React.Component {
   }
 
   updateViewport = viewport => {
-    const { updateViewport } = this.props;
+    const { updateViewport, panBounds } = this.props;
+    console.log("Before: ", viewport.latitude, viewport.longitude);
+    viewport.zoom = Math.max(1.3, viewport.zoom);
+    viewport.latitude = Math.min(50, Math.max(-40, viewport.latitude));
+    viewport.longitude = Math.min(
+      panBounds.maxLongitude,
+      Math.max(panBounds.minLongitude, viewport.longitude)
+    );
+    console.log(viewport);
+    console.log(viewport.latitude, viewport.longitude);
     this.setState({ viewport });
     updateViewport && updateViewport(viewport);
   };
@@ -34,12 +43,17 @@ class BaseMap extends React.Component {
     const { viewport } = this.state;
 
     const styleURL = `https://api.maptiler.com/maps/${MAP_STYLE_ID}/style.json?key=${mapTilerKey}`;
+    const bounds = [
+      [-180, -70], // Southwest coordinates
+      [180, 75] // Northeast coordinates
+    ];
     return (
       <div className={cs.mapContainer}>
         <MapGL
           {...viewport}
           onViewportChange={this.updateViewport}
           mapStyle={styleURL}
+          // mapOptions={{ maxBounds: bounds }}
         >
           {tooltip}
           {markers}
@@ -64,18 +78,25 @@ BaseMap.propTypes = {
   latitude: PropTypes.number,
   longitude: PropTypes.number,
   zoom: PropTypes.number,
+  panBounds: PropTypes.objectOf(PropTypes.number),
   tooltip: PropTypes.node,
   markers: PropTypes.array,
   popups: PropTypes.array
 };
 
 BaseMap.defaultProps = {
-  width: 1000,
+  width: 1250,
   height: 1000,
   // United States framed
   latitude: 40,
   longitude: -98,
-  zoom: 3
+  zoom: 3,
+  panBounds: {
+    minLatitude: -70,
+    maxLatitude: 75,
+    minLongitude: -180,
+    maxLongitude: 180
+  }
 };
 
 export default BaseMap;
