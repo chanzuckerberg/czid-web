a0b33481c7fefb3456b98e5d9c64acb02cf3f2a8
Tiago Carvalho <tcarvalho@chanzuckerberg.com>
2019-05-20 16:25:54 -0700

    Comment on exception to trigger a new pipeline run

--- app/models/pipeline_run.rb
+++ app/models/pipeline_run.rb
@@ -186,6 +186,8 @@ class PipelineRun < ApplicationRecord
   JOB_TAG_KEEP_ALIVE_SECONDS = 600
   DRAINING_TAG = "draining".freeze
 
+  # Triggers a run for new samples by defining output states and run stages configurations.
+  # *Exception* for cloned pipeline runs that alredy have results and finalized status
   before_create :create_output_states, :create_run_stages, unless: :results_finalized?
 
   def parse_dag_vars
