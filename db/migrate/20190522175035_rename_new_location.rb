e9fa8806f2b2a4db1f8316dd7bd141aaa4c6fc71
jshoe <github@jsheu.com>
2019-05-22 11:20:48 -0700

    Update

--- db/migrate/20190522175035_rename_new_location.rb
+++ db/migrate/20190522175035_rename_new_location.rb
@@ -4,6 +4,7 @@ class RenameNewLocation < ActiveRecord::Migration[5.1]
     if existing
       existing.update(name: "collection_location_v2", display_name: "Collection Location v2")
     end
+    Metadatum.where(key: "new_collection_location").update_all(key: "collection_location_v2") # rubocop:disable SkipsModelValidations
   end
 
   def down
@@ -11,5 +12,6 @@ class RenameNewLocation < ActiveRecord::Migration[5.1]
     if existing
       existing.update(name: "new_collection_location", display_name: "New Collection Location")
     end
+    Metadatum.where(key: "collection_location_v2").update_all(key: "new_collection_location") # rubocop:disable SkipsModelValidations
   end
 end
