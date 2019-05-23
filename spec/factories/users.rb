acb93ae37680a5582f1f47aa664347542aefc37a
Tiago Carvalho <tcarvalho@chanzuckerberg.com>
2019-05-23 13:54:29 -0700

    Make emails unique.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,14 @@
 FactoryBot.define do
   factory :admin, class: User do
-    email { "admin@example.com" }
+    email { "admin@example.spec.com" }
     password { "admin_password" }
     name { "Admin User" }
+    role { 1 }
+  end
+
+  factory :joe, class: User do
+    email { "joe@gmail.spec.com" }
+    password { "joe_password" }
+    name { "Joe" }
   end
 end
