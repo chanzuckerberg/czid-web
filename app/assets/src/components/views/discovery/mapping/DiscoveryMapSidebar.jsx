36642d1802eb58bce4659e6fa9631e450e8f7ea3
jshoe <github@jsheu.com>
2019-05-30 16:05:52 -0700

    Add CSS

--- app/assets/src/components/views/discovery/mapping/DiscoveryMapSidebar.jsx
+++ app/assets/src/components/views/discovery/mapping/DiscoveryMapSidebar.jsx
@@ -3,11 +3,13 @@ import cx from "classnames";
 
 import PropTypes from "~/components/utils/propTypes";
 
+import cs from "./discovery_map_sidebar.scss";
+
 export default class DiscoveryMapSidebar extends React.Component {
   render() {
     const { className } = this.props;
 
-    return <div className={cx(className)} />;
+    return <div className={cx(className, cs.sidebar)} />;
   }
 }
 
