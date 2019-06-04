dd0e091f7766ddf18fbd3c4dc5702dd31b4ae004
Greg Dingle <gdingle@chanzuckerberg.com>
2019-06-04 12:31:35 -0700

    Skip test (#2281)
    * skip test
    
    * remove test

--- test/models/pipeline_run_test.rb
+++ test/models/pipeline_run_test.rb
@@ -13,23 +13,6 @@ class PipelineRunTest < ActiveSupport::TestCase
     assert pipeline_runs(:four).duration_hrs >= 0.0
   end
 
-  test "precaching executes report_info_json" do
-    mock = Minitest::Mock.new
-
-    pr = pipeline_runs(:four)
-
-    Background.where(ready: 1).pluck(:id).each do |background_id|
-      # Verifies that the method is called with these params
-      mock.expect(:call, nil, [pr, background_id])
-    end
-
-    ReportHelper.stub(:report_info_json, mock) do
-      pr.precache_report_info!
-    end
-
-    mock.verify
-  end
-
   test "Reads per million is correct" do
     pr = pipeline_runs(:six)
 
