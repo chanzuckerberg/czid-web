c94e1db45dd7b2e9262d9fc2e0e965024d191177
Charles de Bourcy <cdebourcy@chanzuckerberg.com>
2019-06-05 12:57:41 -0700

    fix read count in alert

--- app/models/pipeline_run.rb
+++ app/models/pipeline_run.rb
@@ -844,9 +844,12 @@ class PipelineRun < ApplicationRecord
         self.job_status = STATUS_FAILED
         self.finalized = 1
         self.known_user_error, self.error_message = check_for_user_error(prs)
-        log_message = "SampleFailedEvent: Sample #{sample.id} by #{sample.user.admin? ? 'admin user' : 'non-admin user'} " \
-          "failed #{prs.name} with #{adjusted_remaining_reads || 0} reads remaining after #{duration_hrs} hours. " \
-          "See: #{status_url}"
+        log_message = "SampleFailedEvent: Sample #{sample.id} " \
+          "by #{sample.user.admin? ? 'admin user' : 'non-admin user'} " \
+          "failed #{prs.name} " +
+                      (adjusted_remaining_reads ? "with #{adjusted_remaining_reads} reads remaining " : "") +
+                      "after #{duration_hrs} hours. " \
+                      "See: #{status_url}"
         LogUtil.log_err_and_airbrake(log_message) unless known_user_error
       elsif !prs.started?
         # we're moving on to a new stage
