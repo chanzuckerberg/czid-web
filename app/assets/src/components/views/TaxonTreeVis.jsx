413a120861f81ee2d52c6e36842c61b8935a9515
Greg Dingle <gdingle@chanzuckerberg.com>
2019-06-11 14:22:42 -0700

    move css to file

--- app/assets/src/components/views/TaxonTreeVis.jsx
+++ app/assets/src/components/views/TaxonTreeVis.jsx
@@ -352,7 +352,6 @@ class TaxonTreeVis extends React.Component {
           ref={container => {
             this.treeContainer = container;
           }}
-          style={{ overflowX: "scroll" }}
         >
           <div className="pathogen-labels">{this.renderPathogenLabels()}</div>
         </div>
