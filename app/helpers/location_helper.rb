f537d0c14de2befad38a16f8e8629c038ee8e6c3
jshoe <github@jsheu.com>
2019-06-11 13:02:42 -0700

    Tweak

--- app/helpers/location_helper.rb
+++ app/helpers/location_helper.rb
@@ -1,23 +1,23 @@
 module LocationHelper
   # Adapter function to munge responses from Location IQ API to our format
   def self.adapt_location_iq_response(body)
-    addr = body["address"]
+    address = body["address"]
     geo_level = ["city", "county", "state", "country"].each do |n|
-      break n if addr[n]
+      break n if address[n]
     end || ""
     {
       name: body["display_name"],
       geo_level: geo_level,
-      country_name: addr["country"] || "",
-      state_name: addr["state"] || "",
-      subdivision_name: addr["county"] || "",
+      country_name: address["country"] || "",
+      state_name: address["state"] || "",
+      subdivision_name: address["county"] || "",
       # Normalize extra provider fields to city. normalizecity param doesn't work all the time.
-      city_name: addr[%w[city city_distrct locality town borough municipality village hamlet quarter neighbourhood state_district].find { |k| addr.key?(k) }] || "",
+      city_name: address[%w[city city_distrct locality town borough municipality village hamlet quarter neighbourhood state_district].find { |k| address.key?(k) }] || "",
       # Round coordinates to enhance privacy
       lat: body["lat"] ? body["lat"].to_f.round(2) : nil,
       # LocationIQ uses 'lon'
       lng: body["lon"] ? body["lon"].to_f.round(2) : nil,
-      country_code: addr["country_code"] || "",
+      country_code: address["country_code"] || "",
       osm_id: body["osm_id"].to_i,
       osm_type: body["osm_type"],
       locationiq_id: body["place_id"].to_i
