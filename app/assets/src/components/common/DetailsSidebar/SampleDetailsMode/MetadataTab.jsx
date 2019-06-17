a45c340130c732908d53f67748018ba058fcd5f8
jshoe <github@jsheu.com>
2019-06-17 16:48:48 -0700

    Tweak

--- app/assets/src/components/common/DetailsSidebar/SampleDetailsMode/MetadataTab.jsx
+++ app/assets/src/components/common/DetailsSidebar/SampleDetailsMode/MetadataTab.jsx
@@ -184,17 +184,20 @@ class MetadataTab extends React.Component {
         {validKeys.map(key => (
           <RequestContext.Consumer>
             {requestContext => {
-              if (requestContext) {
-                const { allowedFeatures } = requestContext;
-                // TODO(jsheu): Migrate all to location_v2 after release
-                if (
-                  key === "collection_location_v2" &&
-                  allowedFeatures &&
-                  !allowedFeatures.includes("maps")
-                ) {
-                  return null;
-                }
+              // Don't show collection_location_v2 unless you have maps in requestContext
+              // allowedFeatures.
+              // TODO(jsheu): Migrate all to location_v2 after release
+              if (
+                key === "collection_location_v2" &&
+                !(
+                  requestContext &&
+                  requestContext.allowedFeatures &&
+                  requestContext.allowedFeatures.includes("maps")
+                )
+              ) {
+                return null;
               }
+
               return (
                 <div className={cs.field} key={metadataTypes[key].key}>
                   <div className={cs.label}>{metadataTypes[key].name}</div>
