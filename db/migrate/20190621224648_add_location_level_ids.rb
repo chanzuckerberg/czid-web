e06094875e35242d1a17ae0401825554fe887e74
jshoe <github@jsheu.com>
2019-06-21 15:48:55 -0700

    Tweak

--- db/migrate/20190621224648_add_location_level_ids.rb
+++ db/migrate/20190621224648_add_location_level_ids.rb
@@ -1,4 +1,15 @@
 class AddLocationLevelIds < ActiveRecord::Migration[5.1]
-  def change
+  def up
+    add_column :locations, :country_id, :integer
+    add_column :locations, :state_id, :integer
+    add_column :locations, :subdivision_id, :integer
+    add_column :locations, :city_id, :integer
+  end
+
+  def down
+    remove_column :locations, :country_id
+    remove_column :locations, :state_id
+    remove_column :locations, :subdivision_id
+    remove_column :locations, :city_id
   end
 end
