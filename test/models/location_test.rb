3ed61c7ad2ced6bcacbdc1193f109a184383a13c
jshoe <github@jsheu.com>
2019-06-25 11:55:52 -0700

    Fix existing tests

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -130,10 +130,12 @@ class LocationTest < ActiveSupport::TestCase
     api_response = [true, LocationTestHelper::API_GEOSEARCH_SF_COUNTY_RESPONSE]
     mock = MiniTest::Mock.new
     mock.expect(:call, api_response, [bad_location.country_name, bad_location.state_name, bad_location.subdivision_name])
-    Location.stub :geosearch_by_levels, mock do
-      Location.stub :new_from_params, locations(:sf_county) do
-        new_location = Location.check_and_restrict_specificity(bad_location, "Human")
-        assert_equal locations(:sf_county), new_location
+    Location.stub :find_by, nil do
+      Location.stub :geosearch_by_levels, mock do
+        Location.stub :new_from_params, locations(:sf_county) do
+          new_location = Location.check_and_restrict_specificity(bad_location, "Human")
+          assert_equal locations(:sf_county), new_location
+        end
       end
     end
     assert mock.verify
