736c798b1af9545ce4e4f78e7ab30f3f3d2376e0
Mark Zhang <mark.zhang@chanzuckerberg.com>
2019-06-24 17:09:39 -0700

    Add front-end for Basespace integration.
    Guarded with admin-only.
    Add "Upload with Basespace" option to Upload Sample options.
    Add pop-up window for Basespace OAuth integration.
    Add alternate components to render Basespace sample metadata in upload flow.
    Add projects and samples_for_project endpoints that proxy to Basespace API.

---
+++ app/assets/src/api/basespace.js
@@ -0,0 +1,19 @@
+import { get } from "./core";
+
+export const getBasespaceProjects = accessToken =>
+  get("/basespace/projects", {
+    params: {
+      access_token: accessToken,
+    },
+  });
+
+export const getSamplesForBasespaceProject = (
+  accessToken,
+  basespaceProjectId
+) =>
+  get("/basespace/samples_for_project", {
+    params: {
+      access_token: accessToken,
+      basespace_project_id: basespaceProjectId,
+    },
+  });
