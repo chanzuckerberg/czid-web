5a32f820dc830391886ba2560ccab630128f7f04
Lucia Reynoso <lvreynoso@protonmail.com>
2019-07-09 00:01:03 -0700

    Remove unnecessary async from amr.js

--- app/assets/src/api/amr.js
+++ app/assets/src/api/amr.js
@@ -1,16 +1,16 @@
 import { get } from "./core";
 
-export const getAMRCounts = async sampleIds =>
+export const getAMRCounts = sampleIds =>
   get("amr_heatmap/amr_counts.json", {
     params: {
       sampleIds,
     },
   });
 
-export const getCARDIndex = async accession =>
+export const getCARDIndex = accession =>
   get("amr_heatmap/fetch_card_index.json");
 
-export const getAroEntry = async accession =>
+export const getAroEntry = accession =>
   get("amr_heatmap/fetch_aro_entry.json", {
     params: {
       accession,
