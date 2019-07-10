f04e003b3022a71c59ebef337f13dcfc2f7af3a3
Jonathan Sheu <jshoe@users.noreply.github.com>
2019-07-10 16:45:23 -0700

    [MetadataInput] Add a warning when city-level info is stripped from sensitive locations (#2388)
    * WIP
    
    * Time to clean up
    
    * Tweak
    
    * WIP
    
    * Tweak
    
    * Tweak

--- app/assets/src/components/common/MetadataInput.jsx
+++ app/assets/src/components/common/MetadataInput.jsx
@@ -1,12 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { isArray } from "lodash";
+import { compact, get, isArray } from "lodash/fp";
 
 import Input from "~/components/ui/controls/Input";
 import Dropdown from "~/components/ui/controls/dropdowns/Dropdown";
 import GeoSearchInputBox from "../ui/controls/GeoSearchInputBox";
 
+import cs from "./metadata_input.scss";
+
+export const LOCATION_WARNING = "Set to county/district for privacy.";
+
 class MetadataInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      locationWarning: "",
+    };
+  }
+
+  // For human samples, drop the city part of the name and show a warning.
+  // Note that the backend will redo the geosearch for confirmation, so don't
+  // modify geo_level here.
+  // TODO(jsheu): Consider moving the warnings to the backend and generalizing.
+  processLocationSelection = result => {
+    const { isHuman } = this.props;
+
+    let warning = "";
+    if (isHuman && get("geo_level", result) === "city") {
+      result.name = compact([
+        result.subdivision_name,
+        result.state_name,
+        result.country_name,
+      ]).join(", ");
+      warning = LOCATION_WARNING;
+    }
+    this.setState({ locationWarning: warning });
+    return result;
+  };
+
   render() {
     const {
       value,
@@ -16,6 +48,7 @@ class MetadataInput extends React.Component {
       className,
       isHuman,
     } = this.props;
+    const { locationWarning } = this.state;
 
     if (isArray(metadataType.options)) {
       const options = metadataType.options.map(option => ({
@@ -47,14 +80,20 @@ class MetadataInput extends React.Component {
       );
     } else if (metadataType.dataType === "location") {
       return (
-        <GeoSearchInputBox
-          className={className}
-          // Calls save on selection
-          onResultSelect={({ result }) =>
-            onChange(metadataType.key, result, true)
-          }
-          value={value}
-        />
+        <React.Fragment>
+          <GeoSearchInputBox
+            className={className}
+            // Calls save on selection
+            onResultSelect={({ result }) => {
+              result = this.processLocationSelection(result);
+              onChange(metadataType.key, result, true);
+            }}
+            value={value}
+          />
+          {locationWarning && (
+            <span className={cs.warning}>{locationWarning}</span>
+          )}
+        </React.Fragment>
       );
     } else {
       return (
