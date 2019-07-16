062ea890d9e624ad1c2e9a3c05e4b6fbc4a5c028
jshoe <github@jsheu.com>
2019-07-16 13:13:21 -0700

    Move the file

--- spec/controllers/pipeline_viz_controller_spec.rb
+++ spec/controllers/pipeline_viz_controller_spec.rb
@@ -154,21 +154,5 @@ RSpec.describe PipelineVizController, type: :controller do
         expect(response).to have_http_status 401
       end
     end
-
-    describe "GET raw_results_folder (nonadmin)" do
-      it "can see raw results on the user's own samples" do
-        project = create(:project, users: [@joe])
-        sample = create(:sample, project: project)
-        get :raw_results_folder, params: { id: sample.id }
-        expect(response).to have_http_status :unauthorized
-      end
-
-      it "cannot see raw results on another user's sample" do
-        project = create(:project, users: [@joe])
-        sample = create(:sample, project: project, user: @joe)
-        get :raw_results_folder, params: { id: sample.id }
-        expect(response).to have_http_status :success
-      end
-    end
   end
 end
