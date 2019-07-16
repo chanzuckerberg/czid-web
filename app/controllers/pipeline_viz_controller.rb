039ecb319bddecb74fbce172657f41567bf20e47
Emily Zhong <ezhong@chanzuckerberg.com>
2019-07-16 10:29:05 -0700

    Formats code

--- app/controllers/pipeline_viz_controller.rb
+++ app/controllers/pipeline_viz_controller.rb
@@ -7,7 +7,9 @@ class PipelineVizController < ApplicationController
     feature_allowed = current_user.allowed_feature_list.include?("pipeline_viz")
     sample = current_power.samples.find_by(id: params[:sample_id])
     pipeline_version = params[:pipeline_version]
-    pipeline_run = sample && (pipeline_version ? sample.pipeline_runs.where(pipeline_version: pipeline_version)[0] : sample.first_pipeline_run)
+    pipeline_run = sample && (
+      pipeline_version ? sample.pipeline_runs.where(pipeline_version: pipeline_version)[0] : sample.first_pipeline_run
+    )
 
     if feature_allowed && pipeline_run
       stage_info = {}
