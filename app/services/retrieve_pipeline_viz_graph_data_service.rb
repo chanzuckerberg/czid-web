7405bab686f900d37d4360fb99d86b4305b97e6a
Emily Zhong <ezhong@chanzuckerberg.com>
2019-07-17 17:34:22 -0700

    Move comment, if statement

--- app/services/retrieve_pipeline_viz_graph_data_service.rb
+++ app/services/retrieve_pipeline_viz_graph_data_service.rb
@@ -1,6 +1,24 @@
 class RetrievePipelineVizGraphDataService
   include Callable
 
+  # Structures dag_json of each stage of the pipeline run into the following in @results for drawing
+  # the pipeline visualization graphs on the React side:
+  # stages: An array of stages, each stage being an object with the following fields:
+  #     - jobStatus: The job status of the stage
+  #     - steps: Array of objects, each one defining a node in that stage's graph. This object
+  #       is composed of:
+  #           - stepName: The name to be displayed for this step
+  #               - inputEdges: An array of indices that map to edges in the @edges array (see below), each
+  #                 edge being an input edge to the node.
+  #               - ouputEdges: An array of indices that map to edges in the @edges array, each edge being an
+  #                 output edge from the node
+  #
+  # edges: An array of edges, each edge object having the following structure:
+  #     - from: An object containing a stageIndex and stepIndex, denoting the originating node it is from
+  #     - to: An object containing a stageIndex and stepIndex, denoating the node it ends after
+  #     - files: An array of files that get passed between the from and to nodes. Currently each file is a string,
+  #       but it may become an object containing download url and potentially other information.
+
   def initialize(pipeline_run_id, is_admin)
     @pipeline_run = PipelineRun.find(pipeline_run_id)
     @all_dag_jsons = []
