02a93648b63f04a66bfdf7f3605672e81fd5136f
Greg Dingle <gdingle@chanzuckerberg.com>
2019-07-19 13:21:23 -0700

    add many to many constraints, remove on delete

--- config/initializers/immigrant.rb
+++ config/initializers/immigrant.rb
@@ -10,21 +10,5 @@ Immigrant.ignore_keys = [
   { from_table: 'locations', column: 'city_id' },
   { from_table: 'locations', column: 'country_id' },
   { from_table: 'locations', column: 'state_id' },
-  { from_table: 'locations', column: 'subdivision_id' },
-
-  # Many to many
-  { from_table: 'backgrounds_pipeline_runs', column: 'background_id' },
-  { from_table: 'backgrounds_pipeline_runs', column: 'pipeline_run_id' },
-  { from_table: 'backgrounds_samples', column: 'background_id' },
-  { from_table: 'backgrounds_samples', column: 'sample_id' },
-  { from_table: 'host_genomes_metadata_fields', column: 'host_genome_id' },
-  { from_table: 'host_genomes_metadata_fields', column: 'metadata_field_id' },
-  { from_table: 'metadata_fields_projects', column: 'metadata_field_id' },
-  { from_table: 'metadata_fields_projects', column: 'project_id' },
-  { from_table: 'phylo_trees_pipeline_runs', column: 'phylo_tree_id' },
-  { from_table: 'phylo_trees_pipeline_runs', column: 'pipeline_run_id' },
-  { from_table: 'projects_users', column: 'project_id' },
-  { from_table: 'projects_users', column: 'user_id' },
-  { from_table: 'samples_visualizations', column: 'sample_id' },
-  { from_table: 'samples_visualizations', column: 'visualization_id' }
+  { from_table: 'locations', column: 'subdivision_id' }
 ]
