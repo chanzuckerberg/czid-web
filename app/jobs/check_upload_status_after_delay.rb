c28cbb1bbc56749f3a0e6ee07196187ec79a2ceb
Greg Dingle <gdingle@chanzuckerberg.com>
2019-07-26 21:43:06 -0700

    input_files

--- app/jobs/check_upload_status_after_delay.rb
+++ app/jobs/check_upload_status_after_delay.rb
@@ -9,9 +9,10 @@ class CheckUploadStatusAfterDelay
     if sample.status == 'created' &&
        (Time.current - created_at).minutes > minutes
       duration_hrs = ((Time.current - sample.created_at) / 60 / 60).round(2)
+      input_files = sample.input_files ? sample.input_files.length : 0
       msg = "LongRunningUploadEvent: Sample #{sample.id} by #{sample.user.role_name} " \
         "was created #{duration_hrs} hours ago. " \
-        "#{sample.input_files || 0} input files have been uploaded. " \
+        "#{input_files} input files have been uploaded. " \
         "Last client ping was at #{sample.client_updated_at}. See: #{status_url}"
       LogUtil.log_err_and_airbrake(msg)
     end
