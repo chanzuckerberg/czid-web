7d770c9dbe059cfa07dd1b7dda77872ace7e85d9
jshoe <github@jsheu.com>
2019-08-16 17:35:44 -0700

    Tweak

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -55,7 +55,7 @@ class SamplesController < ApplicationController
   MAX_PAGE_SIZE_V2 = 100
   MAX_BINS = 34
   MIN_CLI_VERSION = '0.7.3'.freeze
-  CLI_DEPRECATION_MSG = "Outdated command line client. Please run `pip install --upgrade git+https://github.com/chanzuckerberg/idseq-cli.git` or with sudo + pip2/pip3 depending on your setup.".freeze
+  CLI_DEPRECATION_MSG = "Outdated command line client. Please run `pip install --upgrade git+https://github.com/chanzuckerberg/idseq-cli.git` or with sudo + pip2/pip3 depending on your setup to update and try again.".freeze
 
   # GET /samples
   # GET /samples.json
