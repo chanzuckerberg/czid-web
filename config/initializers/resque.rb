6dfcfd26edc68b333074f6a008c8c2329dcf5b64
Jonathan Sheu <jshoe@users.noreply.github.com>
2019-08-21 18:04:51 -0700

    IDSEQ-1136 - Improve exception pages (#2540)
    * Finish re-configuring
    
    * Tweak
    
    * Fill commit

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,5 @@
+require 'resque/server'
+
 # require 'airbrake/resque'
 
 # Resque::Failure.backend = Resque::Failure::Airbrake
@@ -5,3 +7,7 @@
 Resque.redis = Redis.new(url: REDISCLOUD_URL)
 
 Dir[Rails.root.join('app', 'jobs', '*.rb')].each { |file| require file }
+
+RESQUE_SERVER = Resque::Server.new
+# Hide verbose exception pages
+RESQUE_SERVER.settings.show_exceptions = false
