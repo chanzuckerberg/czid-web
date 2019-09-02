fb1eb978c6245d1be187e42eb3a0a0e4417d5203
David Rissato Cruz <davidrc@gmail.com>
2019-09-02 14:58:40 -0700

    Improve log util

--- app/lib/log_util.rb
+++ app/lib/log_util.rb
@@ -1,10 +1,16 @@
+# frozen_string_literal: true
+
 class LogUtil
-  def self.log_err_and_airbrake(msg)
-    Rails.logger.error(msg)
+  def self.log_err_and_airbrake(msg, exception = nil)
+    LogUtil.log_backtrace(exception, msg)
     Airbrake.notify(msg)
   end
 
-  def self.log_backtrace(exception)
-    Rails.logger.error("Backtrace:\n\t#{exception.backtrace.join("\n\t")}")
+  def self.log_backtrace(exception, msg = nil)
+    if exception.respond_to? :backtrace
+      Rails.logger.error("#{msg}. Exception: #{exception.try(:message)}. Backtrace:\n\t#{exception.try(:backtrace).try(:join, "\n\t")}")
+    else
+      Rails.logger.error("#{msg}. Exception:\n\t#{exception}")
+    end
   end
 end
