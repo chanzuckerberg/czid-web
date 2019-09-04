d091285d817735ec0ef6d02846340721470ba624
Julie Han <jhan@chanzuckerberg.com>
2019-09-04 10:36:12 -0700

    Make project descriptions editable in the DiscoverySidebar

--- app/assets/src/components/views/discovery/DiscoverySidebar/ProjectDescription.jsx
+++ app/assets/src/components/views/discovery/DiscoverySidebar/ProjectDescription.jsx
@@ -3,8 +3,10 @@ import cx from "classnames";
 import cs from "./project_description.scss";
 
 import PropTypes from "prop-types";
-import { Accordion } from "~/components/layout";
+import Textarea from "~ui/controls/Textarea";
+import MetadataSection from "~/components/common/DetailsSidebar/SampleDetailsMode/MetadataSection";
 
+import { saveProjectDescription } from "~/api";
 import { MAX_DESCRIPTION_LENGTH } from "~/components/views/projects/constants";
 
 class ProjectDescription extends React.Component {
@@ -12,16 +14,69 @@ class ProjectDescription extends React.Component {
     super(props);
 
     this.toggleDisplayDescription = this.toggleDisplayDescription.bind(this);
+    this.toggleEditing = this.toggleEditing.bind(this);
 
     this.state = {
       description: props.project.description,
+      lastValidDescription: props.project.description,
       showLess: true,
+      editing: false,
+      changed: false,
+      savePending: false,
     };
   }
 
   toggleDisplayDescription() {
     this.setState(prevState => ({ showLess: !prevState.showLess }));
   }
+
+  toggleEditing() {
+    this.setState(prevState => ({ editing: !prevState.editing }));
+  }
+
+  handleDescriptionChange = value => {
+    this.setState({
+      description: value,
+      changed: true,
+    });
+  };
+
+  handleDescriptionSave = async () => {
+    if (this.state.changed) {
+      this.setState(
+        {
+          changed: false,
+        },
+        () => {
+          this._save(this.props.project.id, this.state.description);
+        }
+      );
+    }
+  };
+
+  _save = async (projectId, value) => {
+    this.setState({
+      savePending: true,
+    });
+
+    let lastValidDescription = this.state.lastValidDescription;
+
+    const response = await saveProjectDescription(projectId, value);
+
+    // If save fails, revert to last valid description value.
+    if (response.status === 422) {
+      this.setState({ description: lastValidDescription });
+    } else {
+      this.setState({
+        description: value,
+        lastValidDescription: value,
+      });
+    }
+
+    this.setState({
+      savePending: false,
+    });
+  };
 
   render() {
     const { description, showLess } = this.state;
@@ -31,34 +86,56 @@ class ProjectDescription extends React.Component {
       : false;
 
     return (
-      <Accordion
-        className={cs.descriptionContainer}
-        bottomContentPadding
+      <MetadataSection
+        editable={this.props.project.editable}
+        onEditToggle={() => this.toggleEditing()}
+        editing={this.state.editing}
+        title="Description"
+        savePending={this.state.savePending}
+        alwaysShowEditLink={this.props.project.editable}
         open={true}
-        header={<div className={cs.title}>Description</div>}
+        toggleable={true}
       >
-        {description ? (
-          <div>
-            <div
-              className={cx(
-                shouldTruncateDescription && showLess && cs.truncated
-              )}
-            >
-              {description}
+        {this.state.editing ? (
+          <div className={cs.descriptionContainer}>
+            <Textarea
+              onChange={val => this.handleDescriptionChange(val)}
+              onBlur={() => this.handleDescriptionSave()}
+              value={description}
+              className={cs.textarea}
+              maxLength={MAX_DESCRIPTION_LENGTH}
+            />
+            <div className={cs.charCounter}>
+              {MAX_DESCRIPTION_LENGTH - description.length}/
+              {MAX_DESCRIPTION_LENGTH} characters remaining
             </div>
-            {shouldTruncateDescription && (
-              <div
-                className={cs.showHide}
-                onClick={this.toggleDisplayDescription}
-              >
-                {showLess ? "Show More" : "Show Less"}
-              </div>
-            )}
           </div>
         ) : (
-          <div>No description.</div>
+          <div className={cs.descriptionContainer}>
+            {description ? (
+              <div>
+                <div
+                  className={cx(
+                    shouldTruncateDescription && showLess && cs.truncated
+                  )}
+                >
+                  {description}
+                </div>
+                {shouldTruncateDescription && (
+                  <div
+                    className={cs.showHide}
+                    onClick={this.toggleDisplayDescription}
+                  >
+                    {showLess ? "Show More" : "Show Less"}
+                  </div>
+                )}
+              </div>
+            ) : (
+              <div>No description.</div>
+            )}
+          </div>
         )}
-      </Accordion>
+      </MetadataSection>
     );
   }
 }
