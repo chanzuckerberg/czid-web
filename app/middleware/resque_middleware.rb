706a4f6f8b100bc4cd17821c28f17779444bbecf
jshoe <github@jsheu.com>
2019-09-11 17:50:59 -0700

    Add to other envs

--- app/middleware/resque_middleware.rb
+++ app/middleware/resque_middleware.rb
@@ -9,7 +9,8 @@ class ResqueMiddleware
     if env['PATH_INFO'].start_with?("/resque")
       req = Rack::Request.new(env)
       req.params.each do |k, v|
-        req.update_param(k, v.gsub(/[^0-9A-Za-z_]/i, ''))
+        # Don't rely on other sanitize methods that may miss HTML encoding.
+        req.update_param(k, v.gsub(/[^0-9A-Za-z_]/i))
       end
     end
 
