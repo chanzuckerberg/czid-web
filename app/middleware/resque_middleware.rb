b53acbfad3bd9190d68010bff2e046134cc04089
jshoe <github@jsheu.com>
2019-09-16 18:00:41 -0700

    Got the form tags to work

--- app/middleware/resque_middleware.rb
+++ app/middleware/resque_middleware.rb
@@ -7,8 +7,6 @@ class ResqueMiddleware
     # Restrict allowed characters going to Resque server params.
     # update_param will modify the original 'env' object.
     if env['PATH_INFO'].start_with?("/resque")
-      puts "ENV 11:48am", env, "END"
-
       req = Rack::Request.new(env)
       req.params.each do |k, v|
         # Don't rely on other sanitize methods that may miss HTML encoding.
@@ -17,8 +15,31 @@ class ResqueMiddleware
     end
 
     status, headers, response = @app.call(env)
+    puts "RESP 5:07pm: ", response, "END"
 
-    puts "RESP 4:32pm: ", response, "END"
+    if env['PATH_INFO'].start_with?("/resque")
+      puts "INSIDE THE BLOCK 5:12pm"
+
+      puts "CLASS: ", response.class, "END"
+      puts "LEN: ", response.size
+
+      begin
+        doc = Nokogiri::HTML.parse(response[0])
+        puts "The doc is: ", doc, "END"
+        forms = doc.xpath("//form")
+        puts "FORMS 5:51pm", forms, "END"
+        puts "FORM COUNT", forms.size
+        puts "CLASS: ", forms.class
+        forms.each do |form|
+          form.add_child('<input type="hidden" name="csrf" value="yoyoyo"/>')
+        end
+
+        response = [doc.to_s]
+      rescue => err
+        puts "ERROR 5:40pm: "
+        puts err.message
+      end
+    end
 
     [status, headers, response]
   end
