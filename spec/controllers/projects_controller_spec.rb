baead34da7b45719c9bf36aa76c6311d303f63ea
Tiago Carvalho <tcarvalho@chanzuckerberg.com>
2019-09-19 10:26:31 -0700

    Add unit test

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -200,6 +200,25 @@ RSpec.describe ProjectsController, type: :controller do
         end
       end
 
+      describe "GET index for a specific page" do
+        it "sees projects from that page only" do
+          expected_projects = []
+          create(:project, name: "Project A", users: [@user])
+          create(:project, name: "Project B", users: [@user])
+          expected_projects << create(:project, name: "Project C", users: [@user])
+          expected_projects << create(:project, name: "Project D", users: [@user])
+          create(:project, name: "Project E", users: [@user])
+          create(:project, name: "Project F", users: [@user])
+          expected_projects.sort_by!(&:name).reverse!
+
+          get :index, params: { format: "json", limit: 2, offset: 2 }
+
+          json_response = JSON.parse(response.body)
+          expect(json_response.count).to eq(expected_projects.count)
+          expect(json_response.pluck("id")).to eq(expected_projects.pluck("id"))
+        end
+      end
+
       describe "GET index with ascending order" do
         it "sees projects sorted in ascending order" do
           expected_projects = []
