7182f97d902c3afc53cf616e75452ec6a2eb1cd3
jshoe <github@jsheu.com>
2019-09-18 17:17:50 -0700

    Clean up more

--- app/middleware/resque_middleware.rb
+++ app/middleware/resque_middleware.rb
@@ -6,54 +6,53 @@ class ResqueMiddleware
   end
 
   def call(env)
+    # Regular execution for non-Resque paths
+    unless env['PATH_INFO'].start_with?("/resque")
+      return @app.call(env)
+    end
+
+    rack_request = Rack::Request.new(env)
+    rack_session = rack_request.session
+
+    ### INPUT VERIFICATION
+
     # Restrict allowed characters going to Resque server params.
     # update_param will modify the original 'env' object.
-    is_resque = env['PATH_INFO'].start_with?("/resque")
+    rack_request.params.each do |k, v|
+      # Don't rely on other sanitize methods that may miss HTML encoding.
+      req.update_param(k, v.gsub(/[^0-9A-Za-z_]/, '')) unless k == "_csrf"
+    end
 
-    if is_resque
-      req = Rack::Request.new(env)
-      req.params.each do |k, v|
-        # Don't rely on other sanitize methods that may miss HTML encoding.
-        # req.update_param(k, v.gsub(/[^0-9A-Za-z_]/, ''))
+    # Verify every request except GET and HEAD
+    unless rack_request.get? || rack_request.head?
+      valid_token = false
 
-        # Replace this with only the vulnerable fields?
+      # Expect state-changing actions to use a form submit with a _csrf hidden field.
+      form_params = env["rack.request.form_hash"] || {}
+      if form_params.key?("_csrf")
+        valid_token = BASE_CONTROLLER.send(:valid_authenticity_token?, rack_session, form_params["_csrf"])
       end
 
-      post_params = env["rack.request.form_hash"] || {}
-      puts "POST PARAMS: ", post_params
-      if post_params.key?("_csrf")
-        value = post_params["_csrf"]
-        puts "We got: ", value
-        result = BASE_CONTROLLER.send(:valid_authenticity_token?, req.session, value)
-        puts "VALID? ", result
+      # Stop execution with invalid token
+      unless valid_token
+        raise ActionController::InvalidAuthenticityToken
       end
     end
 
-    puts "BEFORE: ", env['PATH_INFO']
+    ### OUTPUT DECORATION
 
+    # Get regular application response
     status, headers, response = @app.call(env)
 
-    puts "PATH INFO: ", env['PATH_INFO']
-    if is_resque
-      begin
-        puts "I am in begin block"
-        doc = Nokogiri::HTML.parse(response[0])
-        forms = doc.xpath("//form")
+    # Add a _csrf token to all Resque UI forms
+    doc = Nokogiri::HTML.parse(response[0])
+    form_nodes = doc.xpath("//form")
+    masked_token = BASE_CONTROLLER.send(:masked_authenticity_token, rack_session)
 
-        req = Rack::Request.new(env)
-        masked = BASE_CONTROLLER.send(:masked_authenticity_token, req.session)
-
-        forms.each do |form|
-          puts "I am prepending 12:34pm"
-          form.prepend_child("<input type=\"hidden\" name=\"_csrf\" value=\"#{masked}\"/>")
-        end
-
-        response = [doc.to_s]
-      rescue => err
-        puts "ERROR 5:40pm: "
-        puts err.message
-      end
+    form_nodes.each do |form|
+      form.prepend_child("<input type=\"hidden\" name=\"_csrf\" value=\"#{masked_token}\"/>")
     end
+    response = [doc.to_s]
 
     [status, headers, response]
   end
