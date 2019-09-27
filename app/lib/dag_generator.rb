600bbf1f4be18fbe9733fbb25e358c79bd213094
Jonathan Sheu <jshoe@users.noreply.github.com>
2019-09-26 21:26:55 -0700

    IDSEQ-1151 - Start switching dag templates from erb to jbuilder (#2604)
    IDSEQ-1151 - Start switching dag templates from erb to jbuilder### Description
    - JIRA: https://jira.czi.team/browse/IDSEQ-1151
    - We decided to switch the dag templates from ERB to Jbuilder, which is the default Rails tool for constructing JSON responses. This makes constructing the dags cleaner and less fault-prone.
    - This is the first PR to do setup work + convert the host_filter dag to start.
    
    ### Notes
    - The code will work with both .jbuilder and .erb now, although after rollout we can discard the old code.
    - For operational safety, I decided to try and flag it with `AppConfig::USE_JBUILDER_TEMPLATES`. After I add all the next templates, I'll start to switch it on to roll out.
    - To verify the template, you should read side-by-side with the original file: https://github.com/chanzuckerberg/idseq-web/blob/master/app/lib/dags/host_filter.json.erb
    
    ### Tests
    - Verified a variety of samples in local development and compared the outputs (ex: `Sample.find(i).pipeline_runs[0].pipeline_run_stages[0].host_filtering_command`). Outputs were identical with tools like jsondiff.com.
    - Submitted multiple local samples and had successful host filter runs: https://us-west-2.console.aws.amazon.com/cloudwatch/home?region=us-west-2#logEventViewer:group=/aws/batch/job;stream=aegea_batch_job_57d721c3/default/f463c0da-dace-4c99-a4f9-95998f8cadb9

--- app/lib/dag_generator.rb
+++ app/lib/dag_generator.rb
@@ -7,13 +7,32 @@ class DagGenerator
     @project_id = project_id
     @sample_id = sample_id
     @template = File.open(template_file, 'r').read
+    @template_file = template_file
     @host_genome = host_genome
+
+    # Add to attribute_dict values for Jbuilder templates for portability
+    attribute_dict[:host_genome] = host_genome
+    attribute_dict[:project_id] = project_id
+    attribute_dict[:sample_id] = sample_id
+
     @attribute_dict = attribute_dict
     @dag_vars_dict = dag_vars_dict
   end
 
+  # See our dag templates in app/lib/dags.
   def render
-    ERB.new(@template).result(binding)
+    # Interoperable between .jbuilder and .erb templates
+    if File.extname(@template_file) == ".jbuilder"
+      rendered = ActionController::Base.new.render_to_string(
+        file: @template_file,
+        locals: { attr: @attribute_dict }
+      )
+      # Couldn't find a more direct way to prettify from template
+      json_object = JSON.parse(rendered)
+      JSON.pretty_generate(json_object)
+    else
+      ERB.new(@template).result(binding)
+    end
   end
 
   def save(file)
--- app/models/app_config.rb
+++ app/models/app_config.rb
@@ -5,4 +5,6 @@ class AppConfig < ApplicationRecord
   SHOW_LANDING_VIDEO_BANNER = 'show_landing_video_banner'.freeze
   # JSON array containing the number of the stages allowed to auto restart. Ex: [1, 3]
   AUTO_RESTART_ALLOWED_STAGES = 'auto_restart_allowed_stages'.freeze
+  # When this is "1", dag generation will use jbuilder templates instead of erb templates.
+  USE_JBUILDER_TEMPLATES = 'use_jbuilder_templates'.freeze
 end
