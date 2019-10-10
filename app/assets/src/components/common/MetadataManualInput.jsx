a79761836b56f7c5f9c14523456304ba01cc7d83
jshoe <github@jsheu.com>
2019-10-09 17:42:56 -0700

    Tweak

--- app/assets/src/components/common/MetadataManualInput.jsx
+++ app/assets/src/components/common/MetadataManualInput.jsx
@@ -299,7 +299,7 @@ class MetadataManualInput extends React.Component {
               sample.name === this.state.applyToAllCell.sampleName &&
               cs.extraPadding,
             // Add extra width to location inputs for long names.
-            column.startsWith("collection_location_v2") && cs.extraWidth
+            column.startsWith("collection_location") && cs.extraWidth
           );
 
           const sampleHostGenomeId = this.getSampleHostGenomeId(sample);
