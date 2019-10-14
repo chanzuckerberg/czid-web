2d9d5f4ce46d6982b1e5e4396382f18f542c5b83
Greg Dingle <gdingle@chanzuckerberg.com>
2019-10-14 16:16:32 -0700

    ignore new tables in schema.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Ignore tables in database prefixed with "_" such as _new_taxid_lineages.
+ActiveRecord::SchemaDumper.ignore_tables = [/^_/]
