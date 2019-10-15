91d82c48ef6861eab80bf6af8803bb2a3027c296
jshoe <github@jsheu.com>
2019-10-15 13:30:56 -0700

    Start the test files

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -2,8 +2,20 @@ require 'rails_helper'
 
 RSpec.describe LocationsController, type: :controller do
   describe "#external_search_action" do
+    # Probably going to point to the other tests
   end
 
   describe "#handle_external_search_results" do
+    it "zips/interpolates autocomplete and geosearch results" do
+    end
+
+    it "filters by OSM search type" do
+    end
+
+    it "de-duplicates by name/geo_level, and osm_id" do
+    end
+
+    it "doesn't error on empty results" do
+    end
   end
 end
