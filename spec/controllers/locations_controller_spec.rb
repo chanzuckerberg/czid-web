7482b839d54e584991d0602637f56aca546dd40f
jshoe <github@jsheu.com>
2019-10-14 18:01:46 -0700

    Start adding tests

---
+++ spec/controllers/locations_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe LocationsController, type: :controller do
+  describe "#external_search_action" do
+  end
+
+  describe "#handle_external_search_results" do
+  end
+end
