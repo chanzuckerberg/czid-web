7cefc95e9d2513713fbdd93e01a8367fe0bc2da0
jshoe <github@jsheu.com>
2019-10-15 17:46:23 -0700

    Tweak

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe LocationsController, type: :controller do
   describe "#external_search_action" do
-    # See tests for external_search path in LocationsControllerTest.
+    # Extracted from LocationsController#external_search.
+    # See tests for external_search path in test/controllers/locations_controller_test.rb.
   end
 end
