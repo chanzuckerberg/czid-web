28a9a567f7a78fd0b528d8c2322d0d94c72dfa4a
Greg Dingle <gdingle@chanzuckerberg.com>
2019-10-23 14:19:45 -0700

    change col limit to be higher and consistent with alignment config

--- db/migrate/20191022201643_change_taxon_lineage_columns.rb
+++ db/migrate/20191022201643_change_taxon_lineage_columns.rb
@@ -1,6 +1,6 @@
 class ChangeTaxonLineageColumns < ActiveRecord::Migration[5.1]
   def change
-    change_column :taxon_lineages, :version_start, :integer, null: false, limit: 1, default: nil
-    change_column :taxon_lineages, :version_end, :integer, null: false, limit: 1, default: nil
+    change_column :taxon_lineages, :version_start, :integer, null: false, limit: 2, default: nil
+    change_column :taxon_lineages, :version_end, :integer, null: false, limit: 2, default: nil
   end
 end
