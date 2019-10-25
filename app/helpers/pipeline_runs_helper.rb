884db2a615d88ddcac02d0fc3f3216de6d0f5948
Andrey Kislyuk <kislyuk@gmail.com>
2019-10-25 16:36:26 -0700

    Fix rubocop issue

--- app/helpers/pipeline_runs_helper.rb
+++ app/helpers/pipeline_runs_helper.rb
@@ -67,11 +67,11 @@ module PipelineRunsHelper
     command = "aegea batch submit --command=#{Shellwords.escape(base_command)} "
     command += " --name=idseq-#{Rails.env}-#{sample_id}-#{stage_name} "
     command += " --ecr-image #{Shellwords.escape(docker_image)} --memory #{memory} --queue #{Shellwords.escape(job_queue)} --vcpus #{vcpus} --job-role idseq-pipeline "
-    if job_queue == Sample::DEFAULT_QUEUE_HIMEM
-      command += " --mount-instance-storage "
-    else
-      command += " --storage /mnt=#{Sample::DEFAULT_STORAGE_IN_GB} --volume-type gp2 "
-    end
+    command += if job_queue == Sample::DEFAULT_QUEUE_HIMEM
+                 " --mount-instance-storage "
+               else
+                 " --storage /mnt=#{Sample::DEFAULT_STORAGE_IN_GB} --volume-type gp2 "
+               end
     command
   end
 
