383eb76fbf69f60c21363a0a9f84925f20f6c870
jshoe <github@jsheu.com>
2019-11-13 13:34:30 -0800

    Moved to helper function

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require "http"
-require 'auth0'
-
 class Auth0Controller < ApplicationController
   skip_before_action :authenticate_user!, :verify_authenticity_token
 
