4ec358ae6b63eb7c64a969bdf81c8c159ac23a14
Greg Dingle <gregdingle@gmail.com>
2019-11-25 15:17:12 -0800

    Add CORS accept header for Cloudfront (#2773) (#2779)
    [release fix] Add CORS accept header for Cloudfront #2773# Description
    
    See #2773

---
+++ config/initializers/rack_cors.rb
@@ -0,0 +1,17 @@
+## Configure Rack CORS Middleware, so that CloudFront can serve our assets.
+## See https://github.com/cyu/rack-cors
+## And see https://stackoverflow.com/a/36585871
+if defined? Rack::Cors
+  Rails.configuration.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins [
+        # SERVER_DOMAIN should be set to the current env root web address
+        # such as https://staging.idseq.net/
+        ENV["SERVER_DOMAIN"],
+        *Rails.application.config.allowed_cors_origins
+        # All other domains should be added in env config files such as prod.rb.
+      ].compact
+      resource '/assets/*'
+    end
+  end
+end
