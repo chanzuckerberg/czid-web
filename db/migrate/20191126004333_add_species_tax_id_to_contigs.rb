df8e36ede69c3f32e0f62abd662f64bb1fe5f5d4
Mark Zhang <mark.zhang@chanzuckerberg.com>
2019-11-28 16:58:30 -0800

    Alter contig taxid migration to include indices and use bulk mode.

--- db/migrate/20191126004333_add_species_tax_id_to_contigs.rb
+++ db/migrate/20191126004333_add_species_tax_id_to_contigs.rb
@@ -2,9 +2,12 @@ class AddSpeciesTaxIdToContigs < ActiveRecord::Migration[5.1]
   def change
     # Do NOT add a default value.
     # Adding a default value can cause the migration to take a long time, because each row must be modified.
-    add_column :contigs, :species_taxid_nt, :integer
-    add_column :contigs, :species_taxid_nr, :integer
-    add_column :contigs, :genus_taxid_nt, :integer
-    add_column :contigs, :genus_taxid_nr, :integer
+    change_table :contigs, bulk: true do |t|
+      t.integer :species_taxid_nt, :species_taxid_nr, :genus_taxid_nt, :genus_taxid_nr
+      t.index ["pipeline_run_id", "species_taxid_nt"]
+      t.index ["pipeline_run_id", "species_taxid_nr"]
+      t.index ["pipeline_run_id", "genus_taxid_nt"]
+      t.index ["pipeline_run_id", "genus_taxid_nr"]
+    end
   end
 end
