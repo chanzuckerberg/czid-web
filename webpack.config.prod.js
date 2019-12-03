b34cc0422033b1883c5745865600276f8d745bd5
Greg Dingle <gdingle@chanzuckerberg.com>
2019-12-03 14:53:40 -0800

    Split out 3rd party JS and CSS

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,14 +6,22 @@ module.exports = merge(commonConfig, {
   devtool: "source-map",
   mode: "production",
   optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: "all",
+        },
+      },
+    },
     minimizer: [
       new UglifyJsPlugin({
         uglifyOptions: {
-          mangle: false
+          mangle: false,
         },
         parallel: true,
-        sourceMap: true
-      })
-    ]
-  }
+        sourceMap: true,
+      }),
+    ],
+  },
 });
