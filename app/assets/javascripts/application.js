1dfcf738ddff4171af04c3a55c1e0cc968e99ffc
Greg Dingle <gdingle@chanzuckerberg.com>
2019-12-04 10:52:07 -0800

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,5 +2,5 @@
 // should contain idseq-written JS and the second file should contain JS from
 // node_modules.
 
-//= require bundle.min
-//= require 1.bundle.min
+//= require main.bundle.min.js
+//= require vendors~main.bundle.min.js
