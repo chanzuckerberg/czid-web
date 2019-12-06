9499a1837763049e89e0cfc9c45b452d969e227a
Julie Han <53838890+j-x-han@users.noreply.github.com>
2019-12-06 13:47:19 -0800

    [Release Fix] Fix pipeline run status check (#2827) (#2828)
    [Release Fix] Fix pipeline run status check# Description
    
    The pipeline run status check was incorrect, resulting in an error rather than displaying an alert warning the user that their pipeline run had failed when attempting to view the report page.
    
    # Notes
    
    Related JIRA task for future cleanup of pipeline run statuses: https://jira.czi.team/browse/IDSEQ-1890
    
    # Tests
    
    * Verified that samples with failed pipeline runs now correctly display an alert rather than causing the page to error and crash.

--- app/services/pipeline_report_service.rb
+++ app/services/pipeline_report_service.rb
@@ -68,7 +68,7 @@ class PipelineReportService
 
   def generate
     pipeline_run, metadata = get_pipeline_status(@pipeline_run_id)
-    if @pipeline_run_id.nil? || !pipeline_run.completed?
+    unless pipeline_run_report_available?(pipeline_run)
       return JSON.dump(
         metadata: metadata
       )
@@ -213,6 +213,7 @@ class PipelineReportService
           errorMessage: nil,
           knownUserError: nil,
           jobStatus: "Waiting to Start or Receive Files",
+          pipelineRunReportAvailable: false,
         },
       ]
     end
@@ -232,8 +233,16 @@ class PipelineReportService
         errorMessage: pipeline_run.error_message,
         knownUserError: pipeline_run.known_user_error,
         jobStatus: pipeline_run.job_status_display,
+        pipelineRunReportAvailable: pipeline_run_report_available?(pipeline_run),
       },
     ]
+  end
+
+  def pipeline_run_report_available?(pipeline_run)
+    # This condition is carried over from the previous version of the report page (report_helper.rb).
+    # TODO: review this condition and clean up the implementation of pipeline_run statuses.
+    # JIRA: https://jira.czi.team/browse/IDSEQ-1890
+    return pipeline_run && (((pipeline_run.adjusted_remaining_reads.to_i > 0 || pipeline_run.finalized?) && !pipeline_run.failed?) || pipeline_run.report_ready?)
   end
 
   def fetch_taxon_counts(_pipeline_run_id, _background_id)
