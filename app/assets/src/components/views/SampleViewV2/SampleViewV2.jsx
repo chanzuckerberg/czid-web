fbf4e6af125140ccb4e9cfd80984ed7388c37ea9
Tiago Carvalho <37312125+tfrcarvalho@users.noreply.github.com>
2019-12-11 17:50:23 -0800

    Fix safari hidden table (#2851)
    [Report Page] Fix issue where table content was not showing in Safari# Description
    
    * Safari treats flex inheritance differently than chrome and other browsers
    * The table height was only the size of the header thus not showing the data
    * Fixed by explicitly setting the display flex to the container
    
    # Tests
    
    * Open any report table in safari and see that data contents are displayed.

--- app/assets/src/components/views/SampleViewV2/SampleViewV2.jsx
+++ app/assets/src/components/views/SampleViewV2/SampleViewV2.jsx
@@ -1000,7 +1000,7 @@ export default class SampleViewV2 extends React.Component {
               />
             </div>
           </div>
-          {view == "table" && (
+          {view === "table" && (
             <div className={cs.reportTable}>
               <ReportTable
                 alignVizAvailable={
