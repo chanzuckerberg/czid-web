d95aaf13e9186439d0ae21144e0f17cdfdca4619
Mark Zhang <mark.zhang@chanzuckerberg.com>
2019-12-11 16:28:19 -0800

    Remove extraneous string conversio.

--- app/models/project.rb
+++ app/models/project.rb
@@ -61,7 +61,7 @@ class Project < ApplicationRecord
   end
 
   def cleaned_project_name
-    name.downcase.split(' ').join('_').to_s
+    name.downcase.split(' ').join('_')
   end
 
   def host_gene_counts_from_params(params)
