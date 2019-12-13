515a34f78282526389d5fc8a8f05ded9ed2dd1ca
David Rissato Cruz <davidrc@gmail.com>
2019-12-13 11:12:22 -0800

    Fix an issue with sessions being lost during server restarts

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,3 +1,4 @@
+require 'digest/sha1'
 
 Rails.application.config.middleware.use Warden::Manager do |manager|
   manager.failure_app = proc { |_env| ['401', { 'Content-Type' => 'application/json' }, { error: 'Unauthorized', code: 401 }] }
@@ -7,7 +8,7 @@ Warden::Manager.serialize_into_session do |user|
   # convert to warden_session_obj
   serialized = user.slice("id", "role")
   # extra field to improve security
-  serialized["authentication_token_hash"] = user["authentication_token"].hash
+  serialized["authentication_token_hash"] = Digest::SHA1.hexdigest(user["authentication_token"])
   serialized
 end
 
@@ -15,7 +16,7 @@ Warden::Manager.serialize_from_session do |warden_session_obj|
   return nil unless warden_session_obj.instance_of?(Hash)
   id, role, authentication_token_hash = warden_session_obj.values_at("id", "role", "authentication_token_hash")
   user = User.find_by(id: id, role: role)
-  if user && authentication_token_hash == user.authentication_token.hash
+  if user && authentication_token_hash == Digest::SHA1.hexdigest(user.authentication_token)
     user
   end
 end
