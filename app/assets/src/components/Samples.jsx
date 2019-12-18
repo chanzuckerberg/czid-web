6c0f2038124b8e481fc01dac45d1672354b8e8a6
Greg Dingle <gdingle@chanzuckerberg.com>
2019-12-18 14:59:23 -0800

    old home page

--- app/assets/src/components/Samples.jsx
+++ app/assets/src/components/Samples.jsx
@@ -240,7 +240,11 @@ class Samples extends React.Component {
       this.handleProjectSelection(result.id);
     } else if (result.category == "Sample") {
       this.applySuggestFilter(result, "sampleIdsParams", "sample_ids");
-    } else if (result.category == "Tissue") {
+    } else if (
+      result.category == "Sample Type" ||
+      result.category == "Tissue"
+    ) {
+      // Tissue is legacy. Check both to be safe.
       this.applySuggestFilter(result, "selectedTissueFilters", "id");
     } else if (result.category == "Host") {
       this.applySuggestFilter(result, "selectedHostIndices", "id");
