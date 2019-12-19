9e9b55d29672060e3ae16f9d10ee325663d0a4c3
jshoe <github@jsheu.com>
2019-12-19 13:38:02 -0800

    Tweak

--- db/migrate/20191218220321_add_fish_genomes.rb
+++ db/migrate/20191218220321_add_fish_genomes.rb
@@ -11,7 +11,7 @@ class AddFishGenomes < ActiveRecord::Migration[5.1]
     unless HostGenome.find_by(name: "Barred Hamlet")
       hg = HostGenome.new(
         name: "Barred Hamlet",
-        s3_star_index_path: "s3://idseq-database/host_filter/barred_hamlet/2019-12-17/barred_hamlet_STAR_genome.tar	",
+        s3_star_index_path: "s3://idseq-database/host_filter/barred_hamlet/2019-12-17/barred_hamlet_STAR_genome.tar",
         s3_bowtie2_index_path: "s3://idseq-database/host_filter/barred_hamlet/2019-12-17/barred_hamlet_bowtie2_genome.tar",
         skip_deutero_filter: nil,
         default_background_id: human_background
@@ -33,8 +33,8 @@ class AddFishGenomes < ActiveRecord::Migration[5.1]
     unless HostGenome.find_by(name: "Tiger Tail Seahorse")
       hg = HostGenome.new(
         name: "Tiger Tail Seahorse",
-        s3_star_index_path: "s3://idseq-database/host_filter/tiger_tail_seahorse/2019-12-17/tiger_tail_seahorse_STAR_genome.tar	",
-        s3_bowtie2_index_path: "s3://idseq-database/host_filter/tiger_tail_seahorse/2019-12-17/tiger_tail_seahorse_bowtie2_genome.tar	",
+        s3_star_index_path: "s3://idseq-database/host_filter/tiger_tail_seahorse/2019-12-17/tiger_tail_seahorse_STAR_genome.tar",
+        s3_bowtie2_index_path: "s3://idseq-database/host_filter/tiger_tail_seahorse/2019-12-17/tiger_tail_seahorse_bowtie2_genome.tar",
         skip_deutero_filter: nil,
         default_background_id: human_background
       )
@@ -45,7 +45,7 @@ class AddFishGenomes < ActiveRecord::Migration[5.1]
       hg = HostGenome.new(
         name: "Torafugu",
         s3_star_index_path: "s3://idseq-database/host_filter/torafugu/2019-12-17/torafugu_STAR_genome.tar",
-        s3_bowtie2_index_path: "s3://idseq-database/host_filter/torafugu/2019-12-17/torafugu_bowtie2_genome.tar	",
+        s3_bowtie2_index_path: "s3://idseq-database/host_filter/torafugu/2019-12-17/torafugu_bowtie2_genome.tar",
         skip_deutero_filter: nil,
         default_background_id: human_background
       )
