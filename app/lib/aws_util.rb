e719cd235266d2ef3f1c4eaa85cb7a1102401e10
Mark Zhang <mark.zhang@chanzuckerberg.com>
2019-12-19 16:54:58 -0800

    Add comment for aws region variable.

--- app/lib/aws_util.rb
+++ app/lib/aws_util.rb
@@ -1,6 +1,9 @@
 class AwsUtil
+  # This is currently used to generate log urls for admins. We assume deployment in us-west-2 region.
+  AWS_REGION = "us-west-2".freeze
+
   def self.get_cloudwatch_url(log_group, log_stream)
-    "https://us-west-2.console.aws.amazon.com/cloudwatch/home?region=us-west-2" \
+    "https://#{AWS_REGION}.console.aws.amazon.com/cloudwatch/home?region=#{AWS_REGION}" \
       "#logEventViewer:group=#{log_group};stream=#{log_stream}"
   end
 end
