cc4ce2b8d27f2dbc2ac0b71e0dc6f9c5d3f29836
Greg Dingle <gdingle@chanzuckerberg.com>
2020-01-08 12:22:01 -0800

    style

--- app/models/sample_type.rb
+++ app/models/sample_type.rb
@@ -1,2 +1,23 @@
+# From https://docs.google.com/spreadsheets/d/1_hPkQe5LI0Zw_C0Ls4HVCEDsc_FNNVOaU_aAfoaiZRE/
+# Each sample type represents a canonical, mutually-exclusive type supported by IDseq.
 class SampleType < ApplicationRecord
+  validates :name, :group, presence: true
+  validates :name, uniqueness: true
+  validates :name, :group, format: {
+    with: /\A[A-Z][\w|\s]+/,
+    message: "allows only word or space chars and first char must be capitalized.",
+  }
+  # names should fit inside autocomplete popup
+  validates :name, :group, length: { in: 3..30 }
+  validates :group, inclusion: { in: [
+    # See CreateSampleTypes
+    "Systemic Inflammation",
+    "CNS Infections",
+    "Respiratory Infections",
+    "Reproductive",
+    "Excrement",
+    "Organs",
+    "Insect Body Parts",
+    "Other",
+  ], }
 end
