ec8572603683aa121e4a8d0fcdf048ccaf793d34
Greg Dingle <gdingle@chanzuckerberg.com>
2020-01-08 13:44:08 -0800

    add z anchor

--- app/models/sample_type.rb
+++ app/models/sample_type.rb
@@ -4,7 +4,7 @@ class SampleType < ApplicationRecord
   validates :name, :group, presence: true
   validates :name, uniqueness: true
   validates :name, :group, format: {
-    with: /\A[A-Z][\w|\s]+/,
+    with: /\A[A-Z][\w|\s]+\z/,
     message: "allows only word or space chars and first char must be capitalized.",
   }
   # names should fit inside autocomplete popup
