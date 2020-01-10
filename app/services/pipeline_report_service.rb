55c1971b46489c69c5cda13ee8d043db6820f678
Julie Han <jhan@chanzuckerberg.com>
2020-01-10 15:27:52 -0800

    Rename COMPLETE to SUCCEEDED and add comments

--- app/services/pipeline_report_service.rb
+++ app/services/pipeline_report_service.rb
@@ -253,11 +253,15 @@ class PipelineReportService
     # (either loaded or failed). This can be true if there were errors, so we still need to check pipeline_run.failed? as well.
     # pipeline_run.report_ready? indicates if taxon_counts output is loaded and available to use in report generation.
     # This is only true if no errors have occurred for taxon_counts.
+
+    # The pipeline is either still in progress or results monitor is waiting to load in outputs.
     pipeline_status = "WAITING"
+    # The pipeline has stopped running and encountered errors.
     if pipeline_run.failed?
       pipeline_status = "FAILED"
+    # The pipeline has finished running without critical errors and all outputs have been loaded.
     elsif pipeline_run.results_finalized?
-      pipeline_status = "COMPLETE"
+      pipeline_status = "SUCCEEDED"
     end
     return {
       pipelineRunStatus: pipeline_status,
