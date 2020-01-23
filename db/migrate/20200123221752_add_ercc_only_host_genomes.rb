51643f49c087a147d356090400b428c9a8ab48db
Greg Dingle <gdingle@chanzuckerberg.com>
2020-01-23 14:31:17 -0800

    Add list of ERCC only hosts;

---
+++ db/migrate/20200123221752_add_ercc_only_host_genomes.rb
@@ -0,0 +1,90 @@
+# This adds a bunch of hosts which users are known to have taken samples from.
+# They initially default to have ERCCs only subtracted.
+# See https://czi.quip.com/yPgpAPj1dtEC/Hosts-Dropdown-Options
+class AddErccOnlyHostGenomes < ActiveRecord::Migration[5.1]
+  HOST_GENOME_NAMES = [
+    'Badger',
+    'Bank Vole',
+    'Bat',
+    'Batfish',
+    'Bear',
+    'Bee',
+    'Carp',
+    'Cat',
+    'Camel',
+    'Chimpanzee',
+    'Cow',
+    'Dik Dik',
+    'Dog',
+    'Donkey',
+    'Chicken',
+    'Ferret',
+    'Field Vole',
+    'Fox',
+    'Giraffe',
+    'Goat',
+    'Gorilla',
+    'Guinea Pig',
+    'Harbor Seal',
+    'Hedgehog',
+    'Horse',
+    'Human',
+    'Kestrel',
+    'Leopard',
+    'Lion',
+    'Llama',
+    'Mallard',
+    'Milu',
+    'Monk Seal',
+    'Monkey',
+    'Mosquito',
+    'Mouse',
+    'Owl',
+    'Penguin',
+    'Pig',
+    'Porpoise',
+    'Prairie Dog',
+    'Raccoon',
+    'Rabbit',
+    'Rat',
+    'Sea Lion',
+    'Seagull',
+    'Seal',
+    'Seastar',
+    'Sloth',
+    'Snake',
+    'Stork',
+    'Taurine Cattle',
+    'Tick',
+    'Tiger',
+    'Turkey',
+    'Unknown',
+    'Water Buffalo',
+  ].freeze
+
+  def up
+    HOST_GENOME_NAMES.each { |name| add(name) }
+  end
+
+  def down
+    # This should never delete good data because of foreign keys
+    HOST_GENOME_NAMES.each do |name|
+      hg = HostGenome.find_by(name: name)
+      hg.destroy! if hg
+    end
+  end
+
+  private
+
+  # Adapted from app/lib/add_host_genome_migration_example.rb
+  def add(name)
+    return if HostGenome.find_by(name: name)
+
+    hg = HostGenome.new
+    hg.name = name
+
+    human_host = HostGenome.find_by(name: "Human")
+    hg.default_background_id = human_host.default_background_id if human_host
+    hg.save!
+  end
+end
