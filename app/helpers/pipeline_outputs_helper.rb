a73fd7aee82ae579d6922d2ce78a0290d7f3f390
Greg Dingle <gregdingle@gmail.com>
2020-01-27 16:07:50 -0800

    Show host subtracted in pipeline info sidebar (#2987)
    * host_subtracted
    
    * first tests
    
    * more test
    
    * fix test
    
    * fix test
    
    * handle empty json

--- app/helpers/pipeline_outputs_helper.rb
+++ app/helpers/pipeline_outputs_helper.rb
@@ -9,6 +9,15 @@ module PipelineOutputsHelper
       pipeline: pipeline_run.pipeline_version,
       alignment_db: pipeline_run.alignment_config.name,
     }
+    host_subtracted = pipeline_run.host_subtracted
+    if host_subtracted
+      pipeline_run_display["host_subtracted"] =
+        if host_subtracted == "ercc"
+          "ERCC only"
+        else
+          host_subtracted.titleize
+        end
+    end
     pipeline_run_display
   end
 
