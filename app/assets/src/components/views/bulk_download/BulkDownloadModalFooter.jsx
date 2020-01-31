78a646caedd8292fa57eaed54870536ee5447d46
Lucia Reynoso <lreynoso@chanzuckerberg.com>
2020-01-31 10:35:36 -0800

    [BulkDownloads] Remove Review Step and add Notification when download created (IDSEQ-1979) (#2994)
    * Add function to pass job status of selected samples to BulkDownloadModal
    
    * Filter out samples that cannot be downloaded for BulkDownloadModal
    
    * Update allowed job statuses to include complete with issues
    
    * Resolve bug with promises
    
    * Style filtered samples warning text to be bold
    
    * Filter out samples marked complete with issues
    
    * Properly pluralize filter warning text
    
    * Fix spacing typo
    
    * Update modal scss to use a standard mixin, use triple equals comparison to undefined
    
    * Change list of invalid samples to scrollable unordered list with alternating backgrounds
    
    * Style the list of filtered out samples
    
    * Fix CSS bug with invalid samples warning
    
    * Clean up choose step SCSS and parallel promise request
    
    * Replace Notification + Accordion combo with an IssueGroup component; also tweak latter
    
    * Factor out custom notification into generic component
    
    * Refactor filter notification into generic component
    
    * Remove unused imports
    
    * Remove unused variable from CompactListNotification
    
    * Create BulkDownloadsValidationService
    
    * Revert IssueGroup changes
    
    * Remove extraneous commented out code
    
    * Complete BulkDownloadsValidationService and controller route
    
    * Complete backend and frontend for validating selected sample ids
    
    * Add first rspec test and fix typos
    
    * Add regular user tests for validate post route
    
    * Add complete tests and clean up validation service for now
    
    * Rework backend to dedupe code and remove max samples validation
    
    * Move css and customization to renamed component AccordionNotification
    
    * Refactor download generation and reduce bulk download modal to one step
    
    * Add notification that bulk download has started
    
    * Set autoclose to 12 seconds
    
    * Properly handle download type field selection
    
    * Remove unused var
    
    * Rename ChooseStep to Chooser
    
    * Complete refactor of logic
    
    * Clean up code and comments
    
    * Rename field selection function prop for consistency
    
    * Reduce modal height, align button to bottom, create constants file
    
    * Add dismiss button to notification
    
    * Some tweaks to styling for bulk download modal.
    
    * Update notification copy and styling
    
    Co-authored-by: Mark Zhang <markazhang@gmail.com>

---
+++ app/assets/src/components/views/bulk_download/BulkDownloadModalFooter.jsx
@@ -0,0 +1,215 @@
+import React from "react";
+import PropTypes from "~/components/utils/propTypes";
+import { get, some, map, isUndefined, reject } from "lodash/fp";
+
+import LoadingMessage from "~/components/common/LoadingMessage";
+import AccordionNotification from "~ui/notifications/AccordionNotification";
+import Notification from "~ui/notifications/Notification";
+import PrimaryButton from "~/components/ui/controls/buttons/PrimaryButton";
+
+import { CONDITIONAL_FIELDS } from "./constants.js";
+import cs from "./bulk_download_modal_footer.scss";
+
+const triggersConditionalField = (conditionalField, selectedFields) =>
+  conditionalField.triggerValues.includes(
+    get(conditionalField.dependentField, selectedFields)
+  );
+
+export default class BulkDownloadModalFooter extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  getSelectedDownloadType = () => {
+    const { downloadTypes, selectedDownloadTypeName } = this.props;
+
+    if (!selectedDownloadTypeName) {
+      return null;
+    }
+
+    return downloadTypes.find(
+      item => item["type"] === selectedDownloadTypeName
+    );
+  };
+
+  // Get all the fields we need to validate for the selected download type.
+  getRequiredFieldsForSelectedType = () => {
+    const { selectedFields, selectedDownloadTypeName } = this.props;
+    const selectedFieldsForType = get(selectedDownloadTypeName, selectedFields);
+    const downloadType = this.getSelectedDownloadType();
+
+    if (!downloadType) return null;
+
+    let requiredFields = downloadType.fields;
+
+    // Remove any conditional fields if they don't meet the criteria.
+    CONDITIONAL_FIELDS.forEach(field => {
+      if (
+        downloadType.type === field.downloadType &&
+        !triggersConditionalField(field, selectedFieldsForType)
+      ) {
+        requiredFields = reject(["type", field.field], requiredFields);
+      }
+    });
+
+    return requiredFields;
+  };
+
+  isSelectedDownloadValid = () => {
+    const {
+      validSampleIds,
+      selectedFields,
+      selectedDownloadTypeName,
+    } = this.props;
+
+    const selectedFieldsForType = get(selectedDownloadTypeName, selectedFields);
+    const downloadType = this.getSelectedDownloadType();
+
+    if (!downloadType || validSampleIds.size < 1) {
+      return false;
+    }
+
+    const requiredFields = this.getRequiredFieldsForSelectedType();
+
+    if (requiredFields) {
+      if (
+        some(
+          Boolean,
+          map(
+            field => isUndefined(get(field.type, selectedFieldsForType)),
+            requiredFields
+          )
+        )
+      ) {
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  renderInvalidSamplesWarning() {
+    const { invalidSampleNames } = this.props;
+
+    const header = (
+      <div>
+        <span className={cs.highlight}>
+          {invalidSampleNames.length} sample
+          {invalidSampleNames.length > 1 ? "s" : ""} won't be included in the
+          bulk download
+        </span>, because they either failed or are still processing:
+      </div>
+    );
+
+    const content = (
+      <span>
+        {invalidSampleNames.map((name, index) => {
+          return (
+            <div key={index} className={cs.messageLine}>
+              {name}
+            </div>
+          );
+        })}
+      </span>
+    );
+
+    return (
+      <AccordionNotification
+        header={header}
+        content={content}
+        open={false}
+        type={"warn"}
+        displayStyle={"flat"}
+      />
+    );
+  }
+
+  renderValidationError() {
+    return (
+      <div className={cs.notificationContainer}>
+        <Notification type="error" displayStyle="flat">
+          <div className={cs.errorMessage}>
+            An error occurred when verifying your selected samples.
+          </div>
+        </Notification>
+      </div>
+    );
+  }
+
+  renderNoValidSamplesError() {
+    return (
+      <div className={cs.notificationContainer}>
+        <Notification type="error" displayStyle="flat">
+          <div className={cs.errorMessage}>
+            No valid samples to download data from.
+          </div>
+        </Notification>
+      </div>
+    );
+  }
+
+  renderDownloadButton() {
+    const {
+      waitingForCreate,
+      createStatus,
+      createError,
+      onDownloadRequest,
+    } = this.props;
+
+    if (waitingForCreate) {
+      return <LoadingMessage message="Starting your download..." />;
+    }
+
+    if (createStatus === "error") {
+      return <Notification type="error">{createError}</Notification>;
+    }
+
+    return (
+      <PrimaryButton
+        disabled={!this.isSelectedDownloadValid()}
+        text="Start Generating Download"
+        onClick={onDownloadRequest}
+      />
+    );
+  }
+
+  render() {
+    const {
+      validSampleIds,
+      invalidSampleNames,
+      validationError,
+      loading,
+    } = this.props;
+
+    const numSamples = validSampleIds.size;
+
+    return (
+      <div className={cs.footer}>
+        <div className={cs.notifications}>
+          {invalidSampleNames.length > 0 && this.renderInvalidSamplesWarning()}
+          {validationError != null && this.renderValidationError()}
+          {numSamples < 1 && !loading && this.renderNoValidSamplesError()}
+        </div>
+        {this.renderDownloadButton()}
+        <div className={cs.downloadDisclaimer}>
+          Downloads for larger files can take multiple hours to generate.
+        </div>
+      </div>
+    );
+  }
+}
+
+BulkDownloadModalFooter.propTypes = {
+  loading: PropTypes.bool,
+  downloadTypes: PropTypes.arrayOf(PropTypes.DownloadType),
+  validSampleIds: PropTypes.instanceOf(Set).isRequired,
+  invalidSampleNames: PropTypes.arrayOf(PropTypes.string),
+  validationError: PropTypes.string,
+  selectedDownloadTypeName: PropTypes.string,
+  // The selected fields of the currently selected download type.
+  selectedFields: PropTypes.objectOf(PropTypes.string),
+  waitingForCreate: PropTypes.bool,
+  createStatus: PropTypes.string,
+  createError: PropTypes.string,
+  onDownloadRequest: PropTypes.func.isRequired,
+};
---
+++ app/assets/src/components/views/bulk_download/constants.js
@@ -0,0 +1,20 @@
+// Stores information about conditional fields for bulk downloads.
+export const CONDITIONAL_FIELDS = [
+  // Note: This first field is referenced directly in renderOption, as
+  // it needs to display a placeholder component. Be careful when modifying.
+  {
+    field: "file_format",
+    // The download type this conditional field applies to.
+    downloadType: "reads_non_host",
+    // The field this conditional field depends on.
+    dependentField: "taxa_with_reads",
+    // The values of the dependent field that trigger the conditional field.
+    triggerValues: ["all", undefined],
+  },
+  {
+    field: "background",
+    downloadType: "combined_sample_taxon_results",
+    dependentField: "metric",
+    triggerValues: ["NR.zscore", "NT.zscore"],
+  },
+];
