2470ac32dd26050cbaa1b7b2efa3b9f955aff545
lvreynoso <lreynoso@chanzuckerberg.com>
2020-02-05 16:47:28 -0800

    Add infobox and placeholder text to project creation form in sample upload flow

--- app/assets/src/components/common/ProjectCreationForm.jsx
+++ app/assets/src/components/common/ProjectCreationForm.jsx
@@ -18,6 +18,7 @@ class ProjectCreationForm extends React.Component {
     publicAccess: -1, // No selection by default
     error: "",
     description: "",
+    showInfo: false,
   };
 
   handleNameChange = name => {
@@ -60,7 +61,15 @@ class ProjectCreationForm extends React.Component {
     }
   };
 
+  toggleInfo = () => {
+    this.setState({
+      showInfo: !this.state.showInfo,
+    });
+  };
+
   render() {
+    const { showInfo } = this.state;
+
     return (
       <div className={cs.projectCreationForm}>
         <div className={cs.field}>
@@ -109,14 +118,44 @@ class ProjectCreationForm extends React.Component {
         </div>
         <div className={cs.field}>
           <div className={cs.label}>
-            Description
-            <span className={cs.optionalLabel}> - Optional</span>
+            Project Description{" "}
+            <span className={cs.infoLink} onClick={this.toggleInfo}>
+              {showInfo ? "Less Info" : "More Info"}
+            </span>
           </div>
+          {showInfo && (
+            <div className={cs.info}>
+              <div className={cs.title}>A project description may include:</div>
+              <ul>
+                <li>
+                  The project goal (benchmarking, identifying an unknown
+                  pathogen, microbiome, etc.)
+                </li>
+                <li>
+                  If this work is part of a larger study, the aim of that study
+                </li>
+                <li>
+                  A summary of where the samples came from (geographically and
+                  collection date) and preparation techniques, if relevant
+                </li>
+                <li>
+                  Any other context that might be helpful in interpreting the
+                  data
+                </li>
+              </ul>
+              <p>
+                <span className={cs.title}>Example project description: </span>Investigation
+                of pathogen diversity in healthy vs. diseased dogs in
+                California. Sequenced RNA extracted from dog stool with a MiSeq.
+              </p>
+            </div>
+          )}
           <Textarea
             onChange={this.handleDescriptionChange}
             value={this.state.description}
             className={cs.descriptionTextArea}
             maxLength={MAX_DESCRIPTION_LENGTH}
+            placeholder="Enter your project goals, information about your study, where your samples came from, etc..."
           />
           <div className={cs.charCounter}>
             {MAX_DESCRIPTION_LENGTH - this.state.description.length}/
