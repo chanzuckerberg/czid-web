3637363f542c93890c042e4bc2b2cfea1a50c8e1
jshoe <github@jsheu.com>
2020-02-07 15:41:53 -0800

    Tweak

--- config/environments/public.rb
+++ config/environments/public.rb
@@ -1,3 +1,4 @@
 # Base off staging config + static assets.
+# Only diff between prod and staging was this one setting.
 require File.expand_path('../staging.rb', __FILE__)
 config.serve_static_assets = true
