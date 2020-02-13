079aed1a4ea56d4c813af9cc43e9eb6f5111100c
Greg Dingle <gdingle@chanzuckerberg.com>
2020-02-12 18:40:19 -0800

    job_stat.rb

--- app/models/job_stat.rb
+++ app/models/job_stat.rb
@@ -1,7 +1,7 @@
 class JobStat < ApplicationRecord
   belongs_to :pipeline_run
-  validates :pipeline_run, presence: true
-  validates :task, presence: true, inclusion: { in: PipelineRunsHelper::ALL_STEP_NAMES }
-  validates :reads_before, numericality: { greater_than: 0 }
-  validates :reads_after, numericality: { greater_than: 0 }
+  validates :pipeline_run, presence: true, if: :mass_validation_enabled?
+  validates :task, presence: true, inclusion: { in: PipelineRunsHelper::ALL_STEP_NAMES }, if: :mass_validation_enabled?
+  validates :reads_before, numericality: { greater_than_or_equal_to: 0 }, if: :mass_validation_enabled?
+  validates :reads_after, numericality: { greater_than_or_equal_to: 0 }, if: :mass_validation_enabled?
 end
