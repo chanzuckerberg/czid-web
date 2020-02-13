3ffb1100ca0c660801436c158dfbff7574afdff9
Greg Dingle <gdingle@chanzuckerberg.com>
2020-02-12 22:09:33 -0800

    taxon_byteranges

--- app/models/taxon_byterange.rb
+++ app/models/taxon_byterange.rb
@@ -1,12 +1,12 @@
 class TaxonByterange < ApplicationRecord
   belongs_to :pipeline_run
-  validates :pipeline_run, presence: true
+  belongs_to :taxon_lineage, class_name: "TaxonLineage", foreign_key: :tax_id, primary_key: :taxid
 
   validates :hit_type, presence: true, inclusion: { in: [
     TaxonCount::COUNT_TYPE_NT,
     TaxonCount::COUNT_TYPE_NR,
-  ], }
+  ], }, if: :mass_validation_enabled?
 
-  validates :first_byte, numericality: { greater_than_or_equal_to: 0 }
-  validates :last_byte, numericality: { greater_than_or_equal_to: 1 }
+  validates :first_byte, numericality: { greater_than_or_equal_to: 0 }, if: :mass_validation_enabled?
+  validates :last_byte, numericality: { greater_than_or_equal_to: 1 }, if: :mass_validation_enabled?
 end
