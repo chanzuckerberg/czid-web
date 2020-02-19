512f7ed6f0ae414cd6ea23a28e2d5c55dde78f91
Mark Zhang <mark.zhang@chanzuckerberg.com>
2020-02-19 13:48:56 -0800

    Future tweaks to public site. (#3085)

--- app/assets/src/components/common/Header.jsx
+++ app/assets/src/components/common/Header.jsx
@@ -364,7 +364,7 @@ const UserMenuDropDown = ({
       <BareDropdown
         trigger={
           <div className={cs.userName}>
-            {!NCOV_PUBLIC_SITE ? userName : "Menu"}
+            {!NCOV_PUBLIC_SITE ? userName : "Terms"}
           </div>
         }
         className={cs.userDropdown}
