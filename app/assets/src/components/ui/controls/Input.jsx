0ed9b4beede7105a407259e4f4dbc6f15e8f630a
Mark Zhang <mark.zhang@chanzuckerberg.com>
2020-02-19 18:30:22 -0800

    Fix bug with uncontrolled inputs. (#3093) (#3094)
    * Fix bug with uncontrolled inputs.
    
    * Remove control of phylo tree creation input.
    
    * Also check for null for input values.

--- app/assets/src/components/ui/controls/Input.jsx
+++ app/assets/src/components/ui/controls/Input.jsx
@@ -14,16 +14,12 @@ class Input extends React.Component {
   };
 
   render() {
-    let { className, disableAutocomplete, value, ...props } = this.props;
+    let { className, disableAutocomplete, ...props } = this.props;
     className = "idseq-ui " + className;
     return (
       <SemanticInput
         {...props}
         className={className}
-        // If undefined is passed to SemanticInput, the previous value of the input will continue to
-        // be displayed instead of an empty string.
-        // Therefore, we force an empty string to be passed.
-        value={value || ""}
         onChange={this.handleChange}
         // Chrome ignores autocomplete="off" on purpose, so use a non-standard
         // label. See: https://stackoverflow.com/questions/15738259/disabling-chrome-autofill
