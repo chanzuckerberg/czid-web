1127ff5e378dee1e94b483e38c155c7f5f105dda
Todd Morse <todd@morsecodist.io>
2020-02-27 13:49:37 -0500

    Surface insert size metrics on samples table, pipeline viz and results folder (#3076)
    * Integrate insert size metrics
    
    * Fix histogram path
    
    * Fix lambda weirdness
    
    * Remove random arr
    
    * Remove double file add
    
    * Separate tables
    
    * Fixed bugs
    
    * Display in UI
    
    * Fix UI bugs
    
    * Finalized
    
    * Fixed JS issues
    
    * Switch back to insert size metrics in pipeline runs
    
    * Removed dash from insert size metrics display
    
    * Cleanup and add to csv
    
    * Added link
    
    * Update db files
    
    * Schema update
    
    * Check schema out from master
    
    * Clean up display insert size
    
    * Schema.rb + get insert size for discovery
    
    * Fix deviation front end
    
    * Update ignoring
    
    * Update schema
    
    * Update schema file
    
    * Revert file_generated
    
    * Fix nil case
    
    * Switch to format utility function
    
    * Optional out
    
    * Linter fixes
    
    * linter fix
    
    * Add nested hash check
    
    * Switch to step name
    
    * Switch to dig
    
    * Simplify optional outputs
    
    * Fix results folder + refactor
    
    * s/optional_outputs/additional_outputs
    
    * Style tweaks from PR review
    
    * Manually update schema
    
    * Separate tables
    
    * More fixes
    
    * Table separation finalized
    
    * Added commas to numberWithPlusOrMinus

---
+++ app/models/insert_size_metric_set.rb
@@ -0,0 +1,13 @@
+class InsertSizeMetricSet < ApplicationRecord
+  validates :pipeline_run_id, presence: true, numericality: :only_integer
+  validates :median, presence: true, numericality: :only_integer
+  validates :mode, presence: true, numericality: :only_integer
+  validates :median_absolute_deviation, presence: true, numericality: :only_integer
+  validates :min, presence: true, numericality: :only_integer
+  validates :max, presence: true, numericality: :only_integer
+  validates :mean, presence: true, numericality: :true
+  validates :standard_deviation, presence: true, numericality: :true
+  validates :read_pairs, presence: true, numericality: :only_integer
+
+  belongs_to :pipeline_run
+end
