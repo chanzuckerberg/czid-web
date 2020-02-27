c65e6464777f1eb34f4f3ad2b7bc9a89e41b1b44
David Rissato Cruz <davidrc@gmail.com>
2020-02-27 15:52:59 -0800

    Fix comment field

--- db/migrate/20200227225541_add_sfn_execution_arn_to_pipeline_runs.rb
+++ db/migrate/20200227225541_add_sfn_execution_arn_to_pipeline_runs.rb
@@ -1,6 +1,6 @@
 class AddSfnExecutionArnToPipelineRuns < ActiveRecord::Migration[5.1]
   def up
-    add_column :pipeline_runs, :sfn_execution_arn, :string, null: true, description: "step function execution ARN for pipeline runs using pipeline_execution_strategy=step_function"
+    add_column :pipeline_runs, :sfn_execution_arn, :string, null: true, comment: "step function execution ARN for pipeline runs using pipeline_execution_strategy=step_function"
   end
 
   def down
