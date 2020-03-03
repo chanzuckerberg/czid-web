bd17defe84238f8ee675710616a449893757b4a4
morsecodist <todd@morsecodist.io>
2020-03-03 16:51:38 -0500

    Clarify ALIGNMENT_CONFIG_DEFAULT_NAME

--- app/models/alignment_config.rb
+++ app/models/alignment_config.rb
@@ -1,5 +1,6 @@
 class AlignmentConfig < ApplicationRecord
   # configuration for alignment database for pipelines
+  #   set in SSM and loaded via chamber
   DEFAULT_NAME = ENV["ALIGNMENT_CONFIG_DEFAULT_NAME"]
   has_many :pipeline_runs, dependent: :restrict_with_exception
 
