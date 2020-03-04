fe2362a2671057b3af3a1992422781bb29488a2d
jshoe <github@jsheu.com>
2020-03-04 10:12:04 -0800

    Add 2020-02-10 AlignmentConfig for local dev

---
+++ db/migrate/20200304012055_add_updated_alignment_config.rb
@@ -0,0 +1,20 @@
+class AddUpdatedAlignmentConfig < ActiveRecord::Migration[5.1]
+  NEW_DATE = "2020-02-10".freeze
+
+  def up
+    unless AlignmentConfig.find_by(name: NEW_DATE)
+      ENV["NCBI_DATE"] = NEW_DATE
+      ENV["LINEAGE_VERSION"] = (TaxonLineage.maximum('version_end') + 1).to_s
+
+      Rails.application.load_tasks
+      Rake::Task['create_alignment_config'].invoke
+    end
+  end
+
+  def down
+    exists = AlignmentConfig.find_by(name: NEW_DATE)
+    if exists
+      exists.delete
+    end
+  end
+end
