2da2bf3c9b63133fb5f9091d8e6a86ba7ba5310d
Greg Dingle <gdingle@chanzuckerberg.com>
2020-03-09 10:31:02 -0700

    add one more conditional

--- app/models/project.rb
+++ app/models/project.rb
@@ -17,7 +17,7 @@ class Project < ApplicationRecord
   # NOTE: not sure why these columns were not created as booleans
   validates :public_access, inclusion: { in: [0, 1] }, if: :mass_validation_enabled?
   # NOTE: all values of background_flag in prod db are currently zero.
-  validates :background_flag, inclusion: { in: [0, 1] }, allow_nil: true
+  validates :background_flag, inclusion: { in: [0, 1] }, allow_nil: true, if: :mass_validation_enabled?
 
   before_create :add_default_metadata_fields
 
