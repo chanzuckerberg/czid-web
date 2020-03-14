fd5dc59ca63cc4257f68bea023f3cc4d80ed4f37
Tiago Carvalho <tcarvalho@chanzuckerberg.com>
2020-03-13 17:43:39 -0700

    Reorganize test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,20 +5,32 @@ USER_FEATURES = ["feature_1", "feature_3"].freeze
 
 describe User, type: :model do
   context "#allowed_feature_list" do
-    let(:user) { create(:user, allowed_features: JSON.dump(USER_FEATURES)) }
     subject { user.allowed_feature_list }
 
-    it "returns both features launched and enabled for user" do
-      create(:app_config, key: AppConfig::LAUNCHED_FEATURES, value: JSON.dump(LAUNCHED_FEATURES))
-      expect(subject).to contain_exactly("feature_1", "feature_2", "feature_3")
+    context "if user has allowed features" do
+      let(:user) { create(:user, allowed_features: JSON.dump(USER_FEATURES)) }
+
+      context "if launched features configured" do
+        let!(:app_config) { create(:app_config, key: AppConfig::LAUNCHED_FEATURES, value: JSON.dump(LAUNCHED_FEATURES)) }
+        it "shows merged features" do
+          expect(subject).to contain_exactly("feature_1", "feature_2", "feature_3")
+        end
+      end
+
+      context "if launched features is nil" do
+        it "returns only user allowed features" do
+          expect(subject).to contain_exactly("feature_1", "feature_3")
+        end
+      end
     end
 
-    it "if launched features not configured return only user allowed features" do
-      expect(subject).to contain_exactly("feature_1", "feature_3")
-    end
+    context "if user has nil allowed features " do
+      let(:user) { create(:user) }
+      let!(:app_config) { create(:app_config, key: AppConfig::LAUNCHED_FEATURES, value: JSON.dump(LAUNCHED_FEATURES)) }
 
-    it "if user features not configured return only launched features" do
-      expect(subject).to contain_exactly("feature_1", "feature_2")
+      it "return only launched features" do
+        expect(subject).to contain_exactly("feature_1", "feature_2")
+      end
     end
   end
 end
