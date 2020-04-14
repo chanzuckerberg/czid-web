1586dea590c6695cb275458dfbf2d147eba4882d
morsecodist <todd@morsecodist.io>
2020-04-14 19:51:33 -0400

    Created a reversible migration, fixed typo, and add constant

--- db/migrate/20200414220332_rename_alignment.rb
+++ db/migrate/20200414220332_rename_alignment.rb
@@ -1,5 +1,10 @@
 class RenameAlignment < ActiveRecord::Migration[5.1]
-  def change
-    ActiveRecord::Base.connection.execute("UPDATE pipeline_run_stages SET name='GSNAPL/RAPSEARCH alignment' WHERE name='GSNAPL/RAPSEARCH alignment'")
+  # Using raw sql here because PipelineRunStage introduces dependencies on constants that may not be initialized
+  def up
+    ActiveRecord::Base.connection.execute("UPDATE pipeline_run_stages SET name='GSNAPL/RAPSEARCH alignment' WHERE name='GSNAPL/RAPSEARCH2 alignment'")
+  end
+
+  def down
+    ActiveRecord::Base.connection.execute("UPDATE pipeline_run_stages SET name='GSNAPL/RAPSEARCH2 alignment' WHERE name='GSNAPL/RAPSEARCH alignment'")
   end
 end
