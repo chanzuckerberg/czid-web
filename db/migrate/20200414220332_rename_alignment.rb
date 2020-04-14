60829a02d12d1a889dd673a92f6eb374da1ebb88
morsecodist <todd@morsecodist.io>
2020-04-14 18:56:49 -0400

    Add require to migration

--- db/migrate/20200414220332_rename_alignment.rb
+++ db/migrate/20200414220332_rename_alignment.rb
@@ -1,3 +1,6 @@
+# This is needed to create constants
+require 'pipeline_run_stage'
+
 class RenameAlignment < ActiveRecord::Migration[5.1]
   def change
     PipelineRunStage.where(name: "GSNAPL/RAPSEARCH alignment").find_each { |u| u.update(name: "GSNAPL/RAPSEARCH2 alignment") }
