77ae555d817988e5c16e8c78f6dd4756af608c09
Todd Morse <todd@morsecodist.io>
2020-04-14 20:09:04 -0400

    [Release fix] Raw sql migration (#3272)
    Add raw sql migration

--- db/migrate/20200414220332_rename_alignment.rb
+++ db/migrate/20200414220332_rename_alignment.rb
@@ -1,8 +1,10 @@
-# This is needed to load the constants in PipelineRunStage
-require 'pipeline_run_stage'
-
 class RenameAlignment < ActiveRecord::Migration[5.1]
-  def change
-    PipelineRunStage.where(name: "GSNAPL/RAPSEARCH alignment").find_each { |u| u.update(name: "GSNAPL/RAPSEARCH2 alignment") }
+  # Using raw sql here because PipelineRunStage introduces dependencies on constants that may not be initialized
+  def up
+    ActiveRecord::Base.connection.execute("UPDATE pipeline_run_stages SET name='GSNAPL/RAPSEARCH2 alignment' WHERE name='GSNAPL/RAPSEARCH alignment'")
+  end
+
+  def down
+    ActiveRecord::Base.connection.execute("UPDATE pipeline_run_stages SET name='GSNAPL/RAPSEARCH alignment' WHERE name='GSNAPL/RAPSEARCH2 alignment'")
   end
 end
