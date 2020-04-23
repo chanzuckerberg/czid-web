3159e816ab6de2c5666ca6a382dac08f1d722ba5
Tiago Carvalho <37312125+tfrcarvalho@users.noreply.github.com>
2020-04-23 15:32:37 -0700

    Add data discovery empty states (#3252)
    * Add first states for projects, samples and visualizations.
    Add banner icons.
    
    * Add empty state modal
    
    * Add comma
    
    * Add space after banner
    
    * Apply suggested changes
    
    * Add modal on first sign in
    
    * Last design fixes
    
    * Add comment
    
    * Add external link option
    
    * Rename components and add default link style
    
    * Fix color bug

---
+++ app/assets/src/components/ui/controls/Link.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import PropTypes from "prop-types";
+import cx from "classnames";
+
+import { logAnalyticsEvent } from "~/api/analytics";
+import cs from "./link.scss";
+
+class Link extends React.Component {
+  onClick = () => {
+    const {
+      analyticsEventData,
+      analyticsEventName,
+      external,
+      href,
+    } = this.props;
+
+    if (analyticsEventName) {
+      logAnalyticsEvent(analyticsEventName, analyticsEventData);
+    } else {
+      logAnalyticsEvent("Link_generic_clicked", {
+        external: external,
+        href: href,
+      });
+    }
+  };
+
+  render() {
+    const { href, className, children, external } = this.props;
+    return (
+      <a
+        href={href}
+        className={cx(cs.link, className)}
+        target={external ? "_blank" : null}
+        rel="noopener noreferrer"
+        onClick={this.onClick}
+      >
+        {children}
+      </a>
+    );
+  }
+}
+
+Link.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  external: PropTypes.bool,
+  href: PropTypes.string,
+
+  // We intentionally don't have an onClick prop, because we don't want to encourage arbitrary onClick handlers,
+  // since there is already an on-click behavior (following the link href). logAnalyticsEvent is an exception.
+  analyticsEventName: PropTypes.string,
+  analyticsEventData: PropTypes.object,
+};
+
+Link.defaultProps = {
+  analyticsEventData: {},
+  externalLink: false,
+};
+
+export default Link;
