121e0d5899dcceb3c69a4e6dec20e22366aa386f
morsecodist <todd@morsecodist.io>
2020-04-24 12:53:15 -0400

    Add nil check

--- db/migrate/20200423161207_alignment_config_to_shelve.rb
+++ db/migrate/20200423161207_alignment_config_to_shelve.rb
@@ -1,7 +1,7 @@
 class AlignmentConfigToShelve < ActiveRecord::Migration[5.1]
   def up
     AlignmentConfig.all.each do |ac|
-      if ac.s3_nt_info_db_path.end_with? '.sqlite3'
+      if ac.s3_nt_info_db_path && ac.s3_nt_info_db_path.end_with?('.sqlite3')
         shelve_path = ac.s3_nt_info_db_path.sub!('.sqlite3', '.db')
         ac.update(s3_nt_info_db_path: shelve_path)
       end
@@ -10,7 +10,7 @@ class AlignmentConfigToShelve < ActiveRecord::Migration[5.1]
 
   def down
     AlignmentConfig.all.each do |ac|
-      if ac.s3_nt_info_db_path.end_with? '.db'
+      if ac.s3_nt_info_db_path && ac.s3_nt_info_db_path.end_with?('.db')
         shelve_path = ac.s3_nt_info_db_path.sub!('.db', '.sqlite3')
         ac.update(s3_nt_info_db_path: shelve_path)
       end
