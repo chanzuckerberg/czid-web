12c6d2dd2684f588cf4d5d168da90a63144cb14e
lvreynoso <lreynoso@chanzuckerberg.com>
2020-04-28 16:55:35 -0700

    Fix elasticsearch host config

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -2,8 +2,16 @@
 ELASTICSEARCH_ON = (Rails.env != 'test')
 
 # Initialize elasticsearch client
+case Rails.env
+when "production", "staging", "sandbox"
+  port = '443'
+when "development"
+  port = '9200'
+end
+
 config = {
-  hosts: { host: ENV['ES_ADDRESS'], port: 443 },
+  hosts: [{ host: ENV['ES_ADDRESS'], port: port }],
   transport_options: { request: { timeout: 200 } },
 }
+
 Elasticsearch::Model.client = Elasticsearch::Client.new(config) if Rails.env != 'test'
