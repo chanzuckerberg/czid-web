3df9a266678a77081501c299a98c4947c4fd1e12
lvreynoso <lreynoso@chanzuckerberg.com>
2020-04-28 11:54:18 -0700

    Explicitly specify port 443 due to changes in elasticsearch gem

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -3,7 +3,7 @@ ELASTICSEARCH_ON = (Rails.env != 'test')
 
 # Initialize elasticsearch client
 config = {
-  host: ENV['ES_ADDRESS'],
+  hosts: { host: ENV['ES_ADDRESS'], port: 443 },
   transport_options: { request: { timeout: 200 } },
 }
 Elasticsearch::Model.client = Elasticsearch::Client.new(config) if Rails.env != 'test'
