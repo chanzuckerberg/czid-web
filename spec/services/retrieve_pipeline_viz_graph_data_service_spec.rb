f0a83db41ee738321ccf81edd43034c6a8c1b06b
Tiago Carvalho <37312125+tfrcarvalho@users.noreply.github.com>
2020-06-02 22:56:52 -0700

    Fix pipeline vix downloads for SFN runs (#3366)

--- spec/services/retrieve_pipeline_viz_graph_data_service_spec.rb
+++ spec/services/retrieve_pipeline_viz_graph_data_service_spec.rb
@@ -1,11 +1,8 @@
 require 'rails_helper'
 require 'json'
 
-RSpec.describe RetrievePipelineVizGraphDataService do
-  let(:fake_output_prefix) { "s3://fake-output-prefix" }
-  let(:fake_sfn_name) { "fake_sfn_name" }
+shared_examples "pipeline viz" do
   let(:fake_sfn_arn) { "fake:sfn:arn".freeze }
-  let(:fake_sfn_execution_arn) { "fake:sfn:execution:arn:#{fake_sfn_name}".freeze }
   let(:fake_sfn_execution_description) do
     {
       execution_arn: fake_sfn_arn,
@@ -17,61 +14,6 @@ RSpec.describe RetrievePipelineVizGraphDataService do
   end
   let(:fake_wdl_version) { "999".freeze }
   let(:fake_dag_version) { "9.999".freeze }
-
-  let(:given_targets_dir) { "s3://idseq.../samples/theProjectId/theSampleId/results/1.0" }
-  let(:output_dir) { "s3://idseq.../samples/theProjectId/theSampleId/results" }
-
-  let(:pr_stages_data) do
-    [{
-      name: "Host Filtering",
-      dag_json: {
-        output_dir_s3: output_dir,
-        targets: {
-          one: ["unmapped1.fq"],
-          two: ["trimmomatic1.fq"],
-          # Testing partially concated file path and no url available.
-          three: ["file/priceseq1.fa"],
-        },
-        steps: [
-          {
-            in: ["one"],
-            out: "two",
-            class: "step_one",
-          }, {
-            in: ["two"],
-            out: "three",
-            class: "step_two",
-          },
-        ],
-        given_targets: {
-          one: {
-            s3_dir: given_targets_dir,
-          },
-        },
-      }.to_json,
-    }, {
-      name: "Experimental",
-      dag_json: {
-        output_dir_s3: output_dir,
-        targets: {
-          three: ["priceseq1.fa"],
-          four: ["bowtie2_1.fa"],
-        },
-        steps: [
-          {
-            in: ["three"],
-            out: "four",
-            class: "step_three",
-          },
-        ],
-        given_targets: {
-          three: {
-            s3_dir: given_targets_dir + "/file",
-          },
-        },
-      }.to_json,
-    },]
-  end
 
   let(:step_status_data) do
     {
@@ -122,7 +64,7 @@ RSpec.describe RetrievePipelineVizGraphDataService do
         {
           from: { stageIndex: 0, stepIndex: 1 },
           to: { stageIndex: 1, stepIndex: 0 },
-          files: [{ displayName: "priceseq1.fa", url: nil }],
+          files: [{ displayName: "priceseq1.fa", url: "test url" }],
           isIntraStage: false,
         },
         {
@@ -136,6 +78,7 @@ RSpec.describe RetrievePipelineVizGraphDataService do
   end
 
   before do
+    stub_const('SAMPLES_BUCKET_NAME', fake_bucket_name)
     Aws.config[:states] = {
       stub_responses: {
         describe_execution: fake_sfn_execution_description,
@@ -147,19 +90,39 @@ RSpec.describe RetrievePipelineVizGraphDataService do
         },
       },
     }
+
+    Aws.config[:s3] = {
+      stub_responses: {
+        list_objects: {
+          contents: %w[
+            unmapped1.fq
+            trimmomatic1.fq
+            priceseq1.fa
+            dedup1.fa
+            lzw1.fa
+            bowtie2_1.fa
+            subsampled_1.fa
+            gsnap_filter_1.fa
+          ].map do |filename|
+            { key: File.join(output_dir_s3.split("/", 4)[-1], filename) }
+          end,
+        },
+      },
+    }
+
+    allow(Sample).to receive(:get_signed_url).and_return("test url")
   end
 
-  describe "Retrieving graph" do
+  describe "retrieving graph" do
     before do
       # Project and sample required to create pipeline run.
       project = create(:public_project)
       @sample = create(:sample, project: project)
-
-      @pr = create(:pipeline_run, sample: @sample, pipeline_run_stages_data: pr_stages_data, sfn_execution_arn: fake_sfn_execution_arn)
     end
 
     it "should be structured correctly" do
-      results = RetrievePipelineVizGraphDataService.call(@pr.id, true, false)
+      results = RetrievePipelineVizGraphDataService.call(pr.id, true, false)
+      puts results
       expect(results).to include_json(expected_stage_results)
       expect(results.keys).to contain_exactly(*expected_stage_results.keys)
     end
@@ -170,7 +133,7 @@ RSpec.describe RetrievePipelineVizGraphDataService do
 
       stub_const("PipelineOutputsHelper::Client", s3)
       # allow(PipelineOutputsHelper).to receive(:get_s3_file).and_return(step_status_data.to_json)
-      results = RetrievePipelineVizGraphDataService.call(@pr.id, true, false)
+      results = RetrievePipelineVizGraphDataService.call(pr.id, true, false)
 
       results[:stages][0][:steps].each do |step|
         # Status should be notStarted if no job status data exists for the step
@@ -190,7 +153,7 @@ RSpec.describe RetrievePipelineVizGraphDataService do
     context "with see_experimental flag" do
       it "sees all stage results" do
         see_experimental = true
-        results = RetrievePipelineVizGraphDataService.call(@pr.id, see_experimental, false)
+        results = RetrievePipelineVizGraphDataService.call(pr.id, see_experimental, false)
 
         # Host filtering and experimental; other stages omitted for brevity.
         expect(results[:stages].length).to be(2)
@@ -200,7 +163,7 @@ RSpec.describe RetrievePipelineVizGraphDataService do
     context "without see_experimental flag" do
       it "sees all stage results except experimental" do
         see_experimental = false
-        results = RetrievePipelineVizGraphDataService.call(@pr.id, see_experimental, false)
+        results = RetrievePipelineVizGraphDataService.call(pr.id, see_experimental, false)
 
         # Only host filtering (other stages omitted for brevity)
         expect(results[:stages].length).to be(1)
@@ -225,7 +188,7 @@ RSpec.describe RetrievePipelineVizGraphDataService do
     context "without host filtering urls" do
       it "does not include host filtering urls" do
         remove_host_filtering = true
-        results = RetrievePipelineVizGraphDataService.call(@pr.id, false, remove_host_filtering)
+        results = RetrievePipelineVizGraphDataService.call(pr.id, false, remove_host_filtering)
 
         # Get files in each edge in Host Filtering stage (except final output) and check for no url
         edges = results[:edges]
@@ -246,7 +209,7 @@ RSpec.describe RetrievePipelineVizGraphDataService do
     context "with host filtering urls" do
       it "includes host filtering urls" do
         remove_host_filtering = false
-        results = RetrievePipelineVizGraphDataService.call(@pr.id, false, remove_host_filtering)
+        results = RetrievePipelineVizGraphDataService.call(pr.id, false, remove_host_filtering)
 
         # Get files in each edge in Host Filtering stage (except final output) and check for url
         edges = results[:edges]
@@ -261,6 +224,89 @@ RSpec.describe RetrievePipelineVizGraphDataService do
             end
           end
         end
+      end
+    end
+  end
+end
+
+RSpec.describe RetrievePipelineVizGraphDataService do
+  let(:fake_bucket_name) { "fake-bucket" }
+  let(:fake_output_prefix) { "s3://#{fake_bucket_name}" }
+  let(:fake_sfn_name) { "fake_sfn_name" }
+  let(:fake_sfn_execution_arn) { "fake:sfn:execution:arn:#{fake_sfn_name}".freeze }
+  let(:project) { create(:public_project) }
+  let(:sample) { create(:sample, project: project) }
+  let(:pr_stages_data) do
+    [
+      {
+        name: "Host Filtering",
+        dag_json: {
+          output_dir_s3: output_dir,
+          targets: {
+            one: ["unmapped1.fq"],
+            two: ["trimmomatic1.fq"],
+            # Testing partially concated file path and no url available.
+            three: ["priceseq1.fa"],
+          },
+          steps: [
+            {
+              in: ["one"],
+              out: "two",
+              class: "step_one",
+            }, {
+              in: ["two"],
+              out: "three",
+              class: "step_two",
+            },
+          ],
+          given_targets: {
+            one: {
+              s3_dir: given_targets_dir,
+            },
+          },
+        }.to_json,
+      },
+      {
+        name: "Experimental",
+        dag_json: {
+          output_dir_s3: output_dir,
+          targets: {
+            three: ["priceseq1.fa"],
+            four: ["bowtie2_1.fa"],
+          },
+          steps: [
+            {
+              in: ["three"],
+              out: "four",
+              class: "step_three",
+            },
+          ],
+          given_targets: {
+            three: {
+              s3_dir: given_targets_dir,
+            },
+          },
+        }.to_json,
+      },
+    ]
+  end
+
+  context "for legacy dag pipeline run" do
+    it_should_behave_like "pipeline viz" do
+      let(:output_dir) { "s3://idseq.../samples/theProjectId/theSampleId/results" }
+      let(:output_dir_s3) { "#{output_dir}/1.0" }
+      let(:given_targets_dir) { "s3://idseq.../samples/theProjectId/theSampleId/results/1.0" }
+      let(:pr) { create(:pipeline_run, sample: sample, pipeline_run_stages_data: pr_stages_data, pipeline_execution_strategy: PipelineRun.pipeline_execution_strategies[:directed_acyclic_graph]) }
+    end
+  end
+
+  context "for step function pipeline run" do
+    it_should_behave_like "pipeline viz" do
+      let(:output_dir) { "#{fake_output_prefix}/#{fake_sfn_name}/wdl-#{fake_wdl_version}/dag-#{fake_dag_version}" }
+      let(:output_dir_s3) { output_dir }
+      let(:given_targets_dir) { "#{fake_output_prefix}/results" }
+      let(:pr) do
+        create(:pipeline_run, sample: sample, pipeline_run_stages_data: pr_stages_data, pipeline_version: fake_dag_version, sfn_execution_arn: fake_sfn_execution_arn)
       end
     end
   end
