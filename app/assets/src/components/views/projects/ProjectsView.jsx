9a83a192d398507db943b878a085a8d978b03127
Omar Valenzuela <51972068+ovalenzuela19@users.noreply.github.com>
2020-06-09 11:17:41 -0700

    [IDSEQ-1913][QA] Remove sortable style from columns that do not sort data (#7)
    * disabled sorting for table headers that had sorting enabled
    
    Signed-off-by: ovalenzuela19 <ovalenzuela@chanzuckerberg.com>
    
    * disabled sorting for project header and modified css
    
    Signed-off-by: ovalenzuela19 <ovalenzuela@chanzuckerberg.com>
    
    * PR review changes
    
    Signed-off-by: ovalenzuela19 <ovalenzuela@chanzuckerberg.com>
    
    * removed changes to base table
    
    Signed-off-by: ovalenzuela19 <ovalenzuela@chanzuckerberg.com>

--- app/assets/src/components/views/projects/ProjectsView.jsx
+++ app/assets/src/components/views/projects/ProjectsView.jsx
@@ -31,6 +31,7 @@ class ProjectsView extends React.Component {
         dataKey: "project",
         flexGrow: 1,
         width: 350,
+        disableSort: true,
         cellRenderer: ({ cellData }) =>
           TableRenderers.renderItemDetails(
             merge(
@@ -50,6 +51,7 @@ class ProjectsView extends React.Component {
         dataKey: "created_at",
         label: "Created On",
         width: 120,
+        disableSort: true,
         cellRenderer: TableRenderers.renderDateWithElapsed,
       },
       {
@@ -68,8 +70,9 @@ class ProjectsView extends React.Component {
       },
       {
         dataKey: "number_of_samples",
-        width: 140,
         label: "No. of Samples",
+        width: 140,
+        disableSort: true,
       },
     ];
 
