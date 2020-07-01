452931217a4d1e2f89f97d6a3f9d6bef5bdd63b3
Andrey Kislyuk <kislyuk@gmail.com>
2020-07-01 14:22:10 -0700

    SFN input: use explicit deuterostome flag (#63)

--- app/services/sfn_pipeline_dispatch_service.rb
+++ app/services/sfn_pipeline_dispatch_service.rb
@@ -157,7 +157,8 @@ class SfnPipelineDispatchService
           accession2taxid_db: @pipeline_run.alignment_config.s3_accession2taxid_path,
           taxon_blacklist: @pipeline_run.alignment_config.s3_taxon_blacklist_path,
           index_dir_suffix: @pipeline_run.alignment_config.index_dir_suffix,
-          deuterostome_db: @sample.skip_deutero_filter_flag == 1 ? @pipeline_run.alignment_config.s3_deuterostome_db_path : nil,
+          use_deuterostome_filter: @sample.skip_deutero_filter_flag == 1 ? false : true,
+          deuterostome_db: @pipeline_run.alignment_config.s3_deuterostome_db_path,
           use_taxon_whitelist: @pipeline_run.use_taxon_whitelist,
         }, Postprocess: {
           nt_db: @pipeline_run.alignment_config.s3_nt_db_path,
@@ -166,7 +167,8 @@ class SfnPipelineDispatchService
           nr_loc_db: @pipeline_run.alignment_config.s3_nr_loc_db_path,
           lineage_db: @pipeline_run.alignment_config.s3_lineage_path,
           taxon_blacklist: @pipeline_run.alignment_config.s3_taxon_blacklist_path,
-          deuterostome_db: @sample.skip_deutero_filter_flag == 1 ? @pipeline_run.alignment_config.s3_deuterostome_db_path : nil,
+          use_deuterostome_filter: @sample.skip_deutero_filter_flag == 1 ? false : true,
+          deuterostome_db: @pipeline_run.alignment_config.s3_deuterostome_db_path,
           use_taxon_whitelist: @pipeline_run.use_taxon_whitelist,
         }, Experimental: {
           nt_db: @pipeline_run.alignment_config.s3_nt_db_path,
