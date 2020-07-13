31665322c2bc8e23b37f635b71d6766439576270
Omar Valenzuela <51972068+ovalenzuela19@users.noreply.github.com>
2020-07-13 16:35:39 -0700

    [Release Fix] Fix 'A copy of xxx has been removed from the module tree but is still active' error (#94)
    * prepended module with :: to avoid error with reloading
    
    * added self. to class methods

--- app/lib/subscribers/action_controller_metric_handler.rb
+++ app/lib/subscribers/action_controller_metric_handler.rb
@@ -7,52 +7,52 @@ module Subscribers
         msg: "process_action.action_controller",
         v: 1,
         details: {
-          controller: event.payload[:controller],
-          action: event.payload[:action],
-          params: event.payload[:params],
-          path: event.payload[:path],
-          request: event.payload[:request],
-          status: event.payload[:status],
-          location: event.payload[:location],
-          db_runtime: event.payload[:db_runtime],
-          event_duration: event.duration, # in milliseconds
+          controller: @event.payload[:controller],
+          action: @event.payload[:action],
+          params: @event.payload[:params],
+          path: @event.payload[:path],
+          request: @event.payload[:request],
+          status: @event.payload[:status],
+          location: @event.payload[:location],
+          db_runtime: @event.payload[:db_runtime],
+          event_duration: @event.duration, # in milliseconds
         },
       }
 
-      # event.payload[:exception] => ["exception name", "exception message"]
-      if event.payload[:exception].present?
+      # @event.payload[:exception] => ["exception name", "exception message"]
+      if @event.payload[:exception].present?
         event_log[:details][:exception] = {
-          name: event.payload[:exception][0],
-          message: event.payload[:exception][1],
+          name: @event.payload[:exception][0],
+          message: @event.payload[:exception][1],
         }
         process_exception_metric
       else
         process_metric
       end
 
-      logger.info(event_log)
+      @logger.info(event_log)
     end
 
     def process_metric
-      domain = event.payload[:params]["domain"]
-      clean_path = "/#{event.payload[:params]['controller']}/#{event.payload[:params]['action']}"
+      domain = @event.payload[:params]["domain"]
+      clean_path = "/#{@event.payload[:params]['controller']}/#{@event.payload[:params]['action']}"
 
       metric_data = [
-        CloudWatchUtil.create_metric_datum("Duration", event.duration, "Milliseconds",
+        CloudWatchUtil.create_metric_datum("Duration", @event.duration, "Milliseconds",
                                            [
-                                             { name: "Controller", value: event.payload[:controller] },
+                                             { name: "Controller", value: @event.payload[:controller] },
                                              { name: "Path", value: clean_path },
                                            ]),
 
-        CloudWatchUtil.create_metric_datum("DB Runtime", event.payload[:db_runtime], "Milliseconds",
+        CloudWatchUtil.create_metric_datum("DB Runtime", @event.payload[:db_runtime], "Milliseconds",
                                            [
-                                             { name: "Controller", value: event.payload[:controller] },
+                                             { name: "Controller", value: @event.payload[:controller] },
                                              { name: "Path", value: clean_path },
                                            ]),
 
         CloudWatchUtil.create_metric_datum("Request Status", 1.0, "Count",
                                            [
-                                             { name: "Status", value: event.payload[:status].to_s },
+                                             { name: "Status", value: @event.payload[:status].to_s },
                                              { name: "Path", value: clean_path },
                                            ]),
       ]
@@ -62,13 +62,13 @@ module Subscribers
     end
 
     def process_exception_metric
-      domain = event.payload[:params]["domain"]
-      clean_path = "/#{event.payload[:params]['controller']}/#{event.payload[:params]['action']}"
+      domain = @event.payload[:params]["domain"]
+      clean_path = "/#{@event.payload[:params]['controller']}/#{@event.payload[:params]['action']}"
 
       metric_data = [
         CloudWatchUtil.create_metric_datum("Exception Occurences", 1.0, "Count", [
-                                             { name: "Path", value: clean_path }, { name: "Exception Name", value: event.payload[:exception][0] },
-                                             { name: "Exception Message", value: event.payload[:exception][1] },
+                                             { name: "Path", value: clean_path }, { name: "Exception Name", value: @event.payload[:exception][0] },
+                                             { name: "Exception Message", value: @event.payload[:exception][1] },
                                            ]),
       ]
 
