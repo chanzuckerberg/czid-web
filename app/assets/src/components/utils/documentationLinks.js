233b27c5471854bab8577662e880b77b5004eed1
Omar Valenzuela <51972068+ovalenzuela19@users.noreply.github.com>
2020-09-30 12:12:23 -0700

    updated cg help center link (#382)

--- app/assets/src/components/utils/documentationLinks.js
+++ app/assets/src/components/utils/documentationLinks.js
@@ -1,4 +1,4 @@
 export const VISUALIZATIONS_DOC_LINK =
   "https://chanzuckerberg.zendesk.com/hc/en-us/articles/360043062453-Creating-a-Heatmap";
 export const CONSENSUS_GENOME_DOC_LINK =
-  "https://docs.google.com/document/d/1VbNvDgBv3tmuZb3j8_bJHK23txuVv9sIvGM2nIQQ_CQ";
+  "https://chanzuckerberg.zendesk.com/hc/en-us/articles/360049787632";
