fa28d2e69d6b376df69563c193884740f43605bf
Omar Valenzuela <51972068+ovalenzuela19@users.noreply.github.com>
2020-10-07 10:20:04 -0700

    [CH-17546] Set up releases in Sentry (#392)
    * created sentry-release action
    
    * remove explicit npm install sentry cli command since it's in the package
    
    * fix typo
    
    * install sentry package with -g
    
    * use sha instead of release tag
    
    * remove prefix flag and update git sha to only 8 chars
    
    * testing error
    
    * source maps work
    
    * test auto commit integration
    
    * fix typo
    
    * update set-commits cmd
    
    * use local flag instead of auto
    
    * use set-commits --auto
    
    * finalize source maps
    
    * GA action for staging and prod only
    
    * remove sentry-cli from package
    
    * move build-img to another step

--- app/assets/src/index.jsx
+++ app/assets/src/index.jsx
@@ -14,6 +14,7 @@ import "./styles/core.scss";
 Sentry.init({
   dsn: window.SENTRY_DSN_FRONTEND,
   environment: window.ENVIRONMENT,
+  release: window.GIT_RELEASE_SHA,
 });
 
 if (!function f() {}.name) {
