14ebcf3c7a25b05633e36ffa3075e7b91223f7ef
Jonathan Sheu <jshoe@users.noreply.github.com>
2020-10-14 10:44:23 -0700

    Add current_user to Sentry Ruby errors (#411)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -129,6 +129,7 @@ class ApplicationController < ActionController::Base
   private
 
   def set_raven_context
+    Raven.user_context(id: current_user.id) if current_user
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 
