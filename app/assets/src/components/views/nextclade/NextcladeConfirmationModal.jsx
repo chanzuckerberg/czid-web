484eee47964f6dd8ee6ff5ae6394ff349e0baea8
Julie Han <53838890+j-x-han@users.noreply.github.com>
2020-12-04 11:34:16 -0800

    [CH-34510] Update legal copy for Nextclade modal (#561)

--- app/assets/src/components/views/nextclade/NextcladeConfirmationModal.jsx
+++ app/assets/src/components/views/nextclade/NextcladeConfirmationModal.jsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { Image } from "semantic-ui-react";
 import Modal from "~ui/containers/Modal";
 import { PrimaryButton, SecondaryButton } from "~ui/controls/buttons";
+import ExternalLink from "~/components/ui/controls/ExternalLink";
 
 import cs from "./nextclade_confirmation_modal.scss";
 
@@ -12,12 +13,22 @@ const NextcladeConfirmationModal = ({ onCancel, onConfirm, open }) => {
     <Modal className={cs.nextcladeConfirmation} narrowest open={open}>
       <Image className={cs.logo} src="/assets/LogoNextclade.png" />
       <div className={cs.title}>
-        Please confirm you're ready to send your data to Nextclade.
+        You’re ready to send your samples to Nextclade.
       </div>
       <div className={cs.text}>
-        You are leaving IDseq and sending your sequence and reference tree to a
-        private visualization on nextstrain.org, which is not controlled by
-        IDseq.
+        Once you click Confirm, you will leave IDseq and send your sequence(s)
+        and reference tree to Nextclade. Please ensure that your Reference Tree
+        does not contain any personally identifiable information. Nextclade is a
+        third-party tool and has its own policies. You can learn more at {}
+        <ExternalLink
+          href="https://clades.nextstrain.org"
+          analyticsEventName={
+            "NextcladeConfirmationModal_nextclade-link_clicked"
+          }
+        >
+          clades.nextstrain.org
+        </ExternalLink>
+        .
       </div>
       <div className={cs.actions}>
         <div className={cs.item}>
