96e09993152b7367ee1bb1ddb40f7528e2cd6d03
Jonathan Sheu <jshoe@users.noreply.github.com>
2021-02-08 16:27:18 -0800

    Add a more generous AwsClient[:states] retry_limit (#675)
    # Description
    - This PR doubles the `Aws::States::Client` `retry_limit` from 3 to 6. I want to fiddle with these settings to see if it will help with errors like the dropped `start_executions`.
    - We could also create a retry locally around specific API calls, but I would rather use the extensive retry logic built into the SDK already, and I think some retry handling is useful globally.
    
    # Tests
    - I restarted and dispatched a sample locally as a sanity test.

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -9,7 +9,14 @@ module AwsClient
 
   CLIENT_INITIALIZERS = {
     s3: -> { Aws::S3::Client.new },
-    states: -> { Aws::States::Client.new },
+    states: lambda {
+      Aws::States::Client.new(
+        # Default retry_limit is 3. Default retry_base_delay is 0.3 sec.
+        # Default retry_backoff is 2**retries * retry_base_delay.
+        # See: https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/States/Client.html
+        retry_limit: 6
+      )
+    },
     sts: -> { Aws::STS::Client.new },
   }.freeze
 
