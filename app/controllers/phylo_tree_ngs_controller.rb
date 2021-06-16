9b718e0ab4e30cc3acba3d42b0d5882bf54a0cd5
Julie Han <53838890+j-x-han@users.noreply.github.com>
2021-06-15 17:24:37 -0700

    Create skeleton for PhyloTreeNgsController (#1011)

---
+++ app/controllers/phylo_tree_ngs_controller.rb
@@ -0,0 +1,33 @@
+class PhyloTreeNgsController < ApplicationController
+  # TODO: CH-142663
+  def index
+  end
+
+  # TODO: CH-142663
+  def show
+  end
+
+  # TODO: CH-142672
+  def new
+  end
+
+  # TODO: CH-142672
+  def create
+  end
+
+  # TODO: CH-142676
+  def choose_taxon
+  end
+
+  # TODO: CH-142676
+  def validate_name
+  end
+
+  # TODO: CH-142676
+  def rerun
+  end
+
+  # TODO: CH-142676
+  def download
+  end
+end
