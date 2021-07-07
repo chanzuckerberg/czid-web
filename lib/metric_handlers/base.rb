c95ca3badf9b10cd6692eba27cdc60f81b3eda6b
Jonathan Sheu <jshoe@users.noreply.github.com>
2021-07-07 14:30:30 -0700

    Update to MetricHandlers (#1084)

--- lib/metric_handlers/base.rb
+++ lib/metric_handlers/base.rb
@@ -5,10 +5,10 @@ module MetricHandlers
     attr_reader :event, :logger
 
     def call(name, started, finished, unique_id, payload)
-      @event = ActiveSupport::Notifications::Event.new(name, started, finished, unique_id, payload)
-      @logger = Ougai::Logger.new(STDOUT)
-      @logger.formatter = Ougai::Formatters::Readable.new if Rails.env.development?
       unless IGNORE_CONTROLLERS.include?(payload&.[](:controller))
+        @event = ActiveSupport::Notifications::Event.new(name, started, finished, unique_id, payload)
+        @logger = Ougai::Logger.new(STDOUT)
+        @logger.formatter = Ougai::Formatters::Readable.new if Rails.env.development?
         process_event
       end
     end
