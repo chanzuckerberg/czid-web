c27008f676bcce3c282b4ce1f1c928498941f548
Jonathan Sheu <jshoe@users.noreply.github.com>
2021-07-07 14:03:36 -0700

    Exclude HealthCheckController from ActionController metrics (#1080)
    * Exclude HealthCheckController from ActionController
    
    * Exclude HealthCheckController from ActionController - add test
    
    * Tweak

--- lib/metric_handlers/base.rb
+++ lib/metric_handlers/base.rb
@@ -1,12 +1,16 @@
 module MetricHandlers
   class Base
+    IGNORE_CONTROLLERS = ["HealthCheck::HealthCheckController"].freeze
+
     attr_reader :event, :logger
 
     def call(name, started, finished, unique_id, payload)
       @event = ActiveSupport::Notifications::Event.new(name, started, finished, unique_id, payload)
       @logger = Ougai::Logger.new(STDOUT)
       @logger.formatter = Ougai::Formatters::Readable.new if Rails.env.development?
-      process_event
+      unless IGNORE_CONTROLLERS.include?(payload&.[](:controller))
+        process_event
+      end
     end
 
     def process_event
