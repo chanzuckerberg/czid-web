2b9e73c24be296148ddcb1e085591468351bdca8
Irene Han <ihan@chanzuckerberg.com>
2021-10-27 12:29:54 -0700

    [CH-163744] In App page Bottom Section (#1398)
    * surface citations
    
    * add hash link
    
    * alpha-sort citations
    
    * tweaks
    
    * tweak

--- app/assets/src/components/views/pathogen_list/PathogenListView.jsx
+++ app/assets/src/components/views/pathogen_list/PathogenListView.jsx
@@ -5,6 +5,7 @@ import { Grid } from "semantic-ui-react";
 import { ANALYTICS_EVENT_NAMES } from "~/api/analytics";
 import { getPathogenList } from "~/api/pathogen_lists";
 import { NarrowContainer } from "~/components/layout";
+import List from "~/components/ui/List";
 import ExternalLink from "~/components/ui/controls/ExternalLink";
 
 import cs from "./pathogen_list_view.scss";
@@ -34,13 +35,16 @@ const PathogenListView = () => {
         <p>
           This list includes pathogens with known pathogenicity in
           immunocompetent human hosts as well as a few common causes of disease
-          in immunocompromised human hosts. It serves to highlight organisms
-          with known pathogenicity in humans, but may not be fully
-          comprehensive. Thus, researchers should reference the literature for
-          rare causes of disease. Additionally, some of these organisms are
-          known pathobionts - commensals within particular body sites (ie
-          Candida aureus may be found in the lung without causing an infection),
-          and should be interpreted with respect to the literature.
+          in immunocompromised human hosts{" "}
+          <a href={window.location.pathname + "#citations"}>
+            [1-{pathogenList["citations"].length}]
+          </a>
+          . It serves to highlight organisms with known pathogenicity in humans,
+          but may not be fully comprehensive. Thus, researchers should reference
+          the literature for rare causes of disease. Additionally, some of these
+          organisms are known pathobionts - commensals within particular body
+          sites (ie Candida aureus may be found in the lung without causing an
+          infection), and should be interpreted with respect to the literature.
         </p>
         <p>
           Reach out to <a href="mailto:help@idseq.net">help@idseq.net</a> to
@@ -77,12 +81,24 @@ const PathogenListView = () => {
     </div>
   );
 
+  const renderCitations = () => (
+    <div className={cs.citations} id={"citations"}>
+      <h3>Citations</h3>
+      <List
+        listClassName={cs.list}
+        listItems={pathogenList["citations"].sort()}
+        ordered={true}
+      ></List>
+    </div>
+  );
+
   if (!pathogenList) return null;
 
   return (
     <NarrowContainer className={cs.pathogenListView} size="small">
       {renderIntro()}
       {renderPathogenList()}
+      {renderCitations()}
     </NarrowContainer>
   );
 };
