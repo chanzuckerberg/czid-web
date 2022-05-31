9b2fc51b2ce170ee68745f49cd05007808755193
Irene Han <ihan@chanzuckerberg.com>
2022-05-31 15:06:00 -0700

    [Hot fix] Revert deadlocking migration (#2021)
    * revert migration
    
    * bump version up by one
    
    * bump migration version

--- db/migrate/20220519161213_add_created_at_indices.rb
+++
@@ -1,7 +0,0 @@
-class AddCreatedAtIndices < ActiveRecord::Migration[6.1]
-  def up
-    add_index :samples, :created_at
-    add_index :workflow_runs, :created_at
-    add_index :projects, :created_at
-  end
-end
