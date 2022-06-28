b1339719a08a9561680ff73363b4c4c1b36ba342
Omar Valenzuela <51972068+ovalenzuela19@users.noreply.github.com>
2022-06-28 15:51:30 -0700

    [Release fix] Update TaxonCount backfill data migration to be safe (#2116)
    * update data migration
    
    * throttle tweak

--- db/data/20220622165355_backfill_taxon_count_decimal_columns.rb
+++ db/data/20220622165355_backfill_taxon_count_decimal_columns.rb
@@ -1,19 +1,12 @@
-# frozen_string_literal: true
-
+# There are three keys to backfilling safely: batching, throttling, and running it outside a transaction.
 class BackfillTaxonCountDecimalColumns < ActiveRecord::Migration[6.1]
+  disable_ddl_transaction!
+
   def up
-    TaxonCount.connection.execute("
-      UPDATE taxon_counts
-      SET percent_identity_decimal = percent_identity
-    ")
-    TaxonCount.connection.execute("
-      UPDATE taxon_counts
-      SET alignment_length_decimal = alignment_length
-    ")
-    TaxonCount.connection.execute("
-      UPDATE taxon_counts
-      SET rpm_decimal = rpm
-    ")
+    TaxonCount.in_batches do |relation|
+      relation.update_all("percent_identity_decimal = percent_identity, alignment_length_decimal = alignment_length, rpm_decimal = rpm")
+      sleep(0.01) # throttle
+    end
   end
 
   def down
