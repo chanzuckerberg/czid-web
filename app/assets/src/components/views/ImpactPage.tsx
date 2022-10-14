08bebc762742b0c76f33c729416000821b97520f
Omar Valenzuela <51972068+ovalenzuela19@users.noreply.github.com>
2022-10-13 18:17:46 -0700

    Sf/impact page v2 (#2450)
    * updates to impact page - new map component and country showcase
    
    * updating disabled nav arrows
    
    * lint error fix
    
    * updating svg's to remove hyphenated attributes
    
    * fix typo in file name
    
    Co-authored-by: Seth Feingold <sfeingold1104@gmail.com>

--- app/assets/src/components/views/ImpactPage.tsx
+++ app/assets/src/components/views/ImpactPage.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import ImpactBottomCTA from "~/components/views/impact_page/ImpactBottomCTA";
-import ImpactCountryGrid from "~/components/views/impact_page/ImpactCountryGrid";
+import ImpactCountryShowcase from "~/components/views/impact_page/ImpactCountryShowcase";
 import ImpactHero from "~/components/views/impact_page/ImpactHero";
 import ImpactIntro from "~/components/views/impact_page/ImpactIntro";
 import ImpactVideoSection from "~/components/views/impact_page/ImpactVideoSection";
 import Footer from "~/components/views/landing_page/Footer";
 import Header from "~/components/views/landing_page/LandingHeaderV2";
 
+// good Impact Page
 const ImpactPage = () => {
   return (
     <>
       <Header impactPage={true} />
       <ImpactHero />
       <ImpactIntro />
-      <ImpactCountryGrid />
+      <ImpactCountryShowcase />
       <ImpactVideoSection />
       <ImpactBottomCTA />
       <Footer />
