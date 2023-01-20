50d0aeb7b6a06ec9168063813d880457aa5cd4f3
Nina Bernick <40436239+ninabernick@users.noreply.github.com>
2023-01-20 11:31:51 -0800

    reference pipeline run constant (#2821)

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1284,7 +1284,7 @@ class Sample < ApplicationRecord
     lowercase_count_type = count_type.downcase
 
     # Contig filtering via DiscoveryView taxon filters is not available for ont_v1
-    filter_statement = contig_arel_table.where(contig_arel_table[:read_count].gteq(PipelineRun::MIN_CONTIG_READS[TECHNOLOGY_INPUT[:illumina]]))
+    filter_statement = contig_arel_table.where(contig_arel_table[:read_count].gteq(PipelineRun::MIN_CONTIG_READS[PipelineRun::TECHNOLOGY_INPUT[:illumina]]))
     filter_statement = filter_statement.where(contig_arel_table["#{tax_level}_taxid_#{lowercase_count_type}"].eq(tax_id))
     filter_statement = filter_statement.where(PipelineRun.arel_table[:id].eq(contig_arel_table[:pipeline_run_id]))
     filter_statement = add_aggregate_arel_node_for_contig_metric(filter_statement, metric, operator, value)
