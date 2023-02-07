dc285da431f7dae18f5c4c19bd6e5ce4ebd12d75
Suzette McCanny <smccanny@deloitte.ca>
2023-02-07 10:01:27 -0500

    Changed format of e2e path (#2892)
    * changed format of path
    
    * fixed failing test
    
    ---------
    
    Co-authored-by: Charles Moga <cmoga@contractor.chanzuckerberg.com>

--- e2e/setup/globalSetup.ts
+++ e2e/setup/globalSetup.ts
@@ -1,15 +1,12 @@
-import { chromium, expect, FullConfig } from "@playwright/test";
-import path from "path";
-import dotenv from "dotenv";
-import { login } from "../utils/login";
 import fs from "fs";
-import { getByTestID, getByText } from "../utils/selectors";
+import path from "path";
+import { chromium, expect, FullConfig } from "@playwright/test";
+import dotenv from "dotenv";
 import { tag } from "../utils/constants";
+import { login } from "../utils/login";
+import { getByTestID, getByText } from "../utils/selectors";
 
 dotenv.config({ path: path.resolve(`.env.${process.env.NODE_ENV}`) });
-
-const username: string = process.env.CZID_USERNAME as string;
-const password: string = process.env.CZID_PASSWORD as string;
 
 /**
  * This function is run once at the start of the test
@@ -18,6 +15,18 @@ const password: string = process.env.CZID_PASSWORD as string;
  * @param config
  */
 async function globalSetup(config: FullConfig): Promise<void> {
+  if (process.env.NODE_ENV === "ci") {
+    process.env.CI = "true";
+  }
+  let username;
+  let password;
+  if (process.env.NODE_ENV === "ci" || process.env.NODE_ENV === "local") {
+    username = process.env.CZID_USERNAME;
+    password = process.env.CZID_PASSWORD;
+  } else {
+    username = process.env.USERNAME;
+    password = process.env.PASSWORD;
+  }
   const { storageState } = config.projects[0].use;
   if (process.env.NODE_ENV === "ci" || !checkCookies()) {
     const browser = await chromium.launch();
@@ -29,9 +38,6 @@ async function globalSetup(config: FullConfig): Promise<void> {
     await page.locator(getByTestID("home-top-nav-login")).click();
     await login(page, username, password);
     await page.context().storageState({ path: storageState as string });
-    const cookieJson = JSON.parse(
-      fs.readFileSync(storageState as string).toString()
-    );
     await browser.close();
   }
 }
@@ -49,7 +55,7 @@ function checkCookies(): boolean {
     const cookieJson = JSON.parse(fs.readFileSync(cookieFile).toString())[
       "cookies"
     ];
-    const cookie = cookieJson.find((cookie) => cookie.name === "auth0");
+    const cookie = cookieJson.find(cookie => cookie.name === "auth0");
     const expires = cookie["expires"] as number;
     const domain = cookie["domain"];
     if (expires > currentTime && domain.includes(process.env.DOMAIN)) {
