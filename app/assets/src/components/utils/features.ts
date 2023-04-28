6b6ec27fc65769ffc3e42a7c1cd7c65c485056a3
Omar Valenzuela <51972068+ovalenzuela19@users.noreply.github.com>
2023-04-28 10:59:44 -0700

    remove feature flag (#3301)

--- app/assets/src/components/utils/features.ts
+++ app/assets/src/components/utils/features.ts
@@ -27,7 +27,6 @@ export const MULTITAG_PATHOGENS_FEATURE = "multitag_pathogens";
 export const HEATMAP_ELASTICSEARCH_FEATURE = "heatmap_elasticsearch";
 export const PATHOGEN_FLAG_FILTER_FEATURE = "pathogen_filter";
 export const HEATMAP_PATHOGEN_FLAGGING_FEATURE = "heatmap_pathogens";
-export const MODERN_HOST_FILTERING = "modern_host_filtering";
 export const BULK_DELETION_FEATURE = "bulk_deletion";
 export const HEATMAP_FILTERS_LEFT_FEATURE = "left_heatmap_filters";
 export const WGS_CG_UPLOAD_FEATURE = "wgs_cg_upload";
