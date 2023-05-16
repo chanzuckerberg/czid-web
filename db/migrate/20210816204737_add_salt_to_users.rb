1f9d63c05ac6510dfa07dff5a96000cab4508e0d
Lucia Reynoso <lreynoso@chanzuckerberg.com>
2023-05-16 11:50:54 -0700

    Fix CI migration issues from missing User methods (#3455)
    * Comment out methods in migrations that refer to removed methods of User
    
    * Clarify comment

--- db/migrate/20210816204737_add_salt_to_users.rb
+++ db/migrate/20210816204737_add_salt_to_users.rb
@@ -12,17 +12,19 @@ class AddSaltToUsers < ActiveRecord::Migration[6.1]
 
   def down
     add_column :users, :authentication_token_encrypted, :binary, limit: 48, unique: true
+    # Note 2023-05-15: Commented out the code referencing methods of User 
+    # that are no longer present, throwing an exception in CI testing.
+    
+    # def encrypt_token(data)
+    #   cipher = OpenSSL::Cipher::AES256.new(:CBC).encrypt
+    #   cipher.key = Base64.decode64(ENV["AUTH_TOKEN_SECRET"])
+    #   iv = cipher.random_iv
+    #   cipher.update(data) + cipher.final + iv
+    # end
 
-    def encrypt_token(data)
-      cipher = OpenSSL::Cipher::AES256.new(:CBC).encrypt
-      cipher.key = Base64.decode64(ENV["AUTH_TOKEN_SECRET"])
-      iv = cipher.random_iv
-      cipher.update(data) + cipher.final + iv
-    end
-
-    User.all.each do |user|
-      user.authentication_token_encrypted = encrypt_token(SecureRandom.base58(24))
-    end
+    # User.all.each do |user|
+    #   user.authentication_token_encrypted = encrypt_token(SecureRandom.base58(24))
+    # end
 
     remove_column :users, :salt
   end
