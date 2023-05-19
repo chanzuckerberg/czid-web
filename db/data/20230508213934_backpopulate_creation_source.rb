6562f629e0d5c0210bacbcc3ee6b5fffe12b37d3
Phoenix Logan <plogan@chanzuckerberg.com>
2023-05-19 12:04:06 -0700

    [Release Fix] backfill creation_source fix (only non_depreciated runs, add logging) (#3485)
    Co-authored-by: Omar Valenzuela <ovalenzuela@chanzuckerberg.com>

--- db/data/20230508213934_backpopulate_creation_source.rb
+++ db/data/20230508213934_backpopulate_creation_source.rb
@@ -2,39 +2,49 @@
 
 class BackpopulateCreationSource < ActiveRecord::Migration[6.1]
   def up
-    WorkflowRun.consensus_genomes.all.each do |cg_workflow_run|
-      inputs = JSON.parse(cg_workflow_run.inputs_json.presence || '{}')
-      inputs[:creation_source] = get_creation_source(cg_workflow_run.sample, cg_workflow_run)
-      cg_workflow_run.update(inputs_json: inputs.to_json)
+    wrs = WorkflowRun.consensus_genomes.non_deprecated.where(status: WorkflowRun::STATUS[:succeeded])
+    num_wrs = wrs.count
+    batch_size = 500
+    num_batches = num_wrs/batch_size
+    current_batch = 0
+
+    puts "starting to backfill workflow_run.inputs_json[:creation_source]"
+    puts "Total batches being processed: #{num_batches}. Total number of workflow_runs per batch: #{batch_size}"
+    wrs.in_batches(of: batch_size)  do |cg_workflow_runs|
+      # The last batch will have probably not have 500 workflow_runs so I count the number of workflow_runs in the batch here
+      size_of_current_batch = cg_workflow_runs.count
+      cg_workflow_runs.each_with_index do |cg_workflow_run, cg_workflow_run_index|
+        inputs = cg_workflow_run.inputs
+        if inputs.instance_of?(Hash)
+          inputs["creation_source"] = get_creation_source(cg_workflow_run.sample, inputs)
+          cg_workflow_run.update(inputs_json: inputs.to_json)
+        else
+          puts "inputs_json from workflow_run #{cg_workflow_run.id} did not have expected inputs_json type (Hash), skipping backfill "
+        end
+        puts "Percent complete in batch #{current_batch}: #{((cg_workflow_run_index + 1)/(size_of_current_batch < 1 ? 1 : size_of_current_batch).to_f) * 100}"
+      end
+      puts "\t--------- Finished batch #{current_batch} out of #{num_batches} ---------"
+      current_batch += 1
     end
+    puts "Successfully backfilled `creation_source` on all workflow_runs"
   end
 
   def down
-    WorkflowRun.consensus_genomes.all.each do |cg_workflow_run|
-      inputs = JSON.parse(cg_workflow_run.inputs_json.presence || '{}')
-      inputs[:creation_source] = nil
-      cg_workflow_run.update(inputs_json: inputs.to_json)
-    end
+    raise ActiveRecord::IrreversibleMigration
   end
 end
 
-def get_creation_source(sample, cg_workflow_run)
-  
-  if cg_workflow_run.inputs_json.presence 
-    workflow_run_inputs_json =JSON.parse(cg_workflow_run.inputs_json)
-    # Modify the logic in this method to return the appropriate creation_source for each sample
-    if technology(workflow_run_inputs_json, sample) == ConsensusGenomeWorkflowRun::TECHNOLOGY_INPUT[:nanopore]
-      # CG kickoff is not available through mNGS nanopore report
-      return ConsensusGenomeWorkflowRun::CREATION_SOURCE[:sars_cov_2_upload]
-    elsif (ref_fasta_input(workflow_run_inputs_json, sample) || workflow_run_inputs_json&.[]("reference_accession"))
-      return ConsensusGenomeWorkflowRun::CREATION_SOURCE[:viral_cg_upload]
-    elsif workflow_run_inputs_json&.[]("accession_id") == ConsensusGenomeWorkflowRun::SARS_COV_2_ACCESSION_ID
-      return ConsensusGenomeWorkflowRun::CREATION_SOURCE[:sars_cov_2_upload]
-    else
-      return ConsensusGenomeWorkflowRun::CREATION_SOURCE[:mngs_report]
-    end
+def get_creation_source(sample, workflow_run_inputs_json)
+  # Modify the logic in this method to return the appropriate creation_source for each sample
+  if technology(workflow_run_inputs_json, sample) == ConsensusGenomeWorkflowRun::TECHNOLOGY_INPUT[:nanopore]
+    # CG kickoff is not available through mNGS nanopore report
+    return ConsensusGenomeWorkflowRun::CREATION_SOURCE[:sars_cov_2_upload]
+  elsif (ref_fasta_input(workflow_run_inputs_json, sample) || workflow_run_inputs_json&.[]("reference_accession"))
+    return ConsensusGenomeWorkflowRun::CREATION_SOURCE[:viral_cg_upload]
+  elsif workflow_run_inputs_json&.[]("accession_id") == ConsensusGenomeWorkflowRun::SARS_COV_2_ACCESSION_ID
+    return ConsensusGenomeWorkflowRun::CREATION_SOURCE[:sars_cov_2_upload]
   else
-    nil
+    return ConsensusGenomeWorkflowRun::CREATION_SOURCE[:mngs_report]
   end
 end
 
