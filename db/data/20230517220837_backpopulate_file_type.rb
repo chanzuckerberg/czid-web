813c4959348e7cad73281aee11f80cdb2d362896
Phoenix Logan <plogan@chanzuckerberg.com>
2023-05-24 15:39:51 -0700

    [Hot Fix] migration fix for file_type (#3525)

--- db/data/20230517220837_backpopulate_file_type.rb
+++ db/data/20230517220837_backpopulate_file_type.rb
@@ -2,38 +2,39 @@
 
 class BackpopulateFileType < ActiveRecord::Migration[6.1]
   def up
-    num_samples = Sample.count
     batch_size = 500
-    num_batches = num_samples/batch_size
-    current_batch = 0
+    input_files = InputFile.arel_table
+    puts "starting to backfill file_type for fastq files"
+    InputFile
+      .where(input_files[:name].matches_regexp("\.?fastq$|fq$|\.fastq\.gz$|\.fq\.gz$"))
+      .in_batches(of: batch_size)
+      .update_all(file_type: InputFile::FILE_TYPE_FASTQ)
+    puts "backfill file_type for fastq files complete"
 
-    puts "starting to backfill sample.input_files.file_type"
-    puts "Total batches being processed: #{num_batches}. Total number of samples"
-
-    # eager load to avoid N+1 queries, use each_record to avoid loading all records into memory
-    Sample.includes(:workflow_runs, :input_files).in_batches(of: batch_size).each_record.with_index do |sample, sample_index|
-        # get first workflow_run for sample if it exists
-        workflow_run = sample.workflow_runs.first
-        # get creation_source from workflow_run.inputs_json if it exists
-        creation_source = workflow_run.inputs["creation_source"] if workflow_run
-        input_files = sample.input_files
-        input_files.all.each do |input_file|
-          if ["fastq.gz", "fastq", "fq.gz", "fq"].include?(input_file.file_extension)
-            input_file.update(file_type: InputFile::FILE_TYPE_FASTQ)
-          elsif ["bed", "bed.gz"].include?(input_file.file_extension)
-            input_file.update(file_type: InputFile::FILE_TYPE_PRIMER_BED)
-          elsif ["fasta", "fa", "fasta.gz", "fa.gz"].include?(input_file.file_extension)
-            if creation_source == ConsensusGenomeWorkflowRun::CREATION_SOURCE[:viral_cg_upload]
-              input_file.update(file_type: InputFile::FILE_TYPE_REFERENCE_SEQUENCE)
-            else
-              # runs other than viral_cg_upload can have fasta files that are treated as fastq files
-              input_file.update(file_type: InputFile::FILE_TYPE_FASTQ)
-            end
-          else
-            puts "file type not found for extension #{input_file.file_extension}"
-          end
+    puts "starting to backfill file_type for bed files"
+    InputFile
+      .where(input_files[:name].matches_regexp("\.bed$|\.bed\.gz$"))
+      .in_batches(of: batch_size)
+      .update_all(file_type: InputFile::FILE_TYPE_PRIMER_BED)
+    puts "backfill file_type for bed files complete"
+    # Update fasta/fa files; they may be viral cg uploads, otherwise treat them as fastqfiles
+    puts "starting to backfill file_type for fasta files"
+    InputFile
+      .includes(sample: [:workflow_runs])
+      .where(input_files[:name].matches_regexp("\.fasta$|\.fa$|\.fasta\.gz$|\.fa\.gz$"))
+      .in_batches(of: batch_size)
+      .each_record do |fasta_file|
+        workflow_run = fasta_file.sample.workflow_runs.first
+        # get creation_source from workflow_run.inputs_json if workflow_run exists and workflow_run.inputs exists
+        creation_source = workflow_run&.inputs&.[]("creation_source")
+        if creation_source == ConsensusGenomeWorkflowRun::CREATION_SOURCE[:viral_cg_upload]
+          fasta_file.update(file_type: InputFile::FILE_TYPE_REFERENCE_SEQUENCE)
+        else
+          # runs other than viral_cg_upload can have fasta files that are treated as fastq files
+          fasta_file.update(file_type: InputFile::FILE_TYPE_FASTQ)
         end
-    end
+      end
+    puts "backfill file_type for fasta files complete"
   end
 
   def down
