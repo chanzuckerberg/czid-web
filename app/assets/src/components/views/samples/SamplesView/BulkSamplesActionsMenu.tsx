861b6c8bacdf31e6f35bfbbc1841f019ad36e2e7
Nina Bernick <40436239+ninabernick@users.noreply.github.com>
2023-07-11 15:34:38 -0700

    remove bulk kickoff AMR from public tab (#3701)
    Co-authored-by: Jerry Fu <jfu@chanzuckerberg.com>

--- app/assets/src/components/views/samples/SamplesView/BulkSamplesActionsMenu.tsx
+++ app/assets/src/components/views/samples/SamplesView/BulkSamplesActionsMenu.tsx
@@ -111,7 +111,7 @@ const BulkSamplesActionsMenu = ({
         onClose={closeActionsMenu}
       >
         {renderKickoffPhyloTree()}
-        {renderBulkKickoffAmr()}
+        {handleBulkKickoffAmr && renderBulkKickoffAmr()}
       </Menu>
     </>
   );
