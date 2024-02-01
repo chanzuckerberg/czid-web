f5a4662d98180ec021b585a7f5ff53d6bcbc354e
Oscar <79811012+ohvillasenor@users.noreply.github.com>
2024-02-01 11:41:43 -0800

    Vse config yml updates (#4244)
    * Vse filter sample report (#4095)
    
    * Added inital page objects
    
    * Updated constants for samples
    
    * Updated sample-report-filters to use POM
    
    * Removed un-used imports
    
    * Added semicolons
    
    * linting
    
    * more linting
    
    * Linting and import fix
    
    * Updates for isTaxonVisible
    
    * Vse filter sample report (#4095)
    
    * Added inital page objects
    
    * Updated constants for samples
    
    * Updated sample-report-filters to use POM
    
    * Removed un-used imports
    
    * Added semicolons
    
    * linting
    
    * more linting
    
    * Linting and import fix
    
    * Updates for isTaxonVisible
    
    * eslint
    
    * Sample filter report test updates
    
    * Vse upload sample (#4106)
    
    * Verify set metadata for samples manually
    
    * eslint
    
    * Moved upload smoke tests to upload-smoke.spec.ts
    
    * lint
    
    * Set Metadata for Samples Manually: MNGS, WGS, SC2
    
    * Added ONT upload test
    
    * Verify Upload Metadata for Sample using CSV
    
    * Stabilze test performance
    
    * lint and cleanup
    
    * undo change
    
    * Updated to const
    
    * Added ProjectInfo validation
    
    * Start upload after review to generate more reports
    
    * test updates
    
    * Test update
    
    * lint
    
    * linted
    
    * Adding sample-deletion-smoke.spec.ts
    
    * Update 'Verify url displayed on the columns'
    
    * Improved scrolling
    
    * Threshold validation
    
    * lint
    
    * test updates
    
    * lint
    
    * removed const
    
    * fix merge
    
    * Remove comment
    
    * wip of deletion smoke tests
    
    * "amr", "mngs", "ONT" sample deletion
    
    * remove comment
    
    * revert staging conf
    
    * Add WORKFLOWS.WGS to delete tests
    
    * Added TCM links
    
    * Download mngs Sample Metadata
    
    * wip unzip for smoke
    
    * fix merge
    
    * remove const
    
    * fix merge
    
    * WIP
    
    * removed debugging
    
    * 22 download smoke tests
    
    * locator update
    
    * Updates for WGS uploads
    
    * Increase retries for longer test
    
    * split suites to reduce runtime
    
    * Completed remaining tests
    
    * stablize tests
    
    * lint
    
    * Moved tests
    
    * Added TCM links
    
    * Added TCM links
    
    * Added TCM links
    
    * Added TCM links
    
    * initial commit
    
    * Data report validation (Human) JAN
    
    * lint
    
    * Customer Reference file download and format
    
    * Compare trim primers files
    
    * Complete P-0 data validation tests
    
    * P-0 data validation tests
    
    * Use projact.name
    
    * Update setup and add SNo
    
    * lint
    
    * SNo 1:
    
    * SNo 1
    
    * Completed SNo 2
    
    * Completed SNo 3
    
    * SNo 4
    
    * Added sample-upload-basespace.spec.ts
    
    * commmit work
    
    * remove un-finished test
    
    * Stabilize heatmap tests
    
    * lint
    
    * adm-zip type
    
    * update import
    
    * lint
    
    * remove P-0 from yml
    
    * Select 1-2 samples for bulk download
    
    * update import
    
    * Sandbox and Staging
    
    * newline

---
+++ e2e/sandbox.config.ts
@@ -0,0 +1,46 @@
+import { devices, PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+  expect: {
+    timeout: 9000,
+  },
+
+  fullyParallel: true,
+  globalSetup: "./setup/globalSetup",
+  outputDir: "./playwright-report",
+
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        video: "on",
+      },
+    },
+  ],
+  reporter: [
+    [
+      "html",
+      {
+        open: "never",
+        outputFolder: "__assets__/html-report/",
+        attachmentsBaseURL: "./",
+      },
+    ],
+  ],
+  testDir: "./tests",
+  timeout: 90000,
+  use: {
+    channel: "chromium",
+    baseURL: "https://sandbox.czid.org",
+    ignoreHTTPSErrors: true,
+    screenshot: "only-on-failure",
+    storageState: "/tmp/state.json",
+    trace: "on",
+    viewport: { width: 800, height: 7200 },
+    permissions: ["clipboard-read"],
+  },
+  workers: 10,
+};
+
+export default config;
---
+++ e2e/setup/sandbox.config.ts
@@ -0,0 +1,48 @@
+import { devices, PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+  expect: {
+    timeout: 30000,
+  },
+
+  fullyParallel: true,
+  globalSetup: "./globalSetup",
+  outputDir: "../playwright-report",
+
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        video: "on",
+      },
+    },
+  ],
+  reporter: [
+    ["list"],
+    [
+      "html",
+      {
+        open: "failure",
+        host: "localhost",
+        port: 9220,
+        outputFolder: "../html-reports",
+      },
+    ],
+  ],
+  testDir: "../tests",
+  timeout: 60000,
+  use: {
+    channel: "chromium",
+    baseURL: "https://sandbox.czid.org",
+    ignoreHTTPSErrors: true,
+    screenshot: "only-on-failure",
+    storageState: "/tmp/state.json",
+    trace: "on",
+    viewport: { width: 800, height: 7200 },
+    permissions: ["clipboard-read"],
+  },
+  workers: 4,
+};
+
+export default config;
