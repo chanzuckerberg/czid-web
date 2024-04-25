772cc43aaddc18854acd305508f947a868ba88bb
Erin Hoops <109251328+ehoops-cz@users.noreply.github.com>
2024-04-25 12:35:46 -0700

    convert NextcladeReferenceTreeOptions to function component (#4614)

--- app/assets/src/components/views/NextcladeModal/components/NextcladeReferenceTreeOptions/NextcladeReferenceTreeOptions.tsx
+++ app/assets/src/components/views/NextcladeModal/components/NextcladeReferenceTreeOptions/NextcladeReferenceTreeOptions.tsx
@@ -1,14 +1,14 @@
-import cx from "classnames";
+import { cx } from "@emotion/css";
 import { head } from "lodash/fp";
 import React from "react";
 import ExternalLink from "~/components/ui/controls/ExternalLink";
+import FilePicker from "~/components/ui/controls/FilePicker";
+import RadioButton from "~/components/ui/controls/RadioButton";
 import {
   NEXTCLADE_DEFAULT_TREE_LINK,
   NEXTCLADE_TREE_FORMAT_LINK,
   NEXTCLADE_TREE_ROOT_LINK,
 } from "~/components/utils/documentationLinks";
-import FilePicker from "~ui/controls/FilePicker";
-import RadioButton from "~ui/controls/RadioButton";
 import cs from "./nextclade_modal_tree_options.scss";
 
 interface NextcladeReferenceTreeOptionsProps {
@@ -18,83 +18,75 @@ interface NextcladeReferenceTreeOptionsProps {
   selectedType?: string;
 }
 
-export class NextcladeReferenceTreeOptions extends React.Component<NextcladeReferenceTreeOptionsProps> {
-  onDrop = acceptedFile => {
-    this.props.onChange(head(acceptedFile));
+export const NextcladeReferenceTreeOptions = ({
+  referenceTree,
+  onChange,
+  onSelect,
+  selectedType,
+}: NextcladeReferenceTreeOptionsProps) => {
+  const onDrop = acceptedFile => {
+    onChange(head(acceptedFile));
   };
 
-  onRejected = () => {
+  const onRejected = () => {
     const msg =
       "Invalid file. Files must be in JSON format and file size must be under 5GB.";
     window.alert(msg);
   };
 
-  getFilePickerTitle = () => {
-    const { referenceTree } = this.props;
+  const filePickerTitle = referenceTree
+    ? `${referenceTree} Selected For Upload`
+    : undefined;
+  const uploadSelected = selectedType === "upload";
 
-    return referenceTree ? `${referenceTree} Selected For Upload` : null;
-  };
-
-  render() {
-    const filePickerTitle = this.getFilePickerTitle();
-    const { onSelect, selectedType } = this.props;
-    const uploadSelected = selectedType === "upload";
-
-    return (
-      <div className={cs.treeTypeContainer}>
-        <div
-          className={cx(cs.treeType, !uploadSelected && cs.selected)}
-          onClick={() => onSelect("global")}
-        >
-          <RadioButton className={cs.radioButton} selected={!uploadSelected} />
-          <div className={cs.content}>
-            <div className={cs.name}>Nextclade Default Tree</div>
-            <div className={cs.description}>
-              This tree includes worldwide data from Nextstrain,{" "}
-              <ExternalLink href={NEXTCLADE_DEFAULT_TREE_LINK}>
-                view the tree.
-              </ExternalLink>
-            </div>
-          </div>
-        </div>
-        <div
-          className={cx(cs.treeType, uploadSelected && cs.selected)}
-          onClick={() => onSelect("upload")}
-        >
-          <RadioButton className={cs.radioButton} selected={uploadSelected} />
-          <div className={cs.content}>
-            <div className={cs.name}>Upload a Tree</div>
-            <div className={cs.description}>
-              You can upload your own info in{" "}
-              <ExternalLink href={NEXTCLADE_TREE_FORMAT_LINK}>
-                Auspice JSON
-              </ExternalLink>{" "}
-              format. For compatibility, make sure your tree&apos;s root is{" "}
-              <ExternalLink href={NEXTCLADE_TREE_ROOT_LINK}>
-                Wuhan/Hu-1/2019.
-              </ExternalLink>
-            </div>
-            {uploadSelected && (
-              <div className={cs.fields}>
-                <FilePicker
-                  className={cx(
-                    cs.localFilePicker,
-                    !filePickerTitle && cs.short,
-                  )}
-                  // @ts-expect-error CZID-8698 expect strictNullCheck error: error TS2322
-                  title={filePickerTitle}
-                  accept=".json"
-                  onChange={this.onDrop}
-                  onRejected={this.onRejected}
-                  multiFile={false}
-                />
-              </div>
-            )}
+  return (
+    <div className={cs.treeTypeContainer}>
+      <div
+        className={cx(cs.treeType, !uploadSelected && cs.selected)}
+        onClick={() => onSelect("global")}
+      >
+        <RadioButton className={cs.radioButton} selected={!uploadSelected} />
+        <div className={cs.content}>
+          <div className={cs.name}>Nextclade Default Tree</div>
+          <div className={cs.description}>
+            This tree includes worldwide data from Nextstrain,{" "}
+            <ExternalLink href={NEXTCLADE_DEFAULT_TREE_LINK}>
+              view the tree.
+            </ExternalLink>
           </div>
         </div>
       </div>
-    );
-  }
-}
-
-export default NextcladeReferenceTreeOptions;
+      <div
+        className={cx(cs.treeType, uploadSelected && cs.selected)}
+        onClick={() => onSelect("upload")}
+      >
+        <RadioButton className={cs.radioButton} selected={uploadSelected} />
+        <div className={cs.content}>
+          <div className={cs.name}>Upload a Tree</div>
+          <div className={cs.description}>
+            You can upload your own info in{" "}
+            <ExternalLink href={NEXTCLADE_TREE_FORMAT_LINK}>
+              Auspice JSON
+            </ExternalLink>{" "}
+            format. For compatibility, make sure your tree&apos;s root is{" "}
+            <ExternalLink href={NEXTCLADE_TREE_ROOT_LINK}>
+              Wuhan/Hu-1/2019.
+            </ExternalLink>
+          </div>
+          {uploadSelected && (
+            <div className={cs.fields}>
+              <FilePicker
+                className={cx(cs.localFilePicker, !filePickerTitle && cs.short)}
+                title={filePickerTitle}
+                accept=".json"
+                onChange={onDrop}
+                onRejected={onRejected}
+                multiFile={false}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
