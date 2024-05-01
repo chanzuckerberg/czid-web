9cb81c270979f36c5e81f677b224007ad7995c87
bchu1 <151689101+bchu1@users.noreply.github.com>
2024-05-01 13:05:44 -0700

    Always log GQL errors to Sentry (#4623)
    * init
    
    * comment

--- app/assets/src/relay/environment.ts
+++ app/assets/src/relay/environment.ts
@@ -1,11 +1,19 @@
 import type { FetchFunction, IEnvironment } from "relay-runtime";
 import { Environment, Network, RecordSource, Store } from "relay-runtime";
+import { logError } from "~/components/utils/logUtil";
 import { getValidIdentity } from "./identify";
+
+/**
+ * Captures the name of a query/mutation, if it exists.
+ *
+ * e.g. "DiscoveryViewFCWorkflowRunsQuery" from "query DiscoveryViewFCWorkflowRunsQuery("
+ */
+const QUERY_NAME_REGEX = /(?:query|mutation)\s+(\S+)\s*\(/;
 
 const generateFetchFn = (shouldReadFromNextGen: boolean): FetchFunction => {
   return async (params, variables) => {
     await getValidIdentity();
-    const response = fetch("/graphqlfed", {
+    const response = await fetch("/graphqlfed", {
       method: "POST",
       headers: [
         ["Content-Type", "application/json"],
@@ -18,7 +26,21 @@ const generateFetchFn = (shouldReadFromNextGen: boolean): FetchFunction => {
       }),
     });
 
-    return response.then(data => data.json());
+    const responseJson = await response.json();
+    if (responseJson.errors != null) {
+      logError({
+        message: `[GQL Error] ${params.text?.match(QUERY_NAME_REGEX)?.[1]}`,
+        details: {
+          query: params.text,
+          // Stringify because Sentry turns arrays and objects into [Array] and [Object] after a
+          // certain depth.
+          variables: JSON.stringify(variables),
+          errors: JSON.stringify(responseJson.errors),
+        },
+      });
+    }
+
+    return responseJson;
   };
 };
 
