9b4f4e64c3ebf8f46b8e5efed1be3c32fb13d9f1
bchu1 <151689101+bchu1@users.noreply.github.com>
2024-06-05 12:20:16 -0700

    Reduce reload time in local dev environment by ~6 seconds (#4722)
    * remove turbolinks
    
    * console log test
    
    * disable eslint
    
    * test loadsh upgrade
    
    * downgrade lodash
    
    * undo
    
    * undo package logck
    
    * ci - prettier and eslint fixes
    
    ---------
    
    Co-authored-by: bchu1 <bchu1@users.noreply.github.com>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,1 @@
-// This file needs to be in sync with webpack.config.prod.js. The first file
-// should contain JS from node_modules and the second file should contain
-// idseq-written JS. The order of requires is important!
-
-//= require vendors~main.bundle.min.js
 //= require main.bundle.min.js
