a19e58d6b6844ca84c0f40a70789d760d6888625
Suzette McCanny <smccanny@deloitte.ca>
2024-06-13 11:54:29 -0400

    Release Fix: fix misalignment caused by noStyleButton (#4757)
    * fix misalignment caused by noStyleButton:
    
    * limit logic in clickable stack

--- app/assets/src/components/views/SampleView/components/MngsReport/components/ReportTable/components/columns/components/NtNrStack/NtNrStack.tsx
+++ app/assets/src/components/views/SampleView/components/MngsReport/components/ReportTable/components/columns/components/NtNrStack/NtNrStack.tsx
@@ -10,30 +10,33 @@ interface NtNrStackProps {
 }
 
 export const NtNrStack = ({ cellData, dbType, onClick }: NtNrStackProps) => {
+  // onClick is only defined when using NtNrStack in the NtNrSelector component
+  if (onClick && cellData) {
+    return (
+      <div className={cs.stack}>
+        <button
+          className={cx("noStyleButton", cs.stackElement)}
+          onClick={() => onClick[0]("nt")}
+        >
+          {cellData[0]}
+        </button>
+        <button
+          className={cx("noStyleButton", cs.stackElement)}
+          onClick={() => onClick[1]("nr")}
+        >
+          {cellData[1]}
+        </button>
+      </div>
+    );
+  }
   return (
     <div className={cs.stack}>
-      <button
-        className={cx(
-          cs.stackElement,
-          dbType === "nt" || cs.lowlightValue,
-          "noStyleButton",
-        )}
-        // @ts-expect-error CZID-8698 expect strictNullCheck error: error TS2322
-        onClick={onClick ? () => onClick[0]("nt") : null}
-      >
+      <div className={cx(cs.stackElement, dbType === "nt" || cs.lowlightValue)}>
         {cellData ? cellData[0] : "-"}
-      </button>
-      <button
-        className={cx(
-          cs.stackElement,
-          dbType === "nr" || cs.lowlightValue,
-          "noStyleButton",
-        )}
-        // @ts-expect-error CZID-8698 expect strictNullCheck error: error TS2322
-        onClick={onClick ? () => onClick[1]("nr") : null}
-      >
+      </div>
+      <div className={cx(cs.stackElement, dbType === "nr" || cs.lowlightValue)}>
         {cellData ? cellData[1] : "-"}
-      </button>
+      </div>
     </div>
   );
 };
