1178a0670887a46c8e4eff89f87452e37164828e
Lucia Reynoso <lreynoso@chanzuckerberg.com>
2024-08-07 10:43:17 -0700

    Fix impact page / Remove impact erb template (#4840)
    * Copy impact page layout from application page
    
    * Delete impact erb template and layout directive in support controller

--- app/controllers/support_controller.rb
+++ app/controllers/support_controller.rb
@@ -11,7 +11,6 @@ class SupportController < ApplicationController
 
   before_action :login_required, except: PUBLIC_ACTIONS
   skip_before_action :authenticate_user!, :verify_authenticity_token, only: PUBLIC_ACTIONS
-  layout "impact", only: :impact
 
   def privacy
   end
